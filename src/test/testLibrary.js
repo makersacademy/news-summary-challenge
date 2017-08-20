49d5e7526faaa1003a9189c57e968ec26e58cbe5
kitkat119 <gprydal@yahoo.co.uk>
2017-08-20 16:01:27 +0100

    updated test lib

--- src/test/testLibrary.js
+++ src/test/testLibrary.js
@@ -1,13 +1,27 @@
 (function(exports) {
   var assert = {
-    isTrue: function(assertionToCheck) {
-      if (!assertionToCheck) {
-        document.getElementById("tests").innerHTML += ("  <b><ul>🙄 <span style='color:red;'> ASSERTION FAILED: " + assertionToCheck + " is not truthy<br> TRY AGAIN...</span></ul></b>");
+    isTrue: function(assertion) {
+      if (!assertion) {
+        document.getElementById("tests").innerHTML += ("  <b><ul>🙄 <span style='color:red;'> ASSERTION FAILED: " + assertion + " is not truthy<br> TRY AGAIN...</span></ul></b>");
       } else {
         document.getElementById("tests").innerHTML += (" <b><ul>👩‍⚕️<span style='color:green;'>  The test has passed, you are amazing!</span></ul></b>");
       }
     }
-  };
+    isFalse: function(assertion) {
+      if (assertion) {
+        document.getElementById("tests").innerHTML += ("  <b><ul>🙄 <span style='color:red;'> ASSERTION FAILED: Expected " + assertion + " to be falsey<br> TRY AGAIN...</span></ul></b>");
+      } else {
+        document.getElementById("tests").innerHTML += (" <b><ul>👩‍⚕️<span style='color:green;'>TEST PASSED. " + assertion + " is false</span></ul></b>");
+      }
+    }
+    isEqual: function(test, expectation) {
+      if (test !== expectation) {
+        document.getElementById("tests").innerHTML += ("  <b><ul>🙄 <span style='color:red;'> ASSERTION FAILED: Expected " + test + " to be equal to " + expectation + "<br> TRY AGAIN...</span></ul></b>");
+      } else {
+        document.getElementById("tests").innerHTML += (" <b><ul>👩‍⚕️<span style='color:green;'>TEST PASSED. " + test + " is equal to " + expectation "</span></ul></b>");
+      }
+    }
+   };
   var describe = function(description, test) {
       document.getElementById("tests").innerHTML += "<br></br><b><span style='font-size:20px;'>" + description + ":</span></b>";
       test();
