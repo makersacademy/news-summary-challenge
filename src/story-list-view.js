18716c652f90bfc2f83c9a9dea498055f54f6adf
Toby Hamand <tobyhamand@gmail.com>
2017-08-21 09:54:13 +0100

    readme

--- src/story-list-view.js
+++ src/story-list-view.js
@@ -8,8 +8,8 @@
   StoryListView.prototype = {
     toHtml: function() {
       var storyListHtml = this._storyList.all().map(function(story) {
-        var imageHtml = "<img src='" + story.url() + "'></br>";
-        var titleHtml = "<a href='#stories/" + story.id() + "'><h2>" + story.title() + "</h2></br></a>";
+        var imageHtml = "<img class='thumbnail' src='" + story.url() + "'></br>";
+        var titleHtml = "<a href='#stories/" + story.id() + "'><h3>" + story.title() + "</h3></br></a>";
         var html = "<div class='story'>" + imageHtml + titleHtml + "</div>";
         return html;
       });
--- src/story-view.js
+++ src/story-view.js
@@ -8,7 +8,7 @@
   StoryView.prototype = {
     toHtml: function() {
       var backButton = "<a class='button' href=#story/all>BACK</a><br>";
-      var imageHtml = "<img src='" + this._story.url() + "'></br>";
+      var imageHtml = "<img class='thumbnail' src='" + this._story.url() + "'></br>";
       var titleHtml = "<h2>" + this._story.title() + "</h2></br>";
       var textHtml = "<p>" + this._story.text() + "</p></br>";
       var html = backButton + "<div id='story'>" + imageHtml + titleHtml + textHtml + "</div>";
