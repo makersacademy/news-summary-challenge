998318e7abecb72c6919a880fa249e89b9a1fad6
James Ransome <jamesransome@msn.com>
2017-09-29 22:49:28 +0100

    add more tests on article spec

--- js/article-model.js
+++ js/article-model.js
@@ -1,11 +1,26 @@
 (function(exports){
-  function Article(content){
-    this._content = content;
+  function Article(data){
+    this._title = data.title;
+    this._thumbnail = data.thumbnail;
+    this._content = data.content;
+    this._url = data.url;
   }
 
   Article.prototype = {
-    text: function(){
+    title: function(){
+      return this._title;
+    },
+
+    thumbnail: function(){
+      return this._thumbnail;
+    },
+
+    content: function(){
       return this._content;
+    },
+
+    url: function(){
+      return this._url;
     }
   };
 
--- spec/unit/article-spec.js
+++ spec/unit/article-spec.js
@@ -1,5 +1,26 @@
-it("Article model: should have article content", function(){
-  var content = "News of something interesting";
-  var article1 = new Article(content);
-  return expect(article1.text()).toEqual(content);
+var data = {
+  title: "News of something interesting",
+  content: "Detail on news of something interesting",
+  thumbnail: "",
+  url: "www.NotTheDailyMail.com"
+}
+
+it("Article model: should have title", function(){
+  var article1 = new Article(data);
+  return expect(article1.title()).toEqual(data.title);
+});
+
+it("Article model: should have a thumbnail", function(){
+  var article1 = new Article(data);
+  return expect(article1.thumbnail()).toEqual(data.thumbnail);
+});
+
+it("Article model: should have content", function(){
+  var article1 = new Article(data);
+  return expect(article1.content()).toEqual(data.content);
+});
+
+it("Article model: should have the URL to the source", function(){
+  var article1 = new Article(data);
+  return expect(article1.url()).toEqual(data.url);
 });
