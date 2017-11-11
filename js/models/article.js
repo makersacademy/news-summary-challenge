baa2f72f3fb624e23066a22d554f733c0f11af67
David Halewood <davidnhalewood@hotmail.com>
2017-11-11 13:26:46 +0000

    Adding url to full story to article object

--- js/models/article.js
+++ js/models/article.js
@@ -2,8 +2,9 @@
 
 (function (exports) {
   exports.Article = function () {
-    var _headline = 'How to be healthy this Christmas!'
+    var _headline = '10 Ways To Have A Healthier Christmas!'
     var _summary = 'Christmas is notoriously a time to indulge, but don’t let this be the green light to overindulge! You can still enjoy all the festivities of the season and get through the Christmas period without too much impact on your health and waistline. Try our top 10 tips for a healthier Christmas.'
+    var _url = 'https://www.realbuzz.com/activities-interests/topics/festive-health-fitness/article/10-ways-to-a-healthier-christmas/'
 
     function showHeadline() {
       return _headline;
@@ -13,9 +14,14 @@
       return _summary;
     }
 
+    function showUrlToFullStory() {
+      return _url;
+    }
+
     return {
       showHeadline: showHeadline,
-      showSummary:  showSummary
+      showSummary:  showSummary,
+      showUrlToFullStory: showUrlToFullStory
     };
   };
 })(this);
--- spec/articleSpec.js
+++ spec/articleSpec.js
@@ -3,10 +3,14 @@
 aWholeNewWorld('Article', function () {
   var article = new Article
   it('is created with a headline', function (){
-    wish(article.showHeadline()).toEqual('How to be healthy this Christmas!')
+    wish(article.showHeadline()).toEqual('10 Ways To Have A Healthier Christmas!')
   })
 
   it('can return a summary of the full story', function () {
     wish(article.showSummary()).toEqual('Christmas is notoriously a time to indulge, but don’t let this be the green light to overindulge! You can still enjoy all the festivities of the season and get through the Christmas period without too much impact on your health and waistline. Try our top 10 tips for a healthier Christmas.')
+  })
+
+  it('has a link to the full story', function () {
+    wish(article.showUrlToFullStory()).toEqual('https://www.realbuzz.com/activities-interests/topics/festive-health-fitness/article/10-ways-to-a-healthier-christmas/')
   })
 })
