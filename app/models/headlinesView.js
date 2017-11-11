948b3ab5e72c932a8ae47bfa3dce7ff1157cb293
Lucy Borthwick <lucyborthwick95@gmail.com>
2017-11-11 12:15:42 +0000

    Converts headline into an html string

--- app/models/headlinesView.js
+++ app/models/headlinesView.js
@@ -1,8 +1,17 @@
 (function(exports) {
 
-  HeadlinesView = function() {
-
+  HeadlinesView = function(headlinesList = new headlinesList) {
+    this._headlinesList = headlinesList
   }
+
+  HeadlinesView.prototype.toHTML = function () {
+    var headlines = this._headlinesList.returnHeadlines();
+    var html = "<ul>"
+    headlines.forEach(function(headline) {
+      html += "<li><div>" + headline + "</div></li>"
+    });
+    return html += "</ul>"
+  };
 
   exports.HeadlinesView = HeadlinesView;
 })(this);
