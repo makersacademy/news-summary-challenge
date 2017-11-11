1da83fe9ff5206f37f17fe7bb9b3284340d9bb23
David Halewood <davidnhalewood@hotmail.com>
2017-11-11 21:34:39 +0000

    Aaplying changes to tests whilst figuring out image and summary API requests

--- spec/articleSpec.js
+++ spec/articleSpec.js
@@ -2,28 +2,28 @@
 
 aWholeNewWorld('Article', function () {
 
-  headline = '10 Ways To Have A Healthier Christmas!'
-  summary = 'Christmas is notoriously a time to indulge, but don’t let this be the green light to overindulge! You can still enjoy all the festivities of the season and get through the Christmas period without too much impact on your health and waistline. Try our top 10 tips for a healthier Christmas.'
-  url = 'https://www.realbuzz.com/activities-interests/topics/festive-health-fitness/article/10-ways-to-a-healthier-christmas/'
-  image = 'https://s3-eu-west-1.amazonaws.com/rb-cms/rbv5/production/uploads/cover_images/30d7070bbc7a7a40e0aaa7f2a275c599139a/i1080x475.jpg'
+  headline = 'Headline'
+  summary = 'Summary'
+  url = 'Url'
+  image = 'Image'
 
-  article = new Article(headline, summary, url, image)
+  article = new Article(headline, url)
 
   it('is created with a headline', function () {
-    wish(article.showHeadline()).toEqual('10 Ways To Have A Healthier Christmas!')
+    wish(article.showHeadline()).toEqual('Headline')
   })
 
-  it('can return a summary of the full story', function () {
-    wish(article.showSummary()).toEqual('Christmas is notoriously a time to indulge, but don’t let this be the green light to overindulge! You can still enjoy all the festivities of the season and get through the Christmas period without too much impact on your health and waistline. Try our top 10 tips for a healthier Christmas.')
-  })
+  // it('can return a summary of the full story', function () {
+  //   wish(article.showSummary()).toEqual('Christmas is notoriously a time to indulge, but don’t let this be the green light to overindulge! You can still enjoy all the festivities of the season and get through the Christmas period without too much impact on your health and waistline. Try our top 10 tips for a healthier Christmas.')
+  // })
 
   it('has a link to the full story', function () {
-    wish(article.showUrlToFullStory()).toEqual('https://www.realbuzz.com/activities-interests/topics/festive-health-fitness/article/10-ways-to-a-healthier-christmas/')
+    wish(article.showUrlToFullStory()).toEqual('Url')
   })
 
-  it('has a picture attached to the story', function () {
-    wish(article.featureImage()).toEqual('https://s3-eu-west-1.amazonaws.com/rb-cms/rbv5/production/uploads/cover_images/30d7070bbc7a7a40e0aaa7f2a275c599139a/i1080x475.jpg')
-  })
+  // it('has a picture attached to the story', function () {
+  //   wish(article.featureImage()).toEqual('https://s3-eu-west-1.amazonaws.com/rb-cms/rbv5/production/uploads/cover_images/30d7070bbc7a7a40e0aaa7f2a275c599139a/i1080x475.jpg')
+  // })
 
   var headline, summary, url, image, article
 })
