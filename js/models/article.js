828b04d11ed4ee4f7ff3f3d07160f2d0a073db92
David Halewood <davidnhalewood@hotmail.com>
2017-11-11 13:20:55 +0000

    Adding a summary to article object

--- js/models/article.js
+++ js/models/article.js
@@ -2,14 +2,20 @@
 
 (function (exports) {
   exports.Article = function () {
-    var _headline = "Where have all the bees gone?"
+    var _headline = 'How to be healthy this Christmas!'
+    var _summary = 'Christmas is notoriously a time to indulge, but don’t let this be the green light to overindulge! You can still enjoy all the festivities of the season and get through the Christmas period without too much impact on your health and waistline. Try our top 10 tips for a healthier Christmas.'
 
     function showHeadline() {
       return _headline;
     }
 
+    function showSummary() {
+      return _summary;
+    }
+
     return {
-      showHeadline: showHeadline
+      showHeadline: showHeadline,
+      showSummary:  showSummary
     };
   };
 })(this);
--- spec/articleSpec.js
+++ spec/articleSpec.js
@@ -3,6 +3,10 @@
 aWholeNewWorld('Article', function () {
   var article = new Article
   it('is created with a headline', function (){
-    wish(article.showHeadline()).toEqual('Where have all the bees gone?')
+    wish(article.showHeadline()).toEqual('How to be healthy this Christmas!')
+  })
+
+  it('can return a summary of the full story', function () {
+    wish(article.showSummary()).toEqual('Christmas is notoriously a time to indulge, but don’t let this be the green light to overindulge! You can still enjoy all the festivities of the season and get through the Christmas period without too much impact on your health and waistline. Try our top 10 tips for a healthier Christmas.')
   })
 })
