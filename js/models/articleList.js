256adb19182f4db929b47c0eaf23540b3cadc515
David Halewood <davidnhalewood@hotmail.com>
2017-11-11 14:17:35 +0000

    Adding ability to create new articles with article list object

--- js/models/articleList.js
+++ js/models/articleList.js
@@ -9,10 +9,15 @@
       return articles;
     }
 
+    function addArticle(headline, summary, url, image) {
+      article = new Article(headline, summary, url, image)
+      articles.push(article);
+    }
     return {
-      allArticles: allArticles
+      allArticles: allArticles,
+      addArticle: addArticle
     }
 
-    var articles;
+    var articles, article;
   }
 })(this);
--- spec/articleListSpec.js
+++ spec/articleListSpec.js
@@ -1,12 +1,22 @@
 'use strict';
 
 aWholeNewWorld('ArticleList', function () {
-  
+
+  headline = 'Headline'
+  summary = 'Summary'
+  url = 'url'
+  image = 'image'
+
   articleList = new ArticleList();
 
   it('is created with an empty array', function () {
     wish(articleList.allArticles().length).toEqual(0)
   })
 
-  var articleList
+  it('creates and stores articles', function () {
+    articleList.addArticle(headline, summary, url, image)
+    wish(articleList.allArticles().length).toEqual(1)
+  })
+
+  var articleList, headline, summary, url, image
 })
