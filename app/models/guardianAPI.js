fc2515395d5ea7a2e12f08dea82479334b8fc4a8
Lucy Borthwick <lucyborthwick95@gmail.com>
2017-11-11 15:23:24 +0000

    Ran this and it worked for one headline - need to test drive

--- app/models/guardianAPI.js
+++ app/models/guardianAPI.js
@@ -1,8 +1,23 @@
 (function(exports) {
 
   function GuardianAPI() {
+    var list = new HeadlinesList();
+    // console.log(list.returnHeadlines());
 
+    var request = new XMLHttpRequest();
+      request.open('GET', ("https://content.guardianapis.com/search?q=economy&api-key=test"));
+      request.send();
+
+    request.onreadystatechange = runRequest;
+
+    function runRequest() {
+      var headlines = document.getElementById("allHeadlines");
+      if (request.readyState === XMLHttpRequest.DONE && request.status === 200) {
+        var dataReceived = JSON.parse(request.responseText);
+        list.addHeadlines(dataReceived.response.results[0].webTitle);
+      }
+      headlines.innerHTML = list.returnHeadlines();
+    }
   };
-  
   exports.GuardianAPI = GuardianAPI;
 })(this);
