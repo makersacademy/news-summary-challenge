1fd7b726b444902fc449dd0942bcf2363159aa20
mormolis <george.lamprakis@gmail.com>
2017-11-11 13:29:25 +0000

    adds jsonParser

---
+++ js/json-parser.js
@@ -0,0 +1,56 @@
+"use strict";
+
+(function (exports) {
+  exports.JsonParser = function () {
+    var _url = [];
+    var _title = [];
+    var _body = [];
+    var _thumbnail = [];
+
+    var _iterateThroughJason = function (out) {
+      for (var i = 0; i < out.response.results.length; i++) {
+        _url[i] = out.response.results[i].webUrl;
+        _title[i] = out.response.results[i].webTitle;
+        _body[i] = out.response.results[i].fields.body;
+        _thumbnail[i] = out.response.results[i].fields.thumbnail;
+      }
+    }
+
+    var parseJsonFromUrl = function (url) {
+      fetch(url).then(res => res.json()).then((out) => {
+        _iterateThroughJason(out);
+        console.log('Checkout this JSON! ', out);
+      }).catch(err => {
+        throw err
+      });
+    }
+
+    var parseJsonFromVar = function (out) {
+      _iterateThroughJason(out);
+    }
+
+    var getUrls = function () {
+      return _url;
+    }
+
+    var getTitles = function () {
+      return _title;
+    }
+
+    var getBodies = function () {
+      return _body;
+    }
+
+    var getThumbnails = function () {
+      return _thumbnail;
+    }
+    return {
+      parseJsonFromVar:parseJsonFromVar,
+      parseJsonFromUrl:parseJsonFromUrl,
+      getUrls: getUrls,
+      getTitles: getTitles,
+      getBodies: getBodies,
+      getThumbnails: getThumbnails
+    }
+  }
+})(this);
---
+++ spec/jason-parser-spec.js
@@ -0,0 +1,23 @@
+"use strict";
+
+showOff("JsonParser", function(){
+  var myParser = new JsonParser();
+  //jsonP is the json object stored in politics.js
+
+  theThingy(".parseJasonFromVar should take a json stored and extract the fields", function(){
+    myParser.parseJsonFromVar(jsonP);
+    hope(myParser.getUrls().length).toEqual(10);
+    hope(myParser.getBodies().length).toEqual(10);
+    hope(myParser.getTitles().length).toEqual(10);
+    hope(myParser.getThumbnails().length).toEqual(10);
+  });
+
+  // theThingy(".parseJasonFromUrl should take json from an api", function(){
+  //   myParser.parseJsonFromUrl("http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/politics?show-fields=all");
+  //   hope(myParser.getUrls().length).toEqual(10);
+  //   hope(myParser.getBodies().length).toEqual(10);
+  //   hope(myParser.getTitles().length).toEqual(10);
+  //   hope(myParser.getThumbnails().length).toEqual(10);
+  // });
+
+});
