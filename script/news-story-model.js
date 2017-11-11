bed643675b312a44a8fe337b0a7a1ea5cfae8e48
Jon Sanders <jonsanders101@aol.com>
2017-11-11 12:11:22 +0000

    Add getSummary function

--- script/news-story-model.js
+++ script/news-story-model.js
@@ -2,11 +2,15 @@
 
   function NewsStoryModel () {
     this._headline = "Headline 1";
+    this._summary = "Summary 1";
   }
 
   NewsStoryModel.prototype = {
     getHeadline: function () {
       return this._headline;
+    },
+    getSummary: function () {
+      return this._summary;
     }
   };
 
--- spec/news-story-model-spec.js
+++ spec/news-story-model-spec.js
@@ -1,7 +1,12 @@
+var newsStory = new NewsStoryModel();
+
 describe('News Story Model',
   assert('getHeadline() returns news story headline', function() {
-      var newsStory = new NewsStoryModel();
       return (newsStory.getHeadline() === "Headline 1");
+    }()
+  ),
+  assert('getSummary() returns story summary', function() {
+      return (newsStory.getSummary() === "Summary 1");
     }()
   )
 );
