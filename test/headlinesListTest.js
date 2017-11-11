938718619ceafd9f0ab307fb37b340cf734eca49
Lucy Borthwick <lucyborthwick95@gmail.com>
2017-11-11 11:36:21 +0000

    Removes instantiation test now more relevent ones are in place

--- test/headlinesListTest.js
+++ test/headlinesListTest.js
@@ -2,12 +2,6 @@
 
   var headlinesList = new HeadlinesList();
 
-  function testsInstantiation() {
-    console.log("Testing Headlines List Instantiates");
-    assert.isTrue(headlinesList.constructor === HeadlinesList);
-  };
-  testsInstantiation();
-
   function testsReturnAllHeadlines() {
     console.log("It returns a list of headlines");
     assert.isTrue(Array.isArray(headlinesList.returnHeadlines()));
