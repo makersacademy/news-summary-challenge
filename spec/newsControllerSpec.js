d2dd6e83bf7f778acf41a18871211948ec089c17
Ainsley <ab.chang@me.com>
2017-11-12 16:03:35 +0000

    added event listeners and summary page methods

--- spec/newsControllerSpec.js
+++ spec/newsControllerSpec.js
@@ -4,48 +4,66 @@ describe('newsController', function () {
 
   var newsController;
 
+  var mockData = {
+    content: {
+      id: "sport/live/2017/nov/11/wrnational-live",
+      webTitle: "testArticle",
+      webUrl: "httpve",
+      fields: { thumbnail: 'http://lorempixel.com/500/300' }
+    }
+  }
+
+  var mockResult = {
+    id: "sport/live/2017/nov/11/wrnational-live",
+    webTitle: "testArticle",
+    webUrl: "httpve",
+  }
+
   var mockAPI = {
     search: function (params, callback, id = '') {
       if (id === '') {
         callback({
           response: {
-            results: [
-              {
-                id: "sport/live/2017/nov/11/wrnational-live",
-                webTitle: "testArticle",
-                webUrl: "httpve",
-              },
-              {
-                id: "sport/live/2017/nov/11/wrnational-live",
-                webTitle: "testArticle",
-                webUrl: "httpve",
-              },
-            ]
+            results: [mockResult, mockResult, mockResult]
           }
         })
       } else {
-        callback({
-          response: {
-            content: {
-              id: "sport/live/2017/nov/11/wrnational-live",
-              webTitle: "testArticle",
-              webUrl: "httpve",
-              fields: { thumbnail: 'http://lorempixel.com/500/300' }
-            }
-          }
-        });
+        callback({ response: mockData });
       }
+    }
+  };
+
+  var mockAlyienAPI = {
+    search: function (articleURL, callback) {
+      callback({ sentences: ["sentence 1", "sentence 2"] });
     }
   };
 
   beforeEach(function () {
 
     var listModel = new ListModel(mockAPI);
+    var summaryModel = new SummaryModel(mockAlyienAPI);
     
     newsController = new NewsController({
       containerId: 'test',
-      ListView: ListView,
-      listModel: listModel
+      listModel: listModel,
+      summaryModel: summaryModel
+    });
+
+  });
+
+  afterEach(function () {
+    var container = document.getElementById('test');
+    container.innerHTML = '';
+  });
+
+  describe('renderSummary', function () {
+    
+    it('renders the summary for a specific news article', function () {
+      newsController.renderRow();
+      newsController.renderSummary("sport/live/2017/nov/11/wrnational-live");
+      var container = document.getElementById('test');
+      expect(container.innerHTML).toInclude('sentence 2');
     });
 
   });
--- src/controllers/newsController.js
+++ src/controllers/newsController.js
@@ -6,46 +6,72 @@
 
     var container = document.getElementById(options.containerId);
     var listModel = options.listModel;
-    // var listView = new options.ListView(listModel);
-    var counter = 0;
-
-    function showListItem(item) {
-      counter++;
-      container.innerHTML += listView.listItemToHTML(item);
-    }
-
-    function numberOfStackedArticles() {
-      var articles = document.getElementsByClassName('article');
-      if (!articles[0]) return ( window.innerHeight / 400);
-      return Math.ceil((window.innerHeight/ articles[0].offsetHeight));
-    }
-
-    function numberOfAdjacentArticles() {
-      var articles = document.getElementsByClassName('article');
-      if (!articles[0]) return (window.innerWidth / 500);
-      return (window.innerWidth / articles[0].offsetWidth);
-    }
-
+    var summaryModel = options.summaryModel;
+    
     function renderPage() {
+      container.innerHTML = '';
       var stackCount = numberOfStackedArticles();
       for (var i = 0; i < stackCount; i++) {
         renderRow();
         stackCount = numberOfStackedArticles();
-      }
-    }
-
+      };
+    };
+    
     function renderRow() {
-      var widthCount = Math.floor(numberOfAdjacentArticles()); 
+      var widthCount = Math.floor(numberOfAdjacentArticles());
       for (var i = 0; i < widthCount; i++) {
         listModel.fetchArticle(showListItem);
       };
-    }
-  
+    };
+
+    function renderSummary(articleId) {
+      var article = listModel.getArticleById(articleId);
+      summaryModel.fetchSummary(article, showSummary)
+    };
+
+    function showListItem(item) {
+      container.innerHTML += listView.listItemToHTML(item);
+    };
+
+    function showSummary(article) {
+      container.innerHTML = summaryView.articleSummaryToHTML(article);
+    };
+
+    function attachEventListeners() {
+      window.addEventListener('hashchange', hashChange)
+    };
+
+    function hashChange() {
+      if (window.location.hash === '') {
+        renderPage();
+      } else {
+        renderSummary(getArticleIdFromUrl(window.location));
+      };
+    };
+
+    function numberOfStackedArticles() {
+      var articles = document.getElementsByClassName('article');
+      if (!articles[0]) return (window.innerHeight / 400);
+      return Math.ceil((window.innerHeight / articles[0].offsetHeight));
+    };
+    
+    function numberOfAdjacentArticles() {
+      var articles = document.getElementsByClassName('article');
+      if (!articles[0]) return (window.innerWidth / 500);
+      return (window.innerWidth / articles[0].offsetWidth);
+    };
+    
+    function getArticleIdFromUrl(location) {
+      return location.hash.split('#')[1];
+    };
+
     return {
       renderPage: renderPage,
-      renderRow: renderRow
-    }
+      renderRow: renderRow,
+      renderSummary: renderSummary,
+      attachEventListeners: attachEventListeners
+    };
 
-  }
+  };
   
 })(this);
