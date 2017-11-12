020523e38ce54a134d3bb5f7117d6ff89a54a48c
Tim Jones <timjones10@hotmail.com>
2017-11-12 17:10:30 +0000

    Added a getSummary function to Story object

--- src/news-story-model.js
+++ src/news-story-model.js
@@ -1,10 +1,15 @@
 (function(exports) {
-  function Story(headline) {
+  function Story(headline, summary) {
     this._headline = headline
+    this._summary = summary
   }
 
   Story.prototype.getHeadline = function() {
     return this._headline;
+  };
+
+  Story.prototype.getSummary = function() {
+    return this._summary;
   };
 
 exports.Story = Story;
--- tests/news-story-model-Spec.js
+++ tests/news-story-model-Spec.js
@@ -1,8 +1,15 @@
 (function(exports) {
 
 function storyHasHeadline() {
-var story = new Story("Dog shoots Cat");
+var story = new Story("Dog shoots Cat", "A dog shot a cat, today.");
 assert.isTrue(story.getHeadline() === "Dog shoots Cat");
 }
 storyHasHeadline();
+
+function storyHasSummary() {
+var story = new Story("Dog shoots Cat", "A dog shot a cat, today.");
+assert.isTrue(story.getSummary() === "A dog shot a cat, today.");
+}
+storyHasSummary();
+
 })(this);
