1108822db6bac898c1c083de3981d25b5ab88dd4
ainsleybc <ab.chang@me.com>
2017-11-13 10:11:04 +0000

    fixed responsive styling bugs

--- public/src/API/guardianAPI.js
+++ public/src/API/guardianAPI.js
@@ -2,8 +2,7 @@
 
 ;(function (exports) {
   
-  // var url = 'http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/';
-  var url = 'http://content.guardianapis.com/';
+  var url = 'http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/';
 
   function parseQueryParams(params) {
     var query = [];
@@ -18,7 +17,7 @@
     if (id) var id = id + '?';
     
     var xhr = new XMLHttpRequest();
-    var request = url + id + parseQueryParams(params)+'&api-key=5d478b85-06aa-4888-95e3-d407910c480c';
+    var request = url + id + parseQueryParams(params);
 
     xhr.onload = function () {
       if (xhr.status === 200) {
