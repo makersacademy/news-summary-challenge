3df567da6315b26fae0a1cc32e151d4904151ecd
Lucy Borthwick <lucyborthwick95@gmail.com>
2017-11-13 10:46:48 +0000

    Stubbing tests for the guardian API headlines

--- test/guardianAPITest.js
+++ test/guardianAPITest.js
@@ -5,24 +5,15 @@
       return "Angry Tiger loose in London!"
     }
   }
-  // var apiCall = new GuardianAPI();
+
   var list = new HeadlinesList();
   var headline = "Angry Tiger loose in London!";
-  //
-  // function testsInstantiation() {
-  //   console.log("Is instantiated");
-  //   assert.isTrue(apiCall.constructor === GuardianAPI);
-  // };
+
 
   function testsHeadlineRequest() {
     console.log("Tests that a headline appears after XHR request")
     assert.isTrue(mockAPI.runRequest() === headline);
   };
-
-  // function testsHeadlineStored() {
-  //   mockAPI.runRequest;
-  //   assert.isTrue(list.returnHeadlines() === headline)
-  // };
 
   runTests = function() {
     testsHeadlineRequest();
