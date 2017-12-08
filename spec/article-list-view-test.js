8624f35b9a0ce2697d9f0bc8fc55122c6a6f6631
Allan Wazacz <cazwazacz@gmail.com>
2017-12-08 16:14:08 +0000

    Create doubles for Article and Article List

--- spec/article-list-view-test.js
+++ spec/article-list-view-test.js
@@ -1,16 +1,37 @@
 (function() {
   'use strict';
 
-  var articleList = new ArticleList(Article);
-  articleList.add('headline', 'image', 'summary', 'articleURL');
-  articleList.add('headline2', 'image2', 'summary2', 'articleURL2');
-  articleList.add('headline3', 'image3', 'summary3', 'articleURL3');
+  function ArticleDouble(image, id, headline) {
+    this._image = image;
+    this._id = id;
+    this._headline = headline;
+  }
+
+  ArticleDouble.prototype = {
+    image: function() {
+      return this._image;
+    },
+    id: function() {
+      return this._id;
+    },
+    headline: function() {
+      return this._headline;
+    }
+  };
+
+  function ArticleListDouble(articlesArray) {
+    this._articles = articlesArray;
+  }
+
+  ArticleListDouble.prototype.articles = function () {
+    return this._articles;
+  };
+
+  var articleList = new ArticleListDouble([new ArticleDouble('image', '1', 'headline'), new ArticleDouble('image2', '2', 'headline2'), new ArticleDouble('image3', '3', 'headline3')]);
   var articleListView = new ArticleListView(articleList);
 
   describe('Article list view converts article to html', function() {
     var HTMLString = `<div><div><img src="image"><a href="#articles/1"><h3>headline</h3></a></div><div><img src="image2"><a href="#articles/2"><h3>headline2</h3></a></div><div><img src="image3"><a href="#articles/3"><h3>headline3</h3></a></div></div>`
-    console.log('expectation', HTMLString);
-    console.log('result', articleListView.render());
     return assert.isTrue(articleListView.render() === HTMLString);
   })
 
