f278c3dacbede608e588cbbba7550d24a863c784
Allan Wazacz <cazwazacz@gmail.com>
2017-12-08 15:28:52 +0000

    Create Article List model which creates and adds new Articles

---
+++ spec/article-list-test.js
@@ -0,0 +1,17 @@
+(function() {
+
+  function ArticleDouble(headline, imageURL, summary, articleURL) {};
+
+  var articleList = new ArticleList(ArticleDouble);
+
+  describe('A new article list has a an empty array of articles', function() {
+    return assert.isTrue(articleList.articles().length === 0);
+  })
+
+  describe('An article list can create add new articles', function() {
+    articleList.add('Headline', 'ImageURL', 'Summary', 'ArticleURL');
+    console.log(articleList.articles());
+    return assert.isTrue(articleList.articles().length === 1);
+  })
+
+}());
---
+++ src/article-list-model.js
@@ -0,0 +1,20 @@
+(function(exports) {
+  function ArticleList(articleModel) {
+    this._articles = [];
+    this._article = articleModel;
+  }
+
+  ArticleList.prototype = {
+    articles: function() {
+      return this._articles;
+    },
+    add: function(headline, image, summary, articleURL) {
+      this._articles.push(new this._article(headline, image, summary, articleURL))
+    }
+  }
+
+  // ArticleList.prototype.articles = function () {
+  // };
+
+  exports.ArticleList = ArticleList;
+}(this));
