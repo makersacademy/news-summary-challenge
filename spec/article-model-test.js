eb527e784ea92a7f5492444752a60fa2437ad7a0
Allan Wazacz <cazwazacz@gmail.com>
2017-12-08 15:11:57 +0000

    Add headline, image, summary and articleURL properties to Article

--- spec/article-model-test.js
+++ spec/article-model-test.js
@@ -1,7 +1,24 @@
 (function() {
-  var article = new Article();
+  var article = new Article('Headline', 'ImageURL', 'Summary', 'ArticleURL');
 
-  describe('An article has an id', function() {
-    return assert.arrayIncludes(Object.getOwnPropertyNames(Article.prototype), 'id');
+  describe('A new article has an id of zero', function() {
+    return assert.isEqual(article.id(), 0);
   })
+
+  describe('A new article has a headline', function() {
+    return assert.isEqual(article.headline(), 'Headline');
+  })
+
+  describe('A new article has an image url', function() {
+    return assert.isEqual(article.image(), 'ImageURL');
+  })
+
+  describe('A new article has a summary', function() {
+    return assert.isEqual(article.summary(), 'Summary');
+  })
+
+  describe('A new article has an article url', function() {
+    return assert.isEqual(article.articleURL(), 'ArticleURL');
+  })
+
 })();
--- src/article-model.js
+++ src/article-model.js
@@ -1,9 +1,33 @@
 (function(exports) {
-  function Article() {}
 
-  Article.prototype.id = function () {
-    return 1;
-  };
+  var id = 0;
+
+  function Article(headline, image, summary, articleURL) {
+    this._id = id;
+    id++;
+    this._headline = headline;
+    this._image = image;
+    this._summary = summary;
+    this._articleURL = articleURL;
+  }
+
+  Article.prototype = {
+    id: function() {
+      return this._id;
+    },
+    headline: function() {
+      return this._headline;
+    },
+    image: function() {
+      return this._image;
+    },
+    summary: function() {
+      return this._summary;
+    },
+    articleURL: function() {
+      return this._articleURL;
+    },
+  }
 
   exports.Article = Article;
 }(this));
