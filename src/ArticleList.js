79fe5ed90e04f970ef8cef929c8a21c1bb1bc46d
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-12-09 20:05:17 +0000

    Added example healdine in order to start html

--- src/ArticleList.js
+++ src/ArticleList.js
@@ -1,5 +1,4 @@
 
-(function(exports){
   function ArticleList(){
     this.list = []
   };
@@ -8,24 +7,26 @@
     this.list.push(article)
   }
   ArticleList.prototype.getData = function(){
+    var articlelist = this
     var response
     var request = new XMLHttpRequest();
 
     request.onreadystatechange = function(){
       if(this.readystate == 4 && this.status == 200){
-        response = JSON.parse(this.responseText)
-        response.forEach(function(results){
+        response = JSON.parse(request.response).results
+          response.forEach(function(results){
           var headline = new Headline(results.webTitle, results.webPublicationDate, results.webUrl);
-          this.addArticle(headline)
+          articlelist.addArticle(headline)
         });
       };
     };
-
+    var response = JSON.parse('{"type":"article","webPublicationDate":"2017-12-10T00:05:00Z","webTitle":"Alarm over restraint of NHS mental health patients", "webUrl":"https://www.theguardian.com/society/2017/dec/09/women-black-patients-physically-restrained-mental-health"}')
+    var headline = new Headline(response.webTitle, response.webPublicationDate, response.webUrl);
+    articlelist.addArticle(headline)
     request.open('GET', 'https://content.guardianapis.com/search?api-key=b7f045b7-75c0-44af-9f61-f7d188b6261d', true);
     request.send()
  };
-  exports.ArticleList = ArticleList
-})(this);
+
 api = new ArticleList()
 api.getData();
 console.log(api.list)
