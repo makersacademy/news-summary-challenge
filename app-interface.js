4ea77d0a2b7ed26d1ade81d2cf159f01ec881ee7
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-12-09 22:33:49 +0000

    Adds ten new articles in html

--- app-interface.js
+++ app-interface.js
@@ -1,2 +1,24 @@
 var newspaper = new ArticleList();
 newspaper.getData();
+var newsworthy = document.querySelector('#newsworthy')
+
+function addArticleToList(){
+    var newArticle = document.createElement('div')
+    var newArticleTitle = document.createElement('h1')
+    var newArticleDate = document.createElement('p')
+    var newArticleUrl = document.createElement('p')
+    newsworthy.insertBefore(newArticle, newsworthy.childNodes[0]);
+    newArticle.setAttribute('class', 'article')
+    newArticle.appendChild(newArticleTitle)
+    newArticle.appendChild(newArticleDate)
+    newArticle.appendChild(newArticleUrl)
+  }
+
+  function addAll(){
+    console.log(newspaper.list)
+    for(i = 0; i < 10; i ++){
+      addArticleToList();
+    };
+  };
+
+  addAll();
--- src/ArticleList.js
+++ src/ArticleList.js
@@ -1,29 +1,27 @@
 
   function ArticleList(){
     this.list = []
+    this.emptyArray = []
   };
 
   ArticleList.prototype.addArticle = function(article){
     this.list.push(article)
   }
+
   ArticleList.prototype.getData = function(){
     var request
-    var dieu = this.list
+    var dieu = this
     var getback
     request = new XMLHttpRequest();
-      request.open('GET', 'https://content.guardianapis.com/search?api-key=b7f045b7-75c0-44af-9f61-f7d188b6261d', true);
-      request.onreadystatechange = function(){
-        if(this.readyState == 4 && this.status == 200){
-          getback = JSON.parse(this.response).response.results
-          getback.forEach(function(result){
-            var headline = new Headline(result.webTitle, result.webPublicationDate, result.webUrl);
-            dieu.push(headline)
-          });
-        };
+    request.open('GET', 'https://content.guardianapis.com/search?api-key=b7f045b7-75c0-44af-9f61-f7d188b6261d', true);
+    request.onreadystatechange = function(){
+      if(this.readyState == 4 && this.status == 200){
+        getback = JSON.parse(this.response).response.results
+        getback.forEach(function(result){
+          var headline = new Headline(result.webTitle, result.webPublicationDate, result.webUrl);
+          dieu.addArticle(headline)
+        });
       };
-    request.send()
+    };
+  request.send()
  };
-
-api = new ArticleList()
-api.getData();
-console.log(api.list)
