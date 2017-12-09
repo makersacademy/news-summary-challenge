def8f79421679ef0cbffb7e4899446c93f498d0a
gabrielabud <gabriela.budeanu@gmail.com>
2017-12-09 21:39:21 +0000

    Refactor article class

--- src/article.js
+++ src/article.js
@@ -11,33 +11,35 @@
     this._id = 0;
   };
 
-  Article.prototype.headline = function () {
-    return this._headline;
-  };
+  Article.prototype = {
+    headline: function() {
+      return this._headline;
+    },
 
-  Article.prototype.body = function () {
-    return this._body;
-  };
+    body: function() {
+      return this._body;
+    },
 
-  Article.prototype.url = function () {
-    return this._url;
-  };
+    url: function() {
+      return this._url;
+    },
 
-  Article.prototype.date = function () {
-    return this._date;
-  };
+    date: function() {
+      return this._date;
+    },
 
-  Article.prototype.thumbImage = function () {
-    return this._thumbImage;
-  };
+    thumbImage: function() {
+      return this._thumbImage;
+    },
 
-  Article.prototype.mainImage = function () {
-    return this._mainImage;
-  };
+    mainImage: function() {
+      return this._mainImage;
+    },
 
-  Article.prototype.id = function () {
-    return this._id;
-  };
+    id: function() {
+      return this._id;
+    },
+  }
 
   exports.Article = Article;
 })(this);
