71c64efa2eae607273fe5fe531abd87e6289dd5c
Allan Wazacz <cazwazacz@gmail.com>
2017-12-09 00:01:28 +0000

    Add returns for clarity

--- src/main-controller.js
+++ src/main-controller.js
@@ -1,7 +1,12 @@
 (function() {
   'use strict';
 
-  var articleController = new ArticleController(ArticleListView, new ArticleList(Article), SingleArticleView, XMLHttpRequest);
+  var articleController = new ArticleController(
+    ArticleListView,
+    new ArticleList(Article),
+    SingleArticleView,
+    XMLHttpRequest
+  );
   articleController.ajaxRequest();
   articleController.hashChangeListener();
 
