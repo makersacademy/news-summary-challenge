164469b81b915a171a9a0cb21838075689897d60
antonio <antoniobelmardacosta@gmail.com>
2017-12-09 22:29:01 +0000

    Add testing framework

---
+++ spec/assertions.js
@@ -0,0 +1,27 @@
+'use strict'
+
+var assert = {
+    isTrue: function(assertionToCheck) {
+        if (!assertionToCheck) {
+            throw new Error("Assertion failed: " + assertionToCheck + " is not truthy");
+        } else {
+          return "true"
+        }
+    },
+
+    isFalse: function(assertionToCheck) {
+        if (assertionToCheck) {
+            throw new Error("Assertion failed: " + assertionToCheck + " is truthy");
+        } else {
+          return "true"
+        }
+    },
+
+    returns: function(methodCall, expectedResult) {
+      if(methodCall !== expectedResult) {
+        throw new Error(methodCall + "does not return" + expectedResult)
+      } else {
+        return "true"
+      }
+    },
+};
