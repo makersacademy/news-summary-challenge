0948fb33ab8dd6727b918af74d655ec886a24ad0
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-12-10 15:10:23 +0000

    Added button to go to url

--- app-interface.js
+++ app-interface.js
@@ -22,14 +22,19 @@ function addArticleToList(){
   console.log(listOfArticles.length)
     listOfArticles.forEach(function(headline){
       newArticle = document.createElement('div')
-      newsworthy.insertBefore(newArticle, newsworthy.childNodes[0]);        newArticle.setAttribute('class', 'article')
+      newsworthy.insertBefore(newArticle, newsworthy.childNodes[0]);
+      newArticle.setAttribute('class', 'article')
       newArticleTitle = document.createElement('h1')
       newArticle.appendChild(newArticleTitle)
-      newArticleTitle.innerHTML = headline.title;        newArticleDate = document.createElement('p')
+      newArticleTitle.innerHTML = headline.title;
+      newArticleDate = document.createElement('p')
       newArticle.appendChild(newArticleDate);
-      newArticleDate.innerHTML = headline.date;              newArticleUrl = document.createElement('p')
+      newArticleDate.innerHTML = headline.date;
+      newArticleUrl = document.createElement('input')
+      newArticleUrl.setAttribute('type','button')
+      newArticleUrl.setAttribute('onclick', `window.location.href='${headline.url}'`)
+      newArticleUrl.setAttribute('class', 'urlbutton')
       newArticle.appendChild(newArticleUrl);
-      newArticleUrl.innerHTML = headline.url;
     });
   };
 
