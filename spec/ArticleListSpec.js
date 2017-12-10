128c24b6edcc32fd77d17623c47ca0b95ca1ec75
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-12-10 16:14:37 +0000

    Passed test adding article to list

--- spec/ArticleListSpec.js
+++ spec/ArticleListSpec.js
@@ -3,14 +3,23 @@ var ArticleList = ArticleListModule.ArticleList;
 
 describe('Article list object',function(){
   var articlelist;
+  var article;
 
   beforeEach(function(){
     articlelist = new ArticleList();
+    article = 'Fake Article'
   });
 
   describe('has list', function(){
     it('is an empty array', function(){
       expect(articlelist.list).toBeEmpty()
+    })
+  })
+
+  describe('#addArticle', function(){
+    it('populates list with article', function(){
+      articlelist.addArticle(article);
+      expect(articlelist.list).toContain('Fake Article')
     })
   })
 
