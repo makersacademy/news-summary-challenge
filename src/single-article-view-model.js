eaaebccc812053da91510e9ebd4e7754ad43929c
Allan Wazacz <cazwazacz@gmail.com>
2017-12-11 10:48:24 +0000

    Add 'Go back' button to single article view

--- src/single-article-view-model.js
+++ src/single-article-view-model.js
@@ -6,7 +6,7 @@
   }
 
   SingleArticleView.prototype.render = function () {
-    return `<div class="single-view-article"><img src="${this._article.image()}"><h3>${this._article.headline()}</h3><div class="article-summary">${this._article.summary()}</div><a href="${this._article.articleURL()}">View the full article</a></div>`
+    return `<div class="single-view-article"><img src="${this._article.image()}"><h3>${this._article.headline()}</h3><div class="article-summary">${this._article.summary()}</div><a class="view-full-article" href="${this._article.articleURL()}">View the full article</a><a class="back-button" href="/#">Go back</a></div>`
   };
 
   exports.SingleArticleView = SingleArticleView;
