93771f3287356b4cc7feee8695d802f08f469ce9
Oleg Grushetskyy <olegfkl@gmail.com>
2017-12-11 16:33:47 +0000

    Add callback function

--- src/newsController.js
+++ src/newsController.js
@@ -2,16 +2,18 @@
   function Controller(render = new RenderNews) {
     this._render = render;
 
-    Controller.prototype.getNews = function() {
+    Controller.prototype.getNews = function(callback) {
       var guardian = "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/world"
       var that = this
       var xhttp = new XMLHttpRequest();
       xhttp.open("GET", guardian, true);
 
       xhttp.onreadystatechange = function() {
+        console.log('2')
         if (this.readyState == 4 && this.status == 200) {
           var data = JSON.parse(this.responseText)
-          that.render(data.response)
+          callback(data.response);
+          // that.render(data.response)
         }
       }
      xhttp.send();
@@ -25,5 +27,9 @@
 
   exports.Controller = Controller;
 })(this);
+
    var controller = new Controller();
-   controller.getNews();
+   controller.getNews(function(data) {
+   controller.render(data)
+     console.log('1', data)
+   });
