9a3442b3c22c6bc5e694ddfbcdbf4754389ddd16
Peter Johnstone <peter.wdj@gmail.com>
2017-12-13 20:38:32 +0000

    Inject synchronicity status to pass tests but retain asynchronicity in production code

--- app/src/stories.js
+++ app/src/stories.js
@@ -1,17 +1,40 @@
 (function(exports) {
-  function Stories() {
+  function Stories(xhrBuilder) {
     this.articles = [];
+  };
+
+  Stories.prototype.addArticles = function(stories) {
+    for (i = 0; i < stories.length; i++) {
+      this.articles.push(stories[i]);
+    }
+  };
+
+  Stories.prototype.getHeadlines = function(url, apiKey, isAsynchronous) {
+    var storiesList = this;
+    var stories = xhrBuilder.buildRequest()
+    stories.open('GET', url + apiKey, isAsynchronous);
+    stories.onload = function() {
+      var storiesData = JSON.parse(stories.responseText);
+      storiesList.addArticles(storiesData.response.results);
+      console.log(storiesList.articles);
+    }
+    stories.send();
   };
 
   exports.Stories = Stories;
 })(this);
 
-Stories.prototype.addArticles = function(array) {
-  for (i = 0; i < array.length; i++) {
-    this.articles.push(array[i])
+
+function XhrBuilder() {
+
+  function buildRequest() {
+    return new XMLHttpRequest();
   }
-};
 
+  return {
+    buildRequest: buildRequest
+  }
+}
 
-// var articleObject = new XMLHttpRequest();
-// articleObject.open('GET', 'http://content.guardianapis.com/search?api-key=' + apiKey(), true);
+var xhrBuilder = new XhrBuilder();
+console.log(xhrBuilder);
--- spec/storiesSpec.js
+++ spec/storiesSpec.js
@@ -1,23 +1,12 @@
-// (function(exports) {
-//   function testStoriesArticlesIsEmptyArray() {
-//     var stories = new Stories();
-//
-//     if (stories.articles !== []) {
-//       throw new Error("Articles is not an empty array");
-//     } else {
-//       console.log("Articles is an empty array; your test passed");
-//     }
-//   };
-//
-//   testStoriesArticlesIsEmptyArray();
-// })(this);
-
 (function(exports) {
   function storiesArticlesShouldBeTenLong() {
     var stories = new Stories();
-    stories.addArticles();
+    var guardianHeadlinesApi = 'https://content.guardianapis.com/search?api-key=';
 
-    if (stories.articles.length != 10) {
+    stories.getHeadlines(guardianHeadlinesApi, apiKey(), false);
+
+    if (stories.articles.length !== 10) {
+      console.log(stories.articles);
       throw new Error("Not all articles were added");
     } else {
       console.log("All stories were added; your test passed")
