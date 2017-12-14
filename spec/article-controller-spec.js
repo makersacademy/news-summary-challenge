c291776defe444c91ef1974bb8a5f97d75f98c6b
Lewis <lewisyoul@gmail.com>
2017-12-14 20:55:17 +0000

    comment out code as to run new tests

--- spec/article-controller-spec.js
+++ spec/article-controller-spec.js
@@ -1,26 +1,21 @@
-describe('returns html string of titles', function(){
-  var allNews = new AllNews()
-  // console.log(allNews.putInElements(testData.response.results))
-  return assert.isTrue(allNews.putInElements(testData.response.results) === '<ul><li><div><a href="https://www.theguardian.com/football/live/2017/dec/09/west-ham-united-v-chelsea-premier-league-live">West Ham United 1-0 Chelsea: Premier League – as it happened</a><p><a id="0" href="#football/live/2017/dec/09/west-ham-united-v-chelsea-premier-league-live">View Summary</a></p></div></li><li><div><a href="https://www.theguardian.com/money/2017/dec/09/millennial-railcard-young-workers-rail-fares">Millennial railcard: a fare deal or quick fix that fails to deliver?</a><p><a id="1" href="#money/2017/dec/09/millennial-railcard-young-workers-rail-fares">View Summary</a></p></div></li></ul>')
-});
 
-describe('renders the text string to the page', function(){
-  var allNews = new AllNews()
-  allNews.renderHTML("<ul><li><div>West Ham United 1-0 Chelsea: Premier League – as it happened</div></li></ul>")
-  return assert.isTrue(document.getElementById('newsfeed').innerHTML === "<ul><li><div>West Ham United 1-0 Chelsea: Premier League – as it happened</div></li></ul>")
+describe('ARTICLE CONTROLLER - renders the text string to the page', function(){
+  var articleController = new ArticleController()
+  articleController.renderHTML('<ul><li><div><a href="#testurl1">testtitle1</a></div></li></ul>')
+  return assert.isTrue(document.getElementById('newsfeed').innerHTML === '<ul><li><div><a href="#testurl1">testtitle1</a></div></li></ul>')
 });
 
 // TESTS BELOW HERE ARE USING SAME VARIABLE + ON TIME DELAY
 
-var allNews = new AllNews()
-allNews.makeRequest("http://127.0.0.1:8080/testdata.html", allNews.putInElementsRenderHTML)
-setTimeout(function() {
-  describe('checks that data is downloaded from the API', function(){
-    return assert.isTrue(allNews._data instanceof Array)
-  });
-
-  describe('returns a specific article when passed the relevant id', function() {
-    var id = "football/live/2017/dec/09/tottenham-v-stoke-real-madrid-v-sevilla-and-more-clockwatch-live"
-    return assert.isTrue(allNews.returnArticleWithSameId(id) === allNews._data[0])
-  });
-}, 5000);
+// var allNews = new AllNews()
+// allNews.makeRequest("http://127.0.0.1:8080/testdata.html", allNews.putInElementsRenderHTML)
+// setTimeout(function() {
+//   describe('ARTICLE CONTROLLER - checks that data is downloaded from the API', function(){
+//     return assert.isTrue(allNews._data instanceof Array)
+//   });
+//
+//   describe('ARTICLE CONTROLLER - returns a specific article when passed the relevant id', function() {
+//     var id = "football/live/2017/dec/09/tottenham-v-stoke-real-madrid-v-sevilla-and-more-clockwatch-live"
+//     return assert.isTrue(allNews.returnArticleWithSameId(id) === allNews._data[0])
+//   });
+// }, 5000);
--- src/controllers/article-controller.js
+++ src/controllers/article-controller.js
@@ -1,10 +1,10 @@
 (function (exports){
 
-  function AllNews(){
+  function ArticleController(){
     this._data
   }
 
-  AllNews.prototype.makeRequest = function(url, callback){
+  ArticleController.prototype.makeRequest = function(url, callback){
     var request = new XMLHttpRequest()
     var self = this
     request.open("GET", url)
@@ -16,30 +16,33 @@
     request.send()
   }
 
-  AllNews.prototype.returnArticleWithSameId = function(id) {
-    var final = this._data.find(function(article){
-      return article.id === id
-    });
-    return final
+  ArticleController.prototype.renderHTML = function(htmlString) {
+    document.getElementById('newsfeed').innerHTML = htmlString
   }
+  // AllNews.prototype.returnArticleWithSameId = function(id) {
+  //   var final = this._data.find(function(article){
+  //     return article.id === id
+  //   });
+  //   return final
+  // }
 
-  AllNews.prototype.putInElementsRenderHTML = function(data, self) {
-    self.renderHTML(self.putInElements(data))
-  }
-
-  AllNews.prototype.renderHTML = function(string){
-    document.getElementById('newsfeed').innerHTML = string
-  }
+  // AllNews.prototype.putInElementsRenderHTML = function(data, self) {
+  //   self.renderHTML(self.putInElements(data))
+  // }
+  //
+  // AllNews.prototype.renderHTML = function(string){
+  //   document.getElementById('newsfeed').innerHTML = string
+  // }
 
   // VIEW
 
-  AllNews.prototype.putInElements = function(data){
-    var titlearr = data.map(function(item, index){
-      return '<li><div><a href="' + item.webUrl + '">'
-        + item.webTitle + '</a><p><a id="'+ index +'" href="#'+ item.id +'">View Summary</a></p></div></li>'
-    });
-    return "<ul>" + titlearr.join('') + "</ul>"
-  }
+  // AllNews.prototype.putInElements = function(data){
+  //   var titlearr = data.map(function(item, index){
+  //     return '<li><div><a href="' + item.webUrl + '">'
+  //       + item.webTitle + '</a><p><a id="'+ index +'" href="#'+ item.id +'">View Summary</a></p></div></li>'
+  //   });
+  //   return "<ul>" + titlearr.join('') + "</ul>"
+  // }
 
-  exports.AllNews = AllNews;
+  exports.ArticleController = ArticleController;
 })(this);
