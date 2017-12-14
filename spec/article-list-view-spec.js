cdf831d5be355ea5b33d80efa23db04f69b69fc7
Lewis <lewisyoul@gmail.com>
2017-12-14 20:29:36 +0000

    generate HTML string
    Create a method in Article List View that will take all titles and urls from each article and interpolate them into a string that can then be rendered to the page

--- spec/article-list-view-spec.js
+++ spec/article-list-view-spec.js
@@ -1,12 +1,20 @@
-describe("checks that the article list is passed as parameter when instantiating a new ArticleListView", function(){
-  var articleListView = new ArticleListView(new ArticleList())
-  return assert.isTrue(articleListView._articleList instanceof ArticleList)
+function DoubleArticle(url, title) {
+  this._url = url
+  this._title = title
+}
+
+function DoubleArticleList() {
+  this._allArticles = [new DoubleArticle("testurl1", "testtitle1"), new DoubleArticle("testurl2", "testtitle2")]
+}
+
+
+describe(" ARTICLE LIST VIEW - checks that the article list is passed as parameter when instantiating a new ArticleListView", function(){
+  var articleListView = new ArticleListView(new DoubleArticleList())
+  return assert.isTrue(articleListView._articleList instanceof DoubleArticleList)
 });
 
-describe("places each title of an article in its respecive HTML elements", function () {
-  var articleListView = new ArticleListView(new ArticleList())
-  return assert.isTrue(articleListView.putInElements() === )
-});
-// console.log(testData)
-// console.log(allNewsView.putInListElements())
-// return assert.isTrue(allNewsView.putInListElements() === "<ol><li><div>West Ham United 1-0 Chelsea: Premier League – as it happened</div></li><li><div>Millennial railcard: a fare deal or quick fix that fails to deliver?</div></li></ol>")
+describe("ARTICLE LIST VIEW - places each title of an article in its respecive HTML elements", function () {
+  var articleListView = new ArticleListView(new DoubleArticleList())
+  console.log(articleListView.putInElements())
+  return assert.isTrue(articleListView.putInElements() === '<ul><li><div><a href="#testurl1">testtitle1</a></div></li><li><div><a href="#testurl2">testtitle2</a></div></li></ul>')
+});                                                        
--- src/views/article-list-view.js
+++ src/views/article-list-view.js
@@ -4,5 +4,12 @@
     this._articleList = articleList
   }
 
+  ArticleListView.prototype.putInElements = function() {
+    var listEls = this._articleList._allArticles.map(function(item) {
+      return `<li><div><a href="#${item._url}">${item._title}</a></div></li>`
+    });
+    return '<ul>' + listEls.join('') + '</ul>'
+  }
+
   exports.ArticleListView = ArticleListView;
 })(this);
