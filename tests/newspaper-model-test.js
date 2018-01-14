0f3c7b4208a980063cfacd26d4aac3b0cf119f8a
Keran <kerbraich@gmail.com>
2018-01-14 13:13:21 +0000

    update test to return headline from list

--- tests/newspaper-model-test.js
+++ tests/newspaper-model-test.js
@@ -2,13 +2,26 @@
 
   function DoubleArticle(headline, url, body) {};
 
+  DoubleArticle.prototype = {
+    displayHeadline: function() {
+      return "Headline";
+    }
+  };
+
   var doubleArticle = new DoubleArticle("Headline", "www.fakeurl.example", "Body")
   var newspaper = new Newspaper();
+  newspaper.addArticle(doubleArticle);
 
   function testArticleInNewspaperArray() {
-    newspaper.addArticle(doubleArticle);
     assert.isTrue(newspaper.listOfArticles.length === 1, "Article is added to newspaper array");
   };
 
+  function testReturnHeadlineFromList() {
+    console.log(newspaper)
+    console.log(newspaper.returnHeadlineFromList(0))
+    assert.isTrue(newspaper.returnHeadlineFromList(0) === "Headline", "Headline is returned from listOfArticles array")
+  };
+
   testArticleInNewspaperArray();
+  testReturnHeadlineFromList()
 })(this);
