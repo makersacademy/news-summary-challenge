039606ed651fa1dc8c8cb870cf633642b0d3434a
Xin Wang <wangxin00163@yahoo.co.uk>
2018-01-14 21:54:42 +0000

    When click on the link, news summary was displayed

--- index.js
+++ index.js
@@ -5,13 +5,11 @@ window.onload = function() {
 
 function loadStories() {
   var request = new XMLHttpRequest();
-  request.open('GET', 'https://content.guardianapis.com/search?api-key=test');
+  request.open('GET', 'http://content.guardianapis.com/search?section=politics&show-fields=thumbnail&api-key=test');
   request.onreadystatechange = function(response) {
     if (request.readyState === 4) {
       var json = JSON.parse(request.response);
       var array = json.response.results;
-
-      console.log(array)
       for (var i = 0; i < array.length; i++) {
         var newsObject = {
           webTitle: array[i].webTitle,
@@ -26,18 +24,14 @@ function loadStories() {
 }
 
 function readStory(id) {
-  var story = List.findStoryById(id);
+
+  var story = controller.list.findStoryById(id);
   var readRequest = new XMLHttpRequest();
   readRequest.open('GET', encodeURI('http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=' + story.url));
   readRequest.onreadystatechange = function(response) {
     if (readRequest.readyState === 4) {
-      // if(length === 0) {
-      //   var json = JSON.parse(readRequest.response).sentences;
-      //   document.getElementById('summaries').innerHTML = json + "<br><button onclick='readStory(" + storyId + ", 1)'>Read Full Story</button>";
-      // } else {
-        var text = JSON.parse(readRequest.response).text;
-        document.getElementById('summaries').innerHTML = text;
-
+        var summary = JSON.parse(readRequest.response).sentences.join("")
+        document.getElementById('summaries').innerHTML = summary;
     }
   };
   readRequest.send();
--- src/controller.js
+++ src/controller.js
@@ -12,3 +12,20 @@ Controller.prototype.listMaker = function(element){
 exports.Controller = Controller;
 
 })(this);
+
+function changeURLAndShowNote(){
+  window.addEventListener("hashchange", showCurrentNews)
+}
+
+function showCurrentNews(location){
+  var id = window.location.hash.split("#")[1].split("/")[1]
+  showNote(id)
+}
+
+
+function showNote(id){
+  var singleNoteView = new SingleNoteView(controller.list.notes[id])
+  document
+    .getElementById("note")
+    .innerHTML = singleNoteView.returnNoteText();
+};
