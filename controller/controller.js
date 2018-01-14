3ae1f2bd8011834cef2b55d0669ad58d307147ca
Eleanor Wintram <eleanorwintram@gmail.com>
2018-01-14 22:46:41 +0000

    adds functionality to the controller to load article body when link is clicked on headline list

--- controller/controller.js
+++ controller/controller.js
@@ -12,4 +12,20 @@
     document.getElementById("app").innerHTML = controller.headlineListView.createHtmlString();
   };
 
+  function displayArticleSummary() {
+    window.addEventListener("hashchange", findArticleFromURL)
+  };
+
+  function findArticleFromURL() {
+    var id = window.location.hash.split("#")[1];
+    var article = controller.headlineList.getArticleById(parseInt(id));
+    renderView(article)
+  };
+
+  function renderView(article) {
+    articleView = new ArticleView(article);
+    document.getElementById("app").innerHTML = articleView.createHtmlString();
+  };
+
 controller = new NewsController();
+displayArticleSummary();
