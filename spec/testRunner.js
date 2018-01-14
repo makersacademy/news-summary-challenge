6a9dd1c21a763a62a19ba7d0c8d47ba904154c33
tom <tomallpress@hotmail.com>
2018-01-14 12:47:22 +0000

    added test assertions, spec runner, and test runner, and gaurdian api key in git ignore

---
+++ spec/testRunner.js
@@ -0,0 +1,19 @@
+(function(exports) {
+  function TestRunner() {
+  this.tests = [];
+  };
+
+  TestRunner.prototype.addTest = function(test) {
+    this.tests.push(test);
+  };
+
+  TestRunner.prototype.runTests = function() {
+    this.tests.forEach((test) => test())
+  };
+
+  exports.TestRunner = TestRunner;
+})(this);
+
+var testRunner = new TestRunner();
+
+testRunner.runTests();
