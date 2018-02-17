6e61356bf4cbec60fafd6c676e67c75305c48d48
Ellie <elliewem@gmail.com>
2018-02-17 22:22:58 +0000

    Specific story link rendered when headline clicked

--- controller/controller.js
+++ controller/controller.js
@@ -9,5 +9,22 @@
     document.getElementById("headlines").innerHTML = this.headlineview.headlineHtml();
   }
 
+  Controller.prototype.getHeadlineId = function () {
+    return window.location.hash.split("#")[1];
+  }
+
+  Controller.prototype.retrieveHeadlineById = function (id) {
+    return this.headlineview.headlinerequester.findHeadlineById(id)
+  };
+
+  Controller.prototype.renderSpecificHeadline = function () {
+    var story = new SingleLinkView(this.retrieveHeadlineById(this.getHeadlineId()));
+    document.getElementById("story").innerHTML = story.singleLinkHtml();
+  }
+
+  Controller.prototype.hashChangeListener = function () {
+    window.addEventListener("hashchange", function () {this.renderSpecificHeadline()}.bind(this));
+  }
+
   exports.Controller = Controller
 })(this);
