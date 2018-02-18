c5b481c4b739f23d428dceff8e40067876668491
Alexy <alexandra.fina@yahoo.com>
2018-02-18 19:36:35 +0000

    Add main.css, iframe for another website, request api (not working)

--- tests/articleTest.js
+++ tests/articleTest.js
@@ -2,8 +2,3 @@ it('Note stored argument passed on instantiation', function() {
    var article = new Article('New gym opened in London');
    return assert.equals(note.title, 'New gym opened in London');
 });
-
-it("Returns a new note object", function(){
-  var note = Note.createArticle('Note 1')
-  return assert.isTrue(note instanceof Note)
-});
