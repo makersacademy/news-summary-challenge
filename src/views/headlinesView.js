e41ea1b7f1f50538045214f2e511bc300b9b780f
Leigh-ann <leighann.gant@gmail.com>
2018-02-18 21:33:37 +0000

    Headlines link to URL - href added to link to expanded article summary

--- src/views/headlinesView.js
+++ src/views/headlinesView.js
@@ -8,9 +8,13 @@
     this.newslog.articles.forEach(function(article){
       var node = document.createElement('LI')
       var headline = document.createElement('a')
+      var headlineExpand = document.createElement('a')
       headline.innerHTML = article.headline
-      headline.href = "#" + (index)
+      headline.href = article.url
+      headlineExpand.innerHTML = '  Click for more information'
+      headlineExpand.href = "#" + (index)
       node.appendChild(headline)
+      node.appendChild(headlineExpand)
       document.getElementById("headlines").appendChild(node)
       index ++
     })
