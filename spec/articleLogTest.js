44d6a8bad52405814bb43afd983c7653be2cd2ae
Tagrand <thomas.a.grand@gmail.com>
2018-02-18 19:01:58 +0000

    move test scripts to run after runner is loaded

--- spec/articleLogTest.js
+++ spec/articleLogTest.js
@@ -1,49 +1,51 @@
-let articleInstance = 0
+function articleLogTest() {
+  let articleInstance = 0
 
-const MockArticle = {
-  createArticle: function(data) {
-    articleInstance = data
-    return articleInstance}
+  const MockArticle = {
+    createArticle: function(data) {
+      articleInstance = data
+      return articleInstance}
+  }
+
+
+  it('should store articles', () => {
+    const articleLog = new ArticleLog(unitMockResponse, MockArticle);
+    assert.isTrue(articleLog.collection().includes(articleInstance));
+  });
+
+  it('should store fixed number of articles by default', () => {
+    const articleLog = new ArticleLog(unitMockResponse, MockArticle);
+    length = articleLog.length;
+    assert.equals(articleLog.collection().length, length);
+  });
+
+  it('should store six articles if told to', () => {
+    const articleLog = new ArticleLog(unitMockResponse, MockArticle, 6);
+    assert.equals(articleLog.collection().length, 6);
+  });
+
+  it('should store store only 1 article on a light news day', () => {
+    const articleLog = new ArticleLog(lightNewsDay, MockArticle);
+    assert.equals(articleLog.collection().length, 1);
+  });
+
+
+  it('should give article data on one news story', () => {
+    const articleLog = new ArticleLog(unitMockResponse, MockArticle);
+    let expectedData = 'first'
+    assert.equals(articleLog.collection()[0], expectedData);
+  });
+
+  it('should give article data on the third news story', () => {
+    const articleLog = new ArticleLog(unitMockResponse, MockArticle);
+    let expectedData = 'third'
+    assert.equals(articleLog.collection()[2], expectedData);
+  });
+
+  it('should store the fifth article', () => {
+    const articleLog = new ArticleLog(unitMockResponse, MockArticle);
+    const expectedData = 'fifth';
+    const lastIndex = articleLog.collection().length - 1;
+    assert.equals(articleLog.collection()[lastIndex], expectedData);
+  });
 }
-
-
-it('should store articles', () => {
-  const articleLog = new ArticleLog(unitMockResponse, MockArticle);
-  assert.isTrue(articleLog.collection().includes(articleInstance));
-});
-
-it('should store fixed number of articles by default', () => {
-  const articleLog = new ArticleLog(unitMockResponse, MockArticle);
-  length = articleLog.length;
-  assert.equals(articleLog.collection().length, length);
-});
-
-it('should store six articles if told to', () => {
-  const articleLog = new ArticleLog(unitMockResponse, MockArticle, 6);
-  assert.equals(articleLog.collection().length, 6);
-});
-
-it('should store store only 1 article on a light news day', () => {
-  const articleLog = new ArticleLog(lightNewsDay, MockArticle);
-  assert.equals(articleLog.collection().length, 1);
-});
-
-
-it('should give article data on one news story', () => {
-  const articleLog = new ArticleLog(unitMockResponse, MockArticle);
-  let expectedData = 'first'
-  assert.equals(articleLog.collection()[0], expectedData);
-});
-
-it('should give article data on the third news story', () => {
-  const articleLog = new ArticleLog(unitMockResponse, MockArticle);
-  let expectedData = 'third'
-  assert.equals(articleLog.collection()[2], expectedData);
-});
-
-it('should store the fifth article', () => {
-  const articleLog = new ArticleLog(unitMockResponse, MockArticle);
-  const expectedData = 'fifth';
-  const lastIndex = articleLog.collection().length - 1;
-  assert.equals(articleLog.collection()[lastIndex], expectedData);
-});
