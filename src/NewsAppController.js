60e59a8a2d455e92cfb1c390607154a91e0ee00e
domvernon <3686359+domvernon@users.noreply.github.com>
2018-02-18 16:25:34 +0000

    displays articles from api (no formatting)

--- src/NewsAppController.js
+++ src/NewsAppController.js
@@ -8,31 +8,43 @@ class NewsAppController {
     this.articleData.response.results.forEach((result) => {
       const headline = result.webTitle;
       const url = result.webUrl;
-      const summary = result.summary.text;
+      // const summary = result.summary.text;
+      const summary = 'summary here';
       const article = new Article(headline, url, summary);
       collection.add(article);
     });
     this.collection = collection;
   }
 
-  getArticles() {
-    // TO DO: implement makers api request
+  getArticles(callback) {
+    const self = this;
     // TO DO: this.getSummaries();
-    const response = {
-      results: [
-        {
-          webUrl: 'url',
-          webTitle: 'title',
-          summary: { text: 'some summary text' },
-        },
-
-        {
-          webUrl: 'anotherurl',
-          webTitle: 'anothertitle',
-          summary: { text: 'different summary text' },
-        },
-      ],
+    const makersApiURL = 'http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=';
+    const contentURL = 'http://content.guardianapis.com/uk-news';
+    const request = new XMLHttpRequest();
+    request.open('GET', makersApiURL + contentURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function load() {
+      self.recordData(request.response);
+      self.processData();
+      callback();
     };
-    this.recordData(response);
+
+    // serverResponse = {
+    //   results: [
+    //     {
+    //       webUrl: 'url',
+    //       webTitle: 'title',
+    //       summary: { text: 'some summary text' },
+    //     },
+    //
+    //     {
+    //       webUrl: 'anotherurl',
+    //       webTitle: 'anothertitle',
+    //       summary: { text: 'different summary text' },
+    //     },
+    //   ],
+    // };
   }
 }
--- src/app.js
+++ src/app.js
@@ -1,16 +1,12 @@
 (function app() {
   const controller = new NewsAppController();
 
-  controller.getArticles();
-
-  controller.processData();
-
-  controller.collection.articles.forEach((article) => {
-    outputHTML(article);
-  });
-
-  function outputHTML(article) {
-    const html = ArticleGenerator.generate(article);
-    document.getElementById('articles').innerHTML += html;
+  function outputHTML() {
+    controller.collection.articles.forEach((article) => {
+      const html = ArticleGenerator.generate(article);
+      document.getElementById('articles').innerHTML += html;
+    });
   }
+
+  controller.getArticles(outputHTML);
 }());
