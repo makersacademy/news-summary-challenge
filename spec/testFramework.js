2f33375f5512e49ad701b49b3fba65c1ba9180c0
Tagrand <thomas.a.grand@gmail.com>
2018-02-18 11:15:27 +0000

    add headline store spec

---
+++ spec/testFramework.js
@@ -0,0 +1,81 @@
+console.log('working testframework')
+
+function addSuccessDiv(message) {
+  newDiv = createDiv('success', message);
+  const currentDiv = document.getElementById('div1');
+  currentDiv.appendChild(newDiv);
+}
+
+function addErrorStack(err) {
+  messageDiv = createDiv('fail', `Failure: ${err}`);
+  stackDiv = createDiv('stack', err.stack);
+
+  failureDiv = createDiv('failure', '');
+  failureDiv.appendChild(messageDiv);
+  failureDiv.appendChild(stackDiv);
+
+  const currentDiv = document.getElementById('div1');
+  // console.log(currentDiv);
+  console.log(document.body);
+  currentDiv.appendChild(failureDiv);
+}
+
+function createDiv(id, text) {
+  const Div = document.createElement('div');
+  Div.id = id;
+  const message = document.createTextNode(text);
+  Div.appendChild(message);
+  return Div;
+}
+
+const it = function (message, tester) {
+  try {
+    tester();
+    addSuccessDiv(message);
+  } catch (err) {
+    addErrorStack(err);
+  }
+};
+
+
+const assert = {
+  isTrue: (value, message) => {
+    if (!value) {
+      throw new Error('Failure: It should be true but is false...');
+    }
+  },
+
+  equals: (actual, expected) => {
+    if (actual != expected) {
+      throw new Error(`Failure!!!! Expected: ${expected}, Actual: ${actual}`);
+    }
+  },
+
+  hasContent(id, string) {
+    const innerText = document.getElementById(id).innerHTML;
+    if (!innerText.includes(string)) {
+      throw new Error(`Expected: ${string}, but we got ${innerText}`);
+    }
+  },
+
+  doesntHaveContent: (id, string) => {
+    const innerText = document.getElementById(id).innerHTML;
+    if (innerText.includes(string)) {
+      throw new Error(`Expected: ${string} to not be present in ${innerText}`);
+    }
+  },
+
+};
+
+// feature tests
+function fillInForm(id, string) {
+  document.getElementById(id).value = string;
+}
+
+function clickButton(id) {
+  document.getElementById(id).click();
+}
+
+function clickLink(id) {
+  document.getElementById(id).click();
+}
