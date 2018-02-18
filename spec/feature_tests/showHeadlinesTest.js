74de11fadf2a0ca46c9bed97229dbfc551120f2a
Tagrand <thomas.a.grand@gmail.com>
2018-02-18 17:09:14 +0000

    test the page contains only 5 headlines

--- spec/feature_tests/showHeadlinesTest.js
+++ spec/feature_tests/showHeadlinesTest.js
@@ -1,4 +1,8 @@
-it('should show five pieces of news', function(){
-  var first_item = 'Ukip members oust Henry Bolton as leader after only five months'
-  assert.hasContent('newsFeed', first_item )
+it('should show headlines of the first five pieces of news', function(){
+  var firstItem = 'Ukip members oust Henry Bolton as leader after only five months'
+  var lastItem = 'Ukip leader Henry Bolton arrives at crucial party meeting';
+  var shouldNotHave = 'Revealed:  rightwing groups plot to ditch EU safety standards on food and drugs'
+  assert.hasContent('newsFeed', firstItem )
+  assert.hasContent('newsFeed', lastItem )
+  assert.doesntHaveContent('newsFeed', shouldNotHave)
 })
