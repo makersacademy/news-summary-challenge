e8c0698a2d998559eefc5723463012d9c7cd96b9
domvernon <3686359+domvernon@users.noreply.github.com>
2018-02-18 17:08:09 +0000

    makes it a bit nicer!

--- src/view/ArticleGenerator.js
+++ src/view/ArticleGenerator.js
@@ -1,6 +1,6 @@
 class ArticleGenerator {
   static generate(article) {
-    const head = '<div id="article">';
+    const head = '<div class="article">';
     const headline = `<div id="headline">${article.headline}</div>\n`;
     const url = `<div id="url">${article.url}</div>\n`;
     const summary = `<div id="summary">${article.summary}</div>`;
