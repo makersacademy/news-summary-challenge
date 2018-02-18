c1cc3c6f76d533b54d33eb68e8edc6f25b7ed661
Lewis Moore <lewis.moore2707@gmail.com>
2018-02-18 16:06:37 +0000

    Add function to add article to article list array

--- spec/articleListSpec.js
+++ spec/articleListSpec.js
@@ -1,8 +1,14 @@
 describe('Viewing articles in a list', function(){
 
   articlelist = new ArticleList()
+  article = new Article('headline', 'body', 'url')
 
   it('starts as an empty array', function(){
     assert.isEqual(articlelist.articles.length, 0)
+  })
+
+  it('create and add an article to an array', function(){
+    articlelist.addArticle(article)
+    assert.isEqual(articlelist.articles.length, 1)
   })
 })
--- src/articleList.js
+++ src/articleList.js
@@ -3,6 +3,10 @@
   function ArticleList(){
     this.articles = []
   }
-  
+
+  ArticleList.prototype.addArticle = function(article){
+    this.articles.push(article)
+  }
+
   exports.ArticleList = ArticleList;
 })(this)
