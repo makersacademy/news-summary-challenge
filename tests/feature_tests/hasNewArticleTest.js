b943a2d3cc1755c8e42a16af03e992edbe6032ff
Alexy <alexandra.fina@yahoo.com>
2018-02-18 12:24:26 +0000

    Add index.html, feature test for hasNewArticleTest

---
+++ tests/feature_tests/hasNewArticleTest.js
@@ -0,0 +1,8 @@
+function hasNewArticleTest(){
+
+  it('Has an article', function() {
+    assert.hasContent('articlelist', "Israel's Netanyahu could act against Iran's 'empire");
+    assert.doesntHaveContent('articlelist', "Prime Minister Benjamin Netanyahu said");
+  })
+
+}
