a23e5885ee7cde8fd28f7d91b918eda7a98c07ba
jenniferemshepherd <jennifer.elaine.mairead@gmail.com>
2018-02-18 19:53:21 +0000

    refactoring and removing files not needed

--- controller/controller.js
+++ controller/controller.js
@@ -2,8 +2,6 @@ function Controller(Headlines) {
   this.content = Headlines
 };
 
-// var SecretKey = "https://content.guardianapis.com/search?api-key=1adbcffe-98b5-4af3-a187-4886d9b3171b"
-
 Controller.prototype.insertHeadlines = function() {
-  document.getElementById('headlines').innerHTML = this.content.makeList();
+  document.getElementById('headlineList').innerHTML = this.content.makeHtmlList();
 };
