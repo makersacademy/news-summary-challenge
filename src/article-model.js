551a88a8ffb60bfc8bd4bbc6b206eae1a10781ac
Ed <ea.litten@gmail.com>
2018-02-18 23:59:23 +0000

    Minor display fixes

--- src/article-model.js
+++ src/article-model.js
@@ -9,10 +9,10 @@ Article.prototype = {
   getHTML: function(index) {
 
     var headline = `<div class="article-headline"><h3>${this.headline}</h3></div>`;
-    var url = `<div class="article-url">${this.url}</div>`;
+    var url = `<div class="article-url"><p><a href="${this.url}">Link to article</a></p></div>`;
     var date = `<div class="article-date"><p>${this.date.toDateString()}</p></div>`;
 
-    return `<div class="article" id="article${index}">` + url + headline + date + `</div>`
+    return `<div class="article" id="article${index}">` + headline + date + url + `</div>`
 
   }
 
