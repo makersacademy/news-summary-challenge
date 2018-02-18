e682665ad34326d105c7f9e80b4460125a8dfc05
Lewis Moore <lewis.moore2707@gmail.com>
2018-02-18 16:52:58 +0000

    Add incrementing to articleid class'

--- spec/articleIDSpec.js
+++ spec/articleIDSpec.js
@@ -2,7 +2,16 @@ describe('article IDs', function(){
 
   articleid = new ArticleID()
 
-  it('article ID initialises to 0', function(){
-    assert.isEqual(articleid, 0)
+  it('initialises to 0', function(){
+    assert.isEqual(articleid.id, 0)
+  })
+
+  it('return an article id', function(){
+    assert.isEqual(articleid.getID(), 0)
+  })
+
+  it('can be incremented', function(){
+    articleid.increment()
+    assert.isEqual(articleid.getID(), 1)
   })
 })
