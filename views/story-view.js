baf1aa26beff5dc6f409d7e789b1c366b00ef23e
rcvink <rrvink@gmail.com>
2018-02-19 01:21:32 +0000

    Add thumbnails to previews

--- views/story-view.js
+++ views/story-view.js
@@ -10,6 +10,7 @@
 
   StoryView.prototype.parse = function () {
     string = `<h1>${this._story.story().fields.headline}</h1>`;
+    string += `<img src="${this._story.story().fields.thumbnail}">`;
     string += `<h2>${this._story.story().fields.trailText}</h2>`;
     string += `<h3>${this._story.story().fields.byline}</h3>`;
     string += this._story.story().fields.body;
