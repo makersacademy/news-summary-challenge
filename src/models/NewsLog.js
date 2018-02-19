b3a2853ec2493c9fa4d93e014d938abc613f2aca
Leigh-ann <leighann.gant@gmail.com>
2018-02-19 08:12:46 +0000

    Summary removed from newslog

--- src/models/NewsLog.js
+++ src/models/NewsLog.js
@@ -10,7 +10,7 @@
 
   NewsLog.prototype._addArticles = function(data, self = this){
     data.response.results.forEach(function(result){
-      self.articles.push({ headline: result.webTitle, url: result.webUrl, summary: '' })
+      self.articles.push({ headline: result.webTitle, url: result.webUrl})
     })
   }
 
