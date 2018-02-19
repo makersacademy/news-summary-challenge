e2b5f06c9dcc65d598587c2fbb2218e32c5169cb
Tom Grand <thomas.a.grand@gmail.com>
2018-02-19 22:12:20 +0000

    add picture to article

--- spec/articlesTest.js
+++ spec/articlesTest.js
@@ -1,6 +1,6 @@
 function articlesTest() {
   mockArticle = {
-    id: 'politics/2018/feb/18/ukip-members-oust-henry-bolton-as-leader-after-five-months', type: 'article', sectionId: 'politics', sectionName: 'Politics', webPublicationDate: '2018-02-18T00:05:29Z', webTitle: 'Ukip members oust Henry Bolton as leader after only five months', webUrl: 'https://www.theguardian.com/politics/2018/feb/18/ukip-members-oust-henry-bolton-as-leader-after-five-months', apiUrl: 'https://content.guardianapis.com/politics/2018/feb/18/ukip-members-oust-henry-bolton-as-leader-after-five-months', isHosted: false, pillarId: 'pillar/news', pillarName: 'News',
+    id: 'politics/2018/feb/18/ukip-members-oust-henry-bolton-as-leader-after-five-months', type: 'article', sectionId: 'politics', sectionName: 'Politics', webPublicationDate: '2018-02-18T00:05:29Z', webTitle: 'Ukip members oust Henry Bolton as leader after only five months', webUrl: 'https://www.theguardian.com/politics/2018/feb/18/ukip-members-oust-henry-bolton-as-leader-after-five-months', apiUrl: 'https://content.guardianapis.com/politics/2018/feb/18/ukip-members-oust-henry-bolton-as-leader-after-five-months', "fields":{"thumbnail":'https://img.buzzfeed.com/buzzfeed-static/static/2015-04/20/9/enhanced/webdr08/original-31679-1429535251-12.jpg?downsize=715:*&output-format=auto&output-quality=auto'}, isHosted: false, pillarId: 'pillar/news', pillarName: 'News',
   };
 
 
@@ -13,6 +13,16 @@ function articlesTest() {
     const article = new Article(mockArticle);
     assert.equals(article.link, 'https://www.theguardian.com/politics/2018/feb/18/ukip-members-oust-henry-bolton-as-leader-after-five-months');
   });
+
+  it('should store a link', () => {
+    const article = new Article(mockArticle);
+    assert.equals(article.link, 'https://www.theguardian.com/politics/2018/feb/18/ukip-members-oust-henry-bolton-as-leader-after-five-months');
+  });
+
+  it('should store a picture', () => {
+    const article = new Article(mockArticle);
+    assert.equals(article.image, 'https://img.buzzfeed.com/buzzfeed-static/static/2015-04/20/9/enhanced/webdr08/original-31679-1429535251-12.jpg?downsize=715:*&output-format=auto&output-quality=auto' )
+  })
 
   it('should create a new article with class method', () => {
     const article = Article.createArticle(mockArticle);
