cdd72d8888a1fcde962122740ab25f924c80509c
jenniferemshepherd <jennifer.elaine.mairead@gmail.com>
2018-02-22 22:59:53 +0000

    adjust controller responsibilities

--- controller/controller.js
+++ controller/controller.js
@@ -1,12 +1,47 @@
-function Controller(Headlines, Summaries) {
+function Controller(Headlines) {
   this.headlines = Headlines
-  this.summaries = Summaries
 };
 
 Controller.prototype.insertHeadlines = function() {
-  document.getElementById('headlineList').innerHTML = this.headlines.makeHtmlList();
+  document.getElementById('headlineList').innerHTML = this.headlines.makeHtmlHeadlines();
 };
 
-Controller.prototype.insertSummaries = function() {
-  document.getElementById('summaryList').innerHTML = this.summaries.makeHtmlList();
+Controller.prototype.makeUrlChangeShowSummaryForCurrentHeadline = function() {
+  window.addEventListener("hashchange", function() {this.showSummaryForCurrentPage()}.bind(this));
+};
+
+Controller.prototype.getStoryID = function() {
+  return window.location.hash.split("#")[1]
+};
+
+Controller.prototype.findStoryById = function(id) {
+  var story = this.headlines.articles.find(function(article) {
+    if (`${id}` === article.id) { return article };
+  });
+  return story
+};
+
+Controller.prototype.showSummaryForCurrentPage = function() {
+  this.singleStory = new SingleStory(this.findStoryById(this.getStoryID()))
+  showThumbnail(this.singleStory.getStoryThumbnail());
+  showSummary(this.singleStory.getStorySummary());
+  showFullStory(this.singleStory.getFullStoryLink());
+};
+
+function showThumbnail(thumbnail) {
+  document
+    .getElementById("image")
+  .innerHTML = thumbnail;
+};
+
+function showSummary(summary) {
+  document
+    .getElementById("summary")
+    .innerHTML = summary;
+};
+
+function showFullStory(link) {
+  document
+    .getElementById("articleLink")
+    .innerHTML = link;
 };
