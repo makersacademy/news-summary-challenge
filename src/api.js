f9d9b6c5c781d3d94cc6c3ba1183b67b3bbf3207
kotpsot <zygmunt.justyna@gmail.com>
2018-03-09 12:55:50 +0000

    Refactor api getNews function

--- src/api.js
+++ src/api.js
@@ -1,8 +1,14 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://content.guardianapis.com/search?api-key=ed6927b1-f9a7-4f6d-b095-12ada4b6f100", false);
-xhr.send();
+getNews = function() {
 
-if(xhr.status == 200)
-console.log(xhr.response)
-var data = JSON.parse(xhr.responseText)
-console.log(data)
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://content.guardianapis.com/search?api-key=ed6927b1-f9a7-4f6d-b095-12ada4b6f100");
+  xhr.onload = function() {
+    if(xhr.status == 200)
+    console.log(xhr.response)
+    var data = JSON.parse(xhr.responseText)
+    console.log(data);
+  }
+
+  xhr.send();
+
+}()
