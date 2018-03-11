9f66a5d0f4a3f955b28f573d21c4a10423736f65
carlostrapet <trapet_llamas@hotmail.com>
2018-03-11 22:52:39 +0000

    added controller, cleaned up, acd

--- src/app.js
+++ src/app.js
@@ -2,3 +2,9 @@
 var newsBlock = new NewsBlock();
 newsBlock.getNews();
 
+newsBlock.allNews.forEach(function(element) {
+  document.write(`<li>
+  <a href="${element.url}">${element.title}</a><br>
+  <img src="${element.imgUrl}">
+  </li>`)
+})
