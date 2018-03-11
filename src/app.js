496eaff5692e673fa0d1282b6a96292387cb8cf7
carlostrapet <trapet_llamas@hotmail.com>
2018-03-11 17:29:15 +0000

    cleaned up controller

--- src/app.js
+++ src/app.js
@@ -1,17 +1,4 @@
-function NewsBlock() {
-  this.allNews = [];
-}
 
 var newsBlock = new NewsBlock();
+newsBlock.getNews();
 
-getNews = function() {
-  var allArticles = new ArticleRequest();
-  allArticles.getArticles();
-  allArticles.content.forEach(function(element) {
-    var title = element.webTitle;
-    var url = element.webUrl;
-    var imgUrl = element.fields.thumbnail;
-    newsBlock.allNews.push( new News(title, url, imgUrl));
-  });
-  console.log(newsBlock.allNews)
-}
--- src/news.js
+++ src/news.js
@@ -1,5 +1,6 @@
 'use strict'
 
+// Individual news story
 function News(title, url, imgUrl, summary) {
   this.title = title
   this.url = url
@@ -7,10 +8,18 @@ function News(title, url, imgUrl, summary) {
   this.summary = summary
 }
 
-News.prototype = {
+// Compilation of all News to render into view from app.js
+function NewsBlock() {
+  this.allNews = [];
+}
 
-  getNews: function() {
-
-  },
-
+NewsBlock.prototype.getNews = function (self = this) {
+  var allArticles = new ArticleRequest();
+  allArticles.getArticles();
+  allArticles.content.forEach(function (element) {
+    var title = element.webTitle;
+    var url = element.webUrl;
+    var imgUrl = element.fields.thumbnail;
+    self.allNews.push(new News(title, url, imgUrl));
+  });
 }
