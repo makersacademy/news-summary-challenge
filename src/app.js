6abe335850a684062a7e45b759c26f9abb38ed8b
Edward <ea.litten@gmail.com>
2018-04-03 18:40:11 +0100

    Fetching articles now uses a promise

--- src/app.js
+++ src/app.js
@@ -6,11 +6,18 @@ refreshArticleList()
 
 
 function refreshArticleList() {
-  articleList.getArticlesFromAPI(displayArticleList);
-}
-
-function displayArticleList() {
-  document.getElementById('article-list').innerHTML = articleList.outputArticles()
+  articleList.getArticlesFromAPI().then(function (response) {
+    console.log("success");
+    response.response.results.forEach(element => {
+      var url = element.webUrl;
+      var headline = element.webTitle;
+      var date = element.webPublicationDate;
+      articleList.addArticle(new Article(url, headline, date))
+    });
+    document.getElementById('article-list').innerHTML = articleList.outputArticles();
+  }, function (error) {
+    console.log("failure");
+  })
 }
 
 
--- src/articleList-model.js
+++ src/articleList-model.js
@@ -8,40 +8,25 @@ ArticleList.prototype = {
     this.articles.push(article)
   },
 
-  getArticlesFromAPI: function (callback, source = 'https://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/technology') {
-    var self = this
-    var request = new XMLHttpRequest()
-    request.onload = function() {
-        this.response.response.results.forEach(element => {
-          var url = element.webUrl;
-          var headline = element.webTitle;
-          var date = element.webPublicationDate;
-          self.addArticle(new Article(url, headline, date))
-        });
-        callback();
-    }
-    request.open('GET', source)
-    request.responseType = 'json';
-    request.send()
+  getArticlesFromAPI: function (source = 'https://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/technology') {
+    return new Promise(function (resolve, reject) {
+      var request = new XMLHttpRequest()
+      request.open('GET', source)
+      request.responseType = 'json';
+      request.onload = () => {
+        if (request.status == 200) {
+          resolve(request.response);
+        }
+        else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.send()
+    });
   },
 
-  // getArticleSummary: function(articleURL) {
-//   // Alyien API almost out of requests, using saved summary for all articles
-//   // var source = 'http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=' + articleURL
-//   var source = 'saved_summary.json'
-
-//   return new Promise((resolve, reject) => {
-//     var summaryRequest = new XMLHttpRequest()
-//     summaryRequest.open('GET', source)
-//     summaryRequest.onload = () => resolve(summaryRequest.response.sentences.join());
-//     summaryRequest.onerror = () => reject(summaryRequest.statusText);
-//     summaryRequest.send()
-//   });
-// },
-
-
   outputArticles: function () {
-    return `<ul>` + this.articles.reduce((HTMLstring, article, index) => HTMLstring + `<li>` +  article.getHTML(index) + `</li>`, "") + `</ul>`
+    return `<ul>` + this.articles.reduce((HTMLstring, article, index) => HTMLstring + `<li>` + article.getHTML(index) + `</li>`, "") + `</ul>`
   }
 
 }
