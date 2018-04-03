d5dd64fceefbd92fff770ca8afa0f5b8c9baafb0
Edward <ea.litten@gmail.com>
2018-04-03 18:49:26 +0100

    Test using async/await

--- src/app.js
+++ src/app.js
@@ -2,8 +2,7 @@
 
 articleList = new ArticleList;
 
-refreshArticleList()
-
+refreshArticleListAS()
 
 function refreshArticleList() {
   articleList.getArticlesFromAPI().then(function (response) {
@@ -20,4 +19,14 @@ function refreshArticleList() {
   })
 }
 
-
+async function refreshArticleListAS() {
+  response = await articleList.getArticlesFromAPI();
+  console.log("success");
+  response.response.results.forEach(element => {
+    var url = element.webUrl;
+    var headline = element.webTitle;
+    var date = element.webPublicationDate;
+    articleList.addArticle(new Article(url, headline, date))
+  });
+  document.getElementById('article-list').innerHTML = articleList.outputArticles();
+}
