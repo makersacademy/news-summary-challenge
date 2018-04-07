2984e315b9562871408581b0e33c23c690575464
Hemesh-Unka <hemeshunka@gmail.com>
2018-04-07 09:50:07 +0100

    Completed test framework & initial file structure

---
+++ spec/assert.js
@@ -0,0 +1,46 @@
+(function(exports) {
+  var PASS_INDICATOR = '.';
+  var FAIL_INDICATOR = 'F';
+
+  function expect(thing) {
+    return {
+      toEqual: function(value) {
+        return _checkAssert(value === thing);
+      },
+      toNotEqual: function(value) {
+        return _checkAssert(value !== thing);
+      },
+      isTypeString: function(string) {
+        return _checkAssert(typeof string === 'string');
+      },
+      isTypeArray: function(array) {
+        return _checkAssert(array instanceof Array);
+      },
+      isSameLength: function(array) {
+        return _checkAssert(array.length === thing.length);
+      }
+    };
+  };
+
+  function _checkAssert(argument) {
+    return (argument) ? PASS_INDICATOR : FAIL_INDICATOR;
+  }
+
+  function describe(string, callback) {
+    console.log(string);
+
+    callback();
+  }
+
+  function it(string, callback) {
+    if(callback() === PASS_INDICATOR) {
+      console.log(`%c ${string}\n ${callback()}`, 'color: green');
+        } else {
+      console.log(`%c ${string}\n ${callback()}`, 'color: red');
+    }
+  }
+
+  exports.expect = expect;
+  exports.describe = describe;
+  exports.it = it;
+})(this);
