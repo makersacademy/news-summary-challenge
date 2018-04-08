3b84316e310509078f265cc5bede9c5e50b060a3
Reenakoudi <kreena92@gmail.com>
2018-04-08 23:06:20 +0100

    Added article model

---
+++ models/article.js
@@ -0,0 +1,19 @@
+function Article(url, headline, summary) {
+  this.url = url;
+  this.headline = headline;
+  this.summary = summary;
+}
+
+Article.prototype = {
+
+  getHTML: function(index) {
+
+    var headline = `<div class="article-headline">${article.headline}</div>`;
+    var url = `<div class="article-url">${article.url}</div>`;
+    var summary = `<div class="article-summary">${article.summary}</div>`;
+
+    return `<div class="article" id="article${index}">` + url + headline + summary + `</div>`
+
+  }
+
+}
