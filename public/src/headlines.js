f8627dbc195804393221866fd3bba0a1dc7e07b4
Christopher Ly <demedial@gmail.com>
2018-04-09 08:39:44 +0100

    Headlines store test red -> green

--- public/src/headlines.js
+++ public/src/headlines.js
@@ -2,4 +2,12 @@ class Headlines {
   constructor() {
     this.headlinesList = [];
   }
+
+  add(headlines) {
+    this.headlinesList.push(headlines);
+  }
+
+  all() {
+    return this.headlinesList;
+  }
 }
