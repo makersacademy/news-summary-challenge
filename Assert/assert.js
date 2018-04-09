825b2b21c306bbb15b52090c4a52c8d7ba73ff0f
Gia1987 <giacomo.n27@gmail.com>
2018-04-09 10:33:43 +0100

    Add assert file

--- Assert/assert.js
+++ Assert/assert.js
@@ -2,6 +2,8 @@ var assert = {
   isTrue: function(description, assertionToCheck) {
     if (!assertionToCheck) {
       throw new Error("Assertion failed: " + assertionToCheck + " is not truthy");
+    }else{
+    console.log('test passed: ' + description);
     }
   };
 };
