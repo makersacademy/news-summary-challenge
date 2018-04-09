b81fb6fcf468e4aac07db1c4c583deb087a7df58
Christopher Ly <demedial@gmail.com>
2018-04-09 08:09:53 +0100

    Rename test files to *Spec, created controller and headlines test files

--- public/tests/featureSpec.js
+++ public/tests/featureSpec.js
@@ -2,4 +2,8 @@ describe('Feature tests', function () {
     it('has a listing_div element', function() {
         return expect('news_listing_div').toBeHtmlElement();
     });
+
+    it('has selected_news_div invisible at start', function() {
+        return expect('selected_news_div').toNotBeVisible();
+    });
 });
