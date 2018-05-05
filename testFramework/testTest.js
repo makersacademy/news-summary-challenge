c597a25514ed94631c756983f23bc163e676accf
Jihin Ip <jim_ip20@hotmail.com>
2018-05-05 13:17:13 +0100

    Refactoring and reallocating files

---
+++ testFramework/testTest.js
@@ -0,0 +1,14 @@
+// console.log("testTest.js loaded.");
+//
+// expect(5).toEqual(5);
+//
+// it("test1 string", function(){
+//   expect("miaow").toEqual("poo");
+// });
+//
+// describe("test2 string", function(){
+//   it("test2 string", function(){
+//     var string = "moo"
+//     expect(typeof(string)).toEqual("string");
+//   });
+// });
