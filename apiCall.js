b12a411174d60227418609adb95708a6292cda47
Louis Moselhi <louismoselhi@macbook-pro-5.mynet>
2018-05-06 15:19:36 +0100

    article titles show as a list, without bullets

--- apiCall.js
+++ apiCall.js
@@ -1,4 +1,4 @@
-let url = 'https://content.guardianapis.com/search?api-key=41dfe7aa-421b-4360-b1bd-5379fe29e9d3';
+let url = 'https://content.guardianapis.com/search?show-fields=body,headline&api-key=41dfe7aa-421b-4360-b1bd-5379fe29e9d3';
 fetch(url).then(res => {
    if (res.status !== 200) {
        console.log('Theres a problem, status code: ' + res.status);
@@ -7,9 +7,19 @@ fetch(url).then(res => {
 
    res.json().then(function(data) {
        console.log(data);
-       console.log(data.response.results[0].id);
        var body = document.getElementById('body');
-       body.innerHTML = data.response.results[0].id; 
+       var headline = document.getElementById('headline');
+       var articleArray = data.response.results;
 
+      for (var i = 0; i < articleArray.length; i++) {
+        var currentArticle = articleArray[i];
+        var listContainer = document.getElementById('articleList');
+        var listContainerItem = document.createElement("li");
+        listContainer.appendChild(listContainerItem);
+        listContainerItem.innerHTML = currentArticle.webTitle;
+      }
+
+
+       headline.innerHTML = data.response.results[0].webTitle;
    });
 });
---
+++ articleModal.js
@@ -0,0 +1,10 @@
+var modal = document.getElementById('myModal');
+var closeButton = document.getElementsByClassName("close")[0];
+closeButton.onclick = function() {
+    modal.style.display = "none";
+};
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
