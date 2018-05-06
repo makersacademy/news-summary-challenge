0c5243f91ea8c021a353c2678565658e6813ce62
Gabriel Hochstatter <gabrielhochstatter@gmail.com>
2018-05-06 15:18:53 +0100

    sanity checks for testing, added index.html

---
+++ spec/headlineListerSpec.js
@@ -0,0 +1,7 @@
+describe("testing example output", function() {
+
+  it("should return OK -- Sanity Check", function(){
+    expect(exampleOutputTester(exampleOutput)).toEqual("ok");
+  });
+
+});
---
+++ src/headlineLister.js
@@ -0,0 +1,3 @@
+function exampleOutputTester(json) {
+  return json.response.status;
+}
