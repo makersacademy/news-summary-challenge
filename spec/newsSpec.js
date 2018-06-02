57cc33e98aba4967a8905e2aff8d71b1654cc734
Charly Mannion <charlottemannion100@gmail.com>
2018-06-02 11:37:57 +0100

    all test not working as expected

--- spec/newsSpec.js
+++ spec/newsSpec.js
@@ -1,10 +1,18 @@
 describe("Headlines", function () {
   var headlines = new Headlines();
-  it("is an empty array", function () {
+  it("is an empty", function () {
     assert.isEmpty(headlines.headlinesArray)
+  });
+  it("is an empty array", function () {
+    assert.isAnArray(headlines.headlinesArray)
   });
   it("contains a headline when one is added", function () {
     headlines.addHeadline("Test Headline: Bacon is Good For You!")
     assert.arrayContains(headlines.headlinesArray, "Test Headline: Bacon is Good For You!")
   });
+  // it("returns all the headlines", function () {
+  //   headlines.addHeadline("test Headline 1: Some Bacon Here!")
+  //   console.log(headlines.allHeadlines())
+  //   assert.isEqual(headlines.allHeadlines(), ["Test Headline: Bacon is Good For You!","test Headline 1: Some Bacon Here!"])
+  // });
 })
