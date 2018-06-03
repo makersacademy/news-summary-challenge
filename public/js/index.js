b0325ad046a994d494eede5b0449fa68c08489c2
Igor Ryabchuk <riabtchuk@gmail.com>
2018-06-03 22:05:06 +0100

    summary displays as a new page

--- public/js/index.js
+++ public/js/index.js
@@ -50,33 +50,58 @@ window.onload = function () {
     }
 
     function processAylienJson(responseContents) {
+      document.getElementById('page_title').innerHTML = 'Article Summary'
+      document.getElementById('news_headlines').hidden = true
+      document.getElementById('news_item_summary').hidden = false
+      document.getElementById('news_item_summary').innerText = ''
+      let backButton = document.createElement('button')
+      backButton.setAttribute('id', 'back_button')
+      backButton.innerHTML = 'Back to Headlines'
       for (let sentence of responseContents.sentences) {
-        console.log(sentence)
+        let paragraph = document.createElement('p')
+        let textNode = document.createTextNode(sentence)
+        paragraph.appendChild(textNode)
+        document.getElementById('news_item_summary').appendChild(paragraph)
       }
+      document.getElementById('news_item_summary').appendChild(backButton)
+      document.getElementById('back_button').addEventListener('click', function() {
+        makeRequest(newsUrl)
+      })
     }
 
     function processGuardianJson (responseContents) {
+      document.getElementById('news_headlines').hidden = false
+      document.getElementById('news_item_summary').hidden = true
+      document.getElementById('page_title').innerHTML = 'Headlines'
       for (let newsHeadline of responseContents.response.results) {
         let newsItemThumbnail = new Image(250, 150)
+        try {
+          newsItemThumbnail.src = newsHeadline.fields.thumbnail
+        } catch {
+          newsItemThumbnail.src = ''
+          newsItemThumbnail.alt = 'No image provided'
+        }
         let section = document.createElement('section')
         let paragraphLinks = document.createElement('p')
         let paragraphNews = document.createElement('p')
         let linkButton = document.createElement('a')
+        let horizontalLine = document.createElement('hr')
+        linkButton.setAttribute('class', 'paragraph_links')
         linkButton.setAttribute('href', `${newsHeadline.webUrl}`)
-        linkButton.innerHTML = "See Full Article"
+        linkButton.innerHTML = "Full Article"
         let summaryButton = document.createElement('a')
         summaryButton.setAttribute('href', `#${newsHeadline.webUrl}`)
-        summaryButton.setAttribute('class', 'summary_link')
-        summaryButton.innerHTML = "See Summary"
-        let newsItemTitle = document.createTextNode(newsHeadline.webTitle)
-        newsItemThumbnail.src = newsHeadline.fields.thumbnail
+        summaryButton.setAttribute('class', 'paragraph_links')
+        summaryButton.innerHTML = "Summary"
+        var newsItemTitle = document.createTextNode(newsHeadline.webTitle)
         section.appendChild(newsItemThumbnail)
         paragraphLinks.appendChild(linkButton)
         paragraphLinks.appendChild(summaryButton)
         paragraphNews.appendChild(newsItemTitle)
         section.appendChild(paragraphLinks)
         section.appendChild(paragraphNews)
-        document.getElementById("news_summary").appendChild(section)
+        section.appendChild(horizontalLine)
+        document.getElementById("news_headlines").appendChild(section)
       }
     }
     makeRequest(newsUrl)
