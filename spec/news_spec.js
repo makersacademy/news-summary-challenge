96b965c29377ce19fe3b02e6a6c2912d9602851e
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-06-03 11:38:09 +0100

    Initial testing for News object

--- spec/news_spec.js
+++ spec/news_spec.js
@@ -1,4 +1,21 @@
-var news = new News()
+var news = new News('example title', 'example url', 'example summary', 'example image')
+
 describe('News')
   it('Exists upon instantiation')
   assert.exists(news)
+
+describe('#title')
+  it('Returns the title of the news')
+  assert.areEqual(news.title, 'example title')
+
+describe('#url')
+  it('Returns the url of the news')
+  assert.areEqual(news.url, 'example url')
+
+describe('#summary')
+  it('Returns the summary of the news')
+  assert.areEqual(news.summary, 'example summary')
+
+describe('#image')
+  it('Returns the image of the news')
+  assert.areEqual(news.image, 'example image')
--- src/news.js
+++ src/news.js
@@ -1,7 +1,10 @@
 (function(exports) {
-  function News() {
-
-    
+  function News(title, url, summary, image) {
+    this.title = title
+    this.url = url
+    this.summary = summary
+    this.image = image
   }
+
   exports.News = News
 })(this);
