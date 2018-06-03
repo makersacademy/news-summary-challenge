5024e7951fc0d3a1e321fc5b24b08ff0d2ba83bf
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-06-03 13:52:49 +0100

    Create SingleNewsView files and spec files

--- spec/news_list_view_spec.js
+++ spec/news_list_view_spec.js
@@ -1,5 +1,27 @@
-var newsListView = new NewsListView
+function NewsDouble() {
+  this.title = 'title'
+  this.url = 'url'
+  this.summary = 'summary'
+  this.image = 'image'
+}
+
+function NewsListDouble() {
+  this.list = [new NewsDouble]
+}
+
+var newsList = new NewsListDouble();
+var newsListView = new NewsListView(newsList);
 
 describe('NewsListView')
   it('Exists upon instantiation')
   assert.exists(newsListView)
+
+describe('#newslist')
+  it('Returns the newslist object')
+  assert.areEqual(newsListView.newslist, newsList)
+
+//RETURN TO THIS AFTER CREATING SINGLE NEWS VIEW
+
+// describe('#NewsListToHTML')
+//   it('Returns an html string of all the news objects in news list')
+//   assert.areEqual(newsListView.NewslistToHTML(), `<p><a href='url'>title</a></p><img src=image`)
--- views/news_list_view.js
+++ views/news_list_view.js
@@ -1,6 +1,10 @@
 (function(exports) {
-  function NewsListView() {
-    
+  function NewsListView(newslist) {
+    this.newslist = newslist
+  }
+
+  NewsListView.prototype.NewsListToHTML = function() {
+    //COME BACK TO THIS AFTER SINGLE NEWS VIEW
   }
 
   exports.NewsListView = NewsListView
