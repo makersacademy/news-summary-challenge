435416a9daf8ce311a0be28ea62311c78adf7402
Igor Ryabchuk <riabtchuk@gmail.com>
2018-06-03 20:06:13 +0100

    working on text summary

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,7 @@
 window.onload = function () {
   (function () {
     var httpRequest
-    var newsItems
+    var responseReceivedFromServer
     var currentUrl
     //var newsUrl = 'http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/uk?show-editors-picks=true&order-by=newest&show-fields=all'
     var newsUrl = 'http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?show-fields=thumbnail'
@@ -23,9 +23,12 @@ window.onload = function () {
     function httpResponse () {
       if (httpRequest.readyState === XMLHttpRequest.DONE) {
         if (httpRequest.status === 200) {
+          responseReceivedFromServer = JSON.parse(httpRequest.responseText)
+          console.log(responseReceivedFromServer)
           if (currentUrl === newsUrl) {
-            newsItems = JSON.parse(httpRequest.responseText)
-            processGuardianJson(newsItems)
+            processGuardianJson(responseReceivedFromServer)
+          } else {
+            console.log(responseReceivedFromServer)
           }
         } else {
           alert('There was a problem with the request.')
@@ -33,20 +36,47 @@ window.onload = function () {
       }
     }
 
+    function summarise () {
+      var url = getUrlFromHash()
+      console.log(url)
+      // currentUrl = 'http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=' + url
+      // makeRequest(currentUrl)
+    }
+
+    function makeUrlChangeShowAnimalForCurrentPage() {
+      window.addEventListener("hashchange", console.log("hash changed"), false)
+    }
+
+    function getUrlFromHash() {
+      return window.location.hash.split("#")[1]
+    }
+
     function processGuardianJson (responseContents) {
-      console.log(responseContents)
       for (let newsHeadline of responseContents.response.results) {
         let newsItemThumbnail = new Image(250, 150)
-        newsItemThumbnail.src = newsHeadline.fields.thumbnail
-        let newsItemTitle = document.createTextNode(newsHeadline.webTitle)
         let section = document.createElement('section')
-        let paragraph = document.createElement('p')
+        let paragraphLinks = document.createElement('p')
+        let paragraphNews = document.createElement('p')
+        let linkButton = document.createElement('a')
+        linkButton.setAttribute('href', `${newsHeadline.webUrl}`)
+        linkButton.innerHTML = "See Full Article"
+        let summaryButton = document.createElement('a')
+        summaryButton.setAttribute('href', `#${newsHeadline.webUrl}`)
+        summaryButton.setAttribute('class', 'summary_link')
+        summaryButton.innerHTML = "See Summary"
+        let newsItemTitle = document.createTextNode(newsHeadline.webTitle)
+        newsItemThumbnail.src = newsHeadline.fields.thumbnail
         section.appendChild(newsItemThumbnail)
-        paragraph.appendChild(newsItemTitle)
-        section.appendChild(paragraph)
+        paragraphLinks.appendChild(linkButton)
+        paragraphLinks.appendChild(summaryButton)
+        paragraphNews.appendChild(newsItemTitle)
+        section.appendChild(paragraphLinks)
+        section.appendChild(paragraphNews)
         document.getElementById("news_summary").appendChild(section)
       }
     }
     makeRequest(newsUrl)
+    makeUrlChangeShowAnimalForCurrentPage()
   })()
+
 }
