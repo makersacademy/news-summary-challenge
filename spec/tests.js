7147710fda60610441e7100153a9332e29e6165e
anguspettifer <angus.pettifer@gmail.com>
2018-06-30 11:27:29 +0100

    Allow controller to edit HTML - feature tested

--- spec/tests.js
+++ spec/tests.js
@@ -1,9 +1,9 @@
 function testNewsControllerHTML() {
   var element = {innerHTML: "hello"}
   var newsController = new NewsController(element);
-  newsController.APIreturnHTML =  function() {
+  newsController.headline =  function() {
     return "<ul><li><div>News healine 1</div></li></ul>"
-  }
+  }()
   newsController.inputHTMLtoDOM()
   assert.isTrue("News Controller HTML", element.innerHTML === "<ul><li><div>News healine 1</div></li></ul>")
 };
