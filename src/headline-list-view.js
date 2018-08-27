03d145fd7e4f2da28dfb2d9e6787dd7e7ca0e4fb
Angela Wolff <warriorwomenblog@gmail.com>
2018-08-27 15:03:30 +0100

    Further MVC improvements. Headline list now rendered from view. Dummy api

--- src/headline-list-view.js
+++ src/headline-list-view.js
@@ -1,18 +1,12 @@
 (function (exports) {
 
+  // newsController injection as views should not have site of models
   function HeadlineListView(newsController) {
     this.newsController = newsController;
-  };
-
-  HeadlineListView.prototype.returnHeadlineHTML = function() {
-    for (var i = 0; i < this.newsController.headlineList.headlineListArray.length; i++) {
-      var headline = this.newsController.headlineList.headlineListArray[i].headline.webTitle;
-      var storyLink = this.newsController.headlineList.headlineListArray[i].headline.webUrl;
-      var image = this.newsController.headlineList.headlineListArray[i].headline.fields.thumbnaill;
-      var html = `<ul><li><div><a href='#${storyLink}'>${headline}<div><img src='${image}'></div></a></div></li><br></ul>`;
-    }
-    return html;
-  };
+    this.newsHeadline = document.getElementById('news');
+    this.newsHeadline.innerHTML = newsController.returnHeadlineHTML();
+    //this.newsHeadline.addEventListener('click', newsController);
+  }
 
   exports.HeadlineListView = HeadlineListView;
 
--- src/news-controller.js
+++ src/news-controller.js
@@ -1,12 +1,25 @@
 (function(exports) {
   function NewsController(headlineList) {
     this.headlineList = headlineList;
+    this.html = '<ul>'
   };
 
-  // the text to display will ultimately be injected from the headline-list-view.js
+  // this is now superceded by returnHeadlineHTML but is kept for testing purposes
    NewsController.prototype.getHeadlineHTML = function(text) {
     var element = document.getElementById('news')
     return element.innerHTML = `<p>${text}</p>`;
   };
-   exports.NewsController = NewsController;
+
+  NewsController.prototype.returnHeadlineHTML = function() {
+    for (var i = 0; i < this.headlineList.headlineListArray.length; i++) {
+      var headline = this.headlineList.headlineListArray[i].headline.webTitle;
+      var storyLink = this.headlineList.headlineListArray[i].headline.webUrl;
+      var image = this.headlineList.headlineListArray[i].headline.fields.thumbnaill;
+      this.html = this.html + `<li><div><a href=${storyLink}>${headline}<div><img src=${image}></div></a></div></li><br>`;
+    }
+    this.html = this.html + '</ul>';
+    return this.html;
+  };
+
+  exports.NewsController = NewsController;
 })(this);
