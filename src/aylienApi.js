c06aad707145e887e1e3188fe0d996ea550e3587
Andres Faraone <andresfaraone@outlook.com>
2018-10-20 19:35:02 +0100

    setup Pretzl testing framework

---
+++ src/aylienApi.js
@@ -0,0 +1,1 @@
+const key = 'bd22049cbba543c5518619c36aab7739'
---
+++ tests/guardianApiSpec.js
@@ -0,0 +1,7 @@
+const GuardianApi = require('../lib/guardianApi').default;
+
+const pretzl = require('pretzl');
+
+let a = new GuardianApi("hello")
+console.log(a)
+console.log(pretzl.Bagel)
