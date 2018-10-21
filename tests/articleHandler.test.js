6ea1df58081feb1608b37274aeb46243c94f6546
Andres Faraone <andresfaraone@outlook.com>
2018-10-21 20:07:01 +0100

    one async test failing

---
+++ tests/articleHandler.test.js
@@ -0,0 +1,31 @@
+const ArticleHandler = require('../lib/articleHandler').default;
+
+const pretzl = require('pretzl');
+
+pretzl.batch("ArticleHandler", () => {
+  let mockGuardian = new pretzl.Bagel("mockG", [["getApi"]])
+  let mockAylien = new pretzl.Bagel("mockA", [["getSummary"]])
+  let subject = new ArticleHandler(mockGuardian, mockAylien)
+  pretzl.batch("initalisation", () => {
+    pretzl.bag("mockguardian injected", () => {
+      pretzl.bite(subject.gh).toTasteLike(mockGuardian)
+    })
+
+    pretzl.bag("mockguardian injected", () => {
+      pretzl.bite(subject.ah).toTasteLike(mockAylien)
+    })
+  })
+
+  pretzl.batch("getEverything", () => {
+    pretzl.bag("getApi called on Guardian", () => {
+      subject.getEverything()
+      pretzl.bite(mockGuardian, "getApi").toHaveBeenEaten()
+    })
+
+    pretzl.bag("getApi called on Guardian", () => {
+      mockGuardian.stub("getApi", [1,2,3,4])
+      subject.getEverything()
+      pretzl.bite(mockAylien, "getSummary").toHaveBeenEaten()
+    })
+  })
+})
