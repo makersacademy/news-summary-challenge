a00357e46331073961d50b0ac2ab72d8818f81a0
shweta patil <shwetapatil@shwetas-mbp.home>
2018-10-21 13:31:18 +0100

    basic structure for api call

---
+++ src/models/newspaper.js
@@ -0,0 +1,20 @@
+(function(exports) {
+
+  function NewsPaper() {
+      this.a = SHWETA;
+
+  };
+
+  NewsPaper.prototype = {
+    details: function() {
+      
+    },
+
+    article: function() {
+      
+    }
+  };
+  
+  exports.NewsPaper = NewsPaper;
+
+})(this); 
