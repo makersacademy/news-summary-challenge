c0d454f5684d879c2c53874b6d2ab7c5fcb18cbc
Sebastian <sebbecker@gmx.net>
2018-10-21 17:57:47 +0100

    fix character problem with headlines

--- src/render.js
+++ src/render.js
@@ -1,11 +1,11 @@
 (function(exports){
-  var headlines_div = document.getElementById('headlines')
+  var content_div = document.getElementById('content')
 
   function render(formattedArticles) {
     formattedArticles.forEach(function(formattedArticle) {
-      headlines_div.innerHTML += '<a style="display: block" href=' + formattedArticle.link + '>'+ formattedArticle.headline +'</div>'
-      headlines_div.innerHTML += '<img style="display: block" src="' + formattedArticle.thumbnail + '"></img>'
-      headlines_div.innerHTML += '<div style="display: block">' + formattedArticle.summary + '</div>'
+      content_div.innerHTML += '</div class="headline"><a href=' + formattedArticle.link + '>'+ formattedArticle.headline +'</a></div>'
+      content_div.innerHTML += '<img class="thumbnail" src="' + formattedArticle.thumbnail + '"></img>'
+      content_div.innerHTML += '<div class="summary">' + formattedArticle.summary + '</div>'
     })
   }
 
