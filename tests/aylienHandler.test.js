0e4175132c9e3139fa749c57bef70dd4b5f87a42
Andres Faraone <andresfaraone@outlook.com>
2018-10-21 20:07:31 +0100

    empty aylienhandler test

--- tests/aylienHandler.test.js
+++ tests/aylienHandler.test.js
@@ -3,16 +3,5 @@ const AylienHandler = require('../lib/aylienHandler').default;
 const pretzl = require('pretzl');
 
 pretzl.batch("AylienHandler", () => {
-  let apiKey = "apikey"
-  let appId = "appId"
-  let subject = new AylienHandler(apiKey, appKey)
-  pretzl.batch("initialisation", () => {
-    pretzl.bag("apikey set to first arg", () => {
-      pretzl.bite(subject.apiKey).toTasteLike(apiKey)
-    })
-
-    pretzl.bag("appId set to first arg", () => {
-      pretzl.bite(subject.appIdy).toTasteLike(appId)
-    })
-  })
+  let subject = new AylienHandler
 })
