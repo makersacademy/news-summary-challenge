645584b9e4887c11bd46da64f401900a48b14003
Sebastian <sebbecker@gmx.net>
2018-10-21 19:00:35 +0100

    style btns

--- src/components/renderHeadlines.js
+++ src/components/renderHeadlines.js
@@ -5,7 +5,7 @@
     formattedArticles.forEach(function(formattedArticle,i) {
       headlines_div.innerHTML += '<div class="headline"><a target="_blank" href=' + formattedArticle.link + '>'+ formattedArticle.headline +'</a></div>'
       headlines_div.innerHTML += '<img class="thumbnail" src="' + formattedArticle.thumbnail + '"></img>'
-      headlines_div.innerHTML += '<button onclick="clickSummaryBtn('+ i +')">Summary</button>'
+      headlines_div.innerHTML += '<button class="btn" onclick="clickSummaryBtn('+ i +')">Summary</button>'
     })
   }
 
--- src/components/renderSummary.js
+++ src/components/renderSummary.js
@@ -5,7 +5,7 @@
       summary_div.innerHTML += '<div class="headline"><a target="_blank" href=' + article.link + '>'+ article.headline +'</a></div>'
       summary_div.innerHTML += '<img class="thumbnail" src="' + article.thumbnail + '"></img>'
       summary_div.innerHTML += '<div class="summary">' + summary + '</div>'
-      summary_div.innerHTML += '<button onclick="goBack()"> Go Back </button>'
+      summary_div.innerHTML += '<button class="btn" onclick="goBack()"> Go Back </button>'
     }
 
   exports.renderSummary = renderSummary
