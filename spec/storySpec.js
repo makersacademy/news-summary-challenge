09799e3d13afbb5c44ef6c4a3353cd343486d27f
Caitlin Cooling <ccooling97@gmail.com>
2018-11-15 09:45:22 +0000

    Test: get headline limits the length of headline 50 characters max

--- spec/storySpec.js
+++ spec/storySpec.js
@@ -3,4 +3,11 @@ describe("Story", function(){
     var story = new Story("News story");
     expect(story.headline).toEqual("News story");
   });
+
+  describe("getHeadline", function(){
+    it("limits the headline to 50 characters", function(){
+      var story = new Story("Today's news story is very exciting and very very long");
+      expect(story.getHeadline()).toEqual("Today's news story is very exciting and very very ...")
+    })
+  })
 });
