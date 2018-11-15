38b8b29ccb7e9254e631b0b7d7eed91fbbce5a27
Caitlin Cooling <ccooling97@gmail.com>
2018-11-15 10:48:56 +0000

    Fix bug in test

--- spec/storyListViewSpec.js
+++ spec/storyListViewSpec.js
@@ -2,7 +2,7 @@ describe("storyListView", function() {
   describe("returnList", function() {
     it("returns the stories with HTML", function() {
       var storyListView = StroyListView(["News story"]);
-      expect(storyListView).toEqual("<ul><li><div>News story</div></li></ul>")
+      expect(storyListView.returnList()).toEqual("<ul><li><div>News story</div></li></ul>")
     });
   });
 });
