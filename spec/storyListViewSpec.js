6083a2e0b5bf5e7a2fba083a6eca4379ad18a126
Caitlin Cooling <ccooling97@gmail.com>
2018-11-15 10:59:29 +0000

    Test: updated existing tests to include ID story list method return list creates a div ID corresponsing to the number that the story is

--- spec/storyListViewSpec.js
+++ spec/storyListViewSpec.js
@@ -1,13 +1,13 @@
 describe("storyListView", function() {
   describe("returnList", function() {
-    it("returns a story with HTML", function() {
+    it("returns a story with HTML and id", function() {
       var storyListView = new StoryListView(["News story"]);
-      expect(storyListView.returnList()).toEqual("<ul><li><div>News story</div></li></ul>")
+      expect(storyListView.returnList()).toEqual("<ul><li><div id='0'>News story</div></li></ul>")
     });
 
-    it("returns the stories with HTML", function() {
+    it("returns the stories with HTML and ids", function() {
       var storyListView = new StoryListView(["News story", "Another news story"]);
-      expect(storyListView.returnList()).toEqual("<ul><li><div>News story</div></li><li><div>Another news story</div></li></ul>")
+      expect(storyListView.returnList()).toEqual("<ul><li><div id='0'>News story</div></li><li><div id='1'>Another news story</div></li></ul>")
     });
   });
 });
