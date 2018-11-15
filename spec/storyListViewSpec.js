53256f847571614417bad4ecda114b9722739eb9
Caitlin Cooling <ccooling97@gmail.com>
2018-11-15 13:53:47 +0000

    Test: Update story list view spec html now contains a tag with href of the id of the story

--- spec/storyListViewSpec.js
+++ spec/storyListViewSpec.js
@@ -4,14 +4,14 @@ describe("storyListView", function() {
       function StoryListDouble(stories) {this.list = stories};
       var storyListDouble = new StoryListDouble(["News story"]);
       var storyListView = new StoryListView(storyListDouble);
-      expect(storyListView.returnList()).toEqual("<ul><li><div id='0'>News story</div></li></ul>")
+      expect(storyListView.returnList()).toEqual("<ul><li><div id='0'><a href='#0'>News story</a></div></li></ul>")
     });
 
     it("returns the stories with HTML and ids", function() {
       function StoryListDouble(stories) {this.list = stories};
       var storyListDouble = new StoryListDouble(["News story", "Another news story"]);
       var storyListView = new StoryListView(storyListDouble);
-      expect(storyListView.returnList()).toEqual("<ul><li><div id='0'>News story</div></li><li><div id='1'>Another news story</div></li></ul>")
+      expect(storyListView.returnList()).toEqual("<ul><li><div id='0'><a href='#0'>News story</a></div></li><li><div id='1'><a href='#1'>Another news story</a></div></li></ul>")
     });
   });
 });
