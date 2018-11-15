06077eae284337a590926394dd9291d819e95774
Caitlin Cooling <ccooling97@gmail.com>
2018-11-15 09:48:21 +0000

    Code: get headline limits length to 50 characters

--- src/story.js
+++ src/story.js
@@ -1,6 +1,15 @@
 (function(exports) {
   function Story (headline){
     this.headline = headline;
+    MAXIMUM_LENGTH = 50;
+  };
+
+  Story.prototype.getHeadline = function () {
+    if(this.headline.length > MAXIMUM_LENGTH) {
+      return `${this.headline.slice(0, MAXIMUM_LENGTH)}...`
+    } else {
+      return this.headline;
+    }
   };
   exports.Story = Story;
 })(this);
