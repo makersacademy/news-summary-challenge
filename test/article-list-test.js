b42495f40c459c8eb81ca2e80350bbc83c990b71
jebax <joel.eliot.baxter@gmail.com>
2018-11-15 21:11:40 +0000

    added toBeTruthy matcher

--- test/article-list-test.js
+++ test/article-list-test.js
@@ -4,5 +4,6 @@ describe('An article list', () => {
   it('can store a list of articles', () => {
     var list = new ArticleList()
     expect(list.articles).isInstanceOf(Array)
+    expect(list.articles.length < 1).toBeTruthy()
   })
 })
