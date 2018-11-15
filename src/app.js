237403d945a9e4f6605a0bcbde405836b4899e56
Henry Cooksley <henry.cooksley@gmail.com>
2018-11-15 18:37:41 +0000

    rewrite app.js to make eslint happy

--- src/app.js
+++ src/app.js
@@ -1,16 +1,17 @@
-let url = "https://content.guardianapis.com/search?api-key=a2b7e67e-d194-46ae-9d15-5b21a1fc543f";
-fetch(url).then(response => {
-  return response.json();
-}).then(data => {
-  // Work with JSON data here
-  console.log(data);
-  let stories = data.response.results;
-  stories.forEach(story => {
-    let para = document.createElement('p');
-    para.innerHTML = `<a href="${story.webUrl}">${story.webTitle}</a>`
-    document.body.appendChild(para);
-  });
-}).catch(err => {
-  // Do something for an error here
-  console.log(err);
-});
+class NewsApp {
+  static render() {
+    const url = 'https://content.guardianapis.com/search?api-key=a2b7e67e-d194-46ae-9d15-5b21a1fc543f';
+    fetch(url)
+      .then(response => response.json())
+      .then((data) => {
+        const stories = data.response.results;
+        stories.forEach((story) => {
+          const para = document.createElement('p');
+          para.innerHTML = `<a href="${story.webUrl}">${story.webTitle}</a>`;
+          document.body.appendChild(para);
+        });
+      });
+  }
+}
+
+NewsApp.render();
