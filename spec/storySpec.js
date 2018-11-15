6797480d800858d03ae5bc443f0a1074b72bd954
Caitlin Cooling <ccooling97@gmail.com>
2018-11-15 11:38:42 +0000

    Test: Story has a headline

--- spec/storySpec.js
+++ spec/storySpec.js
@@ -4,10 +4,15 @@ describe("Story", function(){
     expect(story.headline).toEqual("News story");
   });
 
+  it("has content", function() {
+    var story = new Story("News story", "This news story is great");
+    expect(story.content).toEqual("This news story is great")
+  })
+
   describe("getHeadline", function(){
     it("limits the headline to 50 characters", function(){
       var story = new Story("Today's news story is very exciting and very very long");
       expect(story.getHeadline()).toEqual("Today's news story is very exciting and very very ...")
-    })
-  })
+    });
+  });
 });
