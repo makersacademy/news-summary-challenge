33a2260e8498ee8b29a166c043c9c8828bd05192
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2018-11-15 16:07:22 +0000

    Added testing framework and http

---
+++ spec/featureSpec.js
@@ -0,0 +1,11 @@
+describe("Testing true", function() {
+  it("should show true", function() {
+    expect(true).toEqual(true)
+  })
+})
+
+describe("Testing false", function() {
+  it("should show false", function() {
+    expect(false).toEqual(true)
+  })
+})
