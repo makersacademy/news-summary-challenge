0853a62f433c57e7a98f58eea25bd17debeb92f4
Caitlin Cooling <ccooling97@gmail.com>
2018-11-15 08:27:51 +0000

    Setting up specrunner

---
+++ specRunner.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>News Challenge</title>
+  </head>
+  <body>
+  <script src="spec/framework.js"></script>
+  </body>
+</html>
