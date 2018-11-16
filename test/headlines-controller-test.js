126d4027f423855bef0c4f6b929335a03b78c764
jebax <joel.eliot.baxter@gmail.com>
2018-11-16 11:52:12 +0000

    added headlines controller file

---
+++ test/headlines-controller-test.js
@@ -0,0 +1,9 @@
+import { HeadlinesController } from '../src/controllers/headlines-controller.js'
+
+describe('A headlines controller', () => {
+  var controller
+
+  beforeEach(() => {
+    controller = new HeadlinesController
+  })
+})
