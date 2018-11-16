9c836c0663ed22d10c2635885c2093ff35c8aec3
jebax <joel.eliot.baxter@gmail.com>
2018-11-16 11:38:46 +0000

    successfully implemented beforeEach actions

--- test/article-list-view-test.js
+++ test/article-list-view-test.js
@@ -1,21 +1,24 @@
 import { ArticleListView } from '../src/views/article-list-view.js'
 
 describe('An article list view', () => {
+  var view
+  var list
+  var article
+
+  beforeEach(() => {
+    article = {
+      headline: 'This is a headline.',
+      id: 0
+    }
+    list = { articles: [article] }
+    view = new ArticleListView(list)
+  })
+
   it('stores a list of articles when instantiated', () => {
-    var list = {}
-    var view = new ArticleListView(list)
     expect(view.articleList).isEqualTo(list)
   })
 
   it('can render HTML for its article list', () => {
-    var article = {
-      headline: 'This is a headline.',
-      id: 0
-    }
-    var list = {
-      articles: [article]
-    }
-    var view = new ArticleListView(list)
     expect(view.renderListHTML()).isEqualTo("<ul><li><a href='#articles/0'>This is a headline.</a></li></ul>")
   })
 })
--- test/article-test.js
+++ test/article-test.js
@@ -1,15 +1,19 @@
 import { Article } from '../src/article.js'
 
 describe('An article', () => {
+  var headline
+  var article
+
+  beforeEach(() => {
+    headline = 'This is a headline.'
+    article = new Article(headline)
+  })
+
   it('can be instantiated with a headline', () => {
-    var headline = 'This is a headline.'
-    var article = new Article(headline)
     expect(article.headline).isEqualTo(headline)
   })
 
   it('can be instantiated with an ID', () => {
-    var headline = 'This is a headline.'
-    var article = new Article(headline)
     expect(article.id).isEqualTo(0)
   })
 })
