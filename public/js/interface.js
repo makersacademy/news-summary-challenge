6577f10c1bd23f4e3a5be4488411618c94da0d4c
Aimée Craig <aimeecraig1@gmail.com>
2018-11-16 13:54:49 +0000

    code refactored

--- public/js/interface.js
+++ public/js/interface.js
@@ -3,10 +3,9 @@
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
-    var response = JSON.parse(this.responseText)
-    // var results = JSON.parse(this.responseText.response.response.results)
-    console.log(response.response.results)
-    // document.getElementById("headline").innerHTML = ;
+    var response = JSON.parse(this.responseText);
+    var results = response.response.results;
+    displayContent(results);
     }
   };
 xhttp.open("GET", "https://content.guardianapis.com/search?q=technology&show-fields=thumbnail&api-key=bad91230-b571-4dec-b899-7575ea60a1a4", true);
@@ -14,10 +13,25 @@ xhttp.send();
 
 // Functions
 
-function displayHeadlines(results) {
+function displayContent(results) {
   for (var i = 0; i < results.length; i++) {
-    var listItem = document.createElement('li')
-    document.getElementById('')
-    document.getElementById('headline').
+    // Create div container
+    var container = document.createElement('div');
+    container.setAttribute('id', 'headline' + i);
+    console.log(container);
+    // Import thumbnail
+    var image = document.createElement('img');
+    image.setAttribute('src', results[i].fields.thumbnail);
+    // Create paragraph
+    var element = document.createElement('p');
+    console.log(element);
+    // Import headline
+    var text = document.createTextNode(results[i].webTitle);
+    console.log(image);
+    // Append items
+    element.appendChild(text);
+    container.appendChild(image);
+    container.appendChild(element);
+    document.getElementById('as-headline').appendChild(container);
   }
 }
