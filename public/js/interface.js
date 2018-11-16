b6c66cae8c4d13b22027f3f11bcca373246e552d
Aimée Craig <aimeecraig1@gmail.com>
2018-11-16 15:14:41 +0000

    summary added to page

--- public/js/interface.js
+++ public/js/interface.js
@@ -6,9 +6,11 @@ xhttp.onreadystatechange = function() {
     var response = JSON.parse(this.responseText);
     var results = response.response.results;
     displayContent(results);
+    // createContainers(results);
+    // displayHeadlines(results);
     }
   };
-xhttp.open("GET", "https://content.guardianapis.com/search?q=technology&show-fields=thumbnail&api-key=bad91230-b571-4dec-b899-7575ea60a1a4", true);
+xhttp.open("GET", "https://content.guardianapis.com/search?q=technology&show-fields=thumbnail,trailText&api-key=bad91230-b571-4dec-b899-7575ea60a1a4", true);
 xhttp.send();
 
 // Functions
@@ -18,20 +20,46 @@ function displayContent(results) {
     // Create div container
     var container = document.createElement('div');
     container.setAttribute('id', 'headline' + i);
-    console.log(container);
     // Import thumbnail
     var image = document.createElement('img');
     image.setAttribute('src', results[i].fields.thumbnail);
-    // Create paragraph
-    var element = document.createElement('p');
-    console.log(element);
+    // Create paragraphs
+    var headlineParagraph = document.createElement('p');
+    headlineParagraph.setAttribute('id', 'headlineParagraph' + i);
+    headlineParagraph.setAttribute('class', 'headlineParagraph');
+    var summaryParagraph = document.createElement('p');
+    summaryParagraph.setAttribute('id', 'summaryParagraph' + i);
     // Import headline
-    var text = document.createTextNode(results[i].webTitle);
-    console.log(image);
+    var headlineText = document.createTextNode(results[i].webTitle);
+    // Import summary
+    var summaryText = document.createTextNode(results[i].fields.trailText);
+    // var summaryText = summaryText.replace(/(<([^>]+)>)/ig,"");
     // Append items
-    element.appendChild(text);
+    headlineParagraph.appendChild(headlineText);
+    summaryParagraph.appendChild(summaryText);
+    container.appendChild(headlineParagraph);
     container.appendChild(image);
-    container.appendChild(element);
-    document.getElementById('as-headline').appendChild(container);
+    container.appendChild(summaryParagraph);
+    document.getElementById('article-summary').appendChild(container);
   }
 }
+
+// function createContainers(results) {
+//   for (var i = 0; i < results.length; i++) {
+//     var container = document.createElement('div');
+//     container.setAttribute('id', 'headline' + i.toString());
+//   }
+// }
+//
+// function displayHeadlines(results) {
+//   for (var i = 0; i < results.length; i++) {
+//     var container = document.createElement('div');
+//     container.setAttribute('id', 'headline' + i.toString());
+//     var headlineParagraph = document.createElement('p');
+//     var headlineText = document.createTextNode(results[i].webTitle);
+//     headlineParagraph.setAttribute('id', 'headlineParagraph' + i);
+//     headlineParagraph.setAttribute('class', 'headlineParagraph');
+//     headlineParagraph.appendChild(headlineText);
+//     container.appendChild(headlineParagraph);
+//   }
+// }
