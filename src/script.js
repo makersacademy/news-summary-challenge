a1132af500937bc262c3006aa062d8561d23c0ff
Caitlin Cooling <ccooling97@gmail.com>
2018-11-16 12:21:45 +0000

    Code: when a headline is clicked the headline and content is shown

--- src/script.js
+++ src/script.js
@@ -1,1 +1,20 @@
+function changeURLWhenStorySelected() {
+  window.addEventListener("hashchange", showStory);
+};
 
+function showStory() {
+  show(getID(window.location));
+};
+
+function getID() {
+  return location.hash.split("#")[1];
+};
+
+function show() {
+  var headline = document.getElementById('headline');
+  headline.innerHTML = story.headline;
+  var content = document.getElementById('story');
+  content.innerHTML = story.content;
+};
+
+changeURLWhenStorySelected();
