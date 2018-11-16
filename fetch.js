6ebb03621e00b8d3799693f4052983bc9c1e370e
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-11-16 17:48:00 +0000

    fake fetch function works!

--- fetch.js
+++ fetch.js
@@ -3,7 +3,15 @@ function get() {
   fetch("http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/politics?show-fields=thumbnail"
   )
   .then(function (res) {
-  return res.json()
+    console.log(res);
+    console.log(res.headers);
+    if (res.headers !== undefined) {
+      return res.json()
+    } else {
+      return res
+    }
+
+
   })
   .then(function (res) {
   console.log(res.response);
@@ -12,3 +20,28 @@ function get() {
   })
   })
 }
+
+function fakeFetch(url) {
+  res =
+  {
+    response:
+    {
+      results:
+      [
+        {
+          "id": "1",
+          "webTitle": "this is headline 1"
+        },
+        {
+          "id": "2",
+          "webTitle": "this is headline 2"
+        }
+      ]
+    }
+  }
+  return new Promise(function(resolve, reject) {
+    resolve(res)
+  });
+}
+
+window.fetch = fakeFetch
