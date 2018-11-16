71a6cd90ed970e170ad3a1883d87aed13f9043b3
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-11-16 16:11:14 +0000

    #renderView returns html containing headlines

--- spec/views/articleListViewSpec.js
+++ spec/views/articleListViewSpec.js
@@ -3,4 +3,17 @@ describe('ArticleListView', function() {
     var articleListView = new ArticleListView()
     expect(articleListView).toBeAnInstanceOf(ArticleListView)
   });
+
+  describe('#renderView', function() {
+    it('returns the headlines of 2 articles to be displayed', function() {
+      var articleList = new ArticleList()
+      articleList.add("Headline 1", 'Body 1')
+      articleList.add("Headline 2", 'Body 2')
+
+      var articleListView = new ArticleListView(articleList)
+      console.log(articleListView.renderView());
+      expect(articleListView.renderView())
+        .toEqual('<ul><li>Headline 1</li><li>Headline 2</li></ul>');
+    });
+  });
 });
--- src/views/articleListView.js
+++ src/views/articleListView.js
@@ -1,6 +1,16 @@
 (function (exports) {
-  function ArticleListView() {
-
+  function ArticleListView(articleList) {
+    this.articleList = articleList
   }
+
+  ArticleListView.prototype.renderView = function () {
+    var view = '<ul>'
+    this.articleList.articles.forEach(function (article) {
+      view += '<li>' + article.getHeadline() + '</li>'
+    })
+    view += '</ul>'
+    return view
+  };
+
   exports.ArticleListView = ArticleListView
 })(this)
