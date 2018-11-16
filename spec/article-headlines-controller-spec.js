2ff2abe35b57ee4d4dc1e22e4efefc3bf1291e3c
Anouska Hopkins <anouskahopkins@gmail.com>
2018-11-16 14:57:40 +0000

    Written test for article list headlines controller to instantiate an article list view

--- spec/article-headlines-controller-spec.js
+++ spec/article-headlines-controller-spec.js
@@ -5,5 +5,8 @@ describe("ArticleHeadlinesController", function() {
     it("should take an article list object as a parameter and assign it to a constructor property of this.articleList", function() {
       expect(articleHeadlinesController.articleList).toBe(articleList);
     });
+    it("should initialize a new article list view object which takes the same article list object as the controller", function() {
+      expect(articleHeadlinesController.articleListView.articleList).toBe(articleList);
+    });
   });
 });
