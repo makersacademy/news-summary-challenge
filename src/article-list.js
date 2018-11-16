44d218fe77b188a9d0c176ed297820308fdfd5fc
Anouska Hopkins <anouskahopkins@gmail.com>
2018-11-16 11:56:30 +0000

    Passed test for article list to return expected values from create

--- src/article-list.js
+++ src/article-list.js
@@ -3,5 +3,13 @@
     this.articles = [];
   }
 
+  ArticleList.prototype.create = function(headline) {
+    var article = new Article(this._nextID(), headline);
+    this.articles.push(article);
+  };
+
+  ArticleList.prototype._nextID = function() {
+    return this.articles.length;
+  };
   exports.ArticleList = ArticleList;
 })(this);
