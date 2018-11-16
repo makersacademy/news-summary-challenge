f9ecb1aaf106df2e508bcf0e704f0e410a67828f
Anouska Hopkins <anouskahopkins@gmail.com>
2018-11-16 14:44:05 +0000

    Written test for article headlines controller to take a article list argument as a parameter

---
+++ spec/article-headlines-controller-spec.js
@@ -0,0 +1,9 @@
+describe("ArticleHeadlinesController", function() {
+  var articleList = new ArticleList();
+  var articleHeadlinesController = new ArticleHeadlinesController(articleList);
+  describe(".initialize", function() {
+    it("should take an article list object as a parameter and assign it to a constructor property of this.articleList", function() {
+      expect(articleHeadLinesController.articleList).toBe(articleList);
+    });
+  });
+});
