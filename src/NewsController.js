200aa36b38138469fa7be8cd00bf3b4e9cba5a46
Henry Cooksley <henry.cooksley@gmail.com>
2018-11-16 17:49:32 +0000

    add Aylien code, not working

--- src/NewsController.js
+++ src/NewsController.js
@@ -1,3 +1,4 @@
+/* eslint-disable class-methods-use-this */
 /* eslint-disable import/extensions */
 /* eslint-disable no-console */
 import env from '../.env.js';
@@ -9,19 +10,19 @@ export default class NewsController {
   }
 
   render() {
-    console.log(this);
-    const url = `https://content.guardianapis.com/search?api-key=${env.API_KEY}`;
-    fetch(url)
-      .then(response => response.json())
-      .then((data) => {
-        const stories = data.response.results;
-        this.results = data.response.results;
-        stories.forEach((story) => {
-          const para = document.createElement('p');
-          para.innerHTML = `<a href="${story.webUrl}">${story.webTitle}</a>`;
-          document.body.appendChild(para);
-        });
-      });
+    this.makeButtonWork();
+    this.fetchAndUpdateResults();
+    setTimeout(() => {
+      this.updatePageWithResults();
+    }, 2000);
+  }
+
+  updatePageWithResults() {
+    this.results.forEach((story) => {
+      const para = document.createElement('p');
+      para.innerHTML = `<a href="${story.webUrl}">${story.webTitle}</a>`;
+      document.getElementById('headlines').appendChild(para);
+    });
   }
 
   fetchAndUpdateResults() {
@@ -30,6 +31,26 @@ export default class NewsController {
       .then(response => response.json())
       .then((data) => {
         this.results = data.response.results;
+      });
+  }
+
+  makeButtonWork() {
+    document.getElementById('summaryButton').addEventListener('click', this.fetchFromAylien);
+  }
+
+  fetchFromAylien() {
+    const url = 'https://api.aylien.com/api/v1/summarize?url=https://www.theguardian.com/artanddesign/2018/nov/15/david-hockney-painting-record-auction-living-artist';
+    const request = new Request(url, {
+      headers: new Headers({
+        'X-AYLIEN-TextAPI-Application-Key': '459a45149ca7c4a0b22f50e4a9b13a6e',
+        'X-AYLIEN-TextAPI-Application-ID': '2d7caed4',
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+      }),
+    });
+    fetch(request).then(response => response.json())
+      .then((data) => {
+        console.log(data);
       });
   }
 }
