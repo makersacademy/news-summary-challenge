cff37c46e742a4618523807423425dcabf3e10f7
jebax <joel.eliot.baxter@gmail.com>
2018-11-16 16:33:28 +0000

    stubbed fetch more effectively

--- test/api-controller-test.js
+++ test/api-controller-test.js
@@ -2,39 +2,31 @@ import { APIController } from '../src/controllers/api-controller.js'
 
 describe('An API controller', () => {
   var controller
+  var passedURL
+  var json
+  var response
   var i
-  var savedFetch
+
   beforeEach(() => {
     i = 0
+    passedURL = null
+    json = { text: 'Test'}
+    response = { json: () => { return json; } }
     controller = new APIController()
-    savedFetch = window.fetch
   })
 
-  it('uses the fetch method correctly', () => {
-    var savedFetch = window.fetch
-    window.fetch = args => {
-      return {
-        then: function() {
-          return {
-            then: function() {
-              i++
-            }
-          }
-        }
-      }
+  it('initiailizing headlines uses the fetch method correctly', () => {
+    window.fetch = url => {
+      passedURL = url
+      return new Promise(resolve => {
+        i++
+        resolve(response)
+      })
     }
-    controller.initializeHeadlines()
-    window.fetch = savedFetch
+    let callback = () => {}
+
+    controller.initializeHeadlines(callback)
+
     expect(i).isEqualTo(1)
-  })
-
-  it('uses the fetch method with a callback', () => {
-    var incrementVar = () => {
-      i++
-    }
-    controller.initializeHeadlines(incrementVar)
-    setTimeout(() => {
-      expect(i).isEqualTo(1)
-    }, 500)
   })
 })
