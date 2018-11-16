bbdef13ebcd1afeeaa3ff031d977d65dcb0640cd
Clare Jolly <clarejollyuk@gmail.com>
2018-11-16 12:37:24 +0000

    mainly done - need to extract to functions and tidy up

--- js/interface.js
+++ js/interface.js
@@ -3,11 +3,15 @@ xhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     var data = JSON.parse(this.response);
     var news = data.response.results
+    // console.log(news)
     for (i = 0; i < news.length; i++) {
+    // for (i = 0; i < 8; i++) {
       var div = document.createElement("div")
       div.setAttribute("id", "headline_" + i)
       document.getElementById("headlines").appendChild(div);
       var img = document.createElement("img");
+      // console.log(news[i].fields)
+      // console.log(news[i].fields.thumbnail)
       img.setAttribute("src", news[i].fields.thumbnail);
       img.setAttribute("id", "thumbnail_" + i)
       img.setAttribute("class", "thumbnail");
@@ -30,9 +34,54 @@ xhttp.onreadystatechange = function() {
     };
 
     var showArticle = function(id) {
-      console.log(id)
+      // console.log(id)
       document.getElementById("headlines").style.display = "none"
       document.getElementById("article").style.display = "block"
+      var url = news[id].webUrl
+      // console.log(url)
+      var apiUrl = "http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url="+url+""
+      // console.log(apiUrl)
+      // var apiUrl = "http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=http://worrydream.com/MediaForThinkingTheUnthinkable/note.html"
+      // console.log(apiUrl)
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          var data = JSON.parse(this.response);
+          // console.log(data)
+          var img = document.createElement("img")
+          img.setAttribute("src", news[id].fields.thumbnail);
+          img.setAttribute("id", "thumbnail_" + id)
+          // img.setAttribute("class", "thumbnail");
+          document.getElementById("article").appendChild(img);
+          var heading = document.createElement("h2")
+          // heading.setAttribute("class", "heading");
+          heading.setAttribute("id", "heading_" + id);
+          heading.innerHTML += news[id].webTitle;
+          document.getElementById("article").appendChild(heading);
+          var sentences = data.sentences
+          for (i = 0; i < sentences.length; i++) {
+            var sentence = document.createElement("p")
+            sentence.setAttribute("class", "sentence");
+            sentence.setAttribute("id", "sentence_" + i);
+          sentence.innerHTML = data.sentences[i];
+          document.getElementById("article").appendChild(sentence);
+          }
+          var backbutton = document.createElement("button")
+          backbutton.setAttribute("class", "backbutton");
+          backbutton.setAttribute("value", "Back");
+          backbutton.innerHTML = "Back to headlines"
+          document.getElementById("article").appendChild(backbutton);
+  // backbutton.addEventListener('click', goBack, false);
+  // var goBack = function() {
+  //   document.getElementById("headlines").style.display = "block"
+  //   document.getElementById("article").style.display = "none"
+  //
+  // }
+
+        }
+      }
+      xhttp.open("GET", apiUrl, true);
+      xhttp.send();
     }
 
     for (var i = 0; i < headingclass.length; i++) {
@@ -43,5 +92,5 @@ xhttp.onreadystatechange = function() {
     }
   }
 };
-xhttp.open("GET", "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?show-fields=thumbnail", true);
+xhttp.open("GET", "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?show-fields=thumbnail,headline,trailText&show-tags=all", true);
 xhttp.send();
