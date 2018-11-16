b01c48734d11336ecd8c47df754b9ada7aa90df9
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-11-16 14:30:05 +0000

    #getHeadline returns headline

--- spec/models/articleSpec.js
+++ spec/models/articleSpec.js
@@ -3,4 +3,11 @@ describe('Article', function() {
     var article = new Article()
     expect(article).toBeAnInstanceOf(Article);
   });
+
+  describe('#getHeadline', function() {
+    it('returns headline of an article', function() {
+      var article = new Article("This is a headline")
+      expect(article.getHeadline()).toEqual("This is a headline");
+    });
+  });
 });
