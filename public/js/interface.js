6809a8bb9f6f0b67fc5859b3a291c13f80c9aaa9
Aimée Craig <aimeecraig1@gmail.com>
2018-11-16 17:28:34 +0000

    body added to page

--- public/js/interface.js
+++ public/js/interface.js
@@ -1,25 +1,33 @@
-// API call
+// ************************* API call *************************
+// var results;
+// var headlineContainer;
+// var bodyContainer;
 
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     var response = JSON.parse(this.responseText);
-    var results = response.response.results;
+    results = response.response.results;
     displayContent(results);
     // createContainers(results);
     // displayHeadlines(results);
+    setVariable(response);
     }
   };
-xhttp.open("GET", "https://content.guardianapis.com/search?q=technology&show-fields=thumbnail,trailText&api-key=bad91230-b571-4dec-b899-7575ea60a1a4", true);
+xhttp.open("GET", "https://content.guardianapis.com/search?q=technology&show-fields=thumbnail,trailText,body&api-key=bad91230-b571-4dec-b899-7575ea60a1a4", true);
 xhttp.send();
 
-// Functions
+// ************************* Functions *************************
+
+// function setVariable(response) {
+//   results = response.response.results;
+// }
 
 function displayContent(results) {
   for (var i = 0; i < results.length; i++) {
-    // Create div container
-    var container = document.createElement('div');
-    container.setAttribute('id', 'headline' + i);
+    // Create div headlineContainer
+    var headlineContainer = document.createElement('div');
+    headlineContainer.setAttribute('id', 'headline' + i);
     // Import thumbnail
     var image = document.createElement('img');
     image.setAttribute('src', results[i].fields.thumbnail);
@@ -33,15 +41,61 @@ function displayContent(results) {
     var headlineText = document.createTextNode(results[i].webTitle);
     // Import summary
     var summaryText = document.createTextNode(results[i].fields.trailText);
+    console.log(headlineParagraph);
+    // addClickEvent(headlineParagraph.id);
+    // document.getElementById('headlineParagraph' + i).style.display="hidden";
     // var summaryText = summaryText.replace(/(<([^>]+)>)/ig,"");
     // Append items
     headlineParagraph.appendChild(headlineText);
     summaryParagraph.appendChild(summaryText);
-    container.appendChild(headlineParagraph);
-    container.appendChild(image);
-    container.appendChild(summaryParagraph);
-    document.getElementById('article-summary').appendChild(container);
+    headlineContainer.appendChild(headlineParagraph);
+    headlineContainer.appendChild(image);
+    headlineContainer.appendChild(summaryParagraph);
+    document.getElementById('article').appendChild(headlineContainer);
+
+    // Article content
+    var body = document.createElement('div');
+    body.setAttribute('id', 'body' + i);
+    body.setAttribute('class', 'body');
+    body.style.display="none";
+    var bodyText = document.createTextNode(results[i].fields.body)
+    console.log(bodyText);
+    body.appendChild(bodyText);
+    console.log(body);
+    document.getElementById('article').appendChild(body);
   }
+}
+
+function createContainers(results) {
+  for (var i = 0; i < results.length; i++) {
+    var headlineContainer = document.createElement('div');
+    headlineContainer.setAttribute('id', 'headline' + i);
+    createHeadlineElement(i)
+    var bodyContainer = document.createElement('div');
+    bodyContainer.setAttribute('id', 'body' + i);
+  }
+}
+
+function createHeadlineElement(i) {
+  var image = document.createElement('img');
+  image.setAttribute('src', results[i].fields.thumbnail);
+  var headlineParagraph = document.createElement('p');
+  headlineParagraph.setAttribute('id', 'headlineParagraph' + i);
+  headlineParagraph.setAttribute('class', 'headlineParagraph');
+  var summaryParagraph = document.createElement('p');
+  summaryParagraph.setAttribute('id', 'summaryParagraph' + i);
+}
+
+function showArticle(id) {
+  document.getElementById(id).style.display = 'block';
+}
+
+// ************************* Events *************************
+
+function addClickEvent(id) {
+  document.getElementById(id).addEventListener('click', function() {
+    showArticle(id);
+  })
 }
 
 // function createContainers(results) {
