da6d258d4dd721509df907bd0443c3b9dfd35c9b
Anouska Hopkins <anouskahopkins@gmail.com>
2018-11-16 16:11:35 +0000

    Passed test for article list headlines controller to render the article list to a div

--- src/article-headlines-controller.js
+++ src/article-headlines-controller.js
@@ -3,5 +3,11 @@
     this.articleList = articleList;
     this.articleListView = new ArticleListView(articleList);
   }
+
+  ArticleHeadlinesController.prototype.render = function () {
+    var app = document.getElementById("app");
+    app.innerHTML = this.articleListView.html();
+  };
+
   exports.ArticleHeadlinesController = ArticleHeadlinesController;
 })(this);
