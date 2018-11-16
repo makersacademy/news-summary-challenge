b149cc3464d0eff321e76b1bf155125df98bedaf
jebax <joel.eliot.baxter@gmail.com>
2018-11-16 09:54:12 +0000

    articles are instantiated with an ID

--- src/article-list.js
+++ src/article-list.js
@@ -6,7 +6,11 @@ class ArticleList {
   }
 
   createArticle(headline) {
-    this.articles.push(new Article(headline))
+    this.articles.push(new Article(headline, this._nextArticleID()))
+  }
+
+  _nextArticleID() {
+    return this.articles.length
   }
 }
 
--- test/article-test.js
+++ test/article-test.js
@@ -1,9 +1,15 @@
 import { Article } from '../src/article.js'
 
 describe('An article', () => {
-  it('can be instantiated with a body', () => {
+  it('can be instantiated with a headline', () => {
     var headline = 'This is a headline.'
     var article = new Article(headline)
     expect(article.headline).isEqualTo(headline)
+  })
+
+  it('can be instantiated with an ID', () => {
+    var headline = 'This is a headline.'
+    var article = new Article(headline)
+    expect(article.id).isEqualTo(0)
   })
 })
