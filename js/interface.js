382620b195302e74f1f1e39dbcee9971a0ac5b5c
Br0ckers <neil.brockman@yahoo.co.uk>
2018-11-16 15:15:32 +0000

    add hyperlink

--- js/interface.js
+++ js/interface.js
@@ -10,7 +10,7 @@ function append(parent,el)
 
 const ul = document.getElementById("stories");
 
-const url = 'https://content.guardianapis.com/politics?&show-fields=thumbnail&api-key=3eb03c79-0697-4829-a488-b7bdbd23b47f';
+const url = 'https://content.guardianapis.com/politics?&show-fields=all&api-key=3eb03c79-0697-4829-a488-b7bdbd23b47f';
 
 fetch(url)
   .then((Response) => Response.json())
@@ -21,17 +21,46 @@ fetch(url)
     return stories.map(function(stories){
       // news = stories.webTitle
 
-      let li = createNode('li'),
+      // let li = createNode('li'),
       img = createNode('img'),
-      span = createNode('span');
+      span3 = createNode('span');
 
       img.src=stories.fields.thumbnail;
 
-      span.innerHTML=`${stories.webTitle}`;
+      span3.innerHTML=`${stories.webTitle}`;
 
-      append(li,img);
-      append(li,span);
-      append(ul,li);
+      // let li1 = createNode('li'),
+      span1 = createNode('span');
+      let summary = stories.fields.bodyText.substring(0,200);
+      span1.innerHTML=`${summary}`;
+
+      let hyperlink = stories.webUrl
+      link = createNode("a")
+      href = createNode("span")
+      link.innerHTML=`${hyperlink}`
+      link.innerHTML=`${"Full Article"}`
+      link.href=hyperlink
+
+      span5 = createNode("span")
+      span5.innerHTML=`${"+++++++++++++++++++++++++++++++++++++++++++++++++++++"}`
+      let li1 = createNode('li');
+      let li2 = createNode('li');
+      let li3 = createNode('li');
+      let li4 = createNode('li');
+      let li5 = createNode('li');
+
+
+      append(li1,span1);
+      append(li2,img);
+      append(li3,span3);
+      append(li4,link);
+      append(li5,span5);
+
+      append(ul,li1);
+      append(ul,li2);
+      append(ul,li3);
+      append(ul,li4);
+      append(ul,li5);
     })
 
 })
