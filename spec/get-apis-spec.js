18397e99dbd15d0f6985f21cd3c0fb7290a67413
Anouska Hopkins <anouskahopkins@gmail.com>
2018-11-17 11:11:22 +0000

    Written test for article to initialize with a web url

---
+++ spec/get-apis-spec.js
@@ -0,0 +1,9 @@
+// describe("GetAPIs", function () {
+//   var getAPI = new GetAPIs();
+//   describe(".headlinesAPI", function() {
+//     it("should return a callback", function () {
+//       var dummydata = "hello";
+//       dummyCallback = function () { return dummydata; };
+//     });
+//   });
+// });
--- src/get-apis.js
+++ src/get-apis.js
@@ -1,6 +1,6 @@
 (function(exports) {
   function GetAPIs () {
-    this.articleList = new ArticleList()
+    this.articleList = new ArticleList();
   }
 
   GetAPIs.prototype.headlinesAPI = function() {
@@ -16,6 +16,23 @@
       });
       return this.articleList;
     };
+
+  // GetAPIs.prototype.headlinesAPI = function(callback) {
+  //   fetch('https://content.guardianapis.com/search?api-key=b5a397fe-7a8b-47c4-9727-abc75851d690')
+  //     .then(function(response) {
+  //       return response.json();
+  //     })
+  //     .then(callback);
+  //   };
+  //
+  // GetAPIs.prototype.getHeadlines = function() {
+  //   this.headlinesAPI(function(json) {
+  //     var results = myJson.response.results;
+  //     for (var i = 0; i < results.length; i++) {
+  //       this.articleList.create(results[i].webTitle);
+  //     }
+  //   });
+  // };
 
     exports.GetAPIs = GetAPIs;
   })(this);
