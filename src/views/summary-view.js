5b528bee8d48842865faa2235ef49cdc18666df5
jebax <joel.eliot.baxter@gmail.com>
2018-11-17 16:27:41 +0000

    added first tests for SummaryController

--- src/views/summary-view.js
+++ src/views/summary-view.js
@@ -1,13 +1,13 @@
 class SummaryView {
-  constructor(article) {
+  constructor (article) {
     this.article = article
   }
 
-  renderSummaryHTML() {
+  renderSummaryHTML () {
     return `<article>${this.formatImage(this.article.image)}<h2>${this.article.headline}</h2><p>${this.article.summary}</p></article>`
   }
 
-  formatImage(imageData) {
+  formatImage (imageData) {
     var truncatedData = imageData.split(/src=/)[1].split(/ width/)[0]
     return `<figure class="headline-image"><img src=${truncatedData} width="500" height="300"/></figure>`
   }
