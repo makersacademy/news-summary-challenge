f429937e20bff778abdfb4603a2d8269bed9071b
jebax <joel.eliot.baxter@gmail.com>
2018-11-17 22:18:55 +0000

    made eslint happier

--- test/framework.js
+++ test/framework.js
@@ -1,43 +1,43 @@
-(function(exports) {
+(function (exports) {
   var beforeActions = []
 
-  function beforeEach(actions) {
+  function beforeEach (actions) {
     beforeActions.push(actions)
   }
 
-  function describe(description, func) {
+  function describe (description, func) {
     console.log(description)
     func()
     beforeFunctions = []
   }
 
-  function it(description, func) {
+  function it (description, func) {
     var savedBeforeActions = beforeActions
     beforeActions.forEach(action => { action.call() })
-    describe(" # " + description, func)
+    describe(' # ' + description, func)
     beforeActions = savedBeforeActions
   }
 
-  function expect(arg) {
+  function expect (arg) {
     return matchers(arg)
   }
 
-  function matchers(arg) {
+  function matchers (arg) {
     var pass = '   %cP'
     var passColour = 'color: green'
     var fail = '   %cF'
     var failColour = 'color: red'
 
     return {
-      toBeTruthy: function() {
-        if (!!arg) {
+      toBeTruthy: function () {
+        if (arg) {
           console.log(pass, passColour)
         } else {
           console.log(fail, failColour)
           throw new Error(`Assertion failed: ${arg} is not truthy`)
         }
       },
-      isEqualTo: function(argTwo) {
+      isEqualTo: function (argTwo) {
         if (arg === argTwo) {
           console.log(pass, passColour)
         } else {
@@ -45,7 +45,7 @@
           throw new Error(`Assertion failed: ${arg} does not equal ${argTwo}`)
         }
       },
-      isInstanceOf: function(argTwo) {
+      isInstanceOf: function (argTwo) {
         if (arg instanceof argTwo) {
           console.log(pass, passColour)
         } else {
@@ -53,14 +53,14 @@
           throw new Error(`Assertion failed: ${arg} is not an instance of ${argTwo}`)
         }
       },
-      includes: function(argTwo) {
+      includes: function (argTwo) {
         if (arg.includes(argTwo)) {
           console.log(pass, passColour)
         } else {
           console.log(fail, failColour)
           throw new Error(`Assertion failed: ${arg} does not include ${argTwo}`)
         }
-      },
+      }
     }
   }
 
