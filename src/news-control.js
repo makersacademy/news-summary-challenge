7988ff19a81e37fba5ac1b9f98e11c17e8d30798
Bill Muxworthy <bill_muxworthy@hotmail.com>
2018-11-17 16:36:30 +0000

    html get list of headlines

--- src/news-control.js
+++ src/news-control.js
@@ -3,11 +3,15 @@
     url = "https://content.guardianapis.com/search?";
     api = "15ab9879-6713-40b7-a6d9-0abbeb1a5b71";
     $.get(url + "api-key=" + api, function(data) {
+      listStringHtml = '<ul>';
       for (i = 0; i < data.response.results.length; i++) {
-        $('#news_update').text(data.response.results.webTitle);
-        console.log(data.response.results[i]);
+        listStringHtml += `<li>${data.response.results[i].webTitle}</li>`
+        // $('#news_update').text(data.response.results.webTitle);
       };
+      listStringHtml += '</ul>';
+      $('#news_update').html(listStringHtml);
+      console.log(listStringHtml)
     });
   };
-  exports.newsUpdate = newsUpdate;
+  exports.newsUpdate = newsUpdate();
 })(this);
