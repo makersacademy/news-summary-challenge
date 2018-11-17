641028427eb05e70a467448aab4688ef3ec56c94
jebax <joel.eliot.baxter@gmail.com>
2018-11-17 11:14:43 +0000

    headlines controller renders headlines on page

--- src/controllers/headlines-controller.js
+++ src/controllers/headlines-controller.js
@@ -6,12 +6,22 @@ class HeadlinesController {
     this.headlinesView = new viewExports.HeadlinesView(headlinesList)
   }
 
-  setHeadlinesInformation(resultsList) {
+  listHeadlines(resultsList) {
+    this._setHeadlinesInformation(resultsList)
+    this._renderHeadlinesHTML()
+  }
+
+  _setHeadlinesInformation(resultsList) {
     var images = resultsList.map(resultObject => resultObject.fields.main)
     var headlines = resultsList.map(resultObject => resultObject.webTitle)
     headlines.forEach((headline, index) => {
       this.headlinesView.headlinesList.create(headline, images[index])
     })
+  }
+
+  _renderHeadlinesHTML() {
+    var html = this.headlinesView.renderListHTML()
+    document.getElementById('headlines').innerHTML = html
   }
 }
 
