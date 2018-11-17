112ba95c6f1ffed3e52e2987a27660423cd3c057
jebax <joel.eliot.baxter@gmail.com>
2018-11-17 20:01:27 +0000

    formatted summary text

--- src/views/summary-view.js
+++ src/views/summary-view.js
@@ -4,12 +4,16 @@ class SummaryView {
   }
 
   renderSummaryHTML () {
-    return `<article>${this.formatImage(this.article.image)}<h2>${this.article.headline}</h2><p>${this.article.summary}</p></article>`
+    return `<article>${this.formatImage(this.article.image)}<h2>${this.article.headline}</h2><p>${this.formatSummary(this.article.summary)}</p></article>`
   }
 
   formatImage (imageData) {
     var truncatedData = imageData.split(/src=/)[1].split(/ width/)[0]
     return `<figure class="headline-image"><img src=${truncatedData} width="500" height="300"/></figure>`
+  }
+
+  formatSummary (summaryData) {
+    return summaryData.join('<br><br>')
   }
 }
 
