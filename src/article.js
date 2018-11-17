608a4d51264683c81a238210b61a23d6cf849447
Anouska Hopkins <anouskahopkins@gmail.com>
2018-11-17 12:56:11 +0000

    Passed test for article to initialize with summary

--- src/article.js
+++ src/article.js
@@ -1,8 +1,9 @@
 (function(exports) {
-  function Article(id, headline, webURL) {
+  function Article(id, headline, webURL, summary) {
     this.id = id;
     this.headline = headline;
     this.webURL = webURL;
+    this.summary = summary
   }
 
   exports.Article = Article;
