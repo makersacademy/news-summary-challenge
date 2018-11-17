93a2b13d51ad9ea4a0906d6cfa3ddb6acdebd374
jebax <joel.eliot.baxter@gmail.com>
2018-11-17 17:17:50 +0000

    tested fetch method in SummaryController

--- test/summary-controller-test.js
+++ test/summary-controller-test.js
@@ -3,6 +3,9 @@ import { SummaryController } from '../src/controllers/summary-controller.js'
 describe('A summary controller', () => {
   var controller
   var articleList
+  var passedURL
+  var json
+  var response
 
   beforeEach(() => {
     articleList = {
@@ -12,14 +15,36 @@ describe('A summary controller', () => {
         image: 'This is an image.',
         id: 0 }]
     }
+
+    passedURL = null
+    json = { text: 'Test', sentences: { join: () => 'Test successful' } }
+    response = { json: () => { return json; } }
+
     controller = new SummaryController(articleList)
   })
 
-  it('is instantiated with a headlines list', () => {
+  it('is instantiated with an article list', () => {
     expect(controller.articleList).isEqualTo(articleList)
   })
 
   it('has no summary view object when instantiated', () => {
     expect(controller.summaryView).isEqualTo(null)
+  })
+
+  it('gets a summary uses the fetch method correctly', () => {
+    var i = 0
+    var savedFunctionState = window.fetch
+    window.fetch = url => {
+      passedURL = url
+      return new Promise(resolve => {
+        i++
+        resolve(response)
+      })
+    }
+    let callback = () => {}
+
+    controller.initializeSummary(`http://testurl.com`)
+    expect(passedURL).isEqualTo(`http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=http://testurl.com`)
+    expect(i).isEqualTo(1)
   })
 })
