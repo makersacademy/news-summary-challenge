fce6afb318fa5feeecb766f115f79ba9598d7f92
Anouska Hopkins <anouskahopkins@gmail.com>
2018-11-17 11:12:05 +0000

    Passed test for article to initialize with a web url

--- src/article.js
+++ src/article.js
@@ -1,7 +1,8 @@
 (function(exports) {
-  function Article(id, headline) {
+  function Article(id, headline, webURL) {
     this.id = id;
     this.headline = headline;
+    this.webURL = webURL;
   }
 
   exports.Article = Article;
