0acd8e596109bbed657a0f2ef8bf3ac881f4f9cd
jebax <joel.eliot.baxter@gmail.com>
2018-11-17 14:53:59 +0000

    Removed APIController, added app.js
    app.js initializes headlines when index.html loads

--- src/controllers/headlines-controller.js
+++ src/controllers/headlines-controller.js
@@ -1,25 +1,31 @@
 import * as viewExports from '../views/headlines-view.js'
+import { apiKeys } from '../../api-keys.js'
 
 class HeadlinesController {
   constructor (headlinesList) {
     this.headlinesList = headlinesList
     this.headlinesView = new viewExports.HeadlinesView(headlinesList)
+    this.key = apiKeys.GUARDIAN_KEY
   }
 
-  listHeadlines (resultsList) {
-    this._setHeadlinesInformation(resultsList)
-    this._renderHeadlinesHTML()
+  initializeHeadlines () {
+    fetch(`https://content.guardianapis.com/search?show-fields=main%2Cbody&order-by=newest&page-size=10&q=technology&api-key=${this.key}`)
+      .then(response => { return response.json() })
+      .then(json => {
+        this.setHeadlinesInformation(json.response.results)
+        this.renderHeadlinesHTML()
+      })
   }
 
-  _setHeadlinesInformation (resultsList) {
+  setHeadlinesInformation (resultsList) {
     var images = resultsList.map(resultObject => resultObject.fields.main)
     var headlines = resultsList.map(resultObject => resultObject.webTitle)
     headlines.forEach((headline, index) => {
-      this.headlinesView.headlinesList.create(headline, images[index])
+      this.headlinesView.headlinesList.createArticle(headline, images[index])
     })
   }
 
-  _renderHeadlinesHTML () {
+  renderHeadlinesHTML () {
     var html = this.headlinesView.renderListHTML()
     document.getElementById('headlines').innerHTML = html
   }
