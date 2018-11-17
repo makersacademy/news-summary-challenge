772461d4e5fa1713a5c258596fc1b217757e191b
Bill Muxworthy <bill_muxworthy@hotmail.com>
2018-11-17 17:22:11 +0000

    refactor html article list

--- src/news-control.js
+++ src/news-control.js
@@ -2,20 +2,24 @@
   function newsUpdate() {
     url = "https://content.guardianapis.com/search?";
     api = "15ab9879-6713-40b7-a6d9-0abbeb1a5b71";
-    $.get(url + "api-key=" + api, function(data) {
-      listStringHtml = '<ul>';
-      for (i = 0; i < data.response.results.length; i++) {
-        listStringHtml +=
-          `<li>
-            <a href =${data.response.results[i].webUrl}>
-              ${data.response.results[i].webTitle}
-            </a>
-          </li>`
-      };
-      listStringHtml += '</ul>';
-      $('#news_update').html(listStringHtml);
-      console.log(listStringHtml)
+    $.get(url + "api-key=" + api + "&show-fields=trailText", function(data) {
+      $('#news_update').html(generateArticleListHTML(data));
     });
+  };
+
+  function generateArticleListHTML(data) {
+    listStringHtml = '<ul>';
+    articles = data.response.results
+    for (i = 0; i < articles.length; i++) {
+      listStringHtml +=
+        `<li>
+          <a href =${articles[i].webUrl}>
+            ${articles[i].webTitle}
+          </a>
+          <span>${articles[i].fields.trailText}</span>
+        </li>`
+    };
+    return listStringHtml + '</ul>';
   };
   exports.newsUpdate = newsUpdate();
 })(this);
