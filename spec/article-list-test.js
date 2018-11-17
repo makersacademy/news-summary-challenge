6df22aed10b1124af42f902c9e16889ba8eae822
Imogen Misso <imogenmisso@hotmail.co.uk>
2018-11-17 15:31:29 +0000

    updated some tests

--- spec/article-list-test.js
+++ spec/article-list-test.js
@@ -3,6 +3,7 @@ describe("ArticleList", function() {
   var data = { response:
                 { results: [
                     { webTitle: "Sri Lanka v England: hosts seven down chasing 301 to win second Test – live!",
+                      id: "sport/live/2018/nov/17/sri-lanka-v-england-third-test-day-four-live",
                       webUrl: "https://www.theguardian.com/sport/live/2018/nov/17/sri-lanka-v-england-third-test-day-four-live"
                     }]
                 }
@@ -18,5 +19,11 @@ describe("ArticleList", function() {
     var articleList = new ArticleList();
     articleList.add(data)
     expect(articleList.articles[0].headline).toEqual("Sri Lanka v England: hosts seven down chasing 301 to win second Test – live!")
+  })
+
+  it("returns the url of an article when given the id", function() {
+    var articleList = new ArticleList();
+    articleList.add(data)
+    expect(articleList.getUrlById(result.id)).toEqual("https://www.theguardian.com/sport/live/2018/nov/17/sri-lanka-v-england-third-test-day-four-live")
   })
 });
