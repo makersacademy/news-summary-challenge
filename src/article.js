c8d77c43f70f1417de2c318c4680034d8a97b897
Anouska Hopkins <anouskahopkins@gmail.com>
2018-11-17 13:00:39 +0000

    Rewritten test so summary is not created in initialize but in createSummary

--- src/article.js
+++ src/article.js
@@ -1,9 +1,8 @@
 (function(exports) {
-  function Article(id, headline, webURL, summary) {
+  function Article(id, headline, webURL) {
     this.id = id;
     this.headline = headline;
     this.webURL = webURL;
-    this.summary = summary
   }
 
   exports.Article = Article;
