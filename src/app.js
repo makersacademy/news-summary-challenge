3b152509354b5a707604a3ff2d776e3257910f3e
jebax <joel.eliot.baxter@gmail.com>
2018-11-17 19:53:42 +0000

    App successfully loads article summaries
    Used event listener to trigger summary controller on hashchange

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 import { HeadlinesController } from './controllers/headlines-controller.js'
-import { ArticleList } from './headlines-list.js'
+import { SummaryController } from './controllers/summary-controller.js'
+import { ArticleList } from './article-list.js'
+import * as listeners from './event-listeners.js'
 
-var articleList = new ArticleList()
-var headlinesController = new HeadlinesController(articleList)
+var headlinesController = new HeadlinesController(new ArticleList)
 
 headlinesController.initializeHeadlines()
+
+var summaryController = new SummaryController(headlinesController.headlinesView.articleList)
+
+listeners.loadSummaryOnClick(summaryController)
