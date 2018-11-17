28ec56bf5c88750433e856908b5e9bfa791132ce
jebax <joel.eliot.baxter@gmail.com>
2018-11-17 19:12:12 +0000

    articles can have a URL when created

--- test/article-list-test.js
+++ test/article-list-test.js
@@ -4,11 +4,13 @@ describe('An article list', () => {
   var list
   var headline
   var image
+  var url
 
   beforeEach(() => {
     list = new ArticleList()
     headline = 'This is a headline.'
     image = 'This is an image.'
+    url = 'http://test.com'
   })
 
   it('can store a list of articles', () => {
@@ -17,19 +19,24 @@ describe('An article list', () => {
   })
 
   it('can create an article and store it in its list', () => {
-    list.createArticle(headline, image)
+    list.createArticle(headline, image, url)
     expect(list.articles[0].headline).isEqualTo('This is a headline.')
   })
 
   it('gives an article an ID when created', () => {
-    list.createArticle(headline, image)
-    list.createArticle('This is a second headline.', image)
+    list.createArticle(headline, image, url)
+    list.createArticle('This is a second headline.', image, url)
     expect(list.articles[0].id).isEqualTo(0)
     expect(list.articles[1].id).isEqualTo(1)
   })
 
   it('can create an article with an image', () => {
-    list.createArticle(headline, image)
-    expect(list.articles[0].image).isEqualTo(image)
+    list.createArticle(headline, image, url)
+    expect(list.articles[0].image).isEqualTo(image, url)
+  })
+
+  it('can create an article with an ID', () => {
+    list.createArticle(headline, image, url)
+    expect(list.articles[0].url).isEqualTo(url)
   })
 })
