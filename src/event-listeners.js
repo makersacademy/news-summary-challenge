7c98bcd0bf8cb7923eb48940c2ef2a3a3cb6f120
jebax <joel.eliot.baxter@gmail.com>
2018-11-18 18:34:42 +0000

    changed listeners to ES6 syntax

--- src/event-listeners.js
+++ src/event-listeners.js
@@ -1,14 +1,14 @@
 import { HeadlinesController } from './controllers/headlines-controller.js'
 import { ArticleList } from './article-list.js'
 
-function loadSummaryOnClick (summaryController) {
+let loadSummaryOnClick = summaryController => {
   window.addEventListener('hashchange', () => {
     var id = window.location.hash.split('/')[1]
     summaryController.initializeSummary(id)
   })
 }
 
-function loadHeadlinesOnHomeClick () {
+let loadHeadlinesOnHomeClick = () => {
   document.getElementById('home').addEventListener('click', () => {
     var headlinesController = new HeadlinesController(new ArticleList())
     headlinesController.initializeHeadlines()
