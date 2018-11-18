8b59ade606c7f154eb5cc1704212a90fdd5c001e
chloeverity <chloeverity@hotmail.co.uk>
2018-11-18 00:12:29 +0000

    adding picture and link, unfinished layout

--- src/news-api-request.js
+++ src/news-api-request.js
@@ -1,21 +1,31 @@
 var url = 'http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?show-fields=thumbnail'
 fetch(url).then(res => {
-  if(res.status !== 200) {
-    console.log('res.status not 200, status code: ' + res.status);
-    return;
-  }
+
   res.json().then(function(data) {
+
     var articles = data.response.results;
     for(var i = 0; i < articles.length; i++) {
       var currentArticle = articles[i];
-      var newsList = document.getElementById("headlines");
-      var listItem = document.createElement("h2");
+      var articleList = document.getElementById('headlines');
+      var articleListItem = document.createElement("h2");
       var itemLink = document.createElement("a");
       itemLink.href = currentArticle.webUrl;
       var thumbnail = document.createElement("img");
       thumbnail.src = currentArticle.fields['thumbnail'];
-      document.body.appendChild(listItem);
-      listItem.innerHTML = currentArticle.webTitle;
+      document.body.appendChild(articleListItem);
+      document.body.appendChild(itemLink);
+      document.body.appendChild(thumbnail);
+      articleListItem.innerHTML = currentArticle.webTitle;
+      itemLink.innerHTML = "read more";
+      var summaryUrl = ("http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=" + currentArticle.webUrl)
+      fetch(summaryUrl).then(res => {
+        res.json().then(function(dataSum) {
+          var summary = document.createElement("p");
+          summary.innerHTML = dataSum.sentences;
+          document.body.appendChild(summary);
+          summary.innerHTML = dataSum.sentences;
+        })
+      })
     }
   });
 });
