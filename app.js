3ce21a8adb2b7dcfd180f1463dd5d8aac2745f9f
RyanWolfen7 <wolfen7lada@gmail.com>
2018-11-18 22:20:11 +0000

    un Promised the thing

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 window.onload = function() {
-  const API_KEY = '46d4c3c8937142eca861cda8a7eca0c0'
-  const data = Promise.resolve();
+  const API_KEY = '46d4c3c8937142eca861cda8a7eca0c0';
 
   var url = `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${API_KEY}`
   var req = new Request(url);
   fetch(req)
-    data.then(function(response) {
+    .then(function(response) {
+      return response.json();
+      // console.log(response.json());
+    })
+    .then(function(data) {
       console.log(data);
     })
-
   document.getElementById("app").innerHTML = IndexPage().render(data);
 }
