76ffa068c172a63a9576375dde2241e408173bca
RyanWolfen7 <wolfen7lada@gmail.com>
2018-11-18 20:02:31 +0000

    stuck

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 window.onload = function() {
   const API_KEY = '46d4c3c8937142eca861cda8a7eca0c0'
+  const data = Promise.resolve();
 
   var url = `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${API_KEY}`
   var req = new Request(url);
   fetch(req)
-    .then(function(response) {
-      console.log(response.json());
+    data.then(function(response) {
+      console.log(data);
     })
 
-  document.getElementById("app").innerHTML = IndexPage().render(req);
+  document.getElementById("app").innerHTML = IndexPage().render(data);
 }
