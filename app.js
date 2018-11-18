678901839e1f2ce827c85b0a13ac227e823267b6
RyanWolfen7 <wolfen7lada@gmail.com>
2018-11-18 15:35:59 +0000

    trying some stuff

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-window.addEventListener("load", function() {
-  document.getElementByID('app').innerHTML = IndexPage().render();
-}, false)
+const API_KEY = '1836ff0dabce42ce86ee463e75d05fbd'
+
+
+
+
+window.onload = function() {
+  var url = "https://api.nytimes.com/svc/topstories/v2/home.json?" + API_KEY;
+  
+  loadJSON(url, gotData);
+  document.getElementById("app").innerHTML = IndexPage().render();
+}
