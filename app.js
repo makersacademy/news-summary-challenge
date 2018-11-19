99711ac04ee2288ce216b8721ecbc2a914ded27a
RyanWolfen7 <wolfen7lada@gmail.com>
2018-11-19 09:29:40 +0000

    got the array out

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 window.onload = function() {
   const API_KEY = '46d4c3c8937142eca861cda8a7eca0c0';
+  const Articles = [];
 
   var url = `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${API_KEY}`
   var req = new Request(url);
@@ -8,9 +9,11 @@ window.onload = function() {
       return response.json();
       // console.log(response.json());
     })
-    .then(function(data) {
-      console.log(data);
+    .then((data) => {
+      data.articles.forEach(function(article) {
+        Articles.push(article);
+      })
     })
-    console.log(data)
-  document.getElementById("app").innerHTML = IndexPage().render(data);
+    console.log(Articles)
+  document.getElementById("app").innerHTML = IndexPage().render(Articles);
 }
