8bfae2719639562c92be9699d6de7902e039b957
David Best <david_best@ntlworld.com>
2018-11-19 08:50:27 +0000

    final submission

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 News Summary Challenge
 
 Credit goes to John Littler for assisting me with setting up the fetch function and to Preeti Udas for pointers on debugging.
+
+To load, clone https://github.com/davidbest1882/news-summary-challenge.git.
+Add an API key to line 12 of headlines.js
+Open index.html
+
+I wasn't able to complete the story to show the news summary when the item was selected. I was unable to load the page without a "Cannot read property 'addEventListener' of null" error which appears to be due to the "document.getElementById("image1")" command on line 50 loading before the elements are dynamically created. I experimented with 
--- headlines.js
+++ headlines.js
@@ -8,52 +8,126 @@ function append(parent, el){
 
 const ul = document.getElementById("headlines");
 
-const url = "https://content.guardianapis.com/search?show-fields=all&api-key=11d5b061-787f-4f68-bd78-d98d945bdffb"
+
+const url = "https://content.guardianapis.com/search?show-fields=all&api-key="
 
 fetch(url)
 .then((Response) => Response.json())
 .then(function(data){
   let headlines = data.response.results;
   console.log(headlines);
+  var count = 0;
 
   headlines.map(function(headline){
-
-    let article = headline.webTitle,
+    count += 1;
+    let title = headline.webTitle,
     originalArticle = headline.webUrl,
     thumbnail = headline.fields.thumbnail;
+    summary = headline.fields.body.slice(0,1000);
 
-
-    let li = createNode('li')
-    imgli = createNode('li'),
+    let space = createNode('br'),
+    summ = createNode('li'),
     img = createNode('img'),
     span = createNode('span'),
     link = createNode('a');
     img.src = thumbnail;
 
     link.setAttribute("href",`${originalArticle}`);
+    img.setAttribute("id", "image" + count);
+    summ.setAttribute("id", "news" + count);
 
-    span.innerHTML = `${article}`
+    span.innerHTML = `${title}`
+    summ.innerHTML = `${summary}.......`
     append(link, span);
-    append(imgli, img);
-    append(li, link);
-    append(ul, imgli);
-    append(ul, li);
+    append(ul, img);
+    append(ul, link);
+    append(ul, summ);
+    append(ul, space);
   })
 })
+//
 
-document.getElementById("headlines").addEventListener("click", function() {
-  myFunction();
+document.getElementById("image1").addEventListener("click", function() {
+  var storyElement = "news1";
+  myFunction(storyElement);
 }, false);
 
-document.getElementById("news").addEventListener("click", function() {
-  myFunction();
-}, false);
 
-function myFunction() {
-    var x = document.getElementById("headlines");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
+
+// function myFunction(storyElement) {
+//   var x = document.getElementById(storyElement);
+//   x.style.display = "none";
+//   // for (var i = 1; i < 11; i++) {
+//   //   var elementID = "news" + i;
+//   //   var y = document.getElementById(elementID);
+//   //   if (elementID !== storyElement) {
+//   //     y.style.display = "block";
+//   //
+//   //   }
+//   // }
+// }
+
+// var clickedElement = "";
+//
+// document.getElementById("image1").addEventListener("click", function() {
+//   var storyElement = "news1";
+//   myFunction(storyElement);
+// }, false);
+//
+// document.getElementById("image2").addEventListener("click", function() {
+//   var storyElement = "news2";
+//   myFunction(storyElement);
+// }, false);
+//
+// document.getElementById("image3").addEventListener("click", function() {
+//   var storyElement = "news3";
+//   myFunction(storyElement);
+// }, false);
+//
+// document.getElementById("image4").addEventListener("click", function() {
+//   var storyElement = "news4";
+//   myFunction(storyElement);
+// }, false);
+//
+// document.getElementById("image5").addEventListener("click", function() {
+//   var storyElement = "news5";
+//   myFunction(storyElement);
+// }, false);
+//
+// document.getElementById("image6").addEventListener("click", function() {
+//   var storyElement = "news6";
+//   myFunction(storyElement);
+// }, false);
+//
+// document.getElementById("image7").addEventListener("click", function() {
+//   var storyElement = "news7";
+//   myFunction(storyElement);
+// }, false);
+//
+// document.getElementById("image8").addEventListener("click", function() {
+//   var storyElement = "news8";
+//   myFunction(storyElement);
+// }, false);
+//
+// document.getElementById("image9").addEventListener("click", function() {
+//   var storyElement = "news9";
+//   myFunction(storyElement);
+// }, false);
+//
+// document.getElementById("image10").addEventListener("click", function() {
+//   var storyElement = "news10";
+//   myFunction(storyElement);
+// }, false);
+//
+// function myFunction(storyElement) {
+//   var x = document.getElementById(storyElement);
+//   x.style.display = "block";
+//   for (var i = 1; i < 11; i++) {
+//     var elementID = "news" + i;
+//     var y = document.getElementById(elementID);
+//     if (elementID !== storyElement) {
+//       y.style.display = "none";
+//
+//     }
+//   }
+// }
