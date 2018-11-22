35438a495c32ae7d1c193f02ada7939d50df9ca5
jebax <joel.eliot.baxter@gmail.com>
2018-11-22 21:10:35 +0000

    added Procfile

--- src/article.js
+++ src/article.js
@@ -1,10 +1,10 @@
 class Article {
   constructor (headline, image, url, id) {
     this.headline = headline
-    this.summary = null
     this.image = image
     this.url = url
     this.id = id
+    this.summary = null
   }
 }
 
