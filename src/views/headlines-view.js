a541f6aea422f2dca93c8db36f964b0629e938e8
jebax <joel.eliot.baxter@gmail.com>
2018-11-23 10:45:18 +0000

    added edge case for empty image fields

--- src/views/headlines-view.js
+++ src/views/headlines-view.js
@@ -11,6 +11,7 @@ class HeadlinesView {
   }
 
   formatImage (imageData) {
+    if (!imageData) { return imageData }
     var truncatedData = imageData.split(/src=/)[1].split(/ width/)[0]
     return `<figure><img class="headline-image" src=${truncatedData} /></figure>`
   }
