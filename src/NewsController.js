bc6d022eb5c6c2cdfe8dfb55c41cd9099a96242b
Asad Khan <asad_khan47@hotmail.co.uk>
2018-12-16 22:21:21 +0000

    Improved code layout and catching error response

--- src/NewsController.js
+++ src/NewsController.js
@@ -8,20 +8,23 @@ window.onload = function () {
 }
 
 function loadHeadlines() {
+
   event.preventDefault()
 
   fetch("http://content.guardianapis.com/politics/blog/2014/feb/17/alex-salmond-speech-first-minister-scottish-independence-eu-currency-live?show-fields=body&api-key=9ba73e9a-73b3-454e-b973-7c295c6942f5")
+
   .then(response => {
     return response.json();
   })
+
   .then(data => {
-  // Work with JSON data here
-  console.log(data);
-  let body = data.response.content.fields.body;
-  document.body.innerHTML = body;
+    console.log(data);
+    let body = data.response.content.fields.body;
+    document.body.innerHTML = body;
   })
+
   .catch(err => {
-  // Do something for an error here
-  console.log("🌈");
+    console.log(err);
   });
+
 }
