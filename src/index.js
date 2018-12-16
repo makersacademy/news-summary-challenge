774e871f7a43c9e8c0ee9c4cf83cab5af9427dc3
willmcb <williamjamesmcbrien@gmail.com>
2018-12-16 17:59:02 +0000

    Add initial index page and view logic

---
+++ src/index.js
@@ -0,0 +1,21 @@
+(function(){
+   var summaryText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed " +
+                           "do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+                           "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris " +
+                           " nisi ut aliquip ex ea commodo consequat.";
+    var guardianService = {
+          getStoriesData: function() {
+              return guardianData.response.results;
+          }
+   };
+
+   var summaryService = {
+       addSummaryToStory: function(story){
+           story.summary = summaryText;
+           return story;
+       }
+   };
+   var storiesFactory = new StoriesFactory(guardianService, summaryService);
+   var stories = storiesFactory.getStories();
+   var main = document.getElementById('main');
+})();
