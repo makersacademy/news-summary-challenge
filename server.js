1db40f381325e4c6b485c7f276409301d3580834
Chris Gilbert <chrisgilbert4@icloud.com>
2018-12-24 14:39:34 +0000

    files renamed to use -

--- server.js
+++
@@ -1,6 +0,0 @@
-// var httpServer = require("http-server");
-// var path = require("path");
-//
-// var pathToHtmlAndJsFiles = path.join(__dirname, "./");
-// var server = httpServer.createServer({ root: pathToHtmlAndJsFiles });
-// server.listen(3000);
---
+++ spec/spec-helper.js
@@ -0,0 +1,21 @@
+var tea = {
+
+  describe: function(desc, fn) {
+    console.log(desc)
+    fn()
+  },
+
+  it: function(desc, fn) {
+    this.describe("#" + desc, fn)
+  },
+
+  check: function(assertionToCheck) {
+    if (!assertionToCheck) {
+      throw new Error("Assertion failed: " + assertionToCheck + " is not truthy")
+    } else {
+      console.log('%c Test passed!', "color: green")
+      return true
+    }
+  },
+
+}
