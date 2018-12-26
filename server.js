fe58182bdcb1a02c8f222eff5253a03d7101be0d
Chris Gilbert <chrisgilbert4@icloud.com>
2018-12-26 15:05:49 +0000

    .env added to gitignore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var httpServer = require("http-server");
 var path = require("path");
+require('dotenv').config();
 
 var pathToHtmlAndJsFiles = path.join(__dirname, "");
 var server = httpServer.createServer({ root: pathToHtmlAndJsFiles });
