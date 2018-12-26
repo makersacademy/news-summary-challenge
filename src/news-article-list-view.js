d03f5bfc8bf240aac33624c19bba8ef2003da1fd
Chris Gilbert <chrisgilbert4@icloud.com>
2018-12-26 12:19:38 +0000

    tab spaces fixed

--- src/news-article-list-view.js
+++ src/news-article-list-view.js
@@ -5,16 +5,16 @@
   NewsArticleListView.prototype = {
     render: function(articles) {
       return [
-            Heading(),
-            "<div class='container card-columns text-center'>",
-              articles.map(function(article) {
-                return [
-                  "<div class='card mb-2' style='max-width: 30rem;'>",
+        Heading(),
+          "<div class='container card-columns text-center'>",
+            articles.map(function(article) {
+              return [
+                "<div class='card mb-2' style='max-width: 30rem;'>",
                   "<img class='card-img-top' src=" + article.thumbnail() + ">",
-                    "<div class='card-body'>",
-                      "<a class='btn btn-primary' href='#article/" + article.articleId + "'>" + article.headline() + "</a>",
-                    "</div>",
-                  "</div>"
+                  "<div class='card-body'>",
+                  "<a class='btn btn-primary' href='#article/" +    article.articleId + "'>" + article.headline() + "</a>",
+                  "</div>",
+                "</div>"
                 ].join("")
               }).join(""),
           "</div>"
@@ -24,6 +24,3 @@
 
   exports.NewsArticleListView = NewsArticleListView;
 })(this);
-
-//
-// "<a href='#note/" + note.id + "''>"
