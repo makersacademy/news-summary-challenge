395a17dd725c20b71e7127e1b260a87029fe1840
Shaneil Clarke <shaneilske@yahoo.com>
2019-01-26 22:49:04 +0000

    API requesting news headlines and can display on index page

--- interface.js
+++ interface.js
@@ -2,6 +2,8 @@ window.onload = () => {
   let news = new News
   news.getHeadlines()
   let articles =  news.displayHeadlines()
+  let h1 = document.getElementById('page_title')
+  h1.innerText = "News Headlines Page"
   let div = document.getElementById('headlines')
   div.appendChild(articles)
 };
