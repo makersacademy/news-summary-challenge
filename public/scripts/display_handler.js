8ba8b113c80e0143460486a0421b31f6abff1727
Huxley_Millard <gerogiamillardccm@gmail.com>
2019-01-27 20:50:44 +0000

    pleasently suprised it didnt break

--- public/scripts/display_handler.js
+++ public/scripts/display_handler.js
@@ -1,3 +1,5 @@
+
+
 var base_display_handler = function(handler){
 
 
@@ -35,32 +37,40 @@ var base_display_handler = function(handler){
     }
 
 
-
-
     function make_image(img_src){
         let div = document.createElement('div');
         div.style.backgroundImage = `url(${img_src})`;
         div.style.height = '20vh';
         return div;
     }
-}
+
+    return {
+        convert_all_to_thumb : convert_all_to_thumb,
+        make_image : make_image,
+        make_article_element: make_article_element,
+        make_link : make_link,
+        make_text_element: make_text_element
+    }
+}(guardian_display_handler)
 
 
 
 
 
 
-var display_handler = (function(news_source_articles, target_node){
+var guardian_display_handler = (function(news_source_articles, target_node){
+
+    const base = base_display_handler;
 
     function create_thumbnail(obj){
         let div_node = document.createElement('div');
         let elements = [
-                        make_image(obj.fields.thumbnail),
-                        make_text_element('h3',obj.webTitle),
-                        make_text_element('p', obj.sectionName),
-                        make_text_element('p', obj.webPublicationDate),
-                        make_article_element('article', obj.id),
-                        make_link( obj.webUrl, obj.id)
+                        base.make_image(obj.fields.thumbnail),
+                        base.make_text_element('h3',obj.webTitle),
+                        base.make_text_element('p', obj.sectionName),
+                        base.make_text_element('p', obj.webPublicationDate),
+                        base.make_article_element('article', obj.id),
+                        base.make_link( obj.webUrl, obj.id)
                     ]
         for(let element of elements){
            div_node.appendChild(element);
@@ -77,40 +87,6 @@ var display_handler = (function(news_source_articles, target_node){
         }
     };
     
-    function make_text_element(element, string){
-        let _ = document.createElement(element);
-         _.innerText = string;
-        return _;
-    };
-
-    function make_article_element(element, string){
-        let _ = document.createElement(element);
-        _.innerText = 'hover to see more ....';
-        _.setAttribute('name', string);
-        return _;
-    };
-
-
-
-    function make_link(url, id){
-        var a = document.createElement('a');
-        var linkText = document.createTextNode("visit site");
-        a.appendChild(linkText);
-        a.title = `${id}`;
-        a.href = `${url}`;
-        return a;
-    }
-
-
-
-
-    function make_image(img_src){
-        let div = document.createElement('div');
-        div.style.backgroundImage = `url(${img_src})`;
-        div.style.height = '20vh';
-        return div;
-    }
-
     return {
         create_thumbnail:create_thumbnail,
         convert_all_to_thumb : convert_all_to_thumb
