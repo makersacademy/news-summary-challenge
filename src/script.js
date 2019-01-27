50625ee05ff5b328b05402a84ff55a43aec7b6dc
celine kaslin <celine.kaslin@gmail.com>
2019-01-27 19:01:13 +0000

    partial solution, no article display on the page, error about the key, API call not working

---
+++ src/script.js
@@ -0,0 +1,15 @@
+window.onload = function (){
+
+  var request = new XMLHttpRequest();
+  request.open('GET', 'https://content.guardianapis.com/search?api-key=e5ddc65f-2c51-481f-a8d6-8c9304572e0b&&from-date=2019-01-27', true)
+  request.onload = function (){
+    var results = JSON.parse(this.response.results);
+    results.forEach(function(result){
+      var article = document.createTextNode(result.webTitle);
+      var newItem = document.createElement('li');
+      newItem.appendChild(article);
+      document.getElementById('headlinenews').appendChild(newItem);
+    });
+  };
+  request.send();
+};
