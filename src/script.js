ff4ca4c04cd02f1968d85948ae6bebb35ca7b331
celine kaslin <celine.kaslin@gmail.com>
2019-01-29 09:11:28 +0000

    news displayed

--- src/script.js
+++ src/script.js
@@ -1,9 +1,13 @@
 window.onload = function (){
 
   var request = new XMLHttpRequest();
+
   request.open('GET', 'https://content.guardianapis.com/search?api-key=e5ddc65f-2c51-481f-a8d6-8c9304572e0b&&from-date=2019-01-27', true)
+
   request.onload = function (){
-    var results = JSON.parse(this.response.results);
+    var response = JSON.parse(this.response);
+    window.resp = response;
+    var results = response.response.results
     results.forEach(function(result){
       var article = document.createTextNode(result.webTitle);
       var newItem = document.createElement('li');
