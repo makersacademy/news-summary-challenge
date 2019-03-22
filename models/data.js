4b7ea59d232fadd8662129da862bc051e055414a
matt <mwhitaker1006@gmail.com>
2019-03-22 18:11:48 +0000

    spiked out first version

--- models/data.js
+++ models/data.js
@@ -1,42 +1,21 @@
 function GetAPIData() {
-  this.newsData = {};
-  // var self = this
+  // this.newsData = {};
+  var self = this
 }
 
-
-
-
-// GetAPIData.makeRequest(returnResponse)
-
 GetAPIData.prototype.makeRequest = function(callback) {
-
-  var self = this
-
   // new API request
   var request = new XMLHttpRequest()
   // Open new connection using the GET request on URL endpoint
-  request.open('Get', "https://content.guardianapis.com/search?api-key="+apiKey, true)
+  request.open('Get', "https://content.guardianapis.com/search?api-key="+apiKey+"&show-fields=starRating,body,trailText,headline,thumbnail", true)
 
   request.onload = function() {
-    var newsData = JSON.parse(request.response)
-    console.log(newsData)
-
     if (request.status >= 200 && request.status < 400) {
-      // this.newsData = newsData
-      console.log(newsData);
+      var newsData = JSON.parse(request.response)
       callback(newsData)
-      return newsData
-      // self.newsData = this.newsData
-
     } else {
       console.log('error')
     }
-    // console.log(this.newsData);
-    // return this.newsData
-    self.newsData = newsData
-
   }
-
-
   request.send()
 }
--- news.js
+++ news.js
@@ -1,11 +1,45 @@
- // need to insert callback to controller
-function insertNewsIntoPage(news) {
-  console.log(news)
-  return news
-  //  make
-  // some code to insert news into page
+function NewsController() {
+  this.apiCall = new GetAPIData()
+  this.newsData;
+  // need to insert callback to controller
+  this.insertNewsIntoPage = function(newsJSON) {
+    // return news
+    this.newsData = newsJSON
+  }
 }
 
-var newAPICall = new GetAPIData()
-var thisnewsData = newAPICall.makeRequest(insertNewsIntoPage)
-console.log(thisnewsData);
+NewsController.prototype.refreshNewsData = function() {
+  this.apiCall.makeRequest(this.insertNewsIntoPage.bind(this))
+}
+
+NewsController.prototype.showNewsSummary = function() {
+  console.log(this.newsData.response.results)
+  // var headlineString = ""
+  // headlineString += "<div><ul>"
+  // for (i=0; i<this.newsData.response.results.length; i++) {
+  //   headlineString += "<li>" + "<div id=" + i + ">"
+  //   // headlineString += "<a href='" + this.newsData.response.results[i].webUrl + "'>"
+  //   headlineString += this.newsData.response.results[i].webTitle
+  //   // headlineString += "</a>"
+  //   headlineString += "</div></li>"
+  // }
+  // headlineString += "</ul></div>"
+  // return headlineString
+  for (i=0; i<this.newsData.response.results.length; i++) {
+    document.getElementById(i).innerHTML = this.newsData.response.results[i].webTitle + "<br><img src=" + this.newsData.response.results[i].fields.thumbnail + ">"
+  }
+}
+
+NewsController.prototype.updateSummaryPage = function() {
+  document.getElementById("summary").innerHTML = this.showNewsSummary();
+}
+
+NewsController.prototype.articleView = function(artileId) {
+  document.getElementById("header").innerHTML = this.newsData.response.results[artileId].webTitle + "<br><img src=" + this.newsData.response.results[artileId].fields.thumbnail + ">";
+  document.getElementById("summary").innerHTML = this.newsData.response.results[artileId].fields.body;
+}
+
+// document.getElementById(0).addEventListener("click", function() {
+//   document.getElementById("header").innerHTML = this.newsData.response.results[0].webTitle;
+//   document.getElementById("summary").innerHTML = this.newsData.response.results[0].fields.body;
+// });
