6f8c9c9bf9b8cf5af0c2dd73f56f1c90612db4b9
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-03-24 18:59:51 +0000

    pass test for headlines are in links that cause a hashchange

--- tests/newsControllerTest.js
+++ tests/newsControllerTest.js
@@ -5,7 +5,9 @@ var NewsArticleDouble = function(title){
 var NewsArticlesListDouble = function() {
     var articles = []
     newsDouble1 = new NewsArticleDouble(exampleData.response.results[0].webTitle)
+    newsDouble1.id = 0
     newsDouble2 = new NewsArticleDouble(exampleData.response.results[1].webTitle)
+    newsDouble2.id = 1
     articles.push(newsDouble1)
     articles.push(newsDouble2)
     this.articles = articles
@@ -21,11 +23,13 @@ describe('NewsController', function(){
         controller = new NewsController(articlesList)
         controller.renderPage()
         html = "<p>" + 
+            "<a href=\"#news/0\">" +
             "<img src=\"https://pbs.twimg.com/profile_images/1061915596328263680/EcBjYl5z_400x400.jpg\" style=\"width:48px;height:48px;\">" +
-            "Gibraltar v Republic of Ireland: Euro 2020 qualifier – live!</p>" +
+            "Gibraltar v Republic of Ireland: Euro 2020 qualifier – live!</a></p>" +
             "<p>" + 
+            "<a href=\"#news/1\">" +
             "<img src=\"https://pbs.twimg.com/profile_images/1061915596328263680/EcBjYl5z_400x400.jpg\" style=\"width:48px;height:48px;\">" +
-            "Brexit march: '1 million' rally for people's vote - live updates</p>"
+            "Brexit march: '1 million' rally for people's vote - live updates</a></p>"
         assert.isEqual(document.getElementById("app").innerHTML, html)
     })
 })
--- views/headlinesView.js
+++ views/headlinesView.js
@@ -5,8 +5,9 @@
     HeadlinesView.prototype.getHTML = function () {
         return this.articles.map(function(article) {
             return "<p>" + 
+            "<a href=\"#news/" + article.id + "\">" +
               "<img src=\"" + article.image + "\" style=\"width:48px;height:48px;\">" +
-              article.title + "</p>"
+              article.title + "</a></p>"
         }).join("")
     }
     exports.HeadlinesView = HeadlinesView
