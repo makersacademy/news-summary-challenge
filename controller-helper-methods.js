1c36b914ed328803644b090ed4c43e42226f4531
matt <mwhitaker1006@gmail.com>
2019-03-24 15:15:24 +0000

    updated controller - includes brexit reaction if brexit mentioned in article

--- controller-helper-methods.js
+++ controller-helper-methods.js
@@ -20,5 +20,8 @@ getArticleHeader = function(news,articleId) {
 
 getArticleSummary = function(news,articleId) {
   string = news[articleId].fields.trailText + "<br><br>" + news[articleId].fields.body.substring(0,news[articleId].fields.body.indexOf("</p>")) + "...<br><br>See the full article on <a href='" + news[articleId].webUrl + "'>The Guardian</a>";
+  if (string.includes("Brexit")) {
+    string += "<br><br>brexit, eh... <br><iframe src='https://giphy.com/embed/q4ICE9wYvOwBG' width='448' height='480' frameBorder='0' class='giphy-embed' allowFullScreen></iframe><p><a href='https://giphy.com/gifs/costume-cat-q4ICE9wYvOwBG'>(gif via GIPHY)</a></p>"
+  }
   return string
 }
