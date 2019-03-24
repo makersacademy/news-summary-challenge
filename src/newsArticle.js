db70a765cf507d1d4ed063aa6c3a71d06357710d
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-03-24 13:19:12 +0000

    passing test for NewsArticle has picture and summary (with stubbed api call)

--- src/newsArticle.js
+++ src/newsArticle.js
@@ -1,7 +1,27 @@
 (function(exports){
-    function NewsArticle(title, url) {
+    function NewsArticle(title, newsUrl) {
         this.title = title
-        this.url = url
+        this.newsUrl = newsUrl
+        this.summary = ""
+        this.image = ""
+        this.getArticleDetails()
+    }
+    NewsArticle.prototype.getArticleDetails = function (){
+        var self = this
+        var url = "http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=" 
+                    + "https://api.aylien.com/api/v1/extract?url=" 
+                    + self.newsUrl 
+        fetch(url) 
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) { 
+            self.summary = data.article
+            self.image = data.image
+        })
+        .catch(function(error) {
+            console.log(error)
+        });
     }
     exports.NewsArticle = NewsArticle
 })(this)
