5c9d746a1a4039e5f223962b620ebdac53e04bc3
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-03-24 18:36:36 +0000

    pass test for NewsArticleList gives ids to NewsArticles....except that the logic is in the method that stubs the api call!  But I've used the same logic in the code

--- src/newsArticleList.js
+++ src/newsArticleList.js
@@ -2,6 +2,7 @@
     function NewsArticleList() {
         this.articles = []
         this.getData()
+        this.id = 0
     }
     NewsArticleList.prototype.getData = function() {
         var self = this
@@ -14,6 +15,8 @@
         .then(function(data) {
             data.response.results.forEach(function(newsItem) {
                 var newsArticle = new NewsArticle(newsItem.webTitle, newsItem.webUrl)
+                newsArticle.id = self.id
+                self.id += 1
                 setTimeout(function() {
                     self.articles.push(newsArticle)
                 }, 1000)
