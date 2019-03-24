a41d94ca4fac82613b4b98a47659e7fdbff7b9f5
matt <mwhitaker1006@gmail.com>
2019-03-24 14:34:16 +0000

    added redirects to the pages without reloading from API

--- news.js
+++ news.js
@@ -6,42 +6,47 @@ function NewsController() {
     // return news
     this.newsData = newsJSON
     this.showNewsSummary()
+    console.log(this.newsData);
   }
 }
 
 NewsController.prototype.refreshNewsData = function() {
   this.apiCall.makeRequest(this.insertNewsIntoPage.bind(this))
-
 }
 
 NewsController.prototype.showNewsSummary = function() {
-  console.log(this.newsData.response.results)
-  // var headlineString = ""
-  // headlineString += "<div><ul>"
-  // for (i=0; i<this.newsData.response.results.length; i++) {
-  //   headlineString += "<li>" + "<div id=" + i + ">"
-  //   // headlineString += "<a href='" + this.newsData.response.results[i].webUrl + "'>"
-  //   headlineString += this.newsData.response.results[i].webTitle
-  //   // headlineString += "</a>"
-  //   headlineString += "</div></li>"
-  // }
-  // headlineString += "</ul></div>"
-  // return headlineString
+  document.getElementById("home-button").innerHTML = "<a href='#home'>Home</a>"
+  document.getElementById("header").innerHTML = "Today's headlines: "
+  document.getElementById("summary").innerHTML = "<ul><hr><div id=0 class='container'></div><hr><div id=1 class='container'></div><hr><div id=2 class='container'></div><hr><div id=3 class='container'></div><hr><div id=4 class='container'></div><hr><div id=5 class='container'></div><hr><div id=6 class='container'></div><hr><div id=7 class='container'></div><hr><div id=8 class='container'></div><hr><div id=9 class='container'></div><hr></ul>"
   for (i=0; i<this.newsData.response.results.length; i++) {
-    document.getElementById(i).innerHTML = this.newsData.response.results[i].webTitle + "<br><img src=" + this.newsData.response.results[i].fields.thumbnail + ">"
+    document.getElementById(i).innerHTML = "<a href='#" + i + "'>" + this.newsData.response.results[i].webTitle + "</a><br><img src=" + this.newsData.response.results[i].fields.thumbnail + ">"
   }
 }
 
-NewsController.prototype.updateSummaryPage = function() {
-  document.getElementById("summary").innerHTML = this.showNewsSummary();
-}
-
 NewsController.prototype.articleView = function(artileId) {
+  document.getElementById("home-button").innerHTML = "<a href='#home'>< Back to headlines</a>"
   document.getElementById("header").innerHTML = this.newsData.response.results[artileId].webTitle + "<br><img src=" + this.newsData.response.results[artileId].fields.thumbnail + ">";
-  document.getElementById("summary").innerHTML = this.newsData.response.results[artileId].fields.body;
+   previewString = this.newsData.response.results[artileId].fields.trailText + "<br><br>" + this.newsData.response.results[artileId].fields.body.substring(0,this.newsData.response.results[artileId].fields.body.indexOf("</p>")) + "...<br><br>See the full article on <a href='" + this.newsData.response.results[artileId].webUrl + "'>The Guardian</a>";
+   document.getElementById("summary").innerHTML = previewString
 }
 
-// document.getElementById(0).addEventListener("click", function() {
-//   document.getElementById("header").innerHTML = this.newsData.response.results[0].webTitle;
-//   document.getElementById("summary").innerHTML = this.newsData.response.results[0].fields.body;
-// });
+NewsController.prototype.pageAddEventListener = function() {
+  window.addEventListener("hashchange", this.changePagePath.bind(this))
+}
+
+NewsController.prototype.showPage = function(article) {
+  if (article === "home") {
+    this.showNewsSummary()
+  } else {
+    this.articleView(article)
+    window.scrollTo(0, 0);
+  }
+}
+
+NewsController.prototype.changePagePath = function() {
+  this.showPage(getPathFromURL(window.location));
+}
+
+getPathFromURL = function(location) {
+  return location.hash.split("#")[1];
+}
