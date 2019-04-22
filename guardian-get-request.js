6183d3f4e5e960e0789c5253ace0e22b38ab2552
Wardah <wlt_93@hotmail.com>
2019-04-22 12:16:38 +0100

    displays all news headlines on web page

--- guardian-get-request.js
+++ guardian-get-request.js
@@ -1,3 +1,10 @@
+const app = document.getElementById('root')
+
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
+ 
+app.appendChild(container) 
+
 var request = new XMLHttpRequest()
 
 request.open('GET', 'https://content.guardianapis.com/search?api-key=c7698374-44e2-4f01-bbca-a4f789a9cd9a', true)
@@ -9,13 +16,22 @@ request.onload = function () {
 
   if (request.status >= 200 && request.status < 400) {
     data.forEach(article => {
+    
+      const card = document.createElement('div')
+      card.setAttribute('class', 'card')
+    
+      const h1 = document.createElement('h1')
+      h1.textContent = article.webTitle
+      // const p = document.createElement('p')
+      // p.textContent = article.webURL
 
-    console.log(article.webTitle)
-  })
+      container.appendChild(card)
+
+      card.appendChild(h1)
+      // card.appendChild(p)
+    })
 } else {
   console.log('error')
+  }
 }
-
-}
-
 request.send()
