be7705e81975cb514e26f914de0af4166d55887c
Wardah <wlt_93@hotmail.com>
2019-04-22 11:33:59 +0100

    successfully gets data on guardian get request

---
+++ guardian-get-request.js
@@ -0,0 +1,19 @@
+var request = new XMLHttpRequest()
+
+request.open('GET', 'https://content.guardianapis.com/search?api-key=c7698374-44e2-4f01-bbca-a4f789a9cd9a', true)
+
+request.onload = function () {
+  var news = JSON.parse(this.response)
+
+  var data = news.response.results
+
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(article => {
+
+    console.log(article.webTitle)
+  })
+} 
+
+}
+
+request.send()
