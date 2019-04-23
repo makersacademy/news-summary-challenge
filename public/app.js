641e30a26bda3232135103e01e8e8892eed5d283
Matt Thompson <matt.thompson@johnlewis.co.uk>
2019-04-23 20:49:10 +0100

    Updates app.js with apiKey env variable

--- public/app.js
+++ public/app.js
@@ -13,8 +13,7 @@
 
 var request = new XMLHttpRequest()
 
-// var apiKey = "Replace with api key in .env"
-var apiKey = "91fa1184-3660-4b00-9e8f-e88868d8d66d"
+var apiKey = "Replace with api key in .env"
 request.open('GET', `https://content.guardianapis.com/search?q=cycling&api-key=${apiKey}`, true)
 
 request.onload = function() {
