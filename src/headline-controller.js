862d41e8e8e6c4f2e39570d3689a121687436a99
guilhe0756 <guiheurich@gmail.com>
2019-05-19 13:22:29 +0100

    Added background color and centered text

--- src/headline-controller.js
+++ src/headline-controller.js
@@ -13,6 +13,18 @@
   exports.HeadlineController = HeadlineController;
 })(this);
 
+// var request = new XMLHttpRequest();
+//
+// // Open a new connection, using the GET request on the URL endpoint
+// request.open("GET", "https://ghibliapi.herokuapp.com/films", true);
+//
+// request.onload = function() {
+//   // Begin accessing JSON data here
+// };
+//
+// // Send request
+// request.send();
+
 window.onload = function() {
   var headlineList = new HeadlineList();
   headlineList.add("World says Hello");
