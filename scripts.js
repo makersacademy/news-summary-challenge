f2750f7c8c0bc306698c12004f31ad9648e2198d
Jordan Roberts <jordanroberts@talk21.com>
2019-05-19 09:12:26 +0100

    News articles all display on page with CSS
    This was spiked using a tutorial and now need to write tests and make sure that API key is hidden and mocked

--- scripts.js
+++ scripts.js
@@ -1,8 +1,15 @@
+const app = document.getElementById('root')
+
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
+
+app.appendChild(container)
+
 // Create a request variable and assign a new XMLHttpRequest object to it:
 var request = new XMLHttpRequest()
 
 // Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'api', true)
+request.open('GET', 'api key hidden', true)
 
 request.onload = function() {
   // Begin accessing JSON data here
@@ -11,17 +18,40 @@ request.onload = function() {
 
   if(request.status >= 200 && request.status < 400) {
     data.response.results.forEach(article => {
-      console.log(article.sectionName)
-      console.log(article.webTitle)
-      console.log(article.webURL)
+      // create a div with a card class
+      const card = document.createElement('div')
+      card.setAttribute('class', 'card')
+
+      // create an h1 and set the text content to the article's title
+      const h1 = document.createElement('h1')
+      h1.textContent = article.sectionName
+
+      const h2 = document.createElement('h2')
+      h2.textContent = article.webTitle
+
+      // create a p and set the text content to the article URL
+      const p = document.createElement('p')
+      p.textContent = article.webURL
+
+      container.appendChild(card)
+
+      card.appendChild(h1)
+      card.appendChild(h2)
+      card.appendChild(p)
+
+
+      // console.log(article.sectionName)
+      // console.log(article.webTitle)
+      // console.log(article.webURL)
     })
   } else {
-    console.log('error')
+    const errorMessage = document.createElement('marquee')
+    errorMessage.textContent = `Sorry, not working`
+    app.appendChild(errorMessage)
+    // console.log('error')
   }
 }
 
 // Send request
 
 request.send()
-
-const app = document.getElementById('root')
