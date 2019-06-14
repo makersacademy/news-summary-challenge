e4b7ea429e225f19c730657951b7f0f8afd5f529
Student <magnusarbuthnott@gmail.com>
2019-06-14 23:29:31 +0100

    show the news main text body and back button

--- src/guardianAPI.js
+++ src/guardianAPI.js
@@ -1,24 +1,61 @@
 
 var guardianRequest = new XMLHttpRequest();
-guardianRequest.addEventListener("load", guardianListener);
-guardianRequest.open("GET", "https://content.guardianapis.com/search?q=content&show-fields=thumbnail&api-key=" + MY_API_KEY)
+guardianRequest.addEventListener("load", renderHeadlineListView);
+guardianRequest.open("GET", "https://content.guardianapis.com/search?q=content&show-fields=thumbnail,bodyText&api-key=" + MY_API_KEY)
 guardianRequest.send();
 
-function guardianListener() {
-  var story = JSON.parse(this.responseText).response.results;
-  for (var i = 0; i < story.length; i++) {
 
+function renderHeadlineListView() {
+
+  var stories = JSON.parse(guardianRequest.responseText).response.results;
+
+  document.getElementsByTagName("h1").innerHTML = "Makers Gazette";
+  document.getElementById("body-text").innerHTML = "";
+  document.getElementById("back-button").style.display = "none"
+
+  for (var i = 0; i < stories.length; i++) {
     document.getElementById('news-container').innerHTML +=
-
-      "<a href=" + story[i].webUrl + "><p>"
-      + story[i].webTitle + 
-      "</p><img src=" + story[i].fields.thumbnail + "><br>";
+      "<a href=#" + stories[i].id + "><p>"
+      + stories[i].webTitle + 
+      "</p><img src=" + stories[i].fields.thumbnail + "><br>";
   }
+};
+
+listenForClick();
+
+function listenForClick() {
+  window.addEventListener("hashchange", function() {
+    if (window.location.href.includes('#home')) {
+      renderHeadlineListView();
+    }
+    else {
+      renderSummaryView();
+    }
+  });
+}
+
+function renderSummaryView() {
+
+  var stories = JSON.parse(guardianRequest.responseText).response.results;
+  var storyId = window.location.hash.split("#")[1];
+  var story = findObjectByKey(stories, "id", storyId);
+
+  document.getElementsByTagName("h1").innerHTML = story.webTitle;
+  document.getElementById("news-container").innerHTML = "";
+  document.getElementById("body-text").innerHTML = story.fields.bodyText;
+  document.getElementById("back-button").style.display = "block"
+
+  function findObjectByKey(array, key, value) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i][key] === value) {
+            return array[i];
+        };
+    };
+  };
 };
 
 
 // var guardianRequest = new XMLHttpRequest();
 // guardianRequest.open("GET", "https://content.guardianapis.com/search?q=politics&show-fields=bodyText,thumbnail&api-key=" + MY_API_KEY)
 // guardianRequest.send();
-// var json = JSON.parse(guardianRequest.responseText);
-// var story = json.response.results
+// var json = JSON.parse(guardianRequest.responseText).response.results;
