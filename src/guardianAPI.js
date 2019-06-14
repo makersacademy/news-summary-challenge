e46f272f5fb5d287d9c7eba200330ef5b32cc4b0
Student <magnusarbuthnott@gmail.com>
2019-06-14 19:35:26 +0100

    add href to news titles

--- src/guardianAPI.js
+++ src/guardianAPI.js
@@ -7,6 +7,15 @@ guardianRequest.send();
 function guardianListener() {
   var json = JSON.parse(this.responseText);
   for (var i = 0; i < json.response.results.length; i++) {
-    document.getElementById('news-container').innerHTML += "<div><li>" + json.response.results[i].webTitle + "</li></div><br>";
+    document.getElementById('news-container').innerHTML += 
+      "<a href="+ json.response.results[i].webUrl + "><li>"
+      + json.response.results[i].webTitle+ "</li><br>";
   }
 };
+
+
+// var guardianRequest = new XMLHttpRequest();
+// guardianRequest.open("GET", "https://content.guardianapis.com/search?q=politics&api-key=" + MY_API_KEY)
+// guardianRequest.send();
+// var json = JSON.parse(guardianRequest.responseText);
+// json.response.results
