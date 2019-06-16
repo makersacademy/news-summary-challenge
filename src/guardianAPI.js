10b8ac1c1861251bdf3c5d852d751a41ba294863
Student <magnusarbuthnott@gmail.com>
2019-06-16 14:11:18 +0100

    Added style.css

--- src/guardianAPI.js
+++ src/guardianAPI.js
@@ -3,25 +3,25 @@ var guardianRequest = new XMLHttpRequest();
 guardianRequest.addEventListener("load", renderHeadlineListView);
 guardianRequest.open("GET", "https://content.guardianapis.com/search?q=content&show-fields=thumbnail,bodyText&api-key=" + MY_API_KEY)
 guardianRequest.send();
+listenForClick();
 
 
 function renderHeadlineListView() {
 
   var stories = JSON.parse(guardianRequest.responseText).response.results;
 
-  document.getElementsByTagName("h1").innerHTML = "Makers Gazette";
-  document.getElementById("body-text").innerHTML = "";
-  document.getElementById("back-button").style.display = "none"
+  document.getElementById("list-news").style.display = "block"
+  document.getElementById("summary-body-text").innerHTML = "";
+  document.getElementById("summary-back-button").style.display = "none"
+  document.getElementById("summary-header").innerHTML = "";
+  document.getElementById("summary-image").setAttribute("src", "")
 
   for (var i = 0; i < stories.length; i++) {
-    document.getElementById('news-container').innerHTML +=
-      "<a href=#" + stories[i].id + "><p>"
-      + stories[i].webTitle + 
-      "</p><img src=" + stories[i].fields.thumbnail + "><br>";
+    document.getElementById('list-news').innerHTML +=
+      "<div id='list-item'><a href=#" + stories[i].id + "><img class='image' id='image' src=" + stories[i].fields.thumbnail
+      + "><h4 id='list-header'>" + stories[i].webTitle + "</h4>";
   }
 };
-
-listenForClick();
 
 function listenForClick() {
   window.addEventListener("hashchange", function() {
@@ -31,8 +31,8 @@ function listenForClick() {
     else {
       renderSummaryView();
     }
-  });
-}
+  })
+};
 
 function renderSummaryView() {
 
@@ -40,10 +40,12 @@ function renderSummaryView() {
   var storyId = window.location.hash.split("#")[1];
   var story = findObjectByKey(stories, "id", storyId);
 
-  document.getElementsByTagName("h1").innerHTML = story.webTitle;
-  document.getElementById("news-container").innerHTML = "";
-  document.getElementById("body-text").innerHTML = story.fields.bodyText;
-  document.getElementById("back-button").style.display = "block"
+  document.getElementById("list-news").innerHTML = "";
+  document.getElementById("list-news").style.display = "none"
+  document.getElementById("summary-header").innerHTML = story.webTitle;
+  document.getElementById("summary-image").setAttribute("src", story.fields.thumbnail)
+  document.getElementById("summary-body-text").innerHTML = story.fields.bodyText;
+  document.getElementById("summary-back-button").style.display = "block"
 
   function findObjectByKey(array, key, value) {
     for (var i = 0; i < array.length; i++) {
