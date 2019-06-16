18d71d3fe569b68f6524ada5145e7bfabd3f17ab
VladC <vlad.chiriac24@gmail.com>
2019-06-16 21:05:51 +0100

    Update index.html

--- src/article-list-module.js
+++ src/article-list-module.js
@@ -13,6 +13,28 @@
     this.articles.push(article);
   }
 
+  // ArticleList.prototype.fetchArticles = function(callback) {
+  //   var newsRequest = new XMLHttpRequest();
+  //   var self = this;
+
+  //   newsRequest.open("GET", "https://content.guardianapis.com/search?api-key=80f2e23b-35ed-4933-8ce6-3330e67b642f");
+  //   newsRequest.onload = function() {
+  //     if (newsRequest.status >= 200 && newsRequest.status < 400) {
+  //       var data = JSON.parse(newsRequest.responseText)
+  //       data.response.results.forEach(function(result) {
+  //         self.createArticle(new Article(result.title, result.url));
+  //       });
+  //       if (callback) {
+  //         callback();
+  //       }
+  //     } else {
+  //       alert("ERROR!")
+  //     }
+  //   };
+  //   newsRequest.send();
+  // };
+
+
   exports.ArticleList = ArticleList;
 
 })(this);
