02a99fdae5ab69ee2c035bcb18e0918923deffe1
Sam <samkitchen94@gmail.com>
2019-06-16 17:59:21 +0100

    refactored again and partially working

--- src/article-list-view.js
+++ src/article-list-view.js
@@ -4,13 +4,12 @@
   }
 
   ArticleListView.prototype.createHTML = function() {
-      var div = "<div>"
-      this.articleList.getText().forEach(function(article) {
-          div += `<a href="${article.getURL()}"><img src="${article.getThumbnail()}"></a><h4><a href="${article.getURL()}">${article.getTitle()}</a></h4>`;
-        });
-      return div + "</div>"
-    }
-  };
+    var div = "<div>"
+    this.articleList.getText().forEach(function(article) {
+      div += `<a href="${article.getURL()}"><img src="${article.getThumbnail()}"></a><h4><a href="${article.getURL()}">${article.getTitle()}</a></h4>`;
+    });
+    return div + "</div>"
+  }
 
   exports.ArticleListView = ArticleListView
 })(this);
