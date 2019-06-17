cec47bc3691d4445101e04e9549d16db6be2527e
Sam <samkitchen94@gmail.com>
2019-06-17 09:01:27 +0100

    refactoored

--- controller.js
+++ controller.js
@@ -1,15 +1,15 @@
 window.onload = function() {
-  var articleList = new ArticleList();
+  var list = new ArticleList();
   var request = new XMLHttpRequest();
   request.open('GET', `https://content.guardianapis.com/search?api-key=${apiKey}&show-fields=thumbnail`, true);
   request.onload = function () {
     var data = JSON.parse(this.response);
     var articlesLength = data.response.results.length
     for (var i = 0; i < articlesLength; i++) {
-      articleList.add({title: data.response.results[i].webTitle, url: data.response.results[i].webUrl, thumbnail: data.response.results[i].thumbnail})
+      list.add({title: data.response.results[i].webTitle, url: data.response.results[i].webUrl, thumbnail: data.response.results[i].thumbnail})
     }
-    var articleListView = new ArticleListView(articleList)
-    document.getElementById('app').innerHTML = articleListView.createHTML()
+    var view = new ArticleListView(list)
+    document.getElementById('app').innerHTML = view.createHTML()
   };
 
   request.send();
