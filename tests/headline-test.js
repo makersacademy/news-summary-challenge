3ff0a335da150dd9f291e90e7c91dc51a0b804a9
Ruth Baker <ruthmoog@gmail.com>
2019-07-14 11:38:30 +0100

    Add README and first test

---
+++ tests/headline-test.js
@@ -0,0 +1,16 @@
+(function() {
+  function testHeadlineHasText() {
+    var string = "Flowers Good For Bees, Reports Say"
+    var headline = new Headline(string)
+
+    var expected = string
+    var got = headline.text
+
+    if (expected !== got) {
+      throw new Error(`Expected ${expected}, got ${got}`)
+    } else {
+      console.log("passed: testHeadlineHasText")
+    }
+  }
+  testHeadlineHasText()
+})(this);
