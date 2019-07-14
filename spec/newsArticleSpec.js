d1ca116a889f4d31426d7ec2e29b757c088636d0
Danny <mtme13@hotmail.com>
2019-07-14 16:14:44 +0100

    Define NewsArticle model

---
+++ spec/newsArticleSpec.js
@@ -0,0 +1,13 @@
+describe("NewsArticle", function() {
+  it("stores headline, url, imageUrl, id", function() {
+    let headline = "Novak Djokovic v Roger Federer – live!";
+    let imageUrl = "http://www.example.com/example.jpg";
+    let url = "http://www.example.com";
+    let id = 0
+    let article = new NewsArticle(headline, url, imageUrl, id);
+    expect(article.headline).toEqual(headline);
+    expect(article.imageUrl).toEqual(imageUrl);
+    expect(article.url).toEqual(url);
+    expect(article.id).toEqual(0);
+  })
+})
---
+++ src/newsArticle.js
@@ -0,0 +1,10 @@
+(function(exports) {
+  function NewsArticle(headline, url, imageUrl, id) {
+    this.headline = headline;
+    this.url = url;
+    this.imageUrl = imageUrl;
+    this.id = id;   
+  }
+
+  exports.NewsArticle = NewsArticle;
+})(this);
