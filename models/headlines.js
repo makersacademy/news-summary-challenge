704a3a1bd9b19c3bcb6b43b25237bc1957c3bd21
Ben Scott <bengscott2@gmail.com>
2019-07-14 09:47:13 +0100

    add storeHeadlines function to Headlines

--- models/headlines.js
+++ models/headlines.js
@@ -1,8 +1,29 @@
 (function(exports) {
-  function Headlines (article = Article, headlines) {
-    var article = article
-    var headlines = headlines
-    var articles = []
+  function Headlines (headline = Headline, callReturn) {
+    var headline = headline
+    var callReturn = callReturn
+    var headlines = []
+
+    function getHeadLines() {
+      return headlines
+    }
+
+    function storeHeadlines() {
+      callReturn.forEach(function(entry) {
+        _addHeadline(entry)
+      })
+    }
+
+    function _addHeadline(entry) {
+      var newHeadline = new Headline(entry.headline, entry.shortUrl, entry.thumbnail)
+      headlines.push(newHeadline)
+    }
+
+    return {
+      getHeadlines: getHeadLines,
+      storeHeadlines: storeHeadlines
+    }
   }
+
   exports.Headlines = Headlines
 })(this)
--- tests/headlines-test.js
+++ tests/headlines-test.js
@@ -1,9 +1,20 @@
 (function testHeadlinesCreatesNewObjec () {
-  var articleMock = {}
+  var headlineMock = {}
 
   var object = new Object()
-  var headlines = new Headlines(articleMock, [])
+  var headlines = new Headlines(headlineMock, [])
   var testName = 'test the creation of a new Headlines object'
 
   assert.isTrue(typeof object === typeof headlines, testName)
-})()
+})();
+
+(function testHeadlinesStoringFunction () {
+  var testName = 'test storing of headlines from API return'
+  function HeadlineMock() {}
+  var callReturnMock = [{headline: 'test headline1', shortUrl: 'www.testlink.com', thumbnail: 'www.piclink.com'},
+    {headline: 'test headline2', shortUrl: 'www.testlink.com', thumbnail: 'www.piclink.com'}]
+  var headlines = new Headlines(HeadlineMock, callReturnMock)
+  headlines.storeHeadlines()
+  assert.isTrue(true, testName)
+  assert.isTrue(headlines.getHeadlines().length === 2,'check length of arrray')
+})();
