64a53628c9a95d2071448924dc60b4587816ecdd
Ruth Baker <ruthmoog@gmail.com>
2019-07-14 17:19:16 +0100

    Display headlines as a list on a webpage

---
+++ news-controller.js
@@ -0,0 +1,13 @@
+(function(exports) {
+  function NewsController(newsList) {
+    this.newsList = newsList;
+  }
+
+  NewsController.prototype.displayHeadlines = function() {
+
+    var pageContent = new NewsListHTML(this.newsList);
+    document.getElementById("app").innerHTML = pageContent.generateHTML();
+  };
+
+  exports.NewsController = NewsController;
+})(this);
