0fe02c32776846e6dfad3a48dbbe361a01d392a6
Danny <mtme13@hotmail.com>
2019-07-14 19:45:32 +0100

    Add test for consecutive IDs on articles

--- spec/newsArticleListSpec.js
+++ spec/newsArticleListSpec.js
@@ -24,5 +24,23 @@ describe('NewsArticleList', function() {
 
       expect(article.id).toEqual(0);
     });
+
+    it('adds consecutive id\'s to articles', function() {
+      let newsArticleList = new NewsArticleList();
+      let headline = 'Novak Djokovic v Roger Federer – live!';
+      let imageUrl = 'http://www.example.com/example.jpg';
+      let url = 'http://www.example.com';
+      let article1 = newsArticleList.addArticle(headline, url, imageUrl);
+      let article2 = newsArticleList.addArticle(headline, url, imageUrl);
+      let article3 = newsArticleList.addArticle(headline, url, imageUrl);
+
+      expect(article1.id).toEqual(0);
+      expect(article2.id).toEqual(1);
+      expect(article3.id).toEqual(2);
+    });
+  });
+
+  describe('#', function() {
+
   });
 })
