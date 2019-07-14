b7a4f6c514216c08fc726964e7b20e7dde01263b
Ben Scott <bengscott2@gmail.com>
2019-07-14 10:57:26 +0100

    fix ApiCall to use promise/awaits

--- models/api-call.js
+++ models/api-call.js
@@ -1,41 +1,11 @@
 (function (exports) {
   function ApiCall (xmlHttpRequest = XMLHttpRequest) {
-    var headlineApi = 'https://api.nextgen.guardianapps.co.uk/uk/lite.json'
-    var xhr = new xmlHttpRequest();
+    var headlineApiURL = 'https://api.nextgen.guardianapps.co.uk/uk/lite.json'
 
     function getHeadlines () {
-      _openRequest(headlineApi)
-      xhr.onload = function () {
-        if (_checkStatus(xhr.status)) {
-          console.log(1)
-          tenHeadLines = _tenHeadLines(xhr.response)
-          console.log(tenHeadLines);
-          console.log(3);
-          return tenHeadLines
-        }
-      }
-    }
-
-
-    function _tenHeadLines(data) {
-      console.log(2);
-      var jsonParsedData = JSON.parse(data)
-      var headlines = jsonParsedData.collections[0].content.slice(0,10)
-      return headlines
-    }
-
-
-    function _openRequest(apiUrl) {
-      xhr.open('GET', apiUrl)
-      xhr.send()
-    }
-
-    function _checkStatus (status) {
-      if (status >= 200 && status < 300) {
-        return true
-      } else {
-        console.log('The request failed!');
-      }
+      return fetch(headlineApiURL)
+        .then(response => response.json())
+        .then(data => data.collections)
     }
 
     return {
@@ -45,8 +15,3 @@
 
   exports.ApiCall = ApiCall
 })(this)
-
-
-// var api = new ApiCall()
-// api = api.getHeadlines()
-// console.log(api)
--- tests/api-call-test.js
+++ tests/api-call-test.js
@@ -1,11 +1,9 @@
-// function fullTestOfApiHeadlinesReturn () {
-//   var testName = 'test that the return has only 10 headlines'
-//   var api = new ApiCall()
-//   var result = (async function getResult(api) {
-//     await api.getHeadlines()
-//   })()
-//   console.log(result);
-// }
-// // this is a full test of api. Use only if neccesary
-//
-// fullTestOfApiHeadlinesReturn()
+async function fullTestOfApiHeadlinesReturn () {
+  var testName = 'test that the return is an array of objects'
+  var api = new ApiCall()
+  var result = await api.getHeadlines()
+  assert.isTrue(result.length > 0, testName)
+}
+// this is a full test of api. Use only if neccesary
+
+fullTestOfApiHeadlinesReturn()
