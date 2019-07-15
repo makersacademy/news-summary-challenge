6012ffddd7659088dcad093d469b7b70ea6d4118
Anna Sobolewska <sobolewska.a.m@gmail.com>
2019-07-15 02:45:12 +0100

    last attempt at serving this

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const port = 8000;
 server.use(express.static('public'));
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
+server.use(express.static(`${__dirname}/node_modules/`));
 
 server.get('/', function(req, res) { 
   res.send('index.html');
+  res.send('/lib/interface.js');
   });
 
 server.listen(port, (err) => {
