c82e00172d703d4e6aaf6850288275f3a2514fd2
Danny <mtme13@hotmail.com>
2019-07-21 15:48:54 +0100

    test:  NewsController renders headlines to the page

--- spec/newsControllerSpec.js
+++ spec/newsControllerSpec.js
@@ -6,5 +6,32 @@ describe('NewsController', function() {
     expect(newsController.list).toEqual(newsArticleList);
   });
 
-  
+  it('displays headlines on page', function() {
+    let newsArticleListDouble = {
+      convertToHTML: function () {
+        return htmlString;
+      }
+    };
+    let htmlString = 
+      '<ul>' +
+        '<li id="1">' +
+          '<a href="#articles/1">' +
+           '<h3>England win World Cup</h3>' +
+          '</a>' +
+        '</li>' +
+        '<li id="2">' +
+          '<a href="#articles/2">' +
+            '<h3>Queen rides pony</h3>' +
+          '</a>' +
+        '</li>' +
+      '</ul>';    
+
+    let element = {};
+    let newsController = new NewsController(newsArticleListDouble);
+    
+    newsController.renderHeadlines(element);
+    expect(element.innerHTML).toEqual(htmlString);
+  });
+
+
 });
--- src/newsController.js
+++ src/newsController.js
@@ -1,7 +1,13 @@
 (function(exports) {
   function NewsController(newsArticleList) {
     this.list = newsArticleList;
-  }
+
+    this.renderHeadlines = function (app = document.getElementById("app")) {
+      let target = app;
+      let headlines = newsArticleList.convertToHTML();
+      target.innerHTML = headlines;
+    }
+  }  
 
   exports.NewsController = NewsController;
 }) (this)
