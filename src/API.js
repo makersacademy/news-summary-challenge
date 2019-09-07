bbc4730277e133b07f0136a73a2af1547d492544
Jodi <jodiallysonfoster@gmail.com>
2019-09-07 13:37:13 +0100

    Headlines showing as links on the homepage

--- src/API.js
+++ src/API.js
@@ -10,21 +10,57 @@ function append(parent,el){
 const ul = document.getElementById('app'); // Get the list where we will place our authors
   const url = 'https://content.guardianapis.com/search?api-key=79e705af-3678-45d4-bb69-c209643b81ee'; // Get 10 random users
 
+
+
 fetch(url)
 .then((resp) => resp.json())
 .then(function(data){
 let authors = data.response.results;
+console.log(authors)
   return authors.map(function(author){
-    let li = createNode('li'),
-    // img = createNode('img'),
-    span = createNode('span');
-    // img.src = author.picture.medium;
-    span.innerHTML = `${author.webTitle}`;
-    // append(li, img);
-    append(li, span);
-    append(ul, li);
+    var i
+
+        for(i=0; i < 10; i++){
+          ul.innerHTML = "<ul>"+(authors.map( x => "<li><div><a href='#" + i++ + "'>" + x.webTitle + "</a></div></li>").join(""))+"</ul>"
+
+        }
+
   })
   })
+
+
+// this works
+// fetch(url)
+// .then((resp) => resp.json())
+// .then(function(data){
+// let authors = data.response.results;
+//   return authors.map(function(author){
+//     let li = createNode('li'),
+//     // img = createNode('img'),
+//     span = createNode('span');
+//     // img.src = author.picture.medium;
+//     span.innerHTML = `${author.webTitle}`;
+//     // append(li, img);
+//     append(li, span);
+//     append(ul, li);
+//   })
+//   })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
  // })
 
