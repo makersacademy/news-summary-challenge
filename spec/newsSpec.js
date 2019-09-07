8054f5bec3234b3ccdc7ec34aa42881890be8974
Stegosauruss <russell.wind@gmail.com>
2019-09-07 19:11:57 +0100

    displayImage creates an image object with a given url

--- spec/newsSpec.js
+++ spec/newsSpec.js
@@ -1,14 +1,19 @@
 // Stores all of the tests for the news class
 
 spec("Note Spec", [
-  test("When given a dummy headline it is displayed in the inner text", function() {
-    let news = new News("Dummy Headline", "Dummy Full Article", "http://dummyimage.com")
+  test("displayHeadline creates an element with a given innerText", function() {
+    let news = new News("Dummy Headline", "Dummy Full Article", "http://dummyimage.com/")
     expect(news.displayHeadline().innerText).toEqual("Dummy Headline")
   }),
 
-  test("When given a dummy body it is displayed in the inner text", function() {
-    let news = new News("Dummy Headline", "Dummy Full Article", "http://dummyimage.com")
+  test("displayStory creates an element with a given innerText", function() {
+    let news = new News("Dummy Headline", "Dummy Full Article", "http://dummyimage.com/")
     expect(news.displayStory().innerText).toEqual("Dummy Full Article")
+  }),
+
+  test("displayImage creates an image object with a given url", function() {
+    let news = new News("Dummy Headline", "Dummy Full Article", "http://dummyimage.com/")
+    expect(news.displayImage().src).toEqual("http://dummyimage.com/")
   })
 ])
 
