d7739b51f1dde86dd54dbea9b6b18d197ee1ce18
monmon8 <student@makerss-mbp.home>
2019-09-08 19:18:57 +0100

    basic set up

---
+++ assert.js
@@ -0,0 +1,8 @@
+var assert = {
+  isTrue: function (assertionToCheck) {
+    if (!assertionToCheck) {
+      throw new Error("Assertion failed: " + assertionToCheck + " is not truthy");
+    }
+    console.log("Assertion Passed:" + assertionToCheck + " is truthy")
+  }
+};
