a206a820bff57c602f2398b75b25c5385dbe5521
Stegosauruss <russell.wind@gmail.com>
2019-09-08 19:35:44 +0100

    Able to swap to full stories

--- public/interface.js
+++ public/interface.js
@@ -1,11 +1,24 @@
 // The JS required to make the page dynamic.
 
-let newsManager = new NewsManager()
+var newsManager = new NewsManager()
 newsManager.getArticles()
 newsManager.displayArticles()
+newsManager.displayStories()
 
 var mainDiv = document.getElementById("mainDiv")
 
 setTimeout(function(){
   mainDiv.appendChild(articlesDiv)
-}, 5000)
+  mainDiv.appendChild(fullArticlesDiv)
+}, 1500)
+
+var hideFullList = function() {
+  var fullList = document.getElementById('fullList')
+  fullList.setAttribute('hidden', 'hidden')
+}
+
+var showArticle = function(index) {
+  console.log('article-' + index)
+  var article = document.getElementById('article-' + index)
+  article.removeAttribute('hidden')
+}
