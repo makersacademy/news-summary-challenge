4894fa94631a176658a9825e6613344a8360ce1b
monmon8 <student@makerss-mbp.home>
2019-09-08 21:26:22 +0100

    gets a data using an ajax request

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-console.log("hello world <3");
-
+console.log("WOHO-STARTING");
+fetch('http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search')
+  .then(function (response) {
+    return response.json()
+  }).then(res => {
+    console.log("something ->", res)
+  })
