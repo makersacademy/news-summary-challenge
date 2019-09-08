ec0dad7f44bdcb569c995fa6c8be7e37f2f8d4ec
monmon8 <student@makerss-mbp.home>
2019-09-08 22:38:47 +0100

    listing headlines without pics

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 console.log("WOHO-STARTING");
-fetch('http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search')
+fetch('http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?show-elements=[\'image\']')
   .then(function (response) {
     return response.json()
-  }).then(res => {
-    console.log("something ->", res)
+  }).then(function (json) {
+    return json.response.results
+  }).then(data => {
+    const container = document.getElementById("root");
+    console.log(data)
+    for (let i of data) {
+      const title = i.webTitle;
+      const p = document.createElement("p");
+      container.append(title, p);
+    }
+    console.log(container.childNodes)
   })
