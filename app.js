ebde15efeb1fdb1d52b9b656f45f2c430351ba2f
Dan Gyi <daniel.gyi@gmail.com>
2019-10-01 12:20:23 +0100

    Implements Lazy Loading for headline images

--- app.js
+++ app.js
@@ -1,4 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));;
+
+  if ("IntersectionObserver" in window && "IntersectionObserverEntry" in window && "intersectionRatio" in window.IntersectionObserverEntry.prototype) {
+    let lazyImageObserver = new IntersectionObserver(function (entries) {
+      entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+          let lazyImage = entry.target;
+          $.get("http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/extract?url=https://www.theguardian.com/world/live/2019/oct/01/china-anniversary-nation-marks-70-years-of-communism-amid-hong-kong-protests-live", function (data) {
+            lazyImage.src = data.image
+        })
+          $.get("http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/extract?url=https://www.theguardian.com/world/live/2019/oct/01/china-anniversary-nation-marks-70-years-of-communism-amid-hong-kong-protests-live", function (data) {
+            lazyImage.srcset = data.image
+          });
+          lazyImage.classList.remove("lazy");
+          lazyImageObserver.unobserve(lazyImage);
+        }
+      });
+    });
+
+    lazyImages.forEach(function (lazyImage) {
+      lazyImageObserver.observe(lazyImage);
+    });
+  }
+});
+
 $(document).ready(function() {
+  
   $.get("http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=https://www.theguardian.com/world/live/2019/oct/01/china-anniversary-nation-marks-70-years-of-communism-amid-hong-kong-protests-live?sentences_number=5?", function(data){
     $('#summary').text(data.text)
     console.log(data)
@@ -7,7 +34,7 @@ $(document).ready(function() {
 
   $.get("http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/extract?url=https://www.theguardian.com/world/live/2019/oct/01/china-anniversary-nation-marks-70-years-of-communism-amid-hong-kong-protests-live", function (data) {
     $('#headline').text(data.title)
-    $('#image').html('<img src="' + data.image + '" />')
+    // $('#image').html('<img src="' + data.image + '" />')
     console.log(data)
     // get request extracts all of the meta data about this article
   });
