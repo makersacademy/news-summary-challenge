4934e44010591052b1b2ded0546e9cc717c3dbc8
Dan Gyi <daniel.gyi@gmail.com>
2019-10-03 13:50:58 +0100

    summary page done, needs CSS styling

--- app.js
+++ app.js
@@ -4,22 +4,23 @@ window.addEventListener("hashchange", function (event) {
   all_ids.forEach(function(node){
     document.getElementById(`${node.id}`).innerHTML = ""
   })
-
-  // **** below can be used for clearing all headlines except the one clicked****
-  // var selected_news_id = location.hash.split("/")[1]
-  // var all_ids = document.querySelectorAll('[id^="containerid"]')
-  // all_ids.forEach(function (unique_id) {
-  //   if (unique_id.id !== `containerid${selected_news_id}`) {
-  //     document.getElementById(`${unique_id.id}`).innerHTML = ""
-  //   }
-  // })
+  var selected_news_id = location.hash.split("/")[1]
+  var url_to_retrieve = article_array[selected_news_id].webUrl
+  $.get("http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/extract?url=" + url_to_retrieve, function(data) {
+    console.log(data)
+    $('#summary-headline').text(data.title)
+    $('#summary-image').text(data.image)
+    $('#summary-article').text(data.article)
+    $('#summary-date').text(data.publishDate.substring(0,10))
+    $('#summary-author').text(data.author)
+  });
 });
 
 
 $(document).ready(function () {
 
   $.get('http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?from-date=2019-10-03&to-date=2019-10-03', function (data) {
-    array = data.response.results
+    article_array = data.response.results
 
     // change 2 in the loop initiator below to array.length when ready to stop testing...
     for (let i = 0; i < 2; i++) {
@@ -30,10 +31,10 @@ $(document).ready(function () {
       $summary = $("<h3/>", { id: `summary${i}` })
       $image = $("<img/>", { id: `image${i}`, class: 'news-image' })
       $link = $("<a/>", { id: `link${i}`, href: `#article/${i}` })
-      $dynamiccontainer.append($namingdiv.append($individualborder.append($link.append($headline), $image, $summary))).appendTo('#headlines')
+      $dynamiccontainer.append($namingdiv.append($individualborder.append($link.append($headline), $image, $summary))).appendTo('#headlines-list')
 
       // gets the standard dataset from Guardian API
-      $.get("http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/extract?url=" + array[i].webUrl, function (data) {
+      $.get("http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/extract?url=" + article_array[i].webUrl, function (data) {
         $(`#headline${i}`).text(data.title)
         if (data.image !== '') {
           $(`#image${i}`).attr('src', data.image)
@@ -41,7 +42,7 @@ $(document).ready(function () {
       });
 
       // gets summary from Guardian API via Aylien
-      $.get(`http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=${array[i].webUrl}&sentences_number=3`, function (data) {
+      $.get(`http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=${article_array[i].webUrl}&sentences_number=3`, function (data) {
         $(`#summary${i}`).text(data.sentences.slice(0, 2).join(' '))
       })
     }
