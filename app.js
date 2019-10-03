ebc588c291e7c42ebff81ef226cf50de114c5139
Dan Gyi <daniel.gyi@gmail.com>
2019-10-03 09:34:00 +0100

    spike retrieval of todays articles from guardian API

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
+
+  // lazy loading lines 4-27
   var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));;
 
   if ("IntersectionObserver" in window && "IntersectionObserverEntry" in window && "intersectionRatio" in window.IntersectionObserverEntry.prototype) {
@@ -17,7 +19,6 @@ document.addEventListener("DOMContentLoaded", function () {
         }
       });
     });
-
     lazyImages.forEach(function (lazyImage) {
       lazyImageObserver.observe(lazyImage);
     });
@@ -25,6 +26,10 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 $(document).ready(function() {
+
+  $.get('http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?from-date=2019-10-03&to-date=2019-10-03', function(data) {
+    console.log(data)
+  });
   
   $.get("http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=https://www.theguardian.com/world/live/2019/oct/01/china-anniversary-nation-marks-70-years-of-communism-amid-hong-kong-protests-live?sentences_number=5?", function(data){
     $('#summary').text(data.text)
