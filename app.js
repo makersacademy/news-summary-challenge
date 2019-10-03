b2318bb81fdca8318b59f5f9249488295d80c1da
Dan Gyi <daniel.gyi@gmail.com>
2019-10-03 10:23:23 +0100

    title, image, summary, being pulled dynamically for today

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
           let lazyImage = entry.target;
           $.get("http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/extract?url=https://www.theguardian.com/world/live/2019/oct/01/china-anniversary-nation-marks-70-years-of-communism-amid-hong-kong-protests-live", function (data) {
             lazyImage.src = data.image
-        })
+          })
           $.get("http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/extract?url=https://www.theguardian.com/world/live/2019/oct/01/china-anniversary-nation-marks-70-years-of-communism-amid-hong-kong-protests-live", function (data) {
             lazyImage.srcset = data.image
           });
@@ -25,25 +25,35 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-  $.get('http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?from-date=2019-10-03&to-date=2019-10-03', function(data) {
+  $.get('http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?from-date=2019-10-03&to-date=2019-10-03', function (data) {
     console.log(data)
-  });
-  
-  $.get("http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=https://www.theguardian.com/world/live/2019/oct/01/china-anniversary-nation-marks-70-years-of-communism-amid-hong-kong-protests-live?sentences_number=5?", function(data){
-    $('#summary').text(data.text)
-    console.log(data)
-    // get request extracts summary of article (currently set to 5 sentences)
-  });
+    array = data.response.results
 
-  $.get("http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/extract?url=https://www.theguardian.com/world/live/2019/oct/01/china-anniversary-nation-marks-70-years-of-communism-amid-hong-kong-protests-live", function (data) {
-    $('#headline').text(data.title)
-    // $('#image').html('<img src="' + data.image + '" />')
-    console.log(data)
-    // get request extracts all of the meta data about this article
-  });
+    // change 2 in the loop initiator below to array.length when ready to stop testing...
+    for (let i = 0; i < 1; i++) {
+      // headline block generator, can grow this out to include images etc.
+      let $dynamiccontainer = $("<div/>")
+      $headline = $("<h1/>", { id: `headline${i}` })
+      $summary = $("<h3/>", { id: `summary${i}` })
+      $image = $("<img/>", { id: `image${i}` })
+      $dynamiccontainer.append($headline, $image, $summary).appendTo('#headlines')
 
+      // gets the headline from Guardian API
+      $.get("http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/extract?url=" + array[i].webUrl, function (data) {
+        $(`#headline${i}`).text(data.title)
+        $(`#image${i}`).attr('src', data.image)
+        console.log(data)
+      });
+
+      // gets summary from Guardian API via Aylien
+      $.get("http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=" + array[i].webUrl + "?sentences_number=2?", function (data) {
+        console.log(data)
+        $(`#summary${i}`).text(data.sentences)
+      })
+    }
+  });
 
 });
 
