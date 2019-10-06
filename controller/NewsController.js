cbe8be1ba3bd687254d90a4bf13efb10f9d3472b
Mike <mjwalshe@gmail.com>
2019-10-06 19:20:12 +0100

    Spike API caller function returning plain text

--- controller/NewsController.js
+++ controller/NewsController.js
@@ -11,7 +11,22 @@
     NewsController.prototype.displayHeadlines = function() {
         document.getElementById("headline-list").innerHTML = this.articleListView.outputHtmlString();
     }; 
-  
+
+    function ApiCaller() { 
+    }
+
+    ApiCaller.prototype.getGuardian = function() {
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {    
+                return this.responseText;
+            }
+        };
+        xhttp.open("GET", "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?q=from-date=" + "2019-05-10", true);
+        xhttp.send();
+    };
+
     exports.NewsController = NewsController;
+    exports.ApiCaller = ApiCaller;
 })(this);
-  
+
