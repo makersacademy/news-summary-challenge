d862890ccdd9e7eb17c74a3df5318e856e974cc5
Student <student@Makerss-MacBook-Pro-5.local>
2019-10-06 11:18:41 +0100

    created headlines-model

---
+++ model/headlines-model.js
@@ -0,0 +1,6 @@
+(function(exports){
+  function Headlines() {
+
+  };
+  exports.Headlines = Headlines
+})(this);
---
+++ test/headlines-model-test.js
@@ -0,0 +1,6 @@
+(function() {
+  describe('Headlines')
+  it('is an instance of Headlines')
+  let headlines = new Headlines()
+  assert.isInstanceOf(headlines, Headlines)
+})();
