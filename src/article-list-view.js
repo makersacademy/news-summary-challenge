47011e628a73ff2943e090076e6b2f0b38033ca5
Ben <ben@benzeng.co.uk>
2019-10-06 19:50:01 +0100

    now renders single article view

--- src/article-list-view.js
+++ src/article-list-view.js
@@ -7,19 +7,35 @@
 
     ArticleListView.prototype.returnListViewHTML = function() {
         let articlesIn = this.articleList.getArticles();
-        console.log(articlesIn);
+
         let articlesHTMLOut = [];
 
          if (articlesIn.length === 0)
              return null;
          for (let i = 0; i < articlesIn.length; i++) {
-             articlesHTMLOut.push(`<div id="${articlesIn[i].id}"><img src="${articlesIn[i].img}">${articlesIn[i].title}</div>`);
+             articlesHTMLOut.push(`<div class="articles" id="${articlesIn[i].id}"><img src="${articlesIn[i].img}" id="${articlesIn[i].id}">${articlesIn[i].title}</div>`);
          }
 
          console.log(articlesHTMLOut);
          return `<div>${articlesHTMLOut.join("")}</div>`
 
     };
+
+    ArticleListView.prototype.returnSingleViewHTML = function(id) {
+        let articlesIn = this.articleList.getArticles();
+        let articleOut = " ";
+
+        for (let i = 0; i < articlesIn.length; i++) {
+            if (articlesIn[i].id === id)
+                articleOut = articlesIn[i].body;
+        }
+
+        console.log(articleOut);
+        return(articleOut);
+        // return `<div>${articlesHTMLOut.join("")}</div>`
+
+    };
+
 
 
     exports.ArticleListView = ArticleListView;
--- src/news-controller.js
+++ src/news-controller.js
@@ -8,6 +8,10 @@
         document.getElementById("app").innerHTML = this.articleListView.returnListViewHTML();
     };
 
+    NewsController.prototype.insertSingleViewHTMLToApp = function(id) {
+        document.getElementById("app").innerHTML = this.articleListView.returnSingleViewHTML(id);
+    };
+
     exports.NewsController = NewsController;
 
 })(this);
@@ -18,8 +22,9 @@ let newsController = new NewsController(articleList);
 newsController.insertHTMLToApp();
 
 $( document ).ready(function() {
-    $("a").click(function(event) {
-        alert(event.target.id);
+    $(".articles").click(function(event) {
+        newsController.insertSingleViewHTMLToApp(event.target.id);
+        // alert(event.target.id);
     });
 });
 
