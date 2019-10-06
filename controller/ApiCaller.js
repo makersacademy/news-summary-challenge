97eb9da76b41e0d8195f83d5b74c167cde0bab64
Mike <mjwalshe@gmail.com>
2019-10-06 19:35:12 +0100

    Extract ApiCaller object, add JSON parsing and create mockApiCaller for tests

---
+++ controller/ApiCaller.js
@@ -0,0 +1,13 @@
+(function(exports) {
+    ApiCaller.prototype.getGuardian = function() {
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {  
+                var data = JSON.parse(this.responseText);
+                return data;
+            }
+        };
+        xhttp.open("GET", "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?q=from-date=" + "2019-05-10", true);
+        xhttp.send();
+    };
+})(this);
--- controller/NewsController.js
+++ controller/NewsController.js
@@ -1,7 +1,8 @@
 (function(exports) {
-    function NewsController(articleList) {
+    function NewsController(articleList, apiCaller) {
         this.articleList = articleList;
         this.articleListView = new ArticleListView(articleList);
+        this.apiCaller = apiCaller;
     }
 
     NewsController.prototype.getArticleList = function() {
@@ -11,20 +12,6 @@
     NewsController.prototype.displayHeadlines = function() {
         document.getElementById("headline-list").innerHTML = this.articleListView.outputHtmlString();
     }; 
-
-    function ApiCaller() { 
-    }
-
-    ApiCaller.prototype.getGuardian = function() {
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {    
-                return this.responseText;
-            }
-        };
-        xhttp.open("GET", "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?q=from-date=" + "2019-05-10", true);
-        xhttp.send();
-    };
 
     exports.NewsController = NewsController;
     exports.ApiCaller = ApiCaller;
