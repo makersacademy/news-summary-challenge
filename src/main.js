3c9603ba98c23f3a27f0b0b7ad87167fcdc71a15
Xiaofei Zhang <xiaofeizhang@Xiaofeis-MBP.lan>
2019-10-07 00:08:18 +0100

    Restructure files

--- src/main.js
+++
@@ -1,8 +0,0 @@
-const api = new GuardianApi();
-
-api.getHeadlines(function(response) {
-  const results = response.response.results;
-  for (let i = 0; i < results.length; i++) {
-    console.log(results[i].fields.headline);
-  }
-});
--- tests/assert.js
+++
@@ -1,7 +0,0 @@
-const assert = {
-  isTrue: function(assertionToCheck) {
-    if (!assertionToCheck) {
-      throw new Error("Assertion failed: " + assertionToCheck + " is not truthy");
-    }
-  }
-};
