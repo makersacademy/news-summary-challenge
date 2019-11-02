92bfc4ffdffa26658c4d3e59665e470111d1f70d
Olly Holovchenko <oleh.holo@gmail.com>
2019-11-02 15:40:16 +0000

    Test using news API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: []
+    };
+  }
+  componentDidMount() {
+    fetch(
+      "https://newsapi.org/v2/top-headlines?" +
+        "country=us&" +
+        "apiKey=2ab10248d6ea4276b5ccd96cc3fa8f20"
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(myJson => {
+        this.setState({
+          articles: myJson.articles
+        });
+      });
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <div>
+        Hello World!
+        <br></br>
+        {this.state.articles.map((item, index) => {
+          return (
+            <div key={index}>
+              <h2>{item.title}</h2>
+              <img src={item.urlToImage} alt="" style={{ width: "50vw" }} />
+              <p>{item.description}</p>
+              <p>
+                <a href={item.url}>Read More</a>
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
