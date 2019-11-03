f1d29b81b0dbd7bb0b29d5f619e8c97d81235e66
Olly Holovchenko <oleh.holo@gmail.com>
2019-11-03 18:13:38 +0000

    Test deployment to Heroku

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import ArticleList from "./components/ArticleList";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
