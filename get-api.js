e9ae9398f32534bff166356c5e0c98662dfa5144
Jay Issuree <Jay_Issuree@hotmail.co.uk>
2019-11-03 16:58:44 +0000

    HTML Styling

--- get-api.js
+++ get-api.js
@@ -1,3 +1,11 @@
+const app = document.getElementById('root')
+
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
+
+app.appendChild(container)
+
+
 var request = new XMLHttpRequest()
 
 request.open('GET', 'https://content.guardianapis.com/search?api-key=4c4f5351-da4c-4861-a59c-aa7e44e4c193', true)
@@ -8,6 +16,25 @@ request.onload = function() {
   articles.forEach(function(entry) {
     webTitle = entry.webTitle
     webUrl = entry.webUrl
+    apiUrl = entry.apiUrl
+
+    const card = document.createElement('div')
+    card.setAttribute('class', 'card')
+
+    // Create an h1 and set the text content to the film's title
+    const h1 = document.createElement('h1')
+    h1.textContent = webTitle
+
+    // Create a p and set the text content to the film's description
+    const p = document.createElement('p')
+    p.textContent = webUrl // End with an ellipses
+
+    // Append the cards to the container element
+    container.appendChild(card)
+
+    // Each card will contain an h1 and a p
+    card.appendChild(h1)
+    card.appendChild(p)
   });
 }
 
