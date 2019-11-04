37b97406d911284f3f6ff413878405dd3ae3bbda
Student <student@Makerss-MBP.home>
2019-11-04 00:54:03 +0000

    articles now added to list object

--- public/src/controller.js
+++ public/src/controller.js
@@ -3,13 +3,18 @@
 (function (exports) {
 
   function Controller(articleData) {
+
+    var articleList = new ArticleList
+
     articleData.forEach(function(article, index) {
-      var webTitle = article.webTitle
-      var webUrl = article.webUrl
-      var id = index
-      var rugbyArticle = new RugbyArticle(webTitle, webUrl, id)
-      console.log(rugbyArticle)
+      var webTitle = article.webTitle;
+      var webUrl = article.webUrl;
+      var id = index;
+      var rugbyArticle = new RugbyArticle(webTitle, webUrl, id);
+      articleList.addArticle(rugbyArticle);
     });
+
+    console.log(articleList.allArticles());
   };
 
   exports.Controller = Controller;
