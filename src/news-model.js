cfec8cdc9745bc360a1b491d9442eb84554a7564
ellieSMASH <eliseaston@gmail.com>
2019-12-01 19:42:51 +0000

    headlines displayed on page

--- src/news-model.js
+++ src/news-model.js
@@ -1,13 +1,16 @@
-function Headline(headline) {
-  this.headline = headline;
+function News() {
 }
 
-Headline.prototype = (function() {
-  function returnHeadline() {
-    return this.headline;
-  };
+News.prototype = (function() {
+
+  function getData() {
+  const url = 'https://content.guardianapis.com/search?api-key=f4eb11b1-0a1b-4793-8122-376a8dde0093'
+  fetch(url)
+  .then(data => {return data.json()})
+  .then(result => {console.log(result)})
+  }
 
   return {
-    returnHeadline: returnHeadline
+    getData: getData
   };
 })();
