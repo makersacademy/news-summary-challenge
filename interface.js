941c69436fe05771ba604762a9e1937904f59f9d
Ed Courage <edcourage@live.com>
2019-12-02 11:04:32 +0000

    basic functionality complete

--- interface.js
+++ interface.js
@@ -9,7 +9,7 @@ function listHeadlines() {
   fetch(guardianUrl)
   .then(result => result.json())
   .then(headlines => {
-
+    console.log(headlines)
     var page = document.getElementById('page')
     listformater.headlines(headlines).forEach(function(headline){
       a = document.createElement("a")
@@ -18,10 +18,7 @@ function listHeadlines() {
       a.setAttribute("href", headline.webUrl)
       link = document.createElement("a")
       link.setAttribute('href', "#/"+ headline.id)
-
-
       link.innerHTML = "View Summary"
-      // btn.addEventListener("hs", console.log("fds"))
       a.innerHTML = headline.webTitle
       page.appendChild(a)
       page.appendChild(br2)
@@ -37,31 +34,24 @@ function summary(id) {
   fetch(guardianUrl)
   .then(result => result.json())
   .then(headlines => {
-    // var article = listformater.articleSelector(headlines, id)
     headlines
     window.addEventListener("hashchange", function(object){
-      var newURL = object.newURL
-      var idFromHash = newURL.split("#")[1]
-      headlines.response.results.forEach(function(article){
-        if ("/" + article.id === idFromHash) {
-          selectedArticle = article
-        }
+    var newURL = object.newURL
+    var idFromHash = newURL.split("#")[1]
+    headlines.response.results.forEach(function(article){
+      if ("/" + article.id === idFromHash) {
+        selectedArticle = article
+      }
+    })
+
+    fetch("http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=" + selectedArticle.webUrl)
+      .then(result => result.json())
+      .then(json => {
+        summaryContent = document.getElementById('summary')
+        json.sentences.join(' ')
+        var summary = document.getElementById('summary')
+        summary.innerHTML = `<h2>${selectedArticle.webTitle}</H2><p>${json.sentences.join(' ')}</p>`
       })
-
-      console.log(selectedArticle.webUrl)
-
-      fetch("http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=" + selectedArticle.webUrl)
-        .then(result => {
-
-          console.log(result)
-        })
-
-      var summary = document.getElementById('summary')
-      summary.innerHTML = `<h2>${selectedArticle.webTitle}</H2>`
-
     })
   })
-
-
-
 }
