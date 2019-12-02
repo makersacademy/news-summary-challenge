c75c31d96feaffb9887199ba03669a4942bb70d2
ellieSMASH <eliseaston@gmail.com>
2019-12-02 13:49:03 +0000

    refactor javascript

--- src/news-controller.js
+++ src/news-controller.js
@@ -1,19 +1,23 @@
 document.getElementById('get-news').addEventListener('click', fetchNewsData);
 
 function fetchNewsData(){
-  fetch('https://content.guardianapis.com/search?api-key=f4eb11b1-0a1b-4793-8122-376a8dde0093')
+  let guardianAPI = 'https://content.guardianapis.com/search?api-key=f4eb11b1-0a1b-4793-8122-376a8dde0093'
+  let aylienAPI = 'http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url='
+  fetch(guardianAPI)
   .then(response => response.json())
+  .then(articles => articles.response.results)
   .then(articles => {
+    let headlines = articles.forEach(function(article) {
+      article.webTitle
+    })
+    let readMore = articles.forEach(function(article) {
+      `|  <a href="${article.webUrl}">Read article</a><br>
+      </li><br>`
+    })
     let output = '<h2>Headlines</h2>';
     output += '<ul>';
-    articles.response.results.forEach(function(article) {
-      output += `
-      <li>
-      ${article.webTitle}
-      <a href="${article.webUrl}">Read article</a>
-      </li>
-      `;
-    });
+    output += headlines;
+    output += readMore;
     output += '</ul>'
     document.getElementById("news").innerHTML = output;
   })
