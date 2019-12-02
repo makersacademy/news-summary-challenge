7ff039068bd13ba05dea6cedb1d5386c4e28341b
Sara <srancati0@gmail.com>
2019-12-02 09:12:50 +0000

    added button - summary not yet available

--- public/src/list-view.js
+++ public/src/list-view.js
@@ -8,8 +8,8 @@
         var listTwo = [];
       
         this.list.forEach((element, i) => {
-            listTwo.push("<li><div><a id='single-news-item' href='" + element.webUrl + "'>"
-            + element.webTitle + "</a><br><img src='" + element.fields.thumbnail + "'></div></li><br><br>")
+            listTwo.push("<div><a id='single-news-item' href='" + element.webUrl + "'>"
+            + element.webTitle + "</a><br><img src='" + element.fields.thumbnail + "'>"+ "<br><button id=" + i + ">View summary</button><form></div><br><br>")
         });
         listTwo.unshift("<ul>");
         listTwo.push("</ul>");
