7978f6391943ac69851c153834624aee3c221784
AndreaDiotallevi <andrea.diotallevi@outlook.com>
2020-01-11 17:31:15 +0000

    Add test and code for ArticleList's 'addArticle' method

--- spec/articleListModelSpec.js
+++ spec/articleListModelSpec.js
@@ -1,12 +1,10 @@
-// function testGetEmptyArticleList() {
-//   var articleList = new ArticleList()
-//   assert.isTrue(articleList.articles().length === 0)
-// }
-
-// testGetEmptyArticleList()
-
-
 var testGetEmptyArticleList = (function() {
   var articleList = new ArticleList()
   assert.isTrue(articleList.articles().length === 0)
+})()
+
+var testAddArticle = (function() {
+  var articleList = new ArticleList()
+  articleList.addArticle("Title")
+  assert.isTrue(articleList.articles().length === 1)
 })()
--- src/articleListModel.js
+++ src/articleListModel.js
@@ -7,6 +7,10 @@
     articles() {
       return this._articles
     }
+
+    addArticle(title) {
+      this._articles.push(new Article(title))
+    }
   }
 
   object.ArticleList = ArticleList
