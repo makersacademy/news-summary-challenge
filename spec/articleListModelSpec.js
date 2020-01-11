86d6420c135d4f17eae38718e011136d698f4804
AndreaDiotallevi <andrea.diotallevi@outlook.com>
2020-01-11 17:26:26 +0000

    Refactor tests

--- spec/articleListModelSpec.js
+++ spec/articleListModelSpec.js
@@ -1,6 +1,12 @@
-function testGetEmptyArticleList() {
+// function testGetEmptyArticleList() {
+//   var articleList = new ArticleList()
+//   assert.isTrue(articleList.articles().length === 0)
+// }
+
+// testGetEmptyArticleList()
+
+
+var testGetEmptyArticleList = (function() {
   var articleList = new ArticleList()
   assert.isTrue(articleList.articles().length === 0)
-}
-
-testGetEmptyArticleList()
+})()
--- spec/articleModelSpec.js
+++ spec/articleModelSpec.js
@@ -1,6 +1,4 @@
-function testGetArticleTitle() {
+var testGetArticleTitle = (function() {
   var article = new Article("Title")
   assert.isTrue(article.title() === "Title")
-}
-
-testGetArticleTitle();
+})()
