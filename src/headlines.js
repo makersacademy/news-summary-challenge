e509afb6fc9e686d8e9331738b057f0289db43f8
Ellie <elliemmanifold@gmail.com>
2020-01-12 14:03:33 +0000

    Refactor so tests take in stubbed responses

--- src/headlines.js
+++ src/headlines.js
@@ -5,8 +5,15 @@
 
   Headlines.prototype.individualHeadlines = function() {
     var webUrls = this.request.response.results.map(headline => headline.webUrl)
-    splitUrls = webUrls.map(url => url.split('').slice(27).join(''))
+    splitUrls = webUrls.map(url => "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com" + url.split('').slice(27).join('') + "?show-fields=body")
     return splitUrls
+  }
+
+  Headlines.prototype.httpGet = function(theUrl){
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
   }
 
   exports.Headlines = Headlines;
