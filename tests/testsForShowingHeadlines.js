961ffbaa05d077e428ed4d0a48c98a6aee4ad02a
Jamie95187 <jamiewong63@googlemail.com>
2020-01-12 23:34:21 +0000

    add url into headlines to link to original news article

--- tests/testsForShowingHeadlines.js
+++ tests/testsForShowingHeadlines.js
@@ -4,7 +4,7 @@ function testForShowingHeadlines(){
   var headlines = new Headlines();
   headlines.show('tests/testHeadline.txt');
   console.log(document.getElementById("headlineList").innerHTML)
-  assert.isTrue(document.getElementById("headlineList").innerHTML == "<ul><li>Exeter carry the English standard but French clubs look a real force | Robert Kitson</li></ul>");
+  assert.isTrue(document.getElementById("headlineList").innerHTML == '<ul><li><div><a href="https://www.theguardian.com/sport/2020/jan/12/european-champions-cup-rugby">Exeter carry the English standard but French clubs look a real force | Robert Kitson</a></div></li></ul>');
 };
 
 testForShowingHeadlines();
