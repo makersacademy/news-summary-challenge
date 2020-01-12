6d683690a2633a8d91c618183d410554c4cc4bb3
Hyan18 <Hyan18@github.com>
2020-01-12 15:01:03 +0000

    test and add ArticleList

---
+++ spec/article-list-model-spec.js
@@ -0,0 +1,9 @@
+describe('ArticleList', () => {
+  describe('#createArticle', () => {
+    it('should create a new Article and store it', () => {
+      articleList = new ArticleList()
+      articleList.createArticle('title of article')
+      expect(articleList.show()[0]).isInstanceOf(Article)
+    })
+  })
+})
--- spec/article-model-spec.js
+++ spec/article-model-spec.js
@@ -1,4 +1,4 @@
-describe('article', () => {
+describe('Article', () => {
   it('should have a title', () => {
     var article = new Article('this is a title')
     expect(article.showTitle()).toEq('this is a title')
