ee1cc70f2d088286b8c08637243f638d24a25023
Raluca Ciucu <raluca.ciucu@gmail.com>
2020-01-12 15:56:31 +0000

    Add addTitle function and test

--- spec/title-list-model-test.js
+++ spec/title-list-model-test.js
@@ -1,7 +1,7 @@
 var titleList = new TitleList();
 
-// titleList.addTitle("string1");
-// titleList.addTitle("string2");
+titleList.addTitle("string1");
+titleList.addTitle("string2");
 
 
 function testTitlesArray(){
@@ -9,3 +9,30 @@ function testTitlesArray(){
 };
 
 
+function addTitle() {
+    assert.isTrue(titleList.titles[0].returnText() === "string1")
+    assert.isTrue(titleList.titles[1].returnText() === "string2")
+};
+
+
+testTitlesArray();
+addTitle();
+
+// function testReturnTitles(){
+//     titles = titleList.returnTitles();
+//     assert.isTrue(titles[0])
+// };
+
+///
+
+
+// function testReturnNotes() {
+//   notes = notesList.returnNotes();
+//   assert.isTrue(notes[0].returnText() === "string1")
+//   assert.isTrue(notes[1].returnText() === "string2")
+// }
+
+
+// testNotesArray();
+// addNote();
+// testReturnNotes()
--- src/title-list-model.js
+++ src/title-list-model.js
@@ -4,6 +4,35 @@
         this.titles = [];
     };
 
+    TitleList.prototype.addTitle = function(title) {
+        this.titles.push(new Title(title))
+    };
+
     exports.TitleList = TitleList;
 
 })(this);
+
+
+
+// (function(exports) {
+//     function NotesList() {
+//         this.notes = [];
+//     };
+  
+//     NotesList.prototype.addNote = function (note) {
+//         this.notes.push(new Note(note));
+//     };
+  
+//     NotesList.prototype.returnNotes = function() {
+//         return this.notes;
+//     }
+  
+//     NotesList.prototype.getById = function(id) {
+//       var arr = this.notes.filter(note => note.id == id)
+//       return arr[0]
+//     }
+  
+//     exports.NotesList = NotesList;
+  
+//   })(this);
+  
