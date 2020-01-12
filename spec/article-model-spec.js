e50e3e406fdd029733b848ac56a3817969c67ad8
Hyan18 <Hyan18@github.com>
2020-01-12 15:21:44 +0000

    test and create ArticleListView

--- spec/article-model-spec.js
+++ spec/article-model-spec.js
@@ -1,6 +1,7 @@
 describe('Article', () => {
   it('should have a title', () => {
     var article = new Article('this is a title')
+    
     expect(article.showTitle()).toEq('this is a title')
   })
 })
