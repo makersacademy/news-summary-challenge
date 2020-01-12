7aa3a2600bf158e310cce80efb0ceeedbcac4535
Jade Beresford <jade_fletcher@hotmail.co.uk>
2020-01-12 20:36:47 +0000

    Create guardian api stub

---
+++ news-controller.js
@@ -0,0 +1,3 @@
+// curl "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/politics/blog/2014/feb/17/alex-salmond-speech-first-minister-scottish-independence-eu-currency-live?show-fields=body"
+//
+// curl "http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=http://worrydream.com/MediaForThinkingTheUnthinkable/note.html"
