073f7eafed224062e4dbd5e37acc7863c9c5748d
Hyan18 <Hyan18@github.com>
2020-01-12 22:34:29 +0000

    view full article

--- spec/article-model-spec.js
+++ spec/article-model-spec.js
@@ -1,7 +1,9 @@
 describe('Article', () => {
-  it('should have a title', () => {
-    var article = new Article('this is a title')
+  it('should have a title, url and id', () => {
+    var article = new Article('this is a title','url.com', 'this is a id')
     
     expect(article.showTitle()).toEq('this is a title')
+    expect(article.url).toEq('url.com')
+    expect(article.id).toEq('this is a id')
   })
 })
