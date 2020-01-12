ff2163783b39f0edefcdd2d38911a0bdc30b9db1
Raluca Ciucu <raluca.ciucu@gmail.com>
2020-01-12 15:57:39 +0000

    Remove testTitlesArray test function as it tests for state

--- spec/title-list-model-test.js
+++ spec/title-list-model-test.js
@@ -4,9 +4,9 @@ titleList.addTitle("string1");
 titleList.addTitle("string2");
 
 
-function testTitlesArray(){
-    assert.isTrue(Array.isArray(titleList.titles));
-};
+// function testTitlesArray(){
+//     assert.isTrue(Array.isArray(titleList.titles));
+// };
 
 
 function addTitle() {
