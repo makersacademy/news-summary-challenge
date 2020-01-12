6b89f2a16ed941b7b755b472aec54ab0d036a3af
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-01-12 13:21:52 +0000

    update seed data, add corresponding alyien seed data. start test for article list view

--- spec/seed_data.js
+++ spec/seed_data.js
@@ -1,34 +1,56 @@
 guardian_api_seed = [
   {
-    "id": "football/live/2020/jan/11/crystal-palace-v-arsenal-premier-league-live",
-    "type": "liveblog",
-    "sectionId": "football",
-    "sectionName": "Football",
-    "webPublicationDate": "2020-01-11T13:52:14Z",
-    "webTitle": "Crystal Palace v Arsenal: Premier League – live!",
-    "webUrl": "https://www.theguardian.com/football/live/2020/jan/11/crystal-palace-v-arsenal-premier-league-live",
-    "apiUrl": "https://content.guardianapis.com/football/live/2020/jan/11/crystal-palace-v-arsenal-premier-league-live",
+    "id": "film/2020/jan/12/lullaby-review-french-nanny-from-hell",
+    "type": "article",
+    "sectionId": "film",
+    "sectionName": "Film",
+    "webPublicationDate": "2020-01-12T12:00:34Z",
+    "webTitle": "Lullaby review – a no-thrills thriller",
+    "webUrl": "https://www.theguardian.com/film/2020/jan/12/lullaby-review-french-nanny-from-hell",
+    "apiUrl": "https://content.guardianapis.com/film/2020/jan/12/lullaby-review-french-nanny-from-hell",
     "fields": {
-      "thumbnail": "https://media.guim.co.uk/0109f310815d5e8f08ac8f459e9547be77e82cf9/9_126_2822_1693/500.jpg"
+      "thumbnail": "https://media.guim.co.uk/627be5c20c09e4c908231a72f6ec9c6c86b56d50/0_152_5300_3180/500.jpg"
     },
     "isHosted": false,
-    "pillarId": "pillar/sport",
-    "pillarName": "Sport"
+    "pillarId": "pillar/arts",
+    "pillarName": "Arts"
   },
   {
-    "id": "world/live/2020/jan/11/iran-plane-crach-tehran-admits-firing-missile-ukrainian-airliner-disastrous-mistake-live-news",
-    "type": "liveblog",
+    "id": "world/2020/jan/12/nordic-countries-at-odds-with-eu-over-minimum-wage",
+    "type": "article",
     "sectionId": "world",
     "sectionName": "World news",
-    "webPublicationDate": "2020-01-11T13:50:08Z",
-    "webTitle": "Iran crisis: Tehran admits firing missile at airliner in 'disastrous mistake' – live news",
-    "webUrl": "https://www.theguardian.com/world/live/2020/jan/11/iran-plane-crach-tehran-admits-firing-missile-ukrainian-airliner-disastrous-mistake-live-news",
-    "apiUrl": "https://content.guardianapis.com/world/live/2020/jan/11/iran-plane-crach-tehran-admits-firing-missile-ukrainian-airliner-disastrous-mistake-live-news",
+    "webPublicationDate": "2020-01-12T11:51:53Z",
+    "webTitle": "Nordic countries at odds with EU over minimum wage",
+    "webUrl": "https://www.theguardian.com/world/2020/jan/12/nordic-countries-at-odds-with-eu-over-minimum-wage",
+    "apiUrl": "https://content.guardianapis.com/world/2020/jan/12/nordic-countries-at-odds-with-eu-over-minimum-wage",
     "fields": {
-      "thumbnail": "https://media.guim.co.uk/e98d64ea3fff874857e4b91284a50103abef1701/963_320_4476_2686/500.jpg"
+      "thumbnail": "https://media.guim.co.uk/4fe3ce5411249888cb3606cb68d1b9f39b9a1045/0_91_4116_2470/500.jpg"
     },
     "isHosted": false,
     "pillarId": "pillar/news",
     "pillarName": "News"
-  }
+  },
 ]
+
+alyien_article1 = {
+  "text": "When Myriam (Leïla Bekhti) and her husband, Paul (Antoine Reinartz), employ the unflappable and serene Louise (Karin Viard) as the new nanny for their two children, they don’t anticipate how essential she will become to the smooth running of the household. And for her part, Louise embraces her status as honorary family member a little too eagerly. But while Louise is not all she seems, she’s not well written enough as a character to register as anything more than a string of button-pushing plot devices.\n\nThis is crass and exploitative stuff that feels too contrived – despite the child peril, lingering shots of knives and the increasingly demented behaviour of Louise – to build much in the way of real tension. Viard does her best, but the credibility of the picture, as well as that of her character, is undermined somewhat by her frenzied attack against an imaginary octopus.",
+  "sentences": [
+  "When Myriam (Leïla Bekhti) and her husband, Paul (Antoine Reinartz), employ the unflappable and serene Louise (Karin Viard) as the new nanny for their two children, they don’t anticipate how essential she will become to the smooth running of the household.",
+  "And for her part, Louise embraces her status as honorary family member a little too eagerly.",
+  "But while Louise is not all she seems, she’s not well written enough as a character to register as anything more than a string of button-pushing plot devices.",
+  "This is crass and exploitative stuff that feels too contrived – despite the child peril, lingering shots of knives and the increasingly demented behaviour of Louise – to build much in the way of real tension.",
+  "Viard does her best, but the credibility of the picture, as well as that of her character, is undermined somewhat by her frenzied attack against an imaginary octopus."
+  ]
+}
+
+alyien_article2 = {
+  "text": "High-wage Nordic countries, including Denmark and Sweden, are battling plans to introduce an EU-wide minimum wage over fears the measure will undermine their century-old models of collective bargaining.\n\nThe European commission will take the first step towards a common framework for setting minimum wages in EU member states on Tuesday as part of a pledge by its new president, Ursula von der Leyen to stem a “brain drain” from east to west.\n\nBut with the backing of major unions, politicians in countries where employer-worker negotiations set the rates of pay are set to challenge the proposals.\n\nThey argue that Brussels’ intervention could ultimately lead to lower wages among their workforces.\n\nDenmark’s employment minister, Peter Hummelgaard, told the Guardian that he backed higher wages for those least well paid in Europe but that “the means to achieve this goal must respect national traditions and well-functioning models”.\n\nHe said: “In Denmark, wages are negotiated by the trade unions and the employers’ organisations alone – it has been that way for more than 100 years.”\n\nOf the 28 member states, only Denmark, Italy, Cyprus, Austria, Finland and Sweden do not have a statutory minimum wage.\n\nBut workers in Nordic countries enjoy comparatively high average salaries with employers in Denmark paying labour costs of €43.50 (£37) an hour per worker in 2018 – the highest in the EU.\n\nDanes on even the lowest salaries can expect to be paid around £15 an hour. Swedish and Finnish workers are similarly well paid under their collective bargaining models.\n\nEmployees who are not union members are often covered by the collective agreements in the Nordic states. It is feared that a statutory minimum wage could lead to employers challenging such arrangements on the grounds that a reasonable pay threshold is on the statute books and it does not need be improved upon.\n\nThe commission will launch its consultation this week, but the Danish government is seeking written guarantees that its collective bargaining system will be exempt from any new directive. Finnish and Swedish ministers have voiced similar concerns.\n\nHummelgaard said he had received assurances from Nicolas Schmit, the new European commissioner for jobs and social rights, but that he feared risks remained.\n\n“We will read the forthcoming proposal carefully, as the devil is in the detail and I am still worried that our model could be undermined”, he said. “There is no doubt that the Danish government is doing everything in its power to ensure that models like the Nordic ones are not challenged by EU regulation, which is also why I discuss the matter with my European colleagues whenever I have the chance.”\n\nHenri Lindholm, a Finnish union leader who is also a leading member of Nordiska Unionen, a pan-Nordic union representing workers in the food industry, said the opposition was not to eastern European workers improving their wages, but to a “one size fits all” arrangement for Europe.\n\nLizette Risgaard, president of the Danish trade union confederation, said: “The wage-setting in the so-called Danish model of collective bargaining is based on negotiations between the social partners, and productivity and innovation are crucial elements in these negotiations.\n\n“The precondition for the Danish model is great support from both workers and companies, and wage statistics show that the Danish model in general ensures that most workers in Denmark receive a wage that is both decent and can provide a good standard of living.\n\n“A parallel system based on statutory minimum wage or universal coverage of collective agreements does not ensure that all workers receive a wage ensuring a decent standard of living. The Danish Confederation of Trade Unions therefore fears that a statutory minimum wage can weaken the effectiveness of the Danish model.”\n\nThe commission will not determine a wage level, but wants to find agreement on a set of criteria to be met when governments set their minimum salaries.\n\nBulgaria’s current minimum wage for a full time worker is €286 a month compared to €2,071 a month in Luxembourg.\n\nThe commission’s aim is to ensure member states set a minimum wage equivalent to 60% of the median salary in that country.\n\nSchmit, a former employment minister in Luxembourg, said the commission’s minimum proposal would address wage inequality within the bloc.\n\nHe told the Guardian: “The number of people in employment in the EU is at a record high. But many working people still struggle to make ends meet and can even find themselves slipping into poverty.\n\n“It’s essential that workers have a fair wage that provides for a decent standard of living, a key principle enshrined in the European Pillar of Social Rights.\n\n“Some member states have developed already high standards when it comes to wage setting which are now integral to their social fabrics, including through strong social dialogue and collective bargaining.\n\n“Promoting high standards on wages could support the kind of upward economic and social convergence – the race to the top – that helps to boost the EU’s social market economy.\n\n“An EU framework for setting minimum wages could help to combat inequalities and to avert a destructive race to the bottom in the cost of labour. At the same time, it can help to address some of the root causes of brain-drain and cross-border social dumping – a major concern for many member states.”",
+  "sentences": [
+  "High-wage Nordic countries, including Denmark and Sweden, are battling plans to introduce an EU-wide minimum wage over fears the measure will undermine their century-old models of collective bargaining.",
+  "The European commission will take the first step towards a common framework for setting minimum wages in EU member states on Tuesday as part of a pledge by its new president, Ursula von der Leyen to stem a “brain drain” from east to west.",
+  "“A parallel system based on statutory minimum wage or universal coverage of collective agreements does not ensure that all workers receive a wage ensuring a decent standard of living.",
+  "The Danish Confederation of Trade Unions therefore fears that a statutory minimum wage can weaken the effectiveness of the Danish model.”",
+  "“Some member states have developed already high standards when it comes to wage setting which are now integral to their social fabrics, including through strong social dialogue and collective bargaining."
+  ]
+  }
