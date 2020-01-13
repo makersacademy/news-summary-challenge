5fa2b268e70b9d35a6c129b293d2d0502aa90ec9
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-01-13 02:53:00 +0000

    display title names

--- scripts.js
+++ scripts.js
@@ -1,5 +1,28 @@
+const app = document.getElementById('root')
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
+app.appendChild(container)
+
 var request = new XMLHttpRequest()
 
-request.open('GET', 'https://content.guardianapis.com/search?q=news&api-key=68d87158-e915-4fcd-8d0a-a32396833208', true)
+request.open('GET', 'https://content.guardianapis.com/politics?api-key=68d87158-e915-4fcd-8d0a-a32396833208', true)
+
+request.onload = function(){
+  var data = JSON.parse(this.response)
+  data.response.results.forEach(article => {
+    const report = document.createElement('div')
+    report.setAttribute('class', 'report')
+  
+    const h1 = document.createElement('h1')
+    h1.textContent = article.webTitle
+
+    console.log(article.webTitle)
+
+    container.appendChild(report)
+    report.appendChild(h1)
+    console.log(report)
+    console.log(h1)
+  })
+} 
 
 request.send()
