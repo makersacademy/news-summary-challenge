54655d1ed0adb4c75e0dc28d3cb466f797db67dc
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-01-13 03:36:08 +0000

    image added to articles

--- scripts.js
+++ scripts.js
@@ -3,9 +3,10 @@ const container = document.createElement('div')
 container.setAttribute('class', 'container')
 app.appendChild(container)
 
+
 var request = new XMLHttpRequest()
 
-request.open('GET', 'https://content.guardianapis.com/politics?api-key=68d87158-e915-4fcd-8d0a-a32396833208', true)
+request.open('GET', 'https://content.guardianapis.com/politics?show-elements=image&show-fields=thumbnail&api-key=68d87158-e915-4fcd-8d0a-a32396833208', true)
 
 request.onload = function(){
   var data = JSON.parse(this.response)
@@ -13,15 +14,25 @@ request.onload = function(){
     const report = document.createElement('div')
     report.setAttribute('class', 'report')
   
-    const h1 = document.createElement('h1')
-    h1.textContent = article.webTitle
+    // const h2 = document.createElement('h2')
+    // h2.textContent = article.webTitle
+
+    const anchor = document.createElement('a')
+    anchor.href = `${article.webUrl}`
+    anchor.innerHTML = `${article.webTitle}`
+
+    const img = document.createElement('img')
+    img.src = `${article.fields.thumbnail}`
 
     console.log(article.webTitle)
+    console.log(anchor)
 
     container.appendChild(report)
-    report.appendChild(h1)
+    // report.appendChild(h2)
+    report.appendChild(anchor)
+    report.appendChild(img)
     console.log(report)
-    console.log(h1)
+    // console.log(h2)
   })
 } 
 
