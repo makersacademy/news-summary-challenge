fbfda6fc480a790ffc442c25db2986c5a5c3cec8
David Papamichael <davidpapamichael@gmail.com>
2020-02-09 11:13:58 +0000

    insert function added, test passed - articles now appearing on page.

--- spec/article-controller-test.js
+++ spec/article-controller-test.js
@@ -6,3 +6,20 @@
   articleList.addArticle("Test 2");
   assert.isTrue(articleController.articleListView.htmlIfy() === "<ul><li><div>Test 1</div></li><li><div>Test 2</div></li></ul>")
 })();
+
+(function testInsert(){
+  var fakeDiv = {
+    innerHTML: ""
+  }
+  var fakeDocument = {
+    getElementById: function(){
+      return fakeDiv
+    }
+  }
+  var articleList = new ArticleList();
+  var articleListView = new ArticleListView(articleList);
+  var articleController = new ArticleController(articleListView, fakeDocument);
+  articleList.addArticle("Test");
+  articleController.insert()
+  assert.isTrue(fakeDiv.innerHTML.includes("Test"));
+})();
--- src/article-controller.js
+++ src/article-controller.js
@@ -1,7 +1,15 @@
 (function(exports){
 
-    function ArticleController(articleListView){
+    function ArticleController(articleListView, doc = document){
       this.articleListView = articleListView;
+      this.doc = doc;
+    }
+
+    ArticleController.prototype.insert = function(divId = 'app'){
+      var message = this.articleListView.htmlIfy();
+      var element = this.doc.getElementById(divId);
+      element.innerHTML = message;
+
     }
 
     exports.ArticleController = ArticleController;
