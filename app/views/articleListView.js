1418dbd62fa0d1b54aefd4ed202a56460eddd8d8
Umberto Valente <uvalente89@gmail.com>
2020-02-09 15:02:49 +0000

    Created ListView that display title and body of articles

---
+++ app/views/articleListView.js
@@ -0,0 +1,15 @@
+(function (exports) {
+  class ListView {
+    constructor(articleList) {
+      this._articleList = articleList
+    }
+
+    display () {
+      return this._articleList.list.map(article => {
+        return `<div class='article'><div class='title'>${article.title}</div><div class='body'>${article.body}</div></div>`
+      }).join('')
+    }
+  }
+
+  exports.ListView = ListView
+})(this)
