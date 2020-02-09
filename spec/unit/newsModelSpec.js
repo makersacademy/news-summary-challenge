6ab100dbab929bfc9602d250ba3d46ed9f7eb481
AliciaMerlino <acmerlino1@gmail.com>
2020-02-09 12:42:52 +0000

    artcle array returns article id

--- spec/unit/newsModelSpec.js
+++ spec/unit/newsModelSpec.js
@@ -5,3 +5,5 @@ describe('it starts with a blank news heading array', expect(newsModel.newsArray
 newsModel.addArticle('News title', 'News article summary obtained using the Aylien API. Article taken from the Guardian API.');
 
 describe('it contains  article', expect(newsModel.newsArray.length).toEq(1))
+
+describe('article id is 1', expect(newsModel.newsArray[0]['id']).toEq(1))
