05b6da02a1b7befae0e9377080f0ee16ac6da85e
niki-01 <niki.manoledaki@gmail.com>
2020-02-09 17:58:28 +0000

    Prints string to DOM

--- spec/story-controller-test.js
+++ spec/story-controller-test.js
@@ -5,3 +5,27 @@
   assert.isTrue(controller.story instanceof Story)
 })();
 
+(function TestControllerAddsStory() {
+  var controller = new Controller()
+  controller.addTitle("Hi")
+
+  assert.isTrue(controller.story.title === "Hi")
+})();
+
+(function TestControllerCreatesString() {
+  var controller = new Controller()
+  controller.addTitle("Hi")
+
+  assert.isTrue(controller.createString() === "<ul><li><div><a>Hi</a></div></li></ul>")
+})();
+
+(function TestControllerPrintsString() {
+  var controller = new Controller()
+  controller.addTitle("Test")
+
+  var element = document.createElement('div');
+  element.setAttribute('id', 'app');
+  controller.printDiv();
+
+  assert.isTrue(document.getElementById('app').innerHTML.includes("Test"))
+})();
--- src/story-controller.js
+++ src/story-controller.js
@@ -2,8 +2,21 @@
 
 (function(exports) {
   function Controller() {
-    this.view = new View()
     this.story = new Story()
+    this.view = new View(this.story)
+  }
+
+  Controller.prototype.addTitle = function(text) {
+    return (this.story.addTitle(text))
+  }
+
+  Controller.prototype.createString = function() {
+    return (this.view.createString())
+  }
+
+  Controller.prototype.printDiv = function(divId = 'app') {
+    var string = this.createString()
+    document.getElementById(divId).innerHTML = string
   }
 
   exports.Controller = Controller
