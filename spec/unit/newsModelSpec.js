cd8890daa781b125049bf9d9cd833375613b0c77
AliciaMerlino <acmerlino1@gmail.com>
2020-02-09 12:45:53 +0000

    id increments by 1 for each article

--- spec/unit/newsModelSpec.js
+++ spec/unit/newsModelSpec.js
@@ -7,3 +7,7 @@ newsModel.addArticle('News title', 'News article summary obtained using the Ayli
 describe('it contains  article', expect(newsModel.newsArray.length).toEq(1))
 
 describe('article id is 1', expect(newsModel.newsArray[0]['id']).toEq(1))
+
+newsModel.addArticle('Another news title', 'Another news article summary.');
+
+describe('article id increments by 1 each article', expect(newsModel.newsArray[1]['id']).toEq(2))
