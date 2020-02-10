02d3bbfd1442daadccc491093faa504207349f4a
Lucian Boatright <lucian.boatright@gmail.com>
2020-02-10 08:25:35 +0000

    save state

---
+++ src/single-note-view.js
@@ -0,0 +1,13 @@
+// (function(exports){
+//   function SingleNoteView(singlenote){
+//     this.singlenote = singlenote
+//   }
+
+
+//   SingleNoteView.prototype.singleNoteView = function(){
+//     return (`<div>${this.singlenote.showNote()}</div>`) 
+//   }
+
+
+//   exports.SingleNoteView = SingleNoteView
+// })(this);
