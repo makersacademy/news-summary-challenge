23f38c00f2b2140fc2ab4511eedd5ee1d55b95f6
Jason <jasylwong@gmail.com>
2020-03-08 20:39:15 +0000

    test drive a single article view

---
+++ spec/single-article-view_spec.js
@@ -0,0 +1,8 @@
+describe('SingleArticleView', () => {
+  describe('.returnHTML()', () => {
+    it('returns in HTML the article summary', () => {
+      var singleArticleView = new SingleArticleView(new Article('Article 1'))
+      expect(singleArticleView.returnHTML()).toEq('<div>Article 1</div>')
+    })
+  })
+})
