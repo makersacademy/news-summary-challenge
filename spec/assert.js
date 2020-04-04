9b4a0b9d78cae289c367ad87867ab66b1cc36d25
Jara Santamaria <jarasmar@gmail.com>
2020-04-04 13:22:05 +0100

    testing framework and file structure

--- spec/assert.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-let assert = {
-  isTrue: function(assertionToCheck) {
-    if(!assertionToCheck) {
-      throw new Error ('Assertion failed: ' + assertionToCheck + ' is not truthy');
-    }
-  }
-}
