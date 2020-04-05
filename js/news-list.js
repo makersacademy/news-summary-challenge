d52b419125e1c8f37152a884c049d44a2c11e188
Gareth Harris <gdharris1000@gmail.com>
2020-04-05 11:25:44 +0100

    news list create function added

--- js/news-list.js
+++ js/news-list.js
@@ -1,7 +1,14 @@
 function NewsList(newsItem){
     this.newsItem = newsItem;
+    this.list = []
 }
 
 NewsList.prototype = (function(){
-    
+    function create(title, link, summary, image){
+        this.list.push(new NewsItem(title, link, summary, image));
+    }
+
+    return {
+        create: create
+    }
 })()
--- js/tests/news-list-test.js
+++ js/tests/news-list-test.js
@@ -1,18 +1,30 @@
 function testNewsListIntanciation(){
-    function NewsItemDouble(){
-        this.title = "test title"
-        this.link = "test link"
-        this.summary = "test summary"
-        this.image = "test image"
-    }
+    // function NewsItemDouble(){
+    //     this.title = "test title"
+    //     this.link = "test link"
+    //     this.summary = "test summary"
+    //     this.image = "test image"
+    // }
 
-    newsItem = new NewsItemDouble
-    newsList = new NewsList(newsItem);
+    // newsItem = new NewsItemDouble
+    newsList = new NewsList();
 
-    assert.isTrue(newsList.newsItem.title === "test title", "news item in list has title");
-    assert.isTrue(newsList.newsItem.link === "test link", "news item in list has link");
-    assert.isTrue(newsList.newsItem.summary === "test summary", "news item in list has summary");
-    assert.isTrue(newsList.newsItem.image === "test image", "news item in list has image");
+    // assert.isTrue(newsList.newsItem.title === "test title", "news item in list has title");
+    // assert.isTrue(newsList.newsItem.link === "test link", "news item in list has link");
+    // assert.isTrue(newsList.newsItem.summary === "test summary", "news item in list has summary");
+    // assert.isTrue(newsList.newsItem.image === "test image", "news item in list has image");
+    assert.isTrue(newsList.list.length === 0, "news list is empty on instanciation");
+}
+
+function testNewsListCreate(){
+    newsList = new NewsList();
+    newsList.create("test title", "test link", "test summary", "test image");
+    assert.isTrue(newsList.list.length === 1, "news list has one item");
+    assert.isTrue(newsList.list[0].title === "test title", "news list item 0 has title");
+    assert.isTrue(newsList.list[0].link === "test link", "news list item 0 has link");
+    assert.isTrue(newsList.list[0].summary === "test summary", "news list item 0 has summary");
+    assert.isTrue(newsList.list[0].image === "test image", "news list item 0 has image");
 }
 
 testNewsListIntanciation();
+testNewsListCreate();
