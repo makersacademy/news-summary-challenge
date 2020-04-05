0e78150eea8cc0604795f5ccd96a6060b6f47f99
naeglinghaff <kate.o.haffenden@gmail.com>
2020-04-05 13:53:43 +0100

    Successful API request

--- src/api-requester.js
+++ src/api-requester.js
@@ -7,12 +7,11 @@ class ApiRequester {
 
     let apirequester = this;
 
-    fetch('http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=  https://content.guardianapis.com/search?from-date=2020-01-04&to-date=2020-04-05&q=politics?show-fields=body').then(function(response) {
-	    // The API call was successful!
-	      console.log('success!', response);
-        let data = JSON.parse(response);
+    fetch('http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?from-date=2020-01-04&to-date=2020-04-05&q=politics?show-fields=body').then(function(response) {
+        return response.json();
+      }).then( function(data) {
         console.log(data);
-    }).catch(function (error) {
+      }).catch(function (error) {
 	     // There was an error
 	      console.warn('Uh ohh something went wrong.', error);
     });
