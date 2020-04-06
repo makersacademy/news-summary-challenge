ed18e25718ede2ac3cd4c25f32798dc0f548b448
naeglinghaff <kate.o.haffenden@gmail.com>
2020-04-06 08:02:28 +0100

    Removed shorter url from list view

--- src/article-list-view.js
+++ src/article-list-view.js
@@ -7,8 +7,7 @@ class Articlelistview {
   viewArticleListAsHTML() {
     console.log("count: " + this.articlelist.count)
     let x =  this.articlelist.list.map(article => {
-        let y = article.url.substr(0,20);
-        return (`<a href="${y}"><li><div>${article.headline}</div></li></a>`);
+        return (`<a href="${article.url}"><li><div>${article.headline}</div></li></a>`);
     });
        x = `<ul>${x.join('')}</ul>`;
        return x;
