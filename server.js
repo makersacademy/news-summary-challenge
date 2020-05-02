cc1edab1f3cf60671670b02495b3b54dfa8f5384
Alexa <alexakearns@outlook.com>
2020-05-02 19:07:34 +0100

    moved to server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+
+
+const http = require('http');
+const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
+const util = require('util');
+const formidable = require('formidable');
+const qs = require('querystring');
+const server = http.createServer {
+    res.setHeader('Content-Type': 'application/json');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.writeHead(200, {"Content-type":});// status code HTTP 200/OK
+}
+console.log("connection")
+server.listen(3000, function() {
+    console.log("Listening on port 3000");
+})
