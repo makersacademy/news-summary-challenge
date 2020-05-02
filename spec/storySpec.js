5e279e4b2d0fb61f1315b1e5be1b17c2841a021f
Harry Turnbull <hturnbull@live.co.uk>
2020-05-02 13:13:44 +0100

    R: Story.headlineComponent returns html headline component

--- spec/storySpec.js
+++ spec/storySpec.js
@@ -2,3 +2,8 @@ it('Story should have a headline', () => {
   let story = new Story(headline='Some News')
   expect(story.headline).toEqual('Some News')
 });
+
+it('Story.headlineComponent returns html headline component', () => {
+  let story = new Story(headline='Some News')
+  expect(story.headlineComponent().outerHTML).toEqual("<li><h2>Some News</h2></li>")
+});
