0d7e50eaef6b626ee1f14221288cf6b50e7d686b
Harry Turnbull <hturnbull@live.co.uk>
2020-05-02 20:11:25 +0100

    R: headlineComponent has id in li

--- spec/storySpec.js
+++ spec/storySpec.js
@@ -6,5 +6,5 @@ it('Story should have a headline', () => {
 
 it('Story.headlineComponent returns html headline component', () => {
   let story = new Story(id='some-id', headline='Some News')
-  expect(story.headlineComponent()).returnHTML("<li><h2>Some News</h2></li>")
+  expect(story.headlineComponent()).returnHTML('<li id="some-id" class="headline"><h2>Some News</h2></li>')
 });
