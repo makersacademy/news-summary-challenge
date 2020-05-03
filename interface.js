6da2aae8a1956307ca049a0bc4e435491f603c35
Harry Turnbull <hturnbull@live.co.uk>
2020-05-03 13:55:41 +0100

    interface adds click listeners for each headline

--- interface.js
+++ interface.js
@@ -1,6 +1,24 @@
 var test = '/example-guardian-api-call.json'
-var storyHandler = new StoryHandler(test);
+const storyHandler = new StoryHandler(test);
+var homePage;
 
 window.addEventListener("DOMContentLoaded",() => {
   storyHandler.renderHeadlines()
+    .then(addClickListeners)
+    .then(storeHomePage)
 })
+
+function addClickListeners() {
+  let headlines = document.getElementsByClassName('headline')
+  for (let i = 0; i < headlines.length; i++) {
+    const headline = headlines[i];
+    headline.addEventListener('click', (e) => {
+      storyHandler.renderSummary(e.currentTarget.id)
+    })
+  }
+}
+
+function storeHomePage() {
+  homePage = document.body.innerHTML
+}
+
