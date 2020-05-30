67c2030f4404ae421136d07f857f1cc42433c6a6
patrick <patrick_sawyer@hotmail.co.uk>
2020-05-30 16:16:39 +0100

    one more thing

--- src/interface.js
+++ src/interface.js
@@ -68,12 +68,10 @@ function animate() {
 
 function clear(){
 	document.getElementById('body').innerHTML = "";
-	return document.getElementById('body'); //For test
 }
 
 function load(html){
 	document.getElementById('body').appendChild(html);
-	return document.getElementById('body'); //For test
 }
 
 window.onload = function(){
