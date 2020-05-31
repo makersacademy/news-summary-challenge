700d1e7f0463189861d814c6f38e02182761ca87
GarethWoodman <gareth.woodman92@gmail.com>
2020-05-31 14:41:44 +0100

    #bodyText - returns the body's text of an article

--- src/article.js
+++ src/article.js
@@ -3,12 +3,16 @@ class Article {
     this.object = object;
   }
 
-  get title() {
+  get webTitle() {
     return this.object.webTitle;
   }
 
   get thumbnail() {
     return this.object.fields.thumbnail
+  }
+
+  get bodyText() {
+    return this.object.fields.bodyText
   }
 
 }
