8f98196dcab4112128a8263099ceb4f2243952a4
GarethWoodman <gareth.woodman92@gmail.com>
2020-05-31 16:10:58 +0100

    Added feature to show single article when thumbnail is clicked

--- src/interface.js
+++ src/interface.js
@@ -1,31 +1,53 @@
 function interface() {
   for (article of Articles.all) {
     let webTitle = document.createTextNode(`${article.webTitle}`)
-    let webTitleLink = document.createElement("A")
+    //let webTitleLink = document.createElement("A")
     //webTitleLink.setAttribute("onclick", showArticle(`${article.id}`))
-    webTitleLink.appendChild(webTitle)
+    //webTitleLink.appendChild(webTitle)
 
     let thumbnail = document.createElement("img")
-    thumbnail.setAttribute("onclick", showArticle(`${article.id}`))
+    var x = document.createElement("A")
+    thumbnail.setAttribute("onclick", `showArticle(${article.id})`)
     thumbnail.src = article.thumbnail
+    x.appendChild(thumbnail)
 
-    addElement("container", thumbnail)
-    addLines("container")
-    addElement("container", webTitleLink)
-    addLines("container", 3)
+    let container = document.getElementById("container")
+
+    container.appendChild(x)
+    addLinesTo(container)
+
+    container.appendChild(webTitle)
+    addLinesTo(container, 3)
   }
 }
 
 function showArticle(id) {
+  document.getElementById("container").style.visibility = "hidden"
+  //document.getElementById("selectedArticle").style.visibility = "visible"
 
+  let selectedArticle = document.getElementById("selectedArticle")
+  article = Articles.all[id - 1]
+
+  let thumbnail = document.createElement("img")
+  thumbnail.src = article.thumbnail
+
+  let webTitle = document.createElement("h2")
+  let webTitleText = document.createTextNode(`${article.webTitle}`)
+  webTitle.appendChild(webTitleText)
+
+  let bodyText = document.createTextNode(`${article.bodyText}`)
+
+  selectedArticle.appendChild(thumbnail)
+  addLinesTo(selectedArticle)
+
+  selectedArticle.appendChild(webTitle)
+  addLinesTo(selectedArticle)
+
+  selectedArticle.appendChild(bodyText)
 }
 
-function addLines(container, times = 1) {
+function addLinesTo(element, times = 1) {
   for (var i = 0; i < times; i++) {
-    document.getElementById(container).appendChild(document.createElement("br"));
+    element.appendChild(document.createElement("br"));
   }
-}
-
-function addElement(container, element) {
-  document.getElementById(container).appendChild(element)
 }
