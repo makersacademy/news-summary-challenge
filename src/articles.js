3b1e455ee14c82f2f46f972f3c8fe96721cc2334
Jed Stanton <jedstanton@hotmail.com>
2020-05-31 12:19:32 +0100

    Api class added to get urls from the top 10 news articles

--- src/articles.js
+++ src/articles.js
@@ -1,7 +1,30 @@
+class Api {
+  constructor(){
+    this.apis = []
+  }
+
+  add(json){
+    for(const element of json){
+      this.apis.push("http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=" + element.apiUrl)
+    }
+  }
+}
+
 class Article {
   constructor(article){
     this.api = article.response.content.apiUrl
     this.title = article.response.content.webTitle
     this.body = article.response.content.fields.body
+    Articles.add(article)
+  }
+}
+
+class Articles {
+  static instance(){
+    this.all = []
+  }
+
+  static add(article){
+    this.all.push(article)
   }
 }
--- src/interface.js
+++ src/interface.js
@@ -1,13 +1,30 @@
+function newArticles(callback){
 let xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      let response = JSON.parse(this.response);
-      var articles = response.response.results
-      function listArticles(item){
-        document.getElementById("demo").innerHTML = item.content.fields.body;
-      }
-      articles.forEach(listArticles)
-    }
-};
 xhttp.open("GET", "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?format=json&from-date=2020-05-30&order-by=newest", true);
+
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200 && callback) {
+      let response = JSON.parse(this.response);
+      let results = response.response.results
+      callback(results)
+      }
+    }
 xhttp.send();
+}
+
+var apis = new Api()
+
+function apiGrabber(json){
+  apis.add(json)
+}
+
+newArticles(apiGrabber);
+console.log(apis.apis)
+
+//articlesJson.forEach()
+
+
+// function listArticles(item){
+//   document.getElementById("demo").innerHTML = item.content.fields.body;
+// }
+//articles.forEach(listArticles)
