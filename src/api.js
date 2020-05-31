9b1a22f51463ffd461924e781942d58ebdb0d2f9
GarethWoodman <gareth.woodman92@gmail.com>
2020-05-31 15:00:40 +0100

    Included Articles.create() function

--- src/api.js
+++ src/api.js
@@ -10,6 +10,7 @@ async function main() {
     const data = await getData("https://content.guardianapis.com/search?api-key=5838985c-855e-4d2d-9b5c-12d583111aac&show-fields=all");
 
     var results = data.response.results
+    Articles.create()
     for (item of results) { new Article(item) }
 }
 
