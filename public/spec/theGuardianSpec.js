38026eadd0d1de31665f127824ac79b84c52d3df
tanilserbes <serbestanil@gmail.com>
2020-05-31 15:53:14 +0100

    taking headlines from guardian api

--- public/spec/theGuardianSpec.js
+++ public/spec/theGuardianSpec.js
@@ -1,10 +1,10 @@
-'use strict';
-let api = new Guardian('3d1bd118-3d6f-4211-a61e-3d0f966b08bb',false)
+// 'use strict';
+// let api = new Guardian('3d1bd118-3d6f-4211-a61e-3d0f966b08bb',false)
 
-describe('Connection', function() {
-  it('should return OK', async() => {
-    const { response } = await api.content.search();
-    expect(response.statusCode).toEqual(200) 
-  });
-});
+// describe('Connection', function() {
+//   it('should return OK', async() => {
+//     const { response } = await api.content.search();
+//     expect(response.statusCode).toEqual(200) 
+//   });
+// });
 
---
+++ public/src/scripts.js
@@ -0,0 +1,69 @@
+const app = document.getElementById('root')
+
+const logo = document.createElement('img')
+logo.src='images/logo.png'
+
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
+
+app.appendChild(logo)
+app.appendChild(container)
+
+var request = new XMLHttpRequest()
+request.open('GET', 'https://content.guardianapis.com/search?api-key=test', true)
+request.onload = function() {
+// Begin accessing JSON data here
+var data = JSON.parse(this.response)
+  if (request.status >= 200 && request.status < 400) {
+      data.response.results.forEach(news => {
+
+      // Create a div with a card class
+     const card = document.createElement('div')
+     card.setAttribute('class', 'card')
+
+      // Create an h1 and set the text content to the film's title
+     const h1 = document.createElement('h1')
+     h1.textContent = news.webTitle
+
+     const p = document.createElement('p')
+     news.webTitle = news.webTitle.substring(0, 300) // Limit to 300 chars
+     p.textContent = `${news.webTitle}...` // End with an ellipses
+
+     // Append the cards to the container element
+     container.appendChild(card)
+     // Each card will contain an h1 and a p
+     card.appendChild(h1)
+     card.appendChild(p)
+
+    })
+  } else {
+    const errorMessage = document.createElement('marquee')
+    errorMessage.textContent = `Gah, it's not working!`
+    app.appendChild(errorMessage)
+  }
+}
+
+request.send()
+
+
+
+
+
+// fetch('https://content.guardianapis.com/search?api-key=test')
+// .then(response => {
+//   return response.json()
+// })
+// .then(data => {
+//   //Work with JSON datahere
+//     data.response.results.forEach(news => {
+//     console.log(news)
+//   })
+// })
+// .catch(err => {
+//   // Do something for an error here
+//   console.log('error')
+// })
+
+
+// https://ghibliapi.herokuapp.com/films
+// https://content.guardianapis.com/search?api-key=test
