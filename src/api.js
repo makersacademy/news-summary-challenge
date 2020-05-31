9d999d1a27fb749abfecc8f6f1da1d0bfff0cb6a
GarethWoodman <gareth.woodman92@gmail.com>
2020-05-31 14:57:39 +0100

    Added api

---
+++ src/api.js
@@ -0,0 +1,16 @@
+async function getData(url) {
+    const response = await fetch(url);
+    return response.json()
+}
+
+async function main() {
+    //var api = "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?show-fields=all"
+    //var search = "http://content.guardianapis.com/search?show-fields=all"
+
+    const data = await getData("https://content.guardianapis.com/search?api-key=5838985c-855e-4d2d-9b5c-12d583111aac&show-fields=all");
+
+    var results = data.response.results
+    for (item of results) { new Article(item) }
+}
+
+main()
