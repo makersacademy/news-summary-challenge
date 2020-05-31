68d72fe682d850ce080b0deea9a4dbb2e38ea781
GarethWoodman <gareth.woodman92@gmail.com>
2020-05-31 19:11:53 +0100

    Refactored API

--- src/api.js
+++ src/api.js
@@ -6,10 +6,10 @@ async function getData(url) {
 }
 
 async function main() {
-    //var api = "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?show-fields=all"
-    //var search = "http://content.guardianapis.com/search?show-fields=all"
+    var api = "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl="
+    var search = "http://content.guardianapis.com/search?show-fields=all"
 
-    const data = await getData("https://content.guardianapis.com/search?api-key=5838985c-855e-4d2d-9b5c-12d583111aac&show-fields=all");
+    const data = await getData(api + search);
 
     var results = data.response.results
     for (item of results) { new Article(item) }
