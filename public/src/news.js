2825246fd2a0bfe7fdb046699b52e9c39d66d023
tanilserbes <serbestanil@gmail.com>
2020-06-01 04:57:56 +0100

    href for seeing articles added

--- public/src/news.js
+++ public/src/news.js
@@ -1,4 +1,5 @@
 const app = document.getElementById('root')
+const newlist = document.querySelector('.new-list')
 
 const logo = document.createElement('img')
 logo.src='images/logo.png'
@@ -6,34 +7,61 @@ logo.src='images/logo.png'
 const container = document.createElement('div')
 container.setAttribute('class', 'container')
 
+
+
 app.appendChild(logo)
 app.appendChild(container)
 
 
-fetch('https://content.guardianapis.com/search?api-key=3d1bd118-3d6f-4211-a61e-3d0f966b08bb')
+fetch('https://content.guardianapis.com/search?show-blocks=all&api-key=3d1bd118-3d6f-4211-a61e-3d0f966b08bb')
+
 .then(response => {
   return response.json()
+  
 })
 .then(data => {
-  //Work with JSON datahere
     data.response.results.forEach(news => {
+      //console.log(news)
+    //  pic=news.blocks.main.elements[0].assets[0]
+    //  console.log(pic)
+     window.webUrl=news.webUrl
+     var li = document.createElement('li')
+     var a = document.createElement('a')
+     a.setAttribute('href', onclick=retrieve)
+     a.setAttribute('target', '_blank')
+     a.text = "see article"
+     //console.log(a)
      const card = document.createElement('div')
      card.setAttribute('class', 'card')
-     const h1 = document.createElement('h1')
-     h1.textContent = news.webTitle
-     const p = document.createElement('p')
-     news.webTitle = news.webTitle.substring(0, 100) // Limit to 300 chars
-     p.textContent = `${news.webTitle}...` // End with an ellipses
-
-     //Append the cards to the container element
+     const h3 = document.createElement('h3')
+     h3.textContent = news.webTitle
+     //console.log(button)
+     li.appendChild(a)
      container.appendChild(card)
-     //Each card will contain an h1 and a p
-     card.appendChild(h1)
-     card.appendChild(p)
-  })
+     card.appendChild(h3)
+     card.append(li)
+     
+     
+  })  
 })
 .catch(err => {
    const errorMessage = document.createElement('marquee')
    errorMessage.textContent = `Gah, it's not working!`
    app.appendChild(errorMessage)
 })
+
+function retrieve(e){
+  e.preventDefault()
+
+  var settings = {
+    "url": `https://api.meaningcloud.com/summarization-1.0?key=c855ca1f61d1002436b3f724a17c3a72&url=${window.webUrl}&sentences=1`,
+    "method": "POST",
+    "timeout": 0,
+  };
+
+  $.ajax(settings).done(function (response) {
+    console.log(response.summary);
+  });
+}
+
+
--- public/src/summarizer.js
+++ public/src/summarizer.js
@@ -1,3 +1,4 @@
+
 // var settings = {
 //   "url": "https://api.meaningcloud.com/summarization-1.0?key=c855ca1f61d1002436b3f724a17c3a72&url=https://www.theguardian.com/us-news/live/2020/may/31/george-floyd-protests-latest-news-riots-us-cities-donald-trump-police-brutality-live&sentences=1",
 //   "method": "POST",
@@ -8,6 +9,5 @@
 //   console.log(response)
 //   console.log(response.summary);
 // });
-
 
 
