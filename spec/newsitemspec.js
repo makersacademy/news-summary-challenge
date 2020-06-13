c49a070f8a7e127f4575f0ea6062b65685bd7d1b
Edward Phillips <edd0ddi0@gmail.com>
2020-06-13 15:06:28 +0100

    Feat: test newsItem bodyText creation

--- spec/newsitemspec.js
+++ spec/newsitemspec.js
@@ -32,4 +32,30 @@ describe("newsItem", () => {
     const headline = testItem.getHeadlineHTML();
     expect(headline.className).toEqual("headline");
   });
+  it("can extract an blurb from an api response element", () => {
+    const input = apiResult.response.results[0].fields;
+    const testItem = new newsItem(input, 0);
+    const articleBody = testItem.getbodyTextHTML();
+    expect(articleBody.textContent).toEqual(input.bodyText.substr(0,200) + '...');
+  });
+  it("wraps the blurb from the api in a paragraph tag", () => {
+    const input = apiResult.response.results[0].fields;
+    const testItem = new newsItem(input, 0);
+    const articleBody = testItem.getbodyTextHTML();
+    expect(articleBody.tagName).toEqual("P");
+  });
+  it("gives the blurb an ID based on the article number", () => {
+    const input = apiResult.response.results[0].fields;
+    const articleNumber = 0
+    const testItem = new newsItem(input, articleNumber);
+    const articleBody = testItem.getbodyTextHTML();
+    expect(articleBody.id).toEqual(`Article #${articleNumber} bodytext`)
+  });
+  it("makes the blurb an element of the body class", () => {
+    const input = apiResult.response.results[0].fields;
+    const articleNumber = 0
+    const testItem = new newsItem(input, articleNumber);
+    const articleBody = testItem.getbodyTextHTML();
+    expect(articleBody.className).toEqual("body");
+  });
 })
