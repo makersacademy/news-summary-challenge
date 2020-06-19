c012a2fb676558f9e17664cf0f890d4b5c5e49cc
Harry Turnbull <hturnbull@live.co.uk>
2020-06-19 15:02:59 +0100

    Lint

--- spec/storySpec.js
+++ spec/storySpec.js
@@ -1,10 +1,16 @@
-it('Story should have a headline', () => {
-  let story = new Story(id='some-id', headline='Some News')
-  expect(story.id).toEqual('some-id')
-  expect(story.headline).toEqual('Some News')
+it("Story should have a headline", () => {
+  let story = new Story((id = "some-id"), (headline = "Some News"));
+  expect(story.id).toEqual("some-id");
+  expect(story.headline).toEqual("Some News");
 });
 
-it('Story.headlineComponent returns html headline component', () => {
-  let story = new Story(id='some-id', headline='Some News', thumbnail="/example.png")
-  expect(story.headlineComponent()).returnHTML('<li id="some-id" class="headline"><img src="/example.png"><h2>Some News</h2></li>')
+it("Story.headlineComponent returns html headline component", () => {
+  let story = new Story(
+    (id = "some-id"),
+    (headline = "Some News"),
+    (thumbnail = "/example.png")
+  );
+  expect(story.headlineComponent()).returnHTML(
+    '<li id="some-id" class="headline"><img src="/example.png"><h2>Some News</h2></li>'
+  );
 });
