24fd32363a2938ce9fd8d994d857ab0653236a37
Anthony <a.a.donovan@outlook.com>
2020-06-27 23:19:25 +0100

    replaced XML with fetch()

--- src/news-controller.js
+++ src/news-controller.js
@@ -1,31 +1,15 @@
 (function(exports) {
   function NewsController() {
-
+    this.url = 'http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=http://worrydream.com/MediaForThinkingTheUnthinkable/note.html'
   }
 
   NewsController.prototype.renderPage = function() {  
-    const promise = this.getJSON('http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=http://worrydream.com/MediaForThinkingTheUnthinkable/note.html')
-    promise
-      .then((val) => {
+    fetch(this.url)
+      .then(response => response.json())
+      .then(val => {
         const element = document.getElementById('app')
         element.innerHTML = val.text
       })
-  }
-
-  NewsController.prototype.getJSON = function(url) {
-    return new Promise((resolve, reject) => {
-      var xhr = new XMLHttpRequest();
-      xhr.open('get', url)
-      xhr.onload = () => {
-        if(xhr.status === 200) {
-          let data = JSON.parse(xhr.responseText);
-          resolve(data);
-        } else {
-          reject(Error('data not retrieved from server'))
-        }
-      };
-      xhr.send();
-    })
   }
 
   exports.NewsController = NewsController
