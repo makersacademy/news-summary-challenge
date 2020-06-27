762e6cde3a790aa91459bc0cad01b5beab8b958e
Sumner1185 <alastair.sumner@yahoo.com>
2020-06-27 14:29:57 +0100

    Add function to return headline as string of html

--- spec/news-story-spec.js
+++ spec/news-story-spec.js
@@ -10,5 +10,14 @@ function newsStoryHasAHeadline() {
   assert.isTrue(newsStory.headline === "Boris does it again");
 };
 
+function newsStoryReturnsHeadlineAsHTML() {
+  var newsStory = new NewsStory(1, "Boris does it again");
+  console.log(newsStory.returnHeadlineHtml());
+
+  assert.isTrue(newsStory.returnHeadlineHtml() === '<li id="1" class="headline"><h2>Boris does it again</h2></li>'
+  );
+};
+
 newsStoryHasAnID();
 newsStoryHasAHeadline();
+newsStoryReturnsHeadlineAsHTML();
--- src/news-story.js
+++ src/news-story.js
@@ -5,5 +5,9 @@
     this.headline = headline;
   }
 
+  NewsStory.prototype.returnHeadlineHtml = function () {
+    return `<li id="${this.id}" class="headline"><h2>${this.headline}</h2></li>`
+  };
+
   exports.NewsStory = NewsStory;
 })(this);
