6a7902b2950edd84ae8f60b106ebe73cd7aa7bcf
Sumner1185 <alastair.sumner@yahoo.com>
2020-06-27 13:23:18 +0100

    Add API keys for Aylien

---
+++ testMockNewsApi.js
@@ -0,0 +1,78 @@
+const ApiResult = {
+  "response": {
+    "status":"ok",
+    "userTier":"developer",
+    "total":2201657,
+    "startIndex":1,"pageSize":10,
+    "currentPage":1,
+    "pages":220166,
+    "orderBy":"newest",
+    "results": [
+      {
+        "id":"football/live/2020/jun/27/aston-villa-v-wolves-premier-league-live",
+        "type":"liveblog",
+        "sectionId":"football",
+        "sectionName":"Football",
+        "webPublicationDate":"2020-06-27T11:28:22Z",
+        "webTitle":"Aston Villa v Wolves: Premier League – live!",
+        "webUrl":"https://www.theguardian.com/football/live/2020/jun/27/aston-villa-v-wolves-premier-league-live",
+        "apiUrl":"https://content.guardianapis.com/football/live/2020/jun/27/aston-villa-v-wolves-premier-league-live",
+        "isHosted":false,
+        "pillarId":"pillar/sport",
+        "pillarName":"Sport"
+      },
+      {
+        "id":"stage/2020/mar/17/hottest-front-room-seats-the-best-theatre-and-dance-to-watch-online",
+        "type":"article",
+        "sectionId":"stage",
+        "sectionName":"Stage",
+        "webPublicationDate":"2020-06-27T11:24:58Z",
+        "webTitle":"Hottest front-room seats: the best theatre and dance to watch online",
+        "webUrl":"https://www.theguardian.com/stage/2020/mar/17/hottest-front-room-seats-the-best-theatre-and-dance-to-watch-online",
+        "apiUrl":"https://content.guardianapis.com/stage/2020/mar/17/hottest-front-room-seats-the-best-theatre-and-dance-to-watch-online",
+        "isHosted":false,
+        "pillarId":"pillar/arts",
+        "pillarName":"Arts"
+      },
+      {
+        "id":"football/live/2020/jun/27/championship-leeds-united-fulham-preston-cardiff-live",
+        "type":"liveblog",
+        "sectionId":"football",
+        "sectionName":"Football",
+        "webPublicationDate":"2020-06-27T11:22:40Z",
+        "webTitle":"Championship: Preston v Cardiff, Swansea v Luton and more – live!",
+        "webUrl":"https://www.theguardian.com/football/live/2020/jun/27/championship-leeds-united-fulham-preston-cardiff-live",
+        "apiUrl":"https://content.guardianapis.com/football/live/2020/jun/27/championship-leeds-united-fulham-preston-cardiff-live",
+        "isHosted":false,
+        "pillarId":"pillar/sport",
+        "pillarName":"Sport"
+      },
+      {
+        "id":"world/live/2020/jun/27/coronavirus-live-news-us-records-biggest-daily-case-rise-as-australia-continues-cluster-testing-blitz-brazil-eu-travel-ban-us",
+        "type":"liveblog",
+        "sectionId":"world",
+        "sectionName":"World news",
+        "webPublicationDate":"2020-06-27T11:11:33Z",
+        "webTitle":"Coronavirus live news: India passes 500,000 cases as Australian state of Victoria reports surge",
+        "webUrl":"https://www.theguardian.com/world/live/2020/jun/27/coronavirus-live-news-us-records-biggest-daily-case-rise-as-australia-continues-cluster-testing-blitz-brazil-eu-travel-ban-us",
+        "apiUrl":"https://content.guardianapis.com/world/live/2020/jun/27/coronavirus-live-news-us-records-biggest-daily-case-rise-as-australia-continues-cluster-testing-blitz-brazil-eu-travel-ban-us",
+        "isHosted":false,
+        "pillarId":"pillar/news",
+        "pillarName":"News"
+      },
+      {
+        "id":"world/2020/jun/27/coronavirus-uk-map-the-latest-deaths-and-confirmed-covid-19-cases",
+        "type":"article",
+        "sectionId":"world",
+        "sectionName":"World news",
+        "webPublicationDate":"2020-06-27T11:06:17Z",
+        "webTitle":"Coronavirus UK map: the latest deaths and confirmed Covid-19 cases",
+        "webUrl":"https://www.theguardian.com/world/2020/jun/27/coronavirus-uk-map-the-latest-deaths-and-confirmed-covid-19-cases",
+        "apiUrl":"https://content.guardianapis.com/world/2020/jun/27/coronavirus-uk-map-the-latest-deaths-and-confirmed-covid-19-cases",
+        "isHosted":false,
+        "pillarId":"pillar/news",
+        "pillarName":"News"
+      }
+    ]
+  }
+}
