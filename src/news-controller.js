8684c9784496428b0b2315a50c4b75fd3ad5f214
Anthony <a.a.donovan@outlook.com>
2020-06-28 01:18:50 +0100

    replaced .then with async await

--- src/news-controller.js
+++ src/news-controller.js
@@ -3,13 +3,18 @@
     this.url = 'http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=http://worrydream.com/MediaForThinkingTheUnthinkable/note.html'
   }
 
-  NewsController.prototype.renderPage = function() {  
-    fetch(this.url)
-      .then(response => response.json())
-      .then(val => {
-        const element = document.getElementById('app')
-        element.innerHTML = val.text
-      })
+  NewsController.prototype.renderPage = async function() {  
+    const response = await fetch(this.url)
+    const responseJSON = await response.json()
+    document.getElementById('app').innerHTML = responseJSON.text
+    // fetch(this.url)
+    //   .then(response => response.json())
+    //   .then(val => {
+    //     const element = document.getElementById('app')
+    //     element.innerHTML = val.text
+    //   })
+    //   .catch(err => console.log('unable to retrieve article', err))
+    
   }
 
   exports.NewsController = NewsController
