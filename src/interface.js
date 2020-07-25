f465808b75ea7d9ebb988dee5e407cac90330914
sam landman <sam landman>
2020-07-25 12:09:42 +0100

    Sorted the timeout counters so that they follow 300ms after each other (i*300) in a loop

--- src/interface.js
+++ src/interface.js
@@ -1,23 +1,20 @@
 function speedread() {
 
   var words = document.getElementById('text').value.split(/\s/);
-  
-  for (var x = 0; x <= words.length; x+y) {
-    console.log('in the method'+words[x])
-    setTimeout(function() {
-         var $y = showWord(words,x)
-         return $y
-    }, 3000);
-    y = $y
-  }
-}
+
+  for (let i = 0; i <= words.length; i++) {
+    setTimeout(function() { showWord(words,i) }, i*300);
+  };
 
   function showWord(words,position) {
-    var $displayWord = document.getElementById('displayWord');
-    $displayWord.innerHTML = words[position] + "Text";
-    console.log('in showWord'+words[position])
-    return 1
+    if (words[position]===undefined) {
+      document.getElementById('displayWord').innerHTML = "";
+    } else {
+    document.getElementById('displayWord').innerHTML = words[position];
+    }
   };
+
+};
 
 var btn = document.getElementById("submitButton")
 btn.addEventListener("click", speedread);
