32134d8586419376a0113604bdb9f6de0d1ee6d2
sam landman <sam landman>
2020-07-26 12:15:37 +0100

    added image and time ago into block

--- src/guardiancall.js
+++ src/guardiancall.js
@@ -1,5 +1,5 @@
 var url = 'http://content.guardianapis.com/world?format=json&api-key='+key+'&show-fields=thumbnail'
-
+console.log(url)
 // Call API
 function apiCall() {
   fetch(url)
@@ -23,18 +23,45 @@ function apiCall() {
 //  date
 //  image
         data.response.results.map((newsitem, index) => {
-          console.log(index)
+          console.log(index);
+          // Create Block
           block_to_insert = document.createElement( 'div');
-          block_to_insert.id = 'div'+index
-          block_to_insert.classList.add('newsitem')
+          block_to_insert.id = 'div'+index;
+          block_to_insert.classList.add('newsitem');
           document.getElementById('newsItemGuardian').appendChild(block_to_insert);
+          // Add Image to block
+          var image = document.createElement('img');
+          console.log(newsitem.fields.thumbnail)
+          image.setAttribute("src", newsitem.fields.thumbnail);
+          document.getElementById('div'+index).appendChild(image);
+          // Add Date to block
           var date = document.createElement("P");
-          date.innerHTML = newsitem.webPublicationDate
+          // date.innerHTML = newsitem.webPublicationDate;
+          // dateinput(newsitem.webPublicationDate)
+          var year = newsitem.webPublicationDate.split("-")[0]
+          console.log("year"+year)
+          var month = newsitem.webPublicationDate.split("-")[1]-1
+          console.log("month"+month)
+          var day = newsitem.webPublicationDate.split("-")[2].split("T")[0]
+          console.log("day"+day)
+          var hour = newsitem.webPublicationDate.split("-")[2].split("T")[1].split(':')[0]
+          console.log("hour"+hour)
+          var minute = newsitem.webPublicationDate.split("-")[2].split("T")[1].split(':')[1]
+          console.log("minute"+minute)
+          var second = newsitem.webPublicationDate.split("-")[2].split("T")[1].split(':')[2].slice(0, -1)
+          console.log("second"+second)
+          dateinput = new Date(year,month,day,hour,minute,second)
+          console.log(dateinput)
+          console.log("Wed Mar 25 2015 00:00:00 GMT+0000 (Greenwich Mean Time)")
+          console.log("date" + americanpie(dateinput))
+          date.innerHTML = americanpie(dateinput)
           document.getElementById('div'+index).appendChild(date);
+          // Add Paragraph to block
           var para = document.createElement("P");
-          para.innerHTML = newsitem.webTitle
+          para.innerHTML = newsitem.webTitle;
           document.getElementById('div'+index).appendChild(para);
-          console.log(newsitem.id)
+          
+          // console.log(newsitem.id);
           // console.log(newsitem.webTitle)
           // console.log(newsitem.webPublicationDate)
         })
@@ -42,6 +69,8 @@ function apiCall() {
   })
   // 
 }
+
+function date() {}
 
 
 // Map results with:
