e52e2be18954a392352d05ef85324fe1c3776f0b
sam landman <sam landman>
2020-07-26 09:06:04 +0100

    added API key

--- src/interface.js
+++ src/interface.js
@@ -1,20 +1,4 @@
-function speedread() {
-
-  var words = document.getElementById('text').value.split(/\s/);
-
-  for (let i = 0; i <= words.length; i++) {
-    setTimeout(function() { showWord(words,i) }, i*300);
-  };
-
-  function showWord(words,position) {
-    if (words[position]===undefined) {
-      document.getElementById('displayWord').innerHTML = "";
-    } else {
-    document.getElementById('displayWord').innerHTML = words[position];
-    }
-  };
-
-};
+console.log(key)
 
 var btn = document.getElementById("submitButton")
 btn.addEventListener("click", speedread);
---
+++ src/speedread.js
@@ -0,0 +1,17 @@
+function speedread() {
+
+  var words = document.getElementById('text').value.split(/\s/);
+
+  for (let i = 0; i <= words.length; i++) {
+    setTimeout(function() { showWord(words,i) }, i*300);
+  };
+
+  function showWord(words,position) {
+    if (words[position]===undefined) {
+      document.getElementById('displayWord').innerHTML = "";
+    } else {
+    document.getElementById('displayWord').innerHTML = words[position];
+    }
+  };
+
+};
