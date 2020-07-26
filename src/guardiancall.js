f44cf4b44e61c1f0d095f30275be2079772a8172
sam landman <sam landman>
2020-07-26 09:36:55 +0100

    building up API call

---
+++ src/guardiancall.js
@@ -0,0 +1,3 @@
+var url = 'http://content.guardianapis.com/world?format=json&api-key='+key
+
+console.log(url)
--- src/interface.js
+++ src/interface.js
@@ -1,4 +1,13 @@
 console.log(key)
 
+var headline;
+
+// http://content.guardianapis.com/world?format=json&api-key=fb55a13f-6bcc-4281-b9d6-e8f8331d48c3
+
 var btn = document.getElementById("submitButton")
 btn.addEventListener("click", speedread);
+
+window.addEventListener("DOMContentLoaded", () => {
+  console.log('We have executed the DOM Content Loaded');
+  console.log(url)
+})
