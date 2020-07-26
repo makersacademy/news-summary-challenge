a8d3579a46c03f53cf333ec31cb4c6e0c6dde842
sam landman <sam landman>
2020-07-26 09:59:02 +0100

    handling response status

--- src/guardiancall.js
+++ src/guardiancall.js
@@ -1,3 +1,25 @@
 var url = 'http://content.guardianapis.com/world?format=json&api-key='+key
 
-console.log(url)
+console.log(url);
+apiCall();
+
+// Call API
+function apiCall() {
+  fetch(url)
+
+  .then(
+    function(response) {
+    console.log(response.status)
+  })
+  // console.log("We have got to fetch")
+  // var result = await request.response.json();
+  // console.log('We have a response:'+ result)
+  // console.log(result)
+}
+// Await Response
+// Receive JSON back
+// Map results with:
+//  id
+//  title
+//  date
+//  image
