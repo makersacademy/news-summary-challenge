43829fa7a3e5243cd44611aff0bc6896d94f3528
Tristan Langford <tristanlangford@hotmail.com>
2020-08-05 21:04:04 +0100

    centered back button

--- public/src/article_summary_view.js
+++ public/src/article_summary_view.js
@@ -5,7 +5,7 @@
     }
 
     ArticleSummaryView.prototype.singleArticleView = function() {
-        return `<div class="jumbotron jumbotron-fluid"><div class="container"><h1 class="display-4">${this._article.getTitle()}</h1><p class="lead">${this._article.getSummary()}</p></div></div></div><div class="article-body">${this._article.getBody()}</div><form action='${this._article.getUrl()}' class='go-to-article-site'><input type=submit value='Go to Article' class='btn btn-light'></form><form action='#0' class='go-back-form'><input type=submit value='Go back' class='btn btn-light'></form></div>`
+        return `<div class="jumbotron jumbotron-fluid"><div class="container"><h1 class="display-4">${this._article.getTitle()}</h1><p class="lead">${this._article.getSummary()}</p></div></div></div><div class="article-body">${this._article.getBody()}</div><form action='${this._article.getUrl()}' class='go-to-article-site'><input type=submit value='Go to Article' class='btn btn-light'></form><div id="go-back-div"><button onclick="location.href='#0'" type="button" id="go-back-button" class='btn btn-light'>Go back</button></div></div>`
     }
 
 
