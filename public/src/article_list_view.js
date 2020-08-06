2b3a8f536c868cbfb6b54184450b13813ced6366
Tristan Langford <tristanlangford@hotmail.com>
2020-08-06 09:48:33 +0100

    made artcle box link

--- public/src/article_list_view.js
+++ public/src/article_list_view.js
@@ -9,7 +9,7 @@
             if (i % 3 === 0) {
                 viewArray.push(`</div><div class="row">`)
             }
-            viewArray.push(`<div class="col-sm"><div class="headline">${arrayOfArticles[i].getTitle()}</div><div><img src='${arrayOfArticles[i].getThumbnail()}' class='pic'></div><button onclick="location.href='#${arrayOfArticles[i].getId()}'" type="button" id='see-more-button' class='btn btn-light'>See More</button></div>`)
+            viewArray.push(`<div class="col-sm"><a href='#${arrayOfArticles[i].getId()}'><div class="headline">${arrayOfArticles[i].getTitle()}</div><div><img src='${arrayOfArticles[i].getThumbnail()}' class='pic'></div></a></div>`)
 
         }
         viewArray.push("</div>")
