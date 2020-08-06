f1772332a9066b47f9bcf8b587f5340f2d89aa75
Tristan Langford <tristanlangford@hotmail.com>
2020-08-06 10:48:44 +0100

    refactored

--- public/src/interface.js
+++ public/src/interface.js
@@ -8,16 +8,24 @@ window.addEventListener(`DOMContentLoaded`, function (event) {
         document.getElementById('app').innerHTML = controller.convertListToView()
     } 
 
+    function showArticleSummary(id) {
+        var singleArticle = new ArticleSummaryView(controller.getArrayOfArticles()[(parseInt(id) - 1)])
+        document.getElementById('app').innerHTML = singleArticle.singleArticleView()
+    }
+
+    function returnToListView() {
+        controller.createArticleObjects()
+        document.getElementById('app').innerHTML = controller.convertListToView()
+    }
+
     articles()
 
     window.addEventListener("hashchange", function(event) {
         var id = window.location.hash.slice('#')[1]
         if (id > 0) {
-            var singleArticle = new ArticleSummaryView(controller.getArrayOfArticles()[(parseInt(id) - 1)])
-            document.getElementById('app').innerHTML = singleArticle.singleArticleView()
+            showArticleSummary(id)
         } else {
-            controller.createArticleObjects()
-            document.getElementById('app').innerHTML = controller.convertListToView()
+            returnToListView()
         }
         window.scroll(0, 0)
     })
