ccafb63294ec0e3b5e2330290701f8ae36dc9dd3
Tristan Langford <tristanlangford@hotmail.com>
2020-08-06 11:30:54 +0100

    refactored tests

--- Spec/article_list_view_spec.js
+++ Spec/article_list_view_spec.js
@@ -1,7 +1,7 @@
 function listViewTest1() {
     var articleObjectsArray = [new Article(0, "title 1", "url 1", "image", "body", "summary"), new Article(1, "title 2", "url 2", "image", "body", "summary"), new Article(2, "title 3", "url 3", "image", "body", "summary")]
     var listView = new ListView()
-    var string1 = `<div class="container"><div class="row"></div><div class="row"><div class="col-sm"><div class="headline">title 1</div><div><img src='image' class='pic'></div><form action='#0' class="see-more-form"><input type=submit value='See more' id='see-more-button' class='btn btn-light'></form></div><div class="col-sm"><div class="headline">title 2</div><div><img src='image' class='pic'></div><form action='#1' class="see-more-form"><input type=submit value='See more' id='see-more-button' class='btn btn-light'></form></div><div class="col-sm"><div class="headline">title 3</div><div><img src='image' class='pic'></div><form action='#2' class="see-more-form"><input type=submit value='See more' id='see-more-button' class='btn btn-light'></form></div></div>`
+    var string1 = `<div class="container"><div class="row"></div><div class="row"><div class="col-sm"><a href='#0'><div class="headline">title 1</div><div><img src='image' class='pic'></div></a></div><div class="col-sm"><a href='#1'><div class="headline">title 2</div><div><img src='image' class='pic'></div></a></div><div class="col-sm"><a href='#2'><div class="headline">title 3</div><div><img src='image' class='pic'></div></a></div></div>`
     expect.isEqual(listView.createListView(articleObjectsArray) === string1, "Creates HTML view")
 }
 
