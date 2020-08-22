c97b08cfda37e38c82b35e8564eb87032825d94f
Ryan Viegas <rjkviegas@gmail.com>
2020-08-22 09:30:26 +0100

    Article testInstancesOfArticleHaveAHeadline

--- src/article-model.js
+++ src/article-model.js
@@ -1,5 +1,17 @@
 (function(exports) {
-  var Article = function() {}
+  var Article = function(headline) {
+    this.headline = headline;
+  }
+  Article.prototype = (function() {
+
+    function showHeadline() {
+      return this.headline;
+    };
+
+    return {
+      showHeadline
+    };
+  })();
 
   exports.Article = Article;
 })(this);
--- tests/article-test.js
+++ tests/article-test.js
@@ -4,6 +4,12 @@
 
     assert.isTrue(article instanceof Article);
   };
+  function testInstancesOfArticleHaveAHeadline() {
+    var article = new Article("A Headline");
+
+    assert.isTrue(article.showHeadline() === "A Headline");
+  };
 
   testArticleIsAConstructor();
+  testInstancesOfArticleHaveAHeadline();
 })(this);
