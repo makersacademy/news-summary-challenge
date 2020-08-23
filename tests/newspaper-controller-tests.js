4d7c37c3f3929c0116df31ce451221f3e6694fee
Harry LB <harry.lingardbright@gmail.com>
2020-08-23 13:52:22 +0100

    refactored to include some mocking

--- tests/newspaper-controller-tests.js
+++ tests/newspaper-controller-tests.js
@@ -1,9 +1,22 @@
 (function(exports){
-
+  function ArticleDouble(text, id) {
+    this.text = text;
+    this.id = id;
+    this.articleID = function() {
+      return this.id
+    };
+    this.content = function() {
+      return this.text
+    };
+  } 
   function NewspaperDouble() {
     this.content = [];
     this.articles = function () {
       return this.content;
+    };
+    this.addArticle = function (text) {
+      article = new ArticleDouble(text, 0);
+      this.articles().push(article);
     };
   };
   newspaper = new NewspaperDouble();
@@ -16,9 +29,8 @@
   renderEmptyNewspaper();
 
   function singleArticleViewTest() {
-    var paper = new Newspaper();
-    paper.addArticle('Testing Testing');
-    var controller = new NewspaperController(paper);
+    newspaper.addArticle('Testing Testing');
+    var controller = new NewspaperController(newspaper);
     controller.listenForHashChange(controller.view());
     window.location.hash = '#articles/0';
     window.dispatchEvent(new HashChangeEvent("hashchange"));
