27fb0e92bad01d5000162352afda9e65a6166c45
Melanie Anderson <melanieanderson1995@gmail.com>
2020-08-24 09:25:10 +0100

    new summary

---
+++ src/news.js
@@ -0,0 +1,12 @@
+(function(exports) {
+  function News() {
+    this.webTitle = webTitle;
+    this.webUrl = webUrl;
+  }
+
+News.prototype. = function () {
+
+}
+
+  exports.News = News;
+})(this);
