c067361bd481509ef1b4e0590ea3ed13719f9048
Iliyan-Y <maximizebg@gmail.com>
2020-09-19 12:22:38 +0100

    Titles rednered on home page

--- public/index.js
+++ public/index.js
@@ -1,15 +1,6 @@
 window.addEventListener('load', (event) => {
-  // let getBody = article.response.content.fields.body;
-  // let app = document.getElementById('app');
-  // let hiddenDiv = document.createElement('div');
-  // hiddenDiv.style.visibility = 'hidden';
-  // hiddenDiv.innerHTML = getBody;
-  // let blockDiv = hiddenDiv.getElementsByClassName('block-elements');
-  // let headers = [...blockDiv].map((each) => {
-  //   let pTags = each.getElementsByTagName('p');
-  //   return pTags[0].innerHTML;
-  // });
-  // let firstP = [...blockDiv][0].getElementsByTagName('p');
-  // app.innerHTML = headers.join(' ');
-  // console.log(headers);
+  let homeView = new HomeView();
+  let newsPull = new NewsPull();
+  let homeController = new HomeController(homeView, newsPull);
+  homeController.renderHome();
 });
