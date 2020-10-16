fbfa73994c8b15892993bb32b45bd64e708767bf
Connor Worthington <worthington.connor@gmail.com>
2020-10-16 12:01:44 +0100

    Refactors Article tests

--- spec/articleSpec.js
+++ spec/articleSpec.js
@@ -1,15 +1,15 @@
 describe('Article', () => {
 
+  const article = new Article('Breaking news', 'This is a massive news story')
+  
   describe('.getHeadline', () => {
     it('returns the headline of the article' , () => {
-      article = new Article('Breaking news', 'This is the body');
       expect(article.getHeadline()).toEq('Breaking news')
     })
   })
 
   describe('.getBody', () => {
     it('returns the body of the article', () => {
-      article = new Article('Breaking', 'This is a massive news story');
       expect(article.getBody()).toEq('This is a massive news story')
     })
   })
