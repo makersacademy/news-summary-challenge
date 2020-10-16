541a660c7fa8a17c39f121e9b80056bfb600d595
Connor Worthington <worthington.connor@gmail.com>
2020-10-16 17:43:18 +0100

    Adds getArticles
    function to return list of articles from the articleList

--- spec/articleListViewSpec.js
+++ spec/articleListViewSpec.js
@@ -1,16 +1,38 @@
 describe('ArticleListView', () => {
 
+  class ArticleDouble {
+    constructor(headline, body) {
+      this.headline = headline
+      this.body = body
+    }
+  }
+
   class ArticleListDouble {
     constructor() {
-      this.articles =['article1']
+      this.articles = []
+    }
+    addArticle(article) {
+      this.articles.push(article)
     }
    }
 
   describe('this.articleList', () => {
     it('instantiates with a ArticleList object', () => {
+      article = new ArticleDouble('article1', 'this is an article');
       articleList = new ArticleListDouble();
+      articleList.addArticle(article);
       articleListView = new ArticleListView(articleList);
       expect(articleListView.articleList.articles.length).toEq(1);
+    })
+  })
+
+  describe('.getArticle', () => {
+    it('returns a list of articles', () => {
+      article = new ArticleDouble('article1', 'this is an article');
+      articleList = new ArticleListDouble(article);
+      articleList.addArticle(article);
+      articleListView = new ArticleListView(articleList);
+      expect(articleListView.getArticles(0)).toEq('article1');
     })
   })
 })
--- src/articleListView.js
+++ src/articleListView.js
@@ -3,4 +3,8 @@ class ArticleListView {
   constructor(articlelist) {
     this.articleList = articlelist
   }
+
+  getArticles(id) {
+    return this.articleList.articles[id].headline
+  }
 }
