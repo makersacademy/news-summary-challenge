7a5171385a6e63bac83e3a53bb5e160f953b222e
samcolson4 <samcolson4@gmail.com>
2020-10-16 14:30:54 +0100

    multi article output tested

--- spec/article_view_spec.js
+++ spec/article_view_spec.js
@@ -13,12 +13,25 @@ class ArticleViewSpec {
     aList.createArticle("The cats", "www.cats.com", "Pets")
 
     var aView = new ArticleView(aList)
+    // console.log(aView.output())
 
-    Test.assert(aView.output() == "<div><img src='PLACEHOLDER' class='image'><h1>The cats</h1><h3>Pets</h3><a href='www.cats.com'>Click here to read the full article</a><br><hr><br></div>", "Outputs a string of HTML"
-    )
-    
+    Test.assert(aView.output() == "<div><div><img src='PLACEHOLDER' class='image'><h1>The cats</h1><h3>Pets</h3><a href='www.cats.com' >Click here to read the full article</a><br><hr><br></div></div>", "Outputs a string of HTML")
+  }
+
+  static multiArticleOutput() {
+    var bList = new ArticleList
+    bList.createArticle("The cats", "www.cats.com", "Pets")
+    bList.createArticle("The dogs", "www.dogs.com", "World News")
+
+    var bView = new ArticleView(bList)
+
+    // console.log(bView.output())
+
+    Test.assert(bView.output() == "<div><div><img src='PLACEHOLDER' class='image'><h1>The cats</h1><h3>Pets</h3><a href='www.cats.com' >Click here to read the full article</a><br><hr><br></div><div><img src='PLACEHOLDER' class='image'><h1>The dogs</h1><h3>World News</h3><a href='www.dogs.com' >Click here to read the full article</a><br><hr><br></div></div>", "Outputs a longer string of HTML")
+
   }
 }
 
 ArticleViewSpec.checkAccessTest()
 ArticleViewSpec.checkOutputTest()
+ArticleViewSpec.multiArticleOutput()
--- src/article_view.js
+++ src/article_view.js
@@ -6,13 +6,13 @@ class ArticleView {
   
 
   output(){
-    var htmlString = "<div><img src='PLACEHOLDER' class='image'><h1>"
+    var htmlString = "<div>"
     var allArticles = this.articleList
 
     for (var index = 0; index <= allArticles.length-1; index++) {
-      htmlString += allArticles[index].title + "</h1><h3>" + allArticles[index].section + "</h3><a href='" + allArticles[index].url + "' >Click here to read the full article</a><br><hr><br></div>"
+      htmlString += "<div><img src='PLACEHOLDER' class='image'><h1>" + allArticles[index].title + "</h1><h3>" + allArticles[index].section + "</h3><a href='" + allArticles[index].url + "' >Click here to read the full article</a><br><hr><br></div>"
     }
-    return htmlString
+    return htmlString + "</div>"
 
   }
 
