702ecb45481feab5e7d7133cce2f5f8241311917
Connor Worthington <worthington.connor@gmail.com>
2020-10-16 13:46:21 +0100

    Refactors ArticleList tests

--- spec/articleListSpec.js
+++ spec/articleListSpec.js
@@ -1,22 +1,21 @@
 describe('ArticleList', () => {
 
+  const articleList = new ArticleList();
+
   describe('this.articles', () => {
     it('stores an array of articles', () => {
-      articleList = new ArticleList();
       expect(articleList.articles).toBeTypeOfArray();
     })
   })
 
   describe('.addArticle', () => {
     it('adds an article to the array of articles', () => {
-      articleList = new ArticleList();
       articleList.addArticle('Article');
       expect(articleList.articles.length).toEq(1);
     })
   })
 
   describe('.getArticle', () => {
-    articleList = new ArticleList();
     articleList.addArticle('This is an article')
     expect(articleList.getArticles()).toBeTypeOfArray();
   })
