4be1abb683f8bd2ad7a856db480f1b27605357d8
Connor Worthington <worthington.connor@gmail.com>
2020-10-17 15:27:46 +0100

    attempting to extract tests to seperate runner.html file

--- spec/featureTests.js
+++ spec/featureTests.js
@@ -1,7 +1,5 @@
 describe('Feature Testing', () => {
-
-  // const page = window.open('../index.html')
-  // const pageContent = page.document.body.textContent
+  
   describe('page displays articles', () => {
     it('page includes article1', () => {
       article = new Article('article1', 'this is an article')
