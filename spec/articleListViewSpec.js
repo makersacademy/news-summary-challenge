cc8573ce17b231c822f26c14e6fd560fac2cdfff
Connor Worthington <worthington.connor@gmail.com>
2020-10-17 13:40:55 +0100

    Refactors ALV tests

--- spec/articleListViewSpec.js
+++ spec/articleListViewSpec.js
@@ -16,32 +16,25 @@ describe('ArticleListView', () => {
     }
    }
 
+   const article = new ArticleDouble('article1', 'this is an article');
+   const articleList = new ArticleListDouble();
+   articleList.addArticle(article);
+   const articleListView = new ArticleListView(articleList);
+
   describe('this.articleList', () => {
     it('instantiates with a ArticleList object', () => {
-      article = new ArticleDouble('article1', 'this is an article');
-      articleList = new ArticleListDouble();
-      articleList.addArticle(article);
-      articleListView = new ArticleListView(articleList);
       expect(articleListView.articleList.articles.length).toEq(1);
     })
   })
 
   describe('.getArticle', () => {
     it('returns a list of articles', () => {
-      article = new ArticleDouble('article1', 'this is an article');
-      articleList = new ArticleListDouble();
-      articleList.addArticle(article);
-      articleListView = new ArticleListView(articleList);
       expect(articleListView.getArticles(0)).toEq('article1');
     })
   })
 
   describe('.outputHTML', () => {
     it('returns string of html', () => {
-      article = new ArticleDouble('article1', 'this is an article');
-      articleList = new ArticleListDouble();
-      articleList.addArticle(article);
-      articleListView = new ArticleListView(articleList);
       expect(articleListView.outputHTML()).toEq('<div><h1>article1</h1><p>this is an article</p></div>') 
     })
   })
