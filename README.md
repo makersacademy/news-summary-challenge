877468f9709cbba59ffe0e77de9c9e3e51489c80
Louis Kirkham <louiskirkham@gmail.com>
2020-10-18 21:27:38 +0100

    added summary popup

--- README.md
+++ README.md
@@ -6,6 +6,19 @@ Week 7 - weekend challenge
 
 Some of these stories will need decomposing if they seem too large.
 
+```
+User Story 1
+As a busy politician
+I can see all of today's headlines in one place
+So I know what the big stories of the day are
+```
+
+```
+User Story 2
+As a busy politician
+I can click a link to see the original news article
+So that I can get an in depth understanding of a very important story
+```
 
 ```
 As a busy politician
@@ -15,20 +28,14 @@ So I can get a few more details about an important story
 
 ```
 As a busy politician
-I can see a picture to illustrate each news article when I browse headlines
-So that I have something nice to look at
-```
-
-```
-As a busy politician
-I can see all of today's headlines in one place
-So I know what the big stories of the day are
-```
-
-```
-As a busy politician
 I can read the site comfortably on my phone
 Just in case my laptop breaks
+```
+
+```
+As a busy politician
+I can see a picture to illustrate each news article when I browse headlines
+So that I have something nice to look at
 ```
 
 ```
--- interface.js
+++ interface.js
@@ -1,24 +1,79 @@
-
 fetch('https://content.guardianapis.com/search?q=debate&tag=politics/politics&from-date=2014-01-01&api-key=test')
-.then(response => {
-    const contentType = response.headers.get('content-type');
-    if (!contentType || !contentType.includes('application/json')) {
-      throw new TypeError("Oops, we haven't got JSON!");
+  .then(response => {
+      const contentType = response.headers.get('content-type');
+      if (!contentType || !contentType.includes('application/json')) {
+        throw new TypeError("Oops, we haven't got JSON!");
+      }
+      return response.json();
+  })
+  .then(data => {
+    let newsContainer = document.getElementById('headlines');
+    let newsArray = data.response.results
+    let webLink
+    for(let i = 0; i < newsArray.length; i++) {
+      let headline = newsArray[i].webTitle
+      webLink = newsArray[i].webUrl
+      formattedLink = `
+      <section id='news-story'>
+        <a id='news-story${i+1}' href='${webLink}'>${headline}</a>
+        <div>
+          <a id='summary-link' href=''>Click for a summary</a>
+        </div>
+      </section><br>`
+      newsContainer.innerHTML += formattedLink
     }
-    return response.json();
-})
-.then(data => {
-  let newsContainer = document.getElementById('news-summary');
-  let newsArray = data.response.results
-  for(let i = 0; i < 10; i++) {
-    let headline = newsArray[i].webTitle
-    let webLink = newsArray[i].webUrl
-    formattedLink = `<p><a id='news-story' href='${webLink}'>${headline}</a></p>`
-    newsContainer.innerHTML += formattedLink
-  }
-})
-.catch(error => console.error(error));
+    showSummary()
+  })
+  .catch(error => console.error(error));
 
+function addClickListener(elementId, clickFunction) {
+  let button = document.getElementById(elementId);
+  button.addEventListener("click", function(clickEvent) {
+    clickFunction(clickEvent);
+  });
+}
 
+function showSummary() {
+  document.getElementById('summary-link').addEventListener("click", function(clickEvent) {
+    clickEvent.preventDefault();
+    displaySummary();
+  })
+}
 
+function displaySummary() {
+  // let makersAPI = 'http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url='
+  fetch('./mockSummary.json')
+    .then(response => {
+      const contentType = response.headers.get('content-type');
+      if (!contentType || !contentType.includes('application/json')) {
+        throw new TypeError("Oops, we haven't got JSON!");
+      }
+      return response.json();
+    })
+    .then(data => {
+      let formattedData = data["text"].split("\n\n");
+      let sentences = ''
+      for(let i = 0; i < formattedData.length; i++) {
+        sentences += `<p>${formattedData[i]}</p>`;
+      }
+      document.getElementById('story-summary').innerHTML = sentences
+      hideOverlay();
+      on();
+    })
+}
 
+function hideOverlay() {
+  addClickListener("overlay", function() {
+    off();
+  });
+}
+
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
+
+  
