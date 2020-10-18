33c503ffce567d39a969203cd8c85623cd053913
Veronica Lee <veronicavlee@icloud.com>
2020-10-18 22:04:28 +0100

    Display todays articles on load of index page

--- src/articleController.js
+++ src/articleController.js
@@ -4,30 +4,30 @@ class ArticleController {
   }
 
   getTodaysArticles(callback) {
-    let xhttp = new XMLHttpRequest()
-    let path = '/search'
-    let queryStr = `?from-date=${this._getTodaysDate()}&show-elements=image&show-fields=headline%2Cthumbnail%2Cshort-url&api-key=test`
+    let xhttp = new XMLHttpRequest();
+    let path = '/search';
+    let queryStr = `?from-date=${this._getTodaysDate()}&show-elements=image&show-fields=headline%2Cthumbnail%2Cshort-url&api-key=test`;
     xhttp.onreadystatechange = function () {
       if (xhttp.readyState === 4 && xhttp.status === 200) {
-        let articles = this._convertToArticles(xhttp.responseText)
-        callback(articles)
+        let articles = this._convertToArticles(xhttp.responseText);
+        callback(articles);
       }
-    }.bind(this)
+    }.bind(this);
     xhttp.open("GET", this.newsAPIHost + path + queryStr, true);
     xhttp.send();
   }
 
   _convertToArticles(res) {
-    let resArticles = JSON.parse(res).response.results
-    let articles = []
+    let resArticles = JSON.parse(res).response.results;
+    let articles = [];
     resArticles.forEach(function(article) {
-      let articleObj = new Article(article.id, article.fields.headline, article.fields.thumbnail, article.fields.shortUrl)
+      let articleObj = new Article(article.id, article.fields.headline, article.fields.thumbnail, article.fields.shortUrl);
       articles.push(articleObj);
     })
-    return articles
+    return articles;
   }
 
   _getTodaysDate() {
-    return new Date().toISOString().slice(0, 10)
+    return new Date().toISOString().slice(0, 10);
   }
 }
--- src/articlesView.js
+++ src/articlesView.js
@@ -1,11 +1,28 @@
 newsService = new ArticleController()
+let html = ""
+
+loadArticles()
 
 function loadArticles() {
   newsService.getTodaysArticles(function (articles) {
-    let articleDiv = document.getElementById("articles");
+    let articlesDiv = document.getElementById("articles");
     articles.forEach(function (article) {
-      articleDiv.innerHTML.append(article);
+      articlesDiv.innerHTML += formatArticleHTML(article);
     });
   });
 }
 
+function formatArticleHTML(article) {
+  return `<div class="container">
+            <div class="row" style="padding: 10px">
+              <div class="col">
+                <div class="card" id="${article.id}">
+                <img class="card-img-top" src="${article.thumbnail}" alt="Article img">
+                  <div class="card-body">
+                  <h4 class="card-text" id="article-headline" style="padding: 10px;">${article.headline}</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>`
+}
