e4540d8b2a53866047aa10b27569a8ee967525c1
Dan Holmes <dh@danielholmes.com>
2020-11-12 18:55:08 +0000

    Create story class

---
+++ spec/story-spec.js
@@ -0,0 +1,5 @@
+it("can be instantiated with a headline and link", function () {
+  let story = new Story('Virus defeated', "https://www.bbc.co.uk/news/health-54873105")
+  expect(story.getHeadline()).toEqual('Virus defeated')
+  expect(story.getLink()).toEqual('https://www.bbc.co.uk/news/health-54873105')
+})
---
+++ src/story.js
@@ -0,0 +1,14 @@
+class Story {
+  constructor(headline, link) {
+    this.headline = headline
+    this.link = link
+  }
+
+  getHeadline() {
+    return this.headline
+  }
+
+  getLink() {
+    return this.link
+  }
+}
