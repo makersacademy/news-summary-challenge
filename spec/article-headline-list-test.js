75b87e8da641108268776e6947d306219ce8ff29
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-11-15 10:30:47 +0000

    Testing if ArticleHeadlineList instantiates with empty headline array - not passed

--- spec/article-headline-list-test.js
+++ spec/article-headline-list-test.js
@@ -3,3 +3,7 @@ let articleHeadlineList = new ArticleHeadlineList
 it('can be instantiated', function(){
   expect(articleHeadlineList).toBeAnInstanceOf(ArticleHeadlineList)
 })
+
+it('instantiates with empty headline array', function(){
+  expect(articleHeadlineList.headlines).toBeAnArray()
+})
