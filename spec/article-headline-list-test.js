0afca99fb9c7c4186dfc6db8006a646d917f02da
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-11-15 10:26:00 +0000

    ArticleHeadlineList class defined and can be instantiated - tested and passed

---
+++ spec/article-headline-list-test.js
@@ -0,0 +1,5 @@
+let articleHeadlineList = new ArticleHeadlineList
+
+it('can be instantiated', function(){
+  expect(articleHeadlineList).toBeAnInstanceOf(ArticleHeadlineList)
+})
--- spec/article-list-test.js
+++
@@ -1,5 +0,0 @@
-let articleList = new ArticleList
-
-it('can be instantiated', function(){
-  expect(articleList).toBeAnInstanceOf(ArticleList)
-})
