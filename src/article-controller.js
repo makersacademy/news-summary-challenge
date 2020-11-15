8f3a6b56f3b16b7ea09a28036448951adbe49aea
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-11-15 19:46:33 +0000

    Refactored http request into own function and corrected code to keep access to this

--- src/article-controller.js
+++ src/article-controller.js
@@ -48,19 +48,19 @@ class ArticleController {
   }
 
   fetchCurrentArticles(){
-    let xhttp = new XMLHttpRequest();
     let search = "/search"
     // use makers API key thing - not meant to hardcode key
     let qString = `?from-date=${this._currentDate()}&show-elements-image&show-fields=headline&api-key=test`
     let url = this.newsAPI + search + qString
-    xhttp.onreadystatechange = function() {
-      if (xhttp.readyState == 4 && xhttp.status == 200) {
-        let articleHeadlineListView = new ArticleHeadlineListView(this._retrieveArticleInfo(xhttp.responseText))
-        this._changeInnerHTML(articleHeadlineListView.returnHTML())
-      }
-    }.bind(this)
-    xhttp.open("GET", url, true);
-    xhttp.send()
+
+    this.httpGetAsync(url, function(response){
+      let allArticles = JSON.parse(response).response.results
+      allArticles.forEach(article => {
+        this.articleList.addArticle(article.fields.headline, article.id)
+      })
+      let articleHeadlineListView = new ArticleHeadlineListView(this.articleList)
+      document.getElementById("app").innerHTML = articleHeadlineListView.returnHTML()
+    }.bind(this))
     // let articleHeadlineListView = new ArticleHeadlineListView(this._retrieveArticleInfo(this.mockXMLResponse))
     // this._changeInnerHTML(articleHeadlineListView.returnHTML())
   }
@@ -68,35 +68,36 @@ class ArticleController {
   renderSummary(id){
     // use url to put through aylien summarisation
     let fetchedArticle = this.articleList.getArticle(id)
-    let xhttp = new XMLHttpRequest();
     // use makers API key thing - not meant to hardcode key
     let qString = `/${fetchedArticle.returnSummary()}?show-fields=body&api-key=test`
     let url = this.newsAPI + qString
+    this.httpGetAsync(url, function(response){
+      let summaryInfo = JSON.parse(response).response.content.fields.body
+      let articleSummaryView = new ArticleSummaryView(summaryInfo)
+      document.getElementById("app").innerHTML = articleSummaryView.returnHTML()
+    })
+  }
+
+  httpGetAsync(url, callback){
+    let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (xhttp.readyState == 4 && xhttp.status == 200) {
-        let summaryInfo = JSON.parse(xhttp.responseText).response.content.fields.body
-        console.log(summaryInfo)
-        let articleSummaryView = new ArticleSummaryView(summaryInfo)
-        this._changeInnerHTML(articleSummaryView.returnHTML())
+        callback(xhttp.responseText)
       }
     }.bind(this)
     xhttp.open("GET", url, true);
     xhttp.send()
   }
 
-  _retrieveArticleInfo(responseText){
-  // normall no stringify, normally .fields.headline
-    // let articles = JSON.stringify(responseText)
-    let allArticles = JSON.parse(responseText).response.results
-    allArticles.forEach(article => {
-      this.articleList.addArticle(article.fields.headline, article.id)
-    })
-    return this.articleList
-  }
-
-  _changeInnerHTML(newHTML){
-    document.getElementById("app").innerHTML = newHTML
-  }
+  // _retrieveArticleInfo(responseText){
+  //   // normall no stringify, normally .fields.headline
+  //   // let articles = JSON.stringify(responseText)
+  //   let allArticles = JSON.parse(responseText).response.results
+  //   allArticles.forEach(article => {
+  //     this.articleList.addArticle(article.fields.headline, article.id)
+  //   })
+  //   return this.articleList
+  // }
 
   _currentDate(){
     return (new Date().toJSON().slice(0,10))
