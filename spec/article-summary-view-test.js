2375700970a2899767ad8b996b171793d8c2e214
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-11-15 12:23:57 +0000

    Added a returnHTML funciton to ArticleSummaryView and passed equivalent test

--- spec/article-summary-view-test.js
+++ spec/article-summary-view-test.js
@@ -1,5 +1,15 @@
-let articleSummaryView = new ArticleSummaryView
+let newsArticle = {
+  returnSummary: function(){
+    return "Scientists reveal that the vaccine is only 80% effective"
+  }
+}
+
+let articleSummaryView = new ArticleSummaryView(newsArticle)
 
 it('can be instantiated', function(){
   expect(articleSummaryView).toBeAnInstanceOf(ArticleSummaryView)
+})
+
+it('returns single article summary text in html string', function(){
+  expect(articleSummaryView.returnHTML()).toEqual("<div>Scientists reveal that the vaccine is only 80% effective</div>")
 })
--- src/article-summary-view.js
+++ src/article-summary-view.js
@@ -1,3 +1,9 @@
 class ArticleSummaryView {
-  
+  constructor(article = new Article){
+    this.article = article
+  }  
+
+  returnHTML(){
+    return `<div>${this.article.returnSummary()}</div>`
+  }
 }
