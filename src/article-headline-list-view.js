c917d1566e633966d80196d2f2aaab2a6dbd39f0
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-11-15 22:50:04 +0000

    Pictures upload with headlines and are on the homepage

--- src/article-headline-list-view.js
+++ src/article-headline-list-view.js
@@ -1,13 +1,13 @@
 class ArticleHeadlineListView{
   constructor(articleList = new ArticleList){
-    this.articleList = articleList
+    this.articleList = articleList.articles
     // assuming argument passed is an object which responds to .returnHeadlines()
   }
 
   returnHTML(){
     let htmlHeadlineArray = ""
     for (let i = 0; i < this.articleList.length; i++) {
-      htmlHeadlineArray += `<li><div class="newsheadlineclass"><img class="image" src="${this.articleList[i].returnThumbnail()}" alt="The News Today"><h2 id="headline" href='#articles/${i}'>${this.articleList[i].returnTitle()}</h2></div></li>`
+      htmlHeadlineArray += `<li><div class="newsheadlineclass"><img class="image" src="${this.articleList[i].returnThumbnail()}" alt="The News Today"><a id="headline" href='#articles/${i}'>${this.articleList[i].returnTitle()}</a></div></li>`
     }
     return `<ul style="list-style-type:none">${htmlHeadlineArray}</ul>`
   }
