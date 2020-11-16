efa5b065b3accf272c6d8663c15248577a97f2c6
Mace <maceplus@gmail.com>
2020-11-16 00:08:12 +0000

    add js & change html tags

---
+++ src/main.js
@@ -0,0 +1,35 @@
+const SECRET_API_KEY = "89e36cb9-825d-498a-a539-58c3d6815fa5";
+const URL = `http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/politics?q=&show-fields=starRating,headline,thumbnail,short-url&show-refinements=all&order-by=relevance&api-key=${SECRET_API_KEY}`;
+
+
+function fetchData() {
+  fetch(URL)
+    .then((response) => {
+      if (!response.ok) {
+        throw Error("Bad Response");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      const articles = data.response.results
+        .map((article) => {
+          return `
+         <div class="article">
+          <p><img src="${article.webUrl}" alt=${article.webTitle}" /></p>
+          <h3>Headline: ${article.webTitle}</h3>
+          <p><a href=${article.webUrl} target="${article.webUrl}">Click for full Story:</a></p>
+         </div>
+         `;
+        })
+        .join("");
+      document
+        .querySelector("#news-headlines")
+        .insertAdjacentHTML("afterbegin", articles);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+fetchData();
