314bdd66530579732520386a7b90aab8d929a90c
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-11-16 00:21:00 +0000

    Refactoring article controller to follow SRP. Updated README for future improvements

--- src/article-controller.js
+++ src/article-controller.js
@@ -1,15 +1,15 @@
 class ArticleController {
   constructor(newsAPI = "http://content.guardianapis.com", articleList = new ArticleList){
     this.newsAPI = newsAPI
-    // "http://content.guardianapis.com"
     this.articleList = articleList
   }
 
-  renderCurrentArticles(){
-    // use makers API key thing - not meant to hardcode key
+  fetchCurrentArticles(){
     let url = `${this.newsAPI}/search?from-date=${this._currentDate()}&show-fields=headline%2Cthumbnail&api-key=78fb758b-9cd0-48e8-96c1-fe29eb42c6d0`
+    // let url = `${this.newsAPI}/search?from-date=${this._currentDate()}&show-fields=headline%2Cthumbnail&api-key=test`
+    this._clearArticleArray()
 
-    this.httpGetAsync(url, function(response){
+    this._httpGetAsync(url, function(response){
       let allArticles = JSON.parse(response).response.results
       allArticles.forEach(article => {
         this.articleList.addArticle(article.fields.headline, article.id, article.fields.thumbnail)
@@ -20,18 +20,19 @@ class ArticleController {
   }
 
   renderSummary(id){
-    // use url to put through aylien summarisation
-    let fetchedArticle = this.articleList.getArticle(id)
-    let qString = `/${fetchedArticle.returnUrlId()}?show-fields=body&api-key=test`
-    let url = this.newsAPI + qString
-    this.httpGetAsync(url, function(response){
+    let url = `${this.newsAPI}/${this._retreiveArticle(id).returnUrlId()}?show-fields=body&api-key=test`
+    this._httpGetAsync(url, function(response){
       let summaryInfo = JSON.parse(response).response.content.fields.body
       let articleSummaryView = new ArticleSummaryView(summaryInfo)
       document.getElementById("app").innerHTML = articleSummaryView.returnHTML()
     })
   }
 
-  httpGetAsync(url, callback){
+  _retreiveArticle(id){
+    return this.articleList.getArticle(id)
+  }
+
+  _httpGetAsync(url, callback){
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (xhttp.readyState == 4 && xhttp.status == 200) {
@@ -42,27 +43,21 @@ class ArticleController {
     xhttp.send()
   }
 
-  // _retrieveArticleHeadlines(responseText){
-  //   // normall no stringify, normally .fields.headline
-  //   // let articles = JSON.stringify(responseText)
-  //   let allArticles = JSON.parse(responseText).response.results
-  //   allArticles.forEach(article => {
-  //     this.articleList.addArticle(article.fields.headline, article.id)
-  //   })
-  //   return this.articleList
-  // }
-
   _currentDate(){
     return (new Date().toJSON().slice(0,10))
+  }
+
+  _clearArticleArray(){
+    this.articleList.articles = []
   }
 }
 
 let newsController = new ArticleController
-newsController.renderCurrentArticles()
+newsController.fetchCurrentArticles()
 
 window.addEventListener('hashchange', function(event){
   if (event.newURL.slice(-1) == '/') {
-    newsController.renderCurrentArticles()
+    newsController.fetchCurrentArticles()
   } else {
     let id = event.newURL.slice(-1)
     newsController.renderSummary(id)
