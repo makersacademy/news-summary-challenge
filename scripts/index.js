9534c49c6a8909a9a073649dde391d51f202c625
Ara <hello.ara@pm.me>
2021-01-24 00:34:13 +0900

    added modal button; only first one working

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 newsApi = "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl="
 guardian = "http://content.guardianapis.com/search?show-fields=all"
-summaryApi = "http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url="
+// summaryApi = "http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url="
 
 let guardianNews = newsApi + guardian;
 
@@ -14,27 +14,49 @@ fetch(guardianNews)
     <div class="news-item" id="${news.webUrl}"> 
       <h2>${news.webTitle}</h2>
       <img src="${news.fields.thumbnail}" class="thumbnail">
+
+      <button id="myBtn">Open Modal</button>
+
+      <div id="myModal" class="modal">
+      
+        <!-- Modal content -->
+        <div class="modal-content">
+          <span class="close">&times;</span>
+          <h2><a href="${news.webUrl}">${news.webTitle}</a></h2>
+          <img src="${news.fields.thumbnail}" class="thumbnail">
+          <p>${news.fields.bodyText}</p>
+        </div>
+
+      </div>
     </div>
     `;
-
-    let summaryUrl = summaryApi + news.webUrl;
-    fetch(summaryUrl)
-    .then((res) => res.json())
-    .then((data) => {
-      let result = data.sentences;
-      let summary = ''
-      result.forEach(function(sentence) {
-        summary += `
-        <div id="summary-${news.webUrl}">
-          <h1 a href="${news.webUrl}">${news.webTitle}</h1>
-          <img src="${news.fields.thumbnail}" class="thumbnail">
-          <p>${sentence}</p>
-        </div>
-        `
-      })
-    })
   })
   document.getElementById("news-feed").innerHTML = output;
-  
+    // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on the button, open the modal
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
 })
 .catch((err) => console.log(err))
+
