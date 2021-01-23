af38e4f66e5fa3e662ee1eb18a86976e52d92797
Will-Helliwell <wj.helliwell@gmail.com>
2021-01-23 13:25:53 +0000

    first user story complete

--- views/index.js
+++ views/index.js
@@ -1,1 +1,17 @@
-console.log("hi")
+
+fetch("http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search")
+  .then(response => {
+    // console.log("response:")
+    // console.log(response)
+    return response.json()
+  })
+  .then(data => {
+    // console.log("data:")
+    // console.log(data)
+    let subHeading = document.getElementById("subHeading")
+    data.response.results.forEach((article, index) => {
+      subHeading.insertAdjacentHTML('afterend', `<a href="#${index}">${data.response.results[index].webTitle}</a><br>`)
+    });
+  });
+
+console.log("refreshe")
