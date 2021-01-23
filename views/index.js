9c6e0b6400d831a0cc3b8c824c16c2498852649f
Will-Helliwell <wj.helliwell@gmail.com>
2021-01-23 17:56:18 +0000

    create article preview area at top, currently shows only title and text

--- views/index.js
+++ views/index.js
@@ -13,7 +13,25 @@ fetch("http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://conte
       subHeading.insertAdjacentHTML('afterend', `<br>`)
       subHeading.insertAdjacentHTML('afterend', `<img src="${data.response.results[index].fields.thumbnail}"><br>`)
       subHeading.insertAdjacentHTML('afterend', `<a href="#${index}">${data.response.results[index].webTitle}</a><br>`)
-    });
-  });
+    })
+    return data;
+  })
+  .then(data => {
+    window.addEventListener("hashchange", function() {
+      console.log("hashchange detected")
+      let index = location.hash.split('#')[1];
+      let headline = data.response.results[index].webTitle
+      console.log("Headline:")
+      console.log(headline)
+      let thumbnail = data.response.results[index].fields.thumbnail
+      let previewAreaText = document.getElementById("previewAreaText")
+      let previewImage = document.getElementById("previewImage")
+      previewAreaText.innerHTML = `${headline}`
+      previewImage.remove()
+      previewArea.insertAdjacentHTML("beforeend", `<img id="previewImage" src="${thumbnail}">`)
+    })
+  })
 
-console.log("updatedag")
+
+
+console.log("updated2")
