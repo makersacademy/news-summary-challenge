a6731644d04976932ee0a8fbca02ab31b753459a
Ara <hello.ara@pm.me>
2021-01-23 19:59:39 +0900

    headlines and thumbnails now working

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,18 @@
-const url = "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/politics/blog/2014/feb/17/alex-salmond-speech-first-minister-scottish-independence-eu-currency-live?show-fields=body"
-
+url = "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?show-fields=all"
 fetch(url)
 .then((res) => res.json())
 .then((data) => {
-  let content = data.response.content
-  let url = content.webUrl
-  var newsTitle = document.createTextNode(content.webTitle)
-  var item = document.createElement("p")
-  item.appendChild(newsTitle)
-  document.getElementById("news-feed").appendChild(item)
+  let content = data.response.results
+  console.log(content)
+  let output = ''
+  content.forEach(function(news) {
+    output += `
+    <div class="news-item">
+      <h2><a href="${news.webUrl}">${news.webTitle}</h2>
+      <img src="${news.fields.thumbnail}" class="thumbnail">
+    </div>
+    `;
+  })
+  document.getElementById("news-feed").innerHTML = output;
 })
 .catch((err) => console.log(err))
