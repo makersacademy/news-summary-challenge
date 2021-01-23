6f90e225bceb0b16cf6f98d2656d099f846cad44
calavell <calavell@tcd.ie>
2021-01-23 18:56:55 +0000

    pictures appearing in main feed

--- interface.js
+++ interface.js
@@ -1,13 +1,18 @@
-function getArticle() {
-  fetch('http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?section=football')
+function getArticles() {
+  fetch('http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?show-fields=all')
     .then(
       function(response) {
         response.json().then(function(data) {
           data.response.results.forEach(function(result) {
+            // console.log(data)
+            // console.log(data.response)
+            // console.log(data.response.results)
+            // console.log(data.response.results[0])
             let articlesDiv = document.getElementById("article-list")
-            articlesDiv.insertAdjacentHTML('beforeend', `<p>${result.webTitle}</p>`)
+            articlesDiv.insertAdjacentHTML('beforeend', `<img src="${result.fields.thumbnail}">`)
+            articlesDiv.insertAdjacentHTML('beforeend', `<p>${result.webTitle}</p><br>`)
           })
         });
       })
 }
-getArticle()
+getArticles()
