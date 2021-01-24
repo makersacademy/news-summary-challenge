020bf3c8e869b7174664471f0a3132021ce7991f
Ara <hello.ara@pm.me>
2021-01-24 20:27:12 +0900

    modal fixed, responsive screen

--- scripts/index.js
+++ scripts/index.js
@@ -4,25 +4,6 @@ guardian = "http://content.guardianapis.com/search?show-fields=all"
 
 let guardianNews = newsApi + guardian;
 
-class News {
-  constructor() {
-    this.title;
-    this.url;
-    this.thumbnail;
-    this.summary;
-  }
-}
-
-class Headlines {
-  constructor() {
-    this.collection = [];
-  }
-
-  add(news) {
-    this.collection.push(news)
-  }
-}
-
 fetch(guardianNews)
 .then((res) => res.json())
 .then((data) => {
@@ -38,57 +19,33 @@ fetch(guardianNews)
     headline.add(item)
     output += `
     <div class="news-item" id="${headline.collection.length-1}"> 
-      <h2>${news.webTitle}</h2>
+      <h2 class="trigger">${news.webTitle}</h2>
       <img src="${item.thumbnail}" class="thumbnail">
 
-      <div id="myModal${headline.collection.length-1}" class="modal">
-      
-        <!-- Modal content -->
-        <div class="modal-content">
-          <span class="close">&times;</span>
-          <h2><a href="${item.url}">${item.title}</a></h2>
-          <img src="${item.thumbnail}" class="thumbnail">
-          <p>${item.summary}</p>
-        </div>
-
+      <div class="modal">
+        <span class="btn-close">&times;</span>
+          <div class="modal-content">
+            <h2><a href="${item.url}">${item.title}</a></h2>
+            <img src="${item.thumbnail}" class="modal-thumbnail">
+            <p>${item.summary}</p>
+          </div>
       </div>
     </div>
     `;
    
   document.getElementById("news-feed").innerHTML = output;
 
-  for (var i = 0; i < headline.collection.length; i++) {
-      var news = document.getElementById(`${i}`);
-      news.addEventListener("click", function() {
-        var modal = document.getElementById(`myModal${i}`);
-        modal.setAttribute("class", "modal-visible");
-      })
-      
+  const triggers = document.getElementsByClassName("trigger");
+  const triggerArray = Array.from(triggers).entries();
+  const modals = document.getElementsByClassName("modal");
+  const closeButtons = document.getElementsByClassName("btn-close");
+
+    for (let [index, trigger] of triggerArray) {
+      const toggleModal = () => {
+        modals[index].classList.toggle("show-modal");
+      };
+
+      trigger.addEventListener("click", toggleModal);
+      closeButtons[index].addEventListener("click", toggleModal);
   }
- 
-    // Get the modal
-  var modal = document.getElementById("myModal");
-
-  // Get the button that opens the modal
-  // var btn = document.getElementById("myBtn");
-
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
-
-  // When the user clicks on the button, open the modal
-  // btn.onclick = function() {
-  //   modal.style.display = "block";
-  // }
-
-  // // When the user clicks on <span> (x), close the modal
-  // span.onclick = function() {
-  //   modal.style.display = "none";
-  // }
-
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  }
-  })})
+  })});
