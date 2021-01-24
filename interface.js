5672583c1f5e7498b0c10ceb9ec8269379e8788c
ollienorman <oliver.norman1991@gmail.com>
2021-01-24 14:41:12 +0000

    modal text works as hyperlink - aylien requests commented out due to rate limit exceeded

--- interface.js
+++ interface.js
@@ -6,15 +6,29 @@ window.onload = function() {
 
 window.addEventListener("hashchange", function(){
   let id = window.location.hash.split("#")[1];
-  display(articles[id]);
+  displayArticle(articles[id]);
 })
 
-function display(article) {
-  console.log(article)
+function displayArticle(article) {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "block";
+  // getSummary(article, "modal-text")
+  var body = article.webTitle
+  document.getElementById("modal-text").innerHTML = body.link(article.webUrl)
+  document.getElementsByClassName("close")[0].onclick = function() {
+    modal.style.display = "none";
+    history.replaceState(null, null, ' ');
+  }
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+      history.replaceState(null, null, ' ');
+    }
+  }
 }
 
 function getHeadlines() {
-  fetch(`http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?section=world&show-elements=image&from-date=2021-01-19`, {
+  fetch(`http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?section=world&show-elements=image`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
@@ -24,6 +38,19 @@ function getHeadlines() {
       console.log(data)
       var headlinesArray = data.response.results
       displayHeadlines(headlinesArray);
+    });
+  })
+}
+
+function getSummary(article, element) {
+  fetch(`http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=${article.webUrl}language=english`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then(function(response) {
+    response.json().then(function(data) {
+      document.getElementById(element).innerHTML = data.sentences
     });
   })
 }
