96e303d0f0a3987d447fe91070d496ce0885747b
Luke Wickens <lukewickens1989@gmail.com>
2021-01-24 19:53:18 +0000

    painting titles and catagories into the DOM from the api - attempted linkage to text page but not working correcctly (returning api info for speicifc story).

--- src/newsify.js
+++ src/newsify.js
@@ -6,6 +6,7 @@ class Newsify {
   async getArticles() {
     const response = await fetch("http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?order-by=newest");
     const responseData = await response.json();
+    console.log(responseData)
     return responseData.response.results
   }
 
--- src/ui.js
+++ src/ui.js
@@ -1,21 +1,22 @@
 class UI {
 
   constructor(){
-    this.title = document.getElementById('a-title')
-    this.cata = document.getElementById('a-cata')
+    this.articlesContainer = document.getElementById('articles')
   }
 
   paint(results) {
     let articlesDiv = ''
+    let count = 0
       results.forEach(element => {
-        let articleShortDiv = document.createElement('div')
-        this.title.textContent = element.webTitle;
-        this.cata.textContent = element.sectionName;
-        articleShortDiv.textContent = `${this.cata}: ${this.title}`
+        this.title = element.webTitle
+        this.cata = element.sectionName
+        this.apiLink = element.apiUrl.replace('https', 'http')
+        this.link = `http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=${this.apiLink}`
+        let articleShortDiv = `<div><h3 id="${count}"><a href="${this.link}">${this.cata}: ${this.title}</a></h3></div>`
         articlesDiv += articleShortDiv
+        count++
       });
-      console.log(articlesDiv)
+      this.articlesContainer.innerHTML = articlesDiv
   }
-
 
 }
