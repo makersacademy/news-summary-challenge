384a5465c78a98814732d2eaac46e4ba150d0a33
Ara <hello.ara@pm.me>
2021-01-25 00:34:22 +0900

    refactor create title and thumbnail

--- scripts/news.js
+++ scripts/news.js
@@ -1,8 +1,16 @@
 class News {
-  constructor() {
-    this.title;
-    this.url;
-    this.thumbnail;
-    this.summary;
+  constructor(title, url, thumbnail, summary, sectionName) {
+    this.title = title;
+    this.url = url;
+    this.thumbnail = thumbnail;
+    this.summary = summary;
+    this.sectionName = sectionName
+  }
+
+  createTitle() {
+    return `<h2 class="trigger"><span class="section">${this.sectionName}</span> ${this.title}</h2>`
+  }
+  createThumbnail() {
+    return `<img src="${this.thumbnail}" class="thumbnail"></img>`
   }
 }
