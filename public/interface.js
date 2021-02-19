6e601d8a0742626450a9da59a2c020c31f1bfadc
Dan T <dp.tyler@hotmail.co.uk>
2021-02-19 13:56:13 +0000

    add and show image working so i can now add images to the headlines

--- public/interface.js
+++ public/interface.js
@@ -3,16 +3,27 @@ let imageURL = "http://public.media.smithsonianmag.com/legacy_blog/smiley-face-1
 
 showHeadlines = () => {
   headlines.forEach(headline =>
-    document.getElementById("headlines").innerHTML += `<a href=#${headlines.indexOf(headline)}>` + headline + `</a><div id=photo${headlines.indexOf(headline)}></div>` + `<button onclick=showImage(imageURL,${headlines.indexOf(headline)})>Add smiley</button>` + "<br>");
+    document.getElementById("headlines").innerHTML += `<a href=#${headlines.indexOf(headline)}>` + headline + `</a><div id=photo${headlines.indexOf(headline)}></div>` + "<br>");
 }
 
-showImage = (src, index) => {
+addImage = (src, i) => {
   let img = document.createElement("img");
   img.src = src;
   img.width = 150;
   img.height = 75;
-  document.getElementById(`photo${index}`).appendChild(img);
+  document.getElementById(`photo${i}`).appendChild(img);
 } 
 
-showHeadlines()
+showImage = () => {
+  for(let i = 0; i < headlines.length; i++){
+    addImage(imageURL, i)
+  }
+}
 
+showHeadlines()
+showImage()
+
+// showHeadlines = () => {
+//   headlines.forEach(headline =>
+//     document.getElementById("headlines").innerHTML += `<a href=#${headlines.indexOf(headline)}>` + headline + `</a><div id=photo${headlines.indexOf(headline)}></div>` + `<button onclick=showImage(imageURL,${headlines.indexOf(headline)})>Add smiley</button>` + "<br>");
+// }
