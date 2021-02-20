ca4e118783206dd393e6041d50a3404c9ed9e3bf
Charlie Fischer <charlie.a.fischer@gmail.com>
2021-02-20 16:20:09 +0000

    can take an imageurl tag

--- spec/newsStorySpec.js
+++ spec/newsStorySpec.js
@@ -1,7 +1,14 @@
 describe('newsStory', function() {
   it('can take a news headline and return it', function(){
-    story = new newsStory();
+    let story = new newsStory();
     story.addContent('Testing headline');
     expectEqualMatcher(story.showHeadline(), 'Testing headline');
+  })
+
+  it('can take a url of a photo', function() {
+    let story = new newsStory();
+    let imageTag = 'image_url';
+    story.addContent('Testing headline', imageTag);
+    expectEqualMatcher(story.showImage(), imageTag);
   })
 })
--- src/newsStory.js
+++ src/newsStory.js
@@ -1,10 +1,13 @@
 class newsStory {
   constructor(){
     this._headline = '';
+    this._imageUrl = ''
   }
-  addContent(headline){
+  addContent(headline, imageUrl){
     this._headline = headline;
+    this._imageUrl = imageUrl;
   }
 
-  showHeadline = () => this._headline
+  showHeadline = () => this._headline;
+  showImage = () => this._imageUrl;
 }
