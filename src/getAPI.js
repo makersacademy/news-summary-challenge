08d851cd6cea082fc5b2539fdfee1a369deae057
Charlie Fischer <charlie.a.fischer@gmail.com>
2021-02-20 17:12:36 +0000

    can console log news headline and url)

--- src/getAPI.js
+++ src/getAPI.js
@@ -1,1 +1,8 @@
-fetch("http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/politics/blog/2014/feb/17/alex-salmond-speech-first-minister-scottish-independence-eu-currency-live?show-fields=body")
+getNewsData = () => {
+  fetch("http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/politics/blog/2014/feb/17/alex-salmond-speech-first-minister-scottish-independence-eu-currency-live?show-fields=body").then(response => [
+    response.json().then(newsStory => {
+      console.log(newsStory.response.content.webTitle)
+      console.log(newsStory.response.content.webUrl)
+    })
+  ])
+}
