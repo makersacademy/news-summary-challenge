ea42a2f7463f7e1266dfe93f66646df0ff61bd0e
Charlie Fischer <charlie.a.fischer@gmail.com>
2021-02-20 16:33:52 +0000

    getAPI doesnt do anything yet

---
+++ src/getAPI.js
@@ -0,0 +1,1 @@
+fetch("http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/politics/blog/2014/feb/17/alex-salmond-speech-first-minister-scottish-independence-eu-currency-live?show-fields=body")
