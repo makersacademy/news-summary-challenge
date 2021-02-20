0dc4139bafe17375793c4e234d6215da584d1680
HolsDuckett <hf.duckett@gmail.com>
2021-02-20 15:47:29 +0000

    getNewsTitle function added to retrieve json object

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
-console.log("hello world")
+
+function getNewsTitles() {
+  return fetch("http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?order-by=newest&show-fields=body&show-elements=image&page-size=20&show-fields=thumbnail")
+    .then( (response) => {
+      console.log(response.json())
+      return response.json()
+    })
+}
