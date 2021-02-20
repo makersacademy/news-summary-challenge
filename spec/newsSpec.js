17f71507adb0c9b1ae8f01ca9993bb5ed39b2be8
HolsDuckett <hf.duckett@gmail.com>
2021-02-20 15:45:36 +0000

    basic folder/file set up

---
+++ spec/newsSpec.js
@@ -0,0 +1,22 @@
+
+
+it('prints a greeting to the page', function (){
+  string = "Hello World"
+  expect(helloWorld(string)).toEqual("Hello World")
+});
+
+
+
+
+
+
+
+// it('enables person to add note to list', () => {
+//   notesList.push("note")
+//   expect(notesList[0]).toEqual("note")
+// })
+//
+// it('it abbreviates to the first 20 characters', () => {
+//   string = '123456789012345678901234567890'
+//   expect(abbreviate(string)).toEqual('12345678901234567890')
+// })
