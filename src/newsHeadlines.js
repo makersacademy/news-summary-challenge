a9d9a92022773e32db26e2d80a5d03922f1dd8d0
megan gosnell <megan_gosnell@aol.co.uk>
2021-02-21 11:31:03 +0000

    index typo

--- src/newsHeadlines.js
+++ src/newsHeadlines.js
@@ -1,39 +1,38 @@
 let newsArticles = []
 
- function getNewsTitles() {
-   return fetch("http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?order-by=newest&show-fields=body&show-elements=image&page-size=20&show-fields=thumbnail")
-     .then( (response) => {
-       console.log(response.json())
-       return response.json()
-     })
- }
+function getNewsTitles() {
+  return fetch("http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?order-by=newest&show-fields=body&show-elements=image&page-size=20&show-fields=thumbnail")
+    .then( (response) => {
+      return response.json()
+    })
+}
 
- function extractTitles(dataObject) {
-   let results = dataObject.response.results
+function extractTitles(dataObject) {
+  let results = dataObject.response.results
 
-   for ( let i = 0; i < results.length; i++ ) {
-     let selected = {
-       webTitle: results[i].webTitle,
-     }
-     newsArticles.push(selected)
-   }
-   return newsArticles
- }
+  for ( let i = 0; i < results.length; i++ ) {
+    let selected = {
+      webTitle: results[i].webTitle,
+    }
+    newsArticles.push(selected)
+  }
+  return newsArticles
+}
 
-   getNewsTitles()
-     .then( (response) => {
-     let titlesHash = extractTitles(response)
+  getNewsTitles()
+    .then( (response) => {
+    let titlesHash = extractTitles(response)
 
-     let titlesHTML = renderHTML(titlesHash).join("<br/>")
-     document.getElementById("headlines").innerHTML = titlesHTML
-   })
+    let titlesHTML = renderHTML(titlesHash).join("<br/>")
+    document.getElementById("headlines").innerHTML = titlesHTML
+  })
 
- function renderHTML(data){
-   let headlines = []
-   for ( let i = 0; i < data.length; i++ ){
-     let title = data[i].webTitle
-     let titleHTML = `<p data-titleID="${i}">${title}/p>`
-     headlines.push(titleHTML)
-   }
-   return headlines
- }
+function renderHTML(data){
+  let headlines = []
+  for ( let i = 0; i < data.length; i++ ){
+    let title = data[i].webTitle
+    let titleHTML = `<p data-titleID="${i}">${title}/p>`
+    headlines.push(titleHTML)
+  }
+  return headlines
+}
