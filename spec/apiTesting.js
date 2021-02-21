68aa413be72a270721920c5f804f6014c84e9aad
Hugh Cavanagh <hacavanagh@gmail.com>
2021-02-21 12:12:39 +0000

    appending query string to API requests

--- spec/apiTesting.js
+++ spec/apiTesting.js
@@ -5,3 +5,8 @@ const returnURL = url => ({url: url})
 it("callAPI returns the result of the fetchFunction at the correct URL", () => {
   expect(callAPI("https://example-url.com", returnURL).url).toEqual("https://example-url.com")
 })
+
+it("appends options to the URL in a query string", function() {
+  let options = {option1: "one", option2: "two"}
+  expect(callAPI("https://example-url.com", returnURL, options).url).toEqual("https://example-url.com?option1=one&option2=two")
+})
--- src/apiHandling.js
+++ src/apiHandling.js
@@ -1,5 +1,13 @@
 "use strict";
 
-function callAPI(url, fetchFunction) {
-  return fetchFunction(url)
+function callAPI(url, fetchFunction = fetch, options = {}) {
+  let queryString = ''
+  if(Object.keys(options).length > 0) {
+    let keyValuePairs = []
+    Object.keys(options).forEach((key) => {
+      keyValuePairs.push(`${key}=${options[key]}`)
+    })
+    queryString = '?' + keyValuePairs.join('&')
+  }
+  return fetchFunction(url + queryString)
 }
