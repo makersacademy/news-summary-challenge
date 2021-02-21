a40fbca042066d57beada4975a35735e172c9485
Hugh Cavanagh <hacavanagh@gmail.com>
2021-02-21 12:25:49 +0000

    Refactored to extract queryString() function

--- src/apiHandling.js
+++ src/apiHandling.js
@@ -1,13 +1,14 @@
 "use strict";
 
 function callAPI(url, fetchFunction = fetch, options = {}) {
-  let queryString = ''
-  if(Object.keys(options).length > 0) {
-    let keyValuePairs = []
-    Object.keys(options).forEach((key) => {
-      keyValuePairs.push(`${key}=${options[key]}`)
-    })
-    queryString = '?' + keyValuePairs.join('&')
-  }
-  return fetchFunction(url + queryString)
+  return fetchFunction(url + queryString(options))
+}
+
+function queryString(options) {
+  if(Object.keys(options).length <= 0) { return "" }
+  let keyValuePairs = []
+  Object.keys(options).forEach((key) => {
+    keyValuePairs.push(`${key}=${options[key]}`)
+  })
+  return '?' + keyValuePairs.join('&')
 }
