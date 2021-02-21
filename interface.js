3d07b64c127381ffef3a80f2db38fc07e22741fc
Oscar Bertrand <oscarb1989@yahoo.co.uk>
2021-02-21 13:05:56 +0000

    Added links

--- interface.js
+++ interface.js
@@ -1,7 +1,7 @@
 fetch('https://content.guardianapis.com/search?q=debate&tag=politics/politics&from-date=2020-02-20&api-key=c0cfe07c-acda-4fd8-8a6b-1a24f21528d3')
 .then(response => {
     const contentType = response.headers.get('content-type');
-    if (!contentType || !contentType.includes('application/json')) {
+    if (!contentType ) {
       throw new TypeError("ERROR");
     }
     return response.json();
@@ -10,8 +10,9 @@ fetch('https://content.guardianapis.com/search?q=debate&tag=politics/politics&fr
   let newsContainer = document.getElementById('news-title');
   let newsArray = data.response.results
   for(let i = 0; i < 10; i++) {
-    let newsStory = `<div>${newsArray[i].webTitle}</div>`
+    let newsStory = `<a href>${newsArray[i].webTitle}</a></br>`
     newsContainer.innerHTML += newsStory
+    console.log(newsArray);
   }
 })
 .catch(error => console.error(error));
