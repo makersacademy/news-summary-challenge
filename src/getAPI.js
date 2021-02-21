64184725e99fc1a4862614bdfc121bdcd6eceec7
Charlie Fischer <charlie.a.fischer@gmail.com>
2021-02-21 07:59:41 +0000

    can display the summarised text

--- src/getAPI.js
+++ src/getAPI.js
@@ -1,10 +1,19 @@
 getNewsData = () => {
   fetch("http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/politics/blog/2014/feb/17/alex-salmond-speech-first-minister-scottish-independence-eu-currency-live?show-fields=body").then(response => [
     response.json().then(newsStory => {
-      console.log(newsStory.response.content)
+      storyData = newsStory.response.content
       headlinesSection = document.querySelector('.stories')
-      headlinesSection.innerHTML = `<h1> <a href='${newsStory.response.content.webUrl}'>${newsStory.response.content.webTitle} </a></h1>`
-      // return newsStory.response.content.webTitle
+      headlinesSection.innerHTML = `<h1> <a href='${storyData.webUrl}'>${storyData.webTitle} </a></h1>`
     })
   ])
+}
+
+getNewsSummary = () =>{
+  fetch("http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=http://worrydream.com/MediaForThinkingTheUnthinkable/note.html").then(response => {
+    response.json().then(summary => {
+      newsSummary = summary.sentences.join(' ')
+      headlinesSection = document.querySelector('.stories')
+      headlinesSection.innerHTML += newsSummary
+    })
+  })
 }
--- src/interface.js
+++ src/interface.js
@@ -1,4 +1,4 @@
-getNewsData()
-
+// getNewsData()
+getNewsSummary()
 // a = document.querySelector('.stories')
 // a.innerHTML = 'hello again'
