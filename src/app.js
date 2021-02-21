ab4f07c0f091ecb659f58b424debbd0271821a68
Sean Edwards <seantrevoredwards@gmail.com>
2021-02-21 16:31:51 +0000

    added ability to display news titles on the html page

--- src/app.js
+++ src/app.js
@@ -1,17 +1,36 @@
+
 const articles = [];
 
-fetch('http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?show-fields=body')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    let total = data.response.results.length;
-    console.log(total);
-    for (let i = 0; i < total; i++) {
-      let newsData = data.response.results[i];
-      let news = new News(newsData);
-      console.log(news);
-      articles.push(news);
-    }
+function fetchNews() {
+  fetch('http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?show-fields=body')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      let count = data.response.results.length;
+      console.log(count);
+      for (let i = 0; i < count; i++) {
+        let newsData = data.response.results[i];
+        let news = new News(newsData);
+        console.log(news);
+        articles.push(news);
+        allNews();
+      }
 
-    console.log(articles);
+      console.log(articles);
+    });
+}
+
+fetchNews();
+
+function allNews() {
+  let listDiv = document.getElementById('list');
+  listDiv.innerText = ''; // clear area
+  articles.forEach(function (article, index) {
+    let newsItem = document.createElement('li');
+    let headline = article.eachNewsHeadline(index);
+    console.log(headline);
+    console.log(1);
+    newsItem.appendChild(headline);
+    listDiv.appendChild(newsItem);
   });
+}
--- src/news.js
+++ src/news.js
@@ -2,4 +2,13 @@ class News {
   constructor(newsData) {
     this.newsData = newsData;
   }
+
+  eachNewsHeadline(index) {
+    let headline = document.createElement('a');
+    headline.href = '#' + index;
+    headline.id = 'article' + index;
+    headline.textContent = this.newsData.webTitle;
+    return headline;
+  }
+
 }
