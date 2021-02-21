a4355af8040bc55c3e0e104e059166a5f0a2cc16
Charlie Fischer <charlie.a.fischer@gmail.com>
2021-02-21 07:43:38 +0000

    webpage can load alex salmond headline with url

--- src/getAPI.js
+++ src/getAPI.js
@@ -1,8 +1,10 @@
 getNewsData = () => {
   fetch("http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/politics/blog/2014/feb/17/alex-salmond-speech-first-minister-scottish-independence-eu-currency-live?show-fields=body").then(response => [
     response.json().then(newsStory => {
-      console.log(newsStory.response.content.webTitle)
-      console.log(newsStory.response.content.webUrl)
+      console.log(newsStory.response.content)
+      headlinesSection = document.querySelector('.stories')
+      headlinesSection.innerHTML = `<h1> <a href='${newsStory.response.content.webUrl}'>${newsStory.response.content.webTitle} </a></h1>`
+      // return newsStory.response.content.webTitle
     })
   ])
 }
---
+++ src/interface.js
@@ -0,0 +1,4 @@
+getNewsData()
+
+// a = document.querySelector('.stories')
+// a.innerHTML = 'hello again'
