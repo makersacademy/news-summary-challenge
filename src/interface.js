8e29419c9cb2a8d3162e747b6bb9949f1112c567
jshields123 <jackshields@live.co.uk>
2021-02-22 09:42:00 +0000

    cleaned up a bit

--- src/interface.js
+++ src/interface.js
@@ -1,37 +1,5 @@
 
 let headlinesList = []
-//
-// function updateList() {
-//   var headlines = document.getElementById('headlines')
-//   let tempHTML = ''
-//   for (let i = headlinesList.length - 1; i >= 0 ; i--) {
-//     tempHTML += headlineLayout(headlinesList[i],i)
-//
-//   }
-//   headlines.innerHTML = tempHTML
-//
-// }
-
-// function headlineLayout(string, index){
-//   return `<div id="headline-${index}" ><p id='headline-text-${index}'>${string}</p>`
-//   // <button class="minimize-button" onClick="minimizeNote(${index})" id="minimize-${index}">-</button>
-//   // <button class="expand-button" onClick="expandNote(${index})" id="expand-${index}">
-//   // +</button></div>
-//
-// }
-
-// function getHeadlines() {
-// fetch('http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?show-fields=body')
-//   .then(response => response.json())
-//   .then(data => {
-//     for(i = 0; i < data.response.results.length; i++){
-//       headlinesList.push(data.response.results[i])
-//     }
-// console.log(headlinesList)
-//     return headlinesList
-//     allHeadlines()
-//   });
-// }
 
 async function getHeadlines1() {
   const response = await fetch('http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?show-fields=body');
@@ -55,20 +23,3 @@ getHeadlines1()
 function displayHeadlines(){
 
 }
-
-// function allHeadlines(){
-//   let list = document.getElementById('headlines');
-//   list.innerHTML = '';
-//   headlinesList.forEach(function(headline, i) {
-//     let headlineItem = document.createElement('li')
-//     let headlineTitle = headlinesList.results[i]
-//
-//   });
-//
-// }
-
-
-
-// getHeadlines()
-// updateList()
-// allHeadlines()
