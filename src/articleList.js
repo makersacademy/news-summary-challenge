65f401e035a841f4dc4becd0ec5cd78dcf592159
Karsten Finlay <karstenfinlay@hotmail.com>
2021-02-22 08:30:00 +0000

    Massive ugly commit, spent too much time

---
+++ src/articleList.js
@@ -0,0 +1,13 @@
+class ArticleHeadlineListView{
+  constructor(articleList = new ArticleList){
+    this.articleList = articleList.returnArticles()
+  }
+
+  returnHTML(){
+    let htmlHeadlineArray = ""
+    for (let i = 0; i < this.articleList.length; i++) {
+      htmlHeadlineArray += `<li><div class="newsheadlineclass"><img class="image" src="${this.articleList[i].returnThumbnail()}" alt="The News Today"><a id="headline" href='#articles/${i}'>${this.articleList[i].returnTitle()}</a></div></li>`
+    }
+    return `<ul style="list-style-type:none">${htmlHeadlineArray}</ul>`
+  }
+}
---
+++ src/articleStructure.js
@@ -0,0 +1,20 @@
+class ArticleStucture {
+  constructor(title, urlId, thumbnail, id){
+    this.title = title
+    this.urlId = urlId
+    this.id = id
+    this.thumbnail = thumbnail
+  }
+
+  returnTitle(){
+    return this.title
+  }
+
+  returnUrlId(){
+    return this.urlId
+  }
+
+  returnThumbnail(){
+    return this.thumbnail
+  }
+} 
