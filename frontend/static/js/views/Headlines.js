9bbf75d6008ea3f7a0a91157b15c45e11de24e16
Mornevanzyl <catbeatsadrum@gmail.com>
2021-02-22 09:20:00 +0000

    Format change to Guardian API search

--- frontend/static/js/views/Headlines.js
+++ frontend/static/js/views/Headlines.js
@@ -13,7 +13,7 @@ export default class extends AbstractView {
     }
 
     async getHtml() {
-        return fetch('http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search')
+        return fetch('http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?show-fields=thumbnail')
         .then(res => res.json())
         .then(data => {console.log(data);return data.response.results})
         .then(stories => {
@@ -22,6 +22,7 @@ export default class extends AbstractView {
                 this.stories.push(
                     {headLine: story.webTitle,
                         webUrl: story.webUrl,
+                        thumbnail: story.fields.thumbnail,
                         storySummary: this.getAylien(story.webUrl).then(data => {return data.sentences[0]})
                     }
                 );
@@ -30,7 +31,7 @@ export default class extends AbstractView {
         .then(data => {
             let htmlData = '';
             data.forEach(story => {
-                htmlData = htmlData + `<h3><a href="${story.webUrl}">${story.headLine}</a></h3><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita iure error et. Maiores quo aliquid corporis. Quasi expedita magni cupiditate culpa, praesentium quod consectetur cum quia, dolorem veritatis, est in!</p>`
+                htmlData = htmlData + `<a href="${story.webUrl}"><img src="${story.thumbnail}"></a><h3>${story.headLine}</h3><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita iure error et. Maiores quo aliquid corporis. Quasi expedita magni cupiditate culpa, praesentium quod consectetur cum quia, dolorem veritatis, est in!</p>`
             })
             return htmlData;
         })
