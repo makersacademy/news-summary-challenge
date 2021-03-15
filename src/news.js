e0a860042faa64761ca37aa138ced2c92ee6d8c3
olga <itsglimerry@gmail.com>
2021-03-15 22:50:26 +0000

    Added summary appearing after clicking the title.

--- src/news.js
+++ src/news.js
@@ -16,14 +16,15 @@ function showAllArticles(){
       .then(oneNews => {
         console.log(oneNews.response.content.webTitle)
 
-        var newsContainer = document.createElement("article")
-        newsContainer.setAttribute("id", `${oneNews.response.content.webTitle}`)
-        newsContainer.classList.add('newsContainer')
+        var newsContainer = document.createElement("article");
+        newsContainer.setAttribute("id", `${oneNews.response.content.webTitle}`);
+        newsContainer.classList.add('newsContainer');
 
-        var newsTitle = document.createElement("h1")
+        var newsTitle = document.createElement("a");
         newsTitle.innerHTML = oneNews.response.content.webTitle
+        newsTitle.setAttribute("href", "#" + `${oneNews.response.content.id}`);
 
-        var newsContent = document.createElement("p")
+        var newsContent = document.createElement("p");
         newsContent.innerHTML = oneNews.response.content.fields.body
 
         document.querySelector(".newsWrap").appendChild(newsContainer);
@@ -31,8 +32,32 @@ function showAllArticles(){
         document.getElementById(`${oneNews.response.content.webTitle}`).appendChild(newsContent);
 
         console.log(oneNews.response.content.fields.body)
-      })
+      });
      }
-   })
-
+   });
   }
+
+  makeUrlChangeShowArticleForCurrentPage();
+
+      function makeUrlChangeShowArticleForCurrentPage() {
+        window.addEventListener("hashchange", showArticleForCurrentPage);
+      };
+
+      function showArticleForCurrentPage() {
+        showArticle(getArticleFromUrl(window.location));
+      };
+
+      function getArticleFromUrl(location) {
+        return location.hash.split("#")[1];
+      };
+
+      function showArticle(article) {
+
+
+        fetch("http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=" + "https://www.theguardian.com/" + article )
+          .then(res => res.json())
+          .then(summary => {
+            console.log(summary)
+            document.getElementById('newsWrap').innerHTML = summary.sentences
+          });
+      };
