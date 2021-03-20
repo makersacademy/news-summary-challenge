4b58fda9f416bc751666814eb166a062b2d2b969
charlierdm <charlie.davismiller@gmail.com>
2021-03-20 15:55:37 +0000

    UK News search added

--- public/interface.js
+++ public/interface.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const newsApi = "http://content.guardianapis.com/search?page=2&q=debate&api-key=test";
+const newsApi = "http://content.guardianapis.com/search?section=uk-news&show-elements=image&api-key=test";
 const summaryApi = "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=";
 const guardian = summaryApi + newsApi;
 
@@ -8,6 +8,7 @@ const getGuardianData = () => { return fetch(guardian) };
 
 const apiToHtmlList = (data) => { data.response.results.forEach((headline) => {
   let headlineNews = headline.webTitle
+  let articleUrl = headline.webUrl
   document.getElementById('view-headlines').innerHTML += `<li>${headlineNews}</li><br>`
   })
 };
@@ -22,6 +23,7 @@ ready(() => {
   getGuardianData().then(response => { response.json().then(data => {
   const guardianData = data 
   apiToHtmlList(guardianData)
+   debugger
   })
   
   })
