401bbf11a0c5594797c8248efcba5d92fd0c7a87
charlierdm <charlie.davismiller@gmail.com>
2021-03-20 19:39:15 +0000

    hyperlink to guardian site added

--- public/interface.js
+++ public/interface.js
@@ -2,18 +2,19 @@
 
 const newsApi = "http://content.guardianapis.com/search?show-fields=all&section=uk-news&api-key=test";
 const summaryApi = "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=";
+
 const guardian = summaryApi + newsApi;
 
 const getGuardianData = () => { return fetch(guardian) };
 
 const apiToHtmlList = (data) => { data.response.results.forEach((headline, index) => {
   let headlineNews = headline.fields.headline
+  let shortUrl = headline.fields.shortUrl
+  let url = headline.webUrl
   let thumbnail = headline.fields.thumbnail
-  document.getElementById('view-headlines').innerHTML += `<li><a href="#headline${[index]}"><h3>${headlineNews}</h3></a></li><br><br><img src="${thumbnail}"><br><br>  `
+  document.getElementById('view-headlines').innerHTML += `<li><h3>${headlineNews}</h3><br><a href=${url}>${shortUrl}</a></li><br><br><img src="${thumbnail}"><br><br>  `
   })
 };
-
-const webUrl = (data, index) => { return data.response.results[index].fields.shortUrl }
 
 const ready = (callback) => {
   if (document.readyState != "loading") callback();
@@ -24,7 +25,6 @@ ready(() => {
   getGuardianData().then(response => { response.json().then(data => {
     const guardianData = data 
     apiToHtmlList(guardianData)
-    
   });
  });
 });
