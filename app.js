6ebc911a53eba5de5b931103b9393dab3f6f19cf
Christine Chee <christinechee88@hotmail.com>
2021-03-21 15:48:57 +0000

    links are showing on front page

--- app.js
+++ app.js
@@ -1,12 +1,48 @@
+class News {
+    constructor(title, apiUrl, webUrl, id) {
+      this.title = title;
+      this.apiUrl = apiUrl;
+      this.webUrl = webUrl;
+      this.id = id
+    }
+}
+
 const getHeadlines = async () => {
-    const endpoint = 'https://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?order-by=newest&show-elements=image&api-key=test'
+    const endpoint = 'https://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?order-by=newest&show-elements=image&api-key=test&show-fields=thumbnail'
     try { const response = await fetch(endpoint, {cache: 'no-cache'}); 
     if (response.ok) {
       const jsonResponse = await response.json();
-      console.log(jsonResponse.response.results[0]);
+      return jsonResponse.response.results
     }
     } catch (error) {
       console.log(error)
-    }
-    
   }
+} 
+
+const headlinesBox = document.querySelector("#headlines-container");
+
+function format(headlines) {
+     let count = 1
+     headlines.forEach(news => {
+        link = news.apiUrl.replace('https', 'http')
+        headline = new News(news.webTitle, link, news.webUrl, count)
+        const div = document.createElement("div"); 
+        div.classList.add("headline"); 
+        div.setAttribute("id", headline.id); 
+        div.innerHTML = `                                                 
+                <a href="#${headline.id}">${headline.title}</a>
+        `;
+        headlinesBox.appendChild(div);
+        count++
+     })
+}
+
+function loadHeadlines() {
+    getHeadlines()
+    .then(results => {
+      format(results);
+    })
+    .catch(err => console.log(err))
+  }
+
+  document.addEventListener("DOMContentLoaded", loadHeadlines);
