b2fb6d40e97c6fe2fad41d9da2a2f732e6bb1ab2
Joey <joey.s.hornsby@gmail.com>
2021-03-21 15:24:27 +0000

    toggling displays working

--- javascript/src/interface.js
+++ javascript/src/interface.js
@@ -1,25 +1,35 @@
 articleList = document.getElementById("articles-list")
+singleArticle = document.getElementById("single-article")
 
 const apisource =
-    fetch("https://content.guardianapis.com/search?api-key=test&show-fields=thumbnail")
+    fetch("https://content.guardianapis.com/search?api-key=test&show-fields=thumbnail&show-blocks=all")
     .then(response => response.json())
     .then(data => { 
         return data.response.results });
 
-printArticles = function(articles){
+printArticles = function(articles){ 
     for(article of articles){
         let imgURL = article.fields.thumbnail
         let headlineText = article.webTitle
         let id = article.id
+        let summary = "lorem ipsum"
+        let link = document.createElement("a")
+        link.setAttribute("href", "#")
         let articleSection = document.createElement("div")
         let imgSection = document.createElement("img")
         let headlineSection = document.createElement("h2")
         imgSection.setAttribute("src", imgURL)
         headlineSection.textContent = headlineText
-        articleSection.setAttribute("id", id)
+        link.setAttribute("id", id)
         articleSection.appendChild(imgSection)
-        articleSection.appendChild(headlineSection)
-        articleList.appendChild(articleSection)   
+        link.appendChild(headlineSection)
+        articleSection.appendChild(link)
+        articleList.appendChild(articleSection) 
+        
+        link.addEventListener("click", function(event){
+            event.preventDefault();
+            showArticle(imgURL, headlineText, summary);
+        })
     }
 }
 
@@ -29,4 +39,29 @@ const getArticles = () => {
     });
   };
 
-  getArticles()
+showArticle = function(imgURL, headline, summary){
+    articleImage = document.getElementById("article-pic");
+    articleHeadline = document.getElementById("article-headline");
+    articleSummary = document.getElementById("article-summary");
+    articleImage.setAttribute("src", imgURL);
+    articleHeadline.textContent = headline;
+    articleSummary.textContent = summary;
+    toggleDisplays();
+}
+
+toggleDisplays = function(){
+    let style = getComputedStyle(singleArticle, "display")
+    if (style.display === "none") {
+        singleArticle.style.display = "block";
+        articleList.style.display = "none";
+    } else {
+        singleArticle.style.display = "none";
+        articleList.style.display = "block";
+    }
+}
+
+document.getElementById("back-to-list").addEventListener("click", function(event){
+    toggleDisplays();
+})
+
+getArticles()
