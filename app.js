f00ab2888fac5c51ed9cb4b1af386618494a1d7f
Christine Chee <christinechee88@hotmail.com>
2021-03-21 23:03:16 +0000

    Add styling and css to pages

--- app.js
+++ app.js
@@ -1,29 +1,20 @@
-  function makeURLChangeShowNewsForCurrentPage() {
-    // listener for when a link is clicked (that has a hash)
-    window.addEventListener("hashchange", showNewsForCurrentPage);
-  }
-  
-  function showNewsForCurrentPage() {
-    // show the note on current page
-    showNews(getNewsID(location));
-  }
-  
-  function getNewsID(location) {
-    // get the id of note wanted from link clicked
-    return parseInt(location.hash.split("#")[1]);
-  }
-  
-  function showNews(newsID) {
-    news = newsCollector.filter((article) => {
-      return article.id === newsID
-    })
-    document.getElementById("headlines-container").style.display = "none";
-    document.getElementById(
-        "news-summary"
-      ).innerHTML = `news summary here<br>
-      <img src="${news[0].imageLink}">
-      </br>
-      ${news[0].title}
-      <input type="button" value="Back" onClick="document.location.reload(true)">
-      `;
-  }
+function makeURLChangeShowNewsForCurrentPage() {
+  window.addEventListener("hashchange", showNewsForCurrentPage);
+}
+
+function showNewsForCurrentPage() {
+  showNews(getNewsID(location));
+}
+
+function getNewsID(location) {
+  return parseInt(location.hash.split("#")[1]);
+}
+
+function showNews(newsID) {
+  news = newsCollector.filter((article) => {
+    return article.id === newsID;
+  });
+  document.getElementById("headlines-container").style.display = "none";
+  document.getElementById("heading").style.display = "none";
+  summarize(news);
+}
--- src/headlines.js
+++ src/headlines.js
@@ -1,59 +1,56 @@
-let newsCollector = []
+let newsCollector = [];
 
 const getHeadlines = async () => {
-    const endpoint = 'https://content.guardianapis.com/search?order-by=newest&show-fields=thumbnail&page-size=20&api-key=test'
-    try { const response = await fetch(endpoint, {cache: 'no-cache'}); 
+  const endpoint =
+    "https://content.guardianapis.com/search?order-by=newest&show-fields=thumbnail&page-size=20&api-key=test";
+  try {
+    const response = await fetch(endpoint, { cache: "no-cache" });
     if (response.ok) {
       const jsonResponse = await response.json();
-      return jsonResponse.response.results
+      return jsonResponse.response.results;
     }
-    } catch (error) {
-      console.log(error)
+  } catch (error) {
+    console.log(error);
   }
-} 
+};
 
 const headlinesBox = document.querySelector("#headlines-container");
 
 function format(headlines) {
-     let count = 1
-     headlines.forEach(news => {
-        let link = news.apiUrl.replace('https', 'http')
-        let web = news.webUrl.replace('https', 'http')
-        let imageLink = news.fields.thumbnail
-        headline = new News(news.webTitle, link, web, imageLink, count)
-        newsCollector.push(headline)
-        const div = document.createElement("div"); 
-        div.classList.add("headline"); 
-        div.setAttribute("id", headline.id);
-        div.innerHTML = `                                                 
-                <a href="#${headline.id}"><img src="${headline.imageLink}" width="300"></a>
+  let count = 1;
+  headlines.forEach((news) => {
+    let link = news.apiUrl.replace("https", "http");
+    let web = news.webUrl.replace("https", "http");
+    let title = news.webTitle;
+    if (news.fields === undefined) {
+      imageLink = "";
+    } else {
+      imageLink = news.fields.thumbnail;
+    }
+    headline = new News(title, link, web, imageLink, count);
+    newsCollector.push(headline);
+    const div = document.createElement("div");
+    div.classList.add("headline");
+    div.setAttribute("id", headline.id);
+    div.innerHTML = `                                                 
+                <a href="#${headline.id}"><img src="${headline.imageLink}" width="350"></a>
                 <br>
                 <a href="#${headline.id}">${headline.title}</a>
+                <br>
+                <br>
         `;
-        headlinesBox.appendChild(div);
-        count++
-     })
-}
-
-const getSummary = async () => {
-    const endpoint = 'https://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/extract?url='
-    try { const response = await fetch(endpoint, {cache: 'no-cache'}); 
-    if (response.ok) {
-      const jsonResponse = await response.json();
-      return jsonResponse.response
-    }
-    } catch (error) {
-      console.log(error)
-  }
+    headlinesBox.appendChild(div);
+    count++;
+  });
 }
 
 function loadHeadlines() {
-    getHeadlines()
-    .then(results => {
+  getHeadlines()
+    .then((results) => {
       format(results);
     })
-    .catch(err => console.log(err))
-  }
+    .catch((err) => console.log(err));
+}
 
-  document.addEventListener("DOMContentLoaded", loadHeadlines);
-  makeURLChangeShowNewsForCurrentPage();
+document.addEventListener("DOMContentLoaded", loadHeadlines);
+makeURLChangeShowNewsForCurrentPage();
