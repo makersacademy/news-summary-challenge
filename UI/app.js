b0740383f233d7c363874d7f6044e925cd1f490e
Lou Reade <39278690+loushark@users.noreply.github.com>
2021-04-16 11:16:05 +0100

    api fetch working in html script, printing headlines and thumbnails to page

--- UI/app.js
+++ UI/app.js
@@ -1,1 +1,1 @@
-
+require('dotenv').config()
--- src/news.js
+++ src/news.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 class News {
 
   constructor(){
@@ -7,6 +9,26 @@ class News {
   add(headline){
     return this.headlines.push(headline);
   }
+
+
+// function getPostData() {
+//   return fetch("https://content.guardianapis.com/search?api-key=2a0b05ae-00bc-4442-be8c-e2d3e6c6c644").then(response => {
+//     return response.json();
+//   })
+// }
+//
+// function renderPost(postData) {
+//   let postHeadingHTML = `<h1>${postData.webTitle}</h1>`;
+//   let postWithLinebreaksHTML = postData.webUrl.replaceAll("\n", "<br>\n");
+//   let postBodyHTML = `<p>${postWithLinebreaksHTML}</p>`;
+//   return `${postHeadingHTML}${postBodyHTML}`;
+// }
+//
+// getPostData().then(post => {
+//   let rendered = renderPost(post);
+//   document.getElementById("main").innerHTML = rendered;
+// });
+
 
 
 }
