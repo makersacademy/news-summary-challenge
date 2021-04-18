ac9481090f23a02c7feeb64d1adb81a308c2aaf2
taran314 <tarandeep-nandhra@hotmail.co.uk>
2021-04-18 19:44:56 +0100

    summary and link to web page added

--- src/interface.js
+++ src/interface.js
@@ -1,6 +1,12 @@
 // Typical get request response in response.js
 let url = "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?show-fields=thumbnail&page-size=3";
 
+// hash listner on window
+hashListner();
+
+// get request for news stories
+getNews();
+
 // Used for testing and development
 async function fetchAsync() {
   return response;
@@ -18,18 +24,64 @@ async function fetchAsync () {
 }
 */
 
+// Save news response array in outer scope
+let newsArray;
+
 // trigger async function
 // log response or catch error of fetch promise
-fetchAsync()
+function getNews() {
+  fetchAsync()
   .then(function(data) {
-    console.log(data.response.results[0])
+    newsArray = data.response.results;
     let element = document.getElementById("newsContent");
     var listItem = "";
-    data.response.results.forEach(news => {
-      listItem += `<h1>${news.webTitle}</h1> <br> <img src=${news.fields.thumbnail}> <br>`;
+    data.response.results.forEach((news, index) => {
+      listItem += `<img src=${news.fields.thumbnail}><br>
+      <h1> 
+        <a href=#${index}>
+          ${news.webTitle}
+        </a>
+      </h1><br>`;
     });
     element.innerHTML = listItem; 
+    // reset hash without setting off a hashchange
+    history.replaceState(null, null, document.location.pathname);
   })
   .catch(reason => console.log(reason.message))
+}
 
 
+
+function hashListner() {
+  window.addEventListener("hashchange", getNote);
+}
+
+function getNote() {
+  var index = window.location.hash.split("#")[1];
+  let body = document.getElementById("newsContent")
+  let news = newsArray[index];
+  
+  fetch(`http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=${news.webUrl}`)
+  .then(function(response) {
+    return response.json();
+  }).then(function(data) {
+    body.innerHTML += "<br>" + data.sentences.join("<br>");
+    document.getElementById("return").addEventListener("click", getNews);
+  }).catch(function() {
+    console.log("Booo");
+  });
+
+  // async function fetchAsync () {
+  //   let response = await fetch(url);
+  //   let data = await response.json();
+  //   console.log(data);
+  //   return data;
+  // }
+
+  body.innerHTML = `<img src=${news.fields.thumbnail}><br>
+  <h1> ${news.webTitle}</h1><br> 
+  <button id=return>Return to main page</button> 
+  <a href=${news.webUrl}> 
+    View the full story
+  </a>`;
+}
