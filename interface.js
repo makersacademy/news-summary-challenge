94f22df412a4bfdbbbae302708d9ad76774eaed1
Eds <steadyede@live.co.uk>
2021-04-28 14:07:35 +0100

    headline images and news summary added

--- interface.js
+++ interface.js
@@ -1,21 +1,8 @@
 'use strict';
 
-const dataset = [
-  { 
-    id: 0,
-    heading: "1st dummy heading",
-    img: "images/dummy.jpeg",
-},
-  { 
-    id: 1,
-    heading: "2nd dummy heading",
-    img: "images/dummy.jpeg",
-}
-]
-
 var ready = (callback) => {
   if (document.readyState != "loading") callback();
-  else document.addEventListener("DOMContentLoaded", callback);
+  else document.addEventListener("DOMContentLoaded", callback)
 }
 
 ready(() => {  
@@ -23,12 +10,13 @@ ready(() => {
   fetch("https://content.guardianapis.com/search?from-date=2021-03-23&to-date=2021-03-24&show-fields=headline%20thumbnail&q=uk&api-key=test")
   .then(response => response.json())
   .then( body => {
+    console.log(body)
     // get div element from page
     let div = document.getElementById('headlines')
 
     for(let i = 0; i < body.response.results.length; i++){
       if(body.response.results[i].type === "article") {
-        console.log(body.response.results[i].webTitle)
+        // console.log(body.response.results[i].webTitle)
         const article = new Article(body.response.results[i])
         div.appendChild(article.toNode())
       }
@@ -47,97 +35,46 @@ class Article {
     let node = document.createElement('div')
     let title = document.createElement('h2')
     title.innerText = this.title
+    node.appendChild(title)
+
+    this.fetchPictures(function(data){
+      let divImg = document.createElement('div')
+      let img = document.createElement('img')
+      img.src = data.response.content.fields.thumbnail
+      divImg.appendChild(img)
+      node.appendChild(divImg)
+    })
+
     let link = document.createElement('a')
     link.innerText = 'View Article'
     link.setAttribute('href', this.url)
-    node.appendChild(title)
     node.appendChild(link)
+    
+    this.fetchSummary(function(data){
+      let text = document.createElement('p')
+      text.innerText = data.sentences
+      node.appendChild(text)
+    })
     return node
   }
+
+ fetchSummary(callback) {
+   fetch(`http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=${this.url}`)
+   .then(response => {return response.json() })
+   .then(callback)
+  }
+
+ fetchPictures(callback){
+   const picUrl = this.url.replace('https://www.theguardian.com/','')
+   const url = `http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/${picUrl}?show-fields=thumbnail`
+   fetch(url)
+   .then(response => { return response.json() })
+   .then(callback)
+ }
+
 }
 
-function getHeadlines(stories) {
-  //get date i.e.new Date() and convert
-  fetch("https://content.guardianapis.com/search?from-date=2021-03-23&to-date=2021-03-24&show-fields=headline%20thumbnail&q=uk&api-key=test")
-  .then(response => response.json())
-  .then( body => {
-    for(let i = 0; i < body.response.results.length; i++){
-      if(body.response.results[i].type === "article") {
-        // let apiUrl = body.response.results[i].apiUrl
-        // let id = body.response.results[i].id
-        let webTitle = body.response.results[i].webTitle
-        let webUrl = body.response.results[i].webUrl
-        let entry = { webTitle: webTitle, webUrl: webUrl }
-        stories.push(entry)
-        // console.log(body.response.results[i])
-      }
+// https://www.theguardian.com/world/2021/mar/23/uk-braced-for-chinese-retaliation-over-uighur-abuse-sanctions
 
-    }
-    
-    // console.log(articles) 
-    console.log(body.response.results) 
-  })
-}
-
-function printHeadlines(stories) {
-  let parentdiv = document.createElement('div')
-  parentdiv.id='headlines'
-
-  for(let index = 0; index < stories.length; index++) {
-    if(stories[index].img) {
-      let div = document.createElement('div')
-      let h3 = document.createElement('h3')
-      h3.textContent = stories[index].webTitle
-      // div.appendChild(img)
-      div.appendChild(h3)
-      parentdiv.appendChild(div)
-    }
-  }
-
-  document.body.appendChild(parentdiv)
-};
-
-
-//publish headlines
-
-//use article.webTitle to print a headline
-
-function dummyImgPrint() {
-  let parentdiv = document.createElement('div')
-  parentdiv.id='headlines'
-
-  for(let index = 0; index < dataset.length; index++) {
-    if(dataset[index].img) {
-      let div = document.createElement('div')
-      let h3 = document.createElement('h3')
-      let img = document.createElement('img')
-      img.src = dataset[index].img
-      h3.textContent = dataset[index].heading
-      div.appendChild(img)
-      div.appendChild(h3)
-      parentdiv.appendChild(div)
-    }
-  }
-
-  document.body.appendChild(parentdiv)
-};
-
-function dummyHeadlinesPrint() {
-  let parentdiv = document.createElement('div')
-  parentdiv.id='headlines'
-
-  for(let index = 0; index < dataset.length; index++) {
-    if(dataset[index].img) {
-      let div = document.createElement('div')
-      let h3 = document.createElement('h3')
-      // let img = document.createElement('img')
-      // img.src = dataset[index].img
-      h3.textContent = dataset[index].heading
-      // div.appendChild(img)
-      div.appendChild(h3)
-      parentdiv.appendChild(div)
-    }
-  }
-
-  document.body.appendChild(parentdiv)
-};
+// curl 'http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/media/2021/mar/15/daily-telegraph-plans-link-journalists-pay-article-popularity?show-fields=headline,thumbnail'
+// `http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/${}?show-fields=thumbnail`
