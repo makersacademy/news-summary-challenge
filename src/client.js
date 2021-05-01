b52f9885bb130de6bc36b92d1d13ef9014f19921
Sha <cspoppuppy@hotmail.com>
2021-05-01 21:22:54 +0100

    use thumbnail

--- src/client.js
+++ src/client.js
@@ -1,33 +1,36 @@
-const makersApi = "https://news-summary-api.herokuapp.com";
-const guardianRequest = "/guardian?apiRequestUrl=";
-const AylienRequest = "/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=";
+const makersApi = 'https://news-summary-api.herokuapp.com';
+const guardianRequest = '/guardian?apiRequestUrl=';
+const AylienRequest = '/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=';
 
 function formatDate(date) {
-  year = date.getFullYear();
-  month = (date.getMonth() + 1).toString().padStart(2, '0');
-  day = (date.getDate()).toString().padStart(2, '0');
+	year = date.getFullYear();
+	month = (date.getMonth() + 1).toString().padStart(2, '0');
+	day = date.getDate().toString().padStart(2, '0');
 
-  return `${year}-${month}-${day}`;
+	return `${year}-${month}-${day}`;
 }
 
 class Client {
-  async fetchHeadlines () {
-    const headlineRequest = `http://content.guardianapis.com/search?q=politics&from-date=${formatDate(new Date())}&order-by=newest`;
-    const url = makersApi + guardianRequest + headlineRequest;
-    const response = await fetch(url);
-    return response.json();
-  }
+	fetchHeadlines() {
+		const headlineRequest = `http://content.guardianapis.com/search?show-fields=thumbnail&from-date=${formatDate(
+			new Date()
+		)}&order-by=newest`;
+		const url = makersApi + guardianRequest + headlineRequest;
+		return this.fetchData(url);
+	}
 
-  async fetchArticle (articleUrl) {
-    const url = makersApi + guardianRequest + articleUrl.replace("https", "http") + "?show-fields=body";;
-    const response = await fetch(url);
-    return response.json();
-  }
+	// fetchArticle(articleUrl) {
+	// 	const url = makersApi + guardianRequest + articleUrl.replace('https', 'http') + '?show-fields=body';
+	// 	return this.fetchData(url);
+	// }
 
-  async fetchSummary (articleURL) {
-    const url = makersApi + AylienRequest + articleURL;
-    const response = await fetch(url);
-    return response.json();
-  }
+	fetchSummary(articleURL) {
+		const url = makersApi + AylienRequest + articleURL;
+		return this.fetchData(url);
+	}
+
+	async fetchData(url) {
+		const response = await fetch(url);
+		return response.json();
+	}
 }
-
