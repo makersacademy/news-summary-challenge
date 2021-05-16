a6277795ff450d2f5bf08936ef7fa584a22e28b8
fizzy-fifs <fkinyemi@gmail.com>
2021-05-16 23:54:40 +0100

    Partially completed user story 3

--- src/interface.js
+++ src/interface.js
@@ -2,49 +2,60 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
-  guardianApi = () => {
-    fetch('http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?')
-    .then(res => {return res.json()})
-    .then(data => {
-      for (item in data.response.results){ 
-      let headline = data.response.results[item].webTitle
-      eachHeadline = document.createElement('li')
-      eachHeadline.textContent = headline
-      document.querySelector('ol').append(eachHeadline)}
-    })
-  } 
-  
-  
-
-  console.log(guardianApi())
-
-  displayTodaysHeadlines = () => {
-    
-
-
+  guardianApi = (index) => {
+    return fetch('http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?')
+    .then(res => res.json())
+    .then(data => { return data.response.results[index]})
   }
+  
+  aylienApi = (url, title) => {
+    fetch(`'http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=${url}&title=${title}'`, {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json'
+      }
+    })
+    .then(res => {return res})
+    .then(data => {return data.text})
+  }
+  
+  
+  displayTodaysHeadlines = async() => {
+    for(i=0; i<10; i++){
+      const response = await guardianApi(i)
+      url = response.webUrl
+      title = response.webTitle
+      const summary = await aylienApi(url, title)
 
+      eachHeadline = document.createElement('li')
+      eachHeadline.textContent = title
+      document.querySelector('ul').append(eachHeadline)
+
+
+      para = document.createElement('p')
+      text = document.createTextNode(summary)
+      para.appendChild(text)
+      eachHeadline.appendChild(para)
+
+    }
+  }
+    
+  
+ 
+  
   displayTodaysHeadlines()
-  
-  // displayTodaysHeadlines = async() => {
-    
-  //   for(i=0; i<10; i++){
-  //     let headline = await guardianApi
-  //     console.log(headline)
-      
-  //     eachHeadline = document.createElement('li')
-      
-  //     eachHeadline.textContent = headline
-      
-  //    document.querySelector('ol').append(eachHeadline)
-  //   }
-    
-  // }
 
   
 
-  
+
+
+  // eachHeadline.appendChild(thumbnail)
+
+  // let thumbnail = document.createElement('img')
+  // thumbnail.src = `'${data.response.results[item].apiUrl}'`
   
 
+
+  
   
 })
