3424ab2146b1d5ba5f858cbc94a6d1f3081d18d1
Fabio <alessf13@gmail.com>
2021-07-18 13:55:10 +0200

    background color set to beige and display to flex for .news

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const webNews = document.querySelector('.news')
 
-const api_url = "https://content.guardianapis.com/politics?&show-fields=thumbnail&api-key=test"
+const apiNews = "https://content.guardianapis.com/politics?&show-fields=thumbnail&api-key=test"
+
+// const apiSum = `http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=${}`
+
 
 async function getNews() {
-	const response = await fetch(api_url);
+	const response = await fetch(apiNews);
   const data = await response.json();
-	// console.log(data.response.results[1])
+	console.log(data)
 	return data;
 }
 
@@ -15,6 +18,10 @@ getNews();
 	for(let i = 0; i < 10; i++) {
 
 		data = (await getNews()).response.results[i];
+
+		const img = document.createElement('img');
+		img.src = data.fields.thumbnail;
+		webNews.appendChild(img);
 
 		const title = document.createElement('h2');
 		title.innerText = data.webTitle;
@@ -26,9 +33,14 @@ getNews();
 
 		webNews.appendChild(url);
 
-		const img = document.createElement('img');
-		img.src = data.fields.thumbnail;
-		webNews.appendChild(img);
+		// As a busy politician
+		// I can click a news headline to see a summary and a photo of the news article
+		// So that I can get an in depth understanding of a very important story
+		
+		title.addEventListener('click', () => {
+
+		})
+
 	}
 })()
 
