3c829e3ae5aa944f6a77888e72acb461e8bf8217
LUNDI19 <michaelmcloughlin9@gmail.com>
2021-07-19 08:38:51 +0200

    news array

--- interface.js
+++ interface.js
@@ -1,18 +1,16 @@
-fetch('https://content.guardianapis.com/search?q=debate&tag=politics/politics&from-date=2020-02-20&api-key=c0cfe07c-acda-4fd8-8a6b-1a24f21528d3')
+
+const url = 'https://content.guardianapis.com/search?q=debate&tag=politics/politics&from-date=2020-02-20&api-key=c0cfe07c-acda-4fd8-8a6b-1a24f21528d3'
+
+fetch(url)
 .then(response => {
-    const contentType = response.headers.get('content-type');
-    if (!contentType ) {
-      throw new TypeError("ERROR");
-    }
     return response.json();
 })
 .then(data => {
   let newsContainer = document.getElementById('newsTitle');
   let newsArray = data.response.results
-  for(let i = 0; i < 10; i++) {
-    let newsStory = `<a href>${newsArray[i].webTitle}</a></br>`
+  for(let i = 0; i < newsArray.length; i++) {
+    let newsStory = `<a class='title' href data-linkID="${i}">${newsArray[i].webTitle}</a></br>`
     newsContainer.innerHTML += newsStory
-    console.log(newsArray);
   }
 })
 .catch(error => console.error(error));
