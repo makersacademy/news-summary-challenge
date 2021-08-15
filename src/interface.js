8d42610f9662cd40c010529130985fcf033e12f8
karimabuseer <karimabuseer@gmail.com>
2021-08-15 16:14:10 +0100

    Added styling changes including topic span. Protoype api call for body

--- src/interface.js
+++ src/interface.js
@@ -10,19 +10,30 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const headlinesResponse = fetch("http://content.guardianapis.com/politics?&show-fields=thumbnail&api-key=bd78176b-9496-4a74-90b1-7a9cff22fb46")
       .then(response => response.json())
-      .then(data => { return data.response.results; })
- console.log(headlinesResponse);
+      .then(data => { return data.response.results; });
+  console.log(headlinesResponse)
+
+  const headlinesResponse = function (api) { fetch(`${api}`)
+      .then(response => response.json())
+      .then(data => { return data.response.results; });
+  }
 
   const createCard = async () => {
-    const headlines = await headlinesResponse; 
+    const headlines = await headlinesResponse;
+    const newsBody = await bodyResponse; 
     for(let i = 0; i < 10; i ++) {
       const newDiv = document.createElement("div");
       newDiv.classList.add("story");
       const imgTag = document.createElement("img")
-      const imgLink = headlines[i]['fields']['thumbnail'];
-      console.log(imgLink)
-      imgTag.src = imgLink;
+      const headTag = document.createElement("h3")
+      const topicTag = document.createElement("span")
+      topicTag.classList.add("topic")
+      topicTag.innerHTML = 'Politics / '
+      headTag.innerHTML = headlines[i]['webTitle']
+      imgTag.src = headlines[i]['fields']['thumbnail'];
       newDiv.appendChild(imgTag);
+      newDiv.appendChild(topicTag)
+      newDiv.appendChild(headTag)
       list.appendChild(newDiv);
     }
   }
