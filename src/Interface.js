2057e0cfee7db73712db03d30ad60af16d96bcc6
Yousuf Mohamed <yousuf_mohamed@hotmail.com>
2021-09-12 23:47:18 +0100

    Refactored, added openStory(), summary now visible on click using Aylien API

--- src/Interface.js
+++ src/Interface.js
@@ -1,33 +1,47 @@
-function getHeadline(url) {
-  fetch(`http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=${url}`)
-    .then(response => response.json())
-    .then((text) => {
-      document.getElementById('headlines').appendChild(document.createElement('h1')).innerHTML = text.response.content.webTitle
-    })
-    .catch((error) => {
-      console.error('Error:', error);
+document.addEventListener("DOMContentLoaded", () => {
+  function getStory(url) {
+    url = url.replace('https://www.theguardian.com', 'http://content.guardianapis.com')
+    fetch(`http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=${url}`)
+      .then(response => response.json())
+      .then((text) => {
+        document.getElementById('headlines').appendChild(document.createElement('h1')).setAttribute('id', url);
+        document.getElementById(url).innerHTML = text.response.content.webTitle;
+        openStory()
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+
+  function openStory() {
+    document.querySelectorAll('h1').forEach((item) => {
+      item.addEventListener('click', () => {
+        document.getElementById('headlines').style.display = 'none';
+        getSummary(item.getAttribute('id'))
+
+      });
     });
-}
+  }
 
-function getSummary(url) {
-  fetch(`http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=${url}`)
-    .then(response => response.json())
-    .then((text) => {
-      document.getElementById('textSummary').innerHTML = text.sentences
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-}
+  function getSummary(url) {
+    url = url.replace('http://content.guardianapis.com', 'http://www.theguardian.com')
+    fetch(`http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=${url}`)
+      .then(response => response.json())
+      .then((text) => {
+        document.getElementById('textSummary').innerHTML = text.sentences
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
 
-const guardianStories = [
-  'http://content.guardianapis.com/business/2014/feb/18/uk-inflation-falls-below-bank-england-target',
-  'http://content.guardianapis.com/business/2014/feb/18/uk-inflation-bank-of-england-target-economists-reaction',
-  'http://content.guardianapis.com/business/2014/mar/04/shop-prices-falling-retail-deflation'
-]
+  const guardianStories = [
+    'https://www.theguardian.com/business/2014/feb/18/uk-inflation-falls-below-bank-england-target',
+    'https://www.theguardian.com/business/2014/feb/18/uk-inflation-bank-of-england-target-economists-reaction',
+    'https://www.theguardian.com/business/2014/mar/04/shop-prices-falling-retail-deflation'
+  ]
 
-guardianStories.forEach((element) => {
-  // element.replace('https://www.theguardian.com', 'http://content.guardianapis.com');
-  console.log(element)
-  getHeadline(element);
+  guardianStories.forEach((element) => {
+    getStory(element)
+  });
 });
