72af3c738f1b83421810af0bb063d6c8e9d5bf5d
Arthur Fincham <arthurfincham@me.com>
2021-11-05 18:28:12 +0000

    basic styling for news cards

--- summary.js
+++ summary.js
@@ -1,6 +1,8 @@
 const createCard = (article) => {
+  const newPostContainer = document.createElement('div');
+  newPostContainer.classList.add('container');
   const newPostEl = document.createElement('div');
-  const h = document.createElement("H3")
+  const h = document.createElement("h3")
   const t = document.createTextNode(article.webTitle);
   h.appendChild(t);
   newPostEl.appendChild(h); 
@@ -8,15 +10,17 @@ const createCard = (article) => {
   const q = document.createTextNode(article.sectionId);
   p.appendChild(q);
   newPostEl.appendChild(p); 
-  const image = document.createElement("img")
-  image.src = article.fields.thumbnail
-  newPostEl.appendChild(image); 
   newPostEl.className = 'post';
-  document.body.appendChild(newPostEl);
+  const newPostImg = document.createElement('div');
+  newPostImg.style.backgroundImage = `url('${article.fields.thumbnail}')`;
+  newPostImg.className = 'background_image';
+  newPostContainer.appendChild(newPostEl);
+  newPostContainer.appendChild(newPostImg);
+  document.body.appendChild(newPostContainer);
 }
 
 const getData = () => {
-  fetch("https://content.guardianapis.com/search?page-size=20&api-key=test&format=json&show-fields=body,headline,thumbnail")
+  fetch("https://content.guardianapis.com/search?page-size=5&api-key=test&format=json&show-fields=body,headline,thumbnail")
     .then(response => response.json())
     .then(data => {
       const articles = data.response.results
