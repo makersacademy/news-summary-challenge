47d98c9abb3644d928d3364c64702ea592942759
Sergei Palmer <zigpalmer@gmail.com>
2021-11-05 19:40:17 +0000

    Css + images added

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -25,10 +25,21 @@
         feedEl = document.getElementById("feedRequest");
         fetchedData.forEach((story) => {
           let storyEl = document.createElement("p");
+          let storyImg = document.createElement("img");
           let storyLink = document.createElement("a");
+          let categoryTag = document.createElement("p");
+          let categoryURL = document.createElement("a");
+          categoryURL.href = `https://www.theguardian.com/${story["sectionId"]}`;
+          storyEl.className = "storyCard";
+          storyImg.classname = "storyThumb";
+          categoryURL.innerText = `Category: ${story["sectionName"]}`;
           storyLink.href = story["webUrl"];
+          storyImg.src = story["fields"]["thumbnail"];
           let storyTitle = story["webTitle"];
           storyLink.textContent = storyTitle;
+          categoryTag.appendChild(categoryURL);
+          storyEl.appendChild(storyImg);
+          storyEl.appendChild(categoryTag);
           storyEl.appendChild(storyLink);
           feedEl.appendChild(storyEl);
         });
@@ -42,5 +53,5 @@
   // index.js
   var { getFeed } = require_getFeed();
   var { displayFeed } = require_displayFeed();
-  getFeed("https://content.guardianapis.com/search?page=1&q=debate&api-key=test", displayFeed);
+  getFeed("https://content.guardianapis.com/search?page=1&api-key=test&show-fields=body,headline,thumbnail", displayFeed);
 })();
--- src/displayFeed.js
+++ src/displayFeed.js
@@ -2,10 +2,21 @@ const displayFeed = (fetchedData) => {
   feedEl = document.getElementById('feedRequest');
   fetchedData.forEach((story) => {
     let storyEl = document.createElement('p');
+    let storyImg = document.createElement('img');
     let storyLink = document.createElement('a');
-    storyLink.href = story["webUrl"]
+    let categoryTag = document.createElement('p')
+    let categoryURL = document.createElement('a');
+    categoryURL.href = `https://www.theguardian.com/${story["sectionId"]}`
+    storyEl.className = "storyCard";
+    storyImg.classname = "storyThumb";
+    categoryURL.innerText = `Category: ${story["sectionName"]}`;
+    storyLink.href = story["webUrl"];
+    storyImg.src = story["fields"]["thumbnail"];
     let storyTitle = story["webTitle"];
     storyLink.textContent = storyTitle;
+    categoryTag.appendChild(categoryURL);
+    storyEl.appendChild(storyImg);
+    storyEl.appendChild(categoryTag);
     storyEl.appendChild(storyLink);
     feedEl.appendChild(storyEl);
   })
