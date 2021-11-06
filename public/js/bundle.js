7d599888913b0537d726afa6a9279275bbaba066
Arthur Fincham <arthurfincham@me.com>
2021-11-06 11:47:31 +0000

    remove sectioned request

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -15,14 +15,6 @@
             });
           });
         };
-        getSectionArticles = (card2, section) => {
-          fetch(`https://content.guardianapis.com/section?q=${section}&page-size=10&api-key=test&format=json&show-fields=body,headline,thumbnail`).then((response) => response.json()).then((data) => {
-            const articles = data.response.results;
-            articles.forEach((article) => {
-              card2.createCard(article);
-            });
-          });
-        };
       };
       module.exports = Summary2;
     }
--- src/Summary.js
+++ src/Summary.js
@@ -11,15 +11,5 @@ class Summary {
     })
   }
 
-  getSectionArticles = (card, section) => {
-    fetch(`https://content.guardianapis.com/section?q=${section}&page-size=10&api-key=test&format=json&show-fields=body,headline,thumbnail`)
-    .then(response => response.json())
-    .then(data => {
-      const articles = data.response.results
-      articles.forEach((article) => {
-        card.createCard(article)
-      });
-    })
-  }
 }
 module.exports = Summary;
