b9314b1874bf980af9d6129624cb604bca63e501
Arthur Fincham <arthurfincham@me.com>
2021-11-06 11:39:41 +0000

    add get section articles

--- src/Summary.js
+++ src/Summary.js
@@ -10,5 +10,16 @@ class Summary {
       });
     })
   }
+
+  getSectionArticles = (card, section) => {
+    fetch(`https://content.guardianapis.com/section?q=${section}&page-size=10&api-key=test&format=json&show-fields=body,headline,thumbnail`)
+    .then(response => response.json())
+    .then(data => {
+      const articles = data.response.results
+      articles.forEach((article) => {
+        card.createCard(article)
+      });
+    })
+  }
 }
 module.exports = Summary;
