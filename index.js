ace0e779b680b432f82ee79b9c33b1325c56c45d
Alex Boyling <alexboyling@gmail.com>
2021-11-06 14:27:04 +0000

    added headlines and images

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-console.log("JS")
+console.log("Running...")
+
+const fetchContent = (callback) => {
+  fetch('https://content.guardianapis.com/search?page=1&api-key=test&show-fields=body,headline,thumbnail')
+  .then(response => response.json())
+  .then(data =>  {
+    console.log('Success:', data);
+    callback(data);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+}
+
+const displayContent = (content) => {
+  console.log("Displaying content...");
+  feedEL = document.getElementById("headlines");
+  feedEL.innerHTML = "";
+  let results = content.response.results
+  results.forEach((result) => {
+    // story card
+    let storyEl = document.createElement('article');
+    storyEl.className = "storyCard";
+    // image
+    let storyImg = document.createElement('img');
+    storyImg.className = "storyThumb";
+    storyImg.src = result.fields.thumbnail;
+    // headline and link
+    let storyHeadline = document.createElement('h2');
+    let storyLink = document.createElement('a');
+    storyHeadline.appendChild(storyLink);
+    storyLink.textContent = result.webTitle;
+    storyLink.href = result.webUrl;
+
+
+
+    // append all elements to story
+    storyEl.appendChild(storyImg);
+    storyEl.appendChild(storyHeadline);
+
+    // append story to feed
+    feedEl = document.getElementById("headlines");
+    feedEl.appendChild(storyEl);
+  })
+}
+
+fetchContent(displayContent);
--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1,4 +1,35 @@
 (() => {
   // index.js
-  console.log("JS");
+  console.log("Running...");
+  var fetchContent = (callback) => {
+    fetch("https://content.guardianapis.com/search?page=1&api-key=test&show-fields=body,headline,thumbnail").then((response) => response.json()).then((data) => {
+      console.log("Success:", data);
+      callback(data);
+    }).catch((error) => {
+      console.error("Error:", error);
+    });
+  };
+  var displayContent = (content) => {
+    console.log("Displaying content...");
+    feedEL = document.getElementById("headlines");
+    feedEL.innerHTML = "";
+    let results = content.response.results;
+    results.forEach((result) => {
+      let storyEl = document.createElement("article");
+      storyEl.className = "storyCard";
+      let storyImg = document.createElement("img");
+      storyImg.className = "storyThumb";
+      storyImg.src = result.fields.thumbnail;
+      let storyHeadline = document.createElement("h2");
+      let storyLink = document.createElement("a");
+      storyHeadline.appendChild(storyLink);
+      storyLink.textContent = result.webTitle;
+      storyLink.href = result.webUrl;
+      storyEl.appendChild(storyImg);
+      storyEl.appendChild(storyHeadline);
+      feedEl = document.getElementById("headlines");
+      feedEl.appendChild(storyEl);
+    });
+  };
+  fetchContent(displayContent);
 })();
