56069693b0d4762093d25076cf391a7677828471
Sergei Palmer <zigpalmer@gmail.com>
2021-11-08 23:22:03 +0000

    Updated api url

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const { getFeed } = require('./src/getFeed');
 const { displayFeed } = require('./src/displayFeed');
 const newsFeed = require('./src/newsFeed');
 const allStoriesBtn = document.getElementById("allStoriesButton");
-// const feedUrl = 'https://content.guardianapis.com/search?api-key=test&show-fields=body,headline,thumbnail&show-tags=contributor';
-const feedUrl = 'https://content.guardianapis.com/search?q=debate%20AND%20economy&api-key=test&show-fields=body,headline,thumbnail&show-tags=contributor';
+const feedUrl = 'https://content.guardianapis.com/search?api-key=test&page-size=20&show-fields=body,headline,thumbnail&show-tags=contributor';
+// const feedUrl = 'https://content.guardianapis.com/search?q=debate%20AND%20economy&page-size=20&api-key=test&show-fields=body,headline,thumbnail&show-tags=contributor';
 feed = new newsFeed ();
-
 
 getFeed(feedUrl, displayFeed, feed);
 
