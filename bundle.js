6fea3c9d62564f7c98596ccbd8ff789982615682
Ryan Grimes <ryanpaulgrimes@gmail.com>
2021-12-04 13:04:57 +0100

    hyperlinks added to page

--- bundle.js
+++ bundle.js
@@ -23,8 +23,11 @@
         feedElement = document.getElementById("feedRequest");
         fetchedData.forEach((article) => {
           let articleElement = document.createElement("p");
-          let articleContent = article["webTitle"];
-          articleElement.textContent = articleContent;
+          let articleTitle = article["webTitle"];
+          let articleURL = document.createElement("a");
+          articleURL.href = article["webUrl"];
+          articleURL.textContent = articleTitle;
+          articleElement.appendChild(articleURL);
           feedElement.appendChild(articleElement);
         });
       };
