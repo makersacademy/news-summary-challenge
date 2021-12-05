43b6a3ce5fc5f47d68d3c316f8aa02bdca9cf6d1
Sigita <sigita.zeb92@gmail.com>
2021-12-05 13:14:07 +0000

    Fetch Gardians newspaper api

--- bundle.js
+++ bundle.js
@@ -1,2 +1,11 @@
 (() => {
+  // index.js
+  var fetchGardiansApi = () => {
+    fetch("https://content.guardianapis.com/search?page=1&api-key=test&show-fields=headlines").then((response) => response.json()).then((data) => {
+      console.log("Success:", data);
+    }).catch((error) => {
+      console.error("Error:", error);
+    });
+  };
+  fetchGardiansApi();
 })();
---
+++ index.js
@@ -0,0 +1,14 @@
+const fetchGardiansApi = () => {
+  fetch(
+    "https://content.guardianapis.com/search?page=1&api-key=test&show-fields=headlines"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Success:", data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+};
+
+fetchGardiansApi();
