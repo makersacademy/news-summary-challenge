4a57e5cdc1e71e24fce4e57a2ac3daa8c88e5bba
Mark McLeod <markmcleod45@gmail.com>
2021-12-05 17:28:27 +0000

    Refactor index.html so it's not hard coded. Refactor displayArticle.js to iterate through the data returned from the API call

--- bundle.js
+++ bundle.js
@@ -18,36 +18,23 @@
     "displayArticle.js"(exports, module) {
       var displayArticle2 = (data) => {
         console.log(data);
-        const article1Header = document.querySelector("#article1Header");
-        article1Header.innerText = data.response.results[0].webTitle;
-        const article1Link = document.querySelector("#article1Link");
-        article1Link.href = data.response.results[0].webUrl;
-        const article1Image = document.querySelector("#article1Image");
-        article1Image.src = data.response.results[0].fields.thumbnail;
-        const article2Header = document.querySelector("#article2Header");
-        article2Header.innerText = data.response.results[1].webTitle;
-        const article2Link = document.querySelector("#article2Link");
-        article2Link.href = data.response.results[1].webUrl;
-        const article2Image = document.querySelector("#article2Image");
-        article2Image.src = data.response.results[1].fields.thumbnail;
-        const article3Header = document.querySelector("#article3Header");
-        article3Header.innerText = data.response.results[2].webTitle;
-        const article3Link = document.querySelector("#article3Link");
-        article3Link.href = data.response.results[2].webUrl;
-        const article3Image = document.querySelector("#article3Image");
-        article3Image.src = data.response.results[2].fields.thumbnail;
-        const article4Header = document.querySelector("#article4Header");
-        article4Header.innerText = data.response.results[3].webTitle;
-        const article4Link = document.querySelector("#article4Link");
-        article4Link.href = data.response.results[3].webUrl;
-        const article4Image = document.querySelector("#article4Image");
-        article4Image.src = data.response.results[3].fields.thumbnail;
-        const article5Header = document.querySelector("#article5Header");
-        article5Header.innerText = data.response.results[4].webTitle;
-        const article5Link = document.querySelector("#article5Link");
-        article5Link.href = data.response.results[4].webUrl;
-        const article5Image = document.querySelector("#article5Image");
-        article5Image.src = data.response.results[4].fields.thumbnail;
+        const articles = document.querySelector("#articles");
+        let results = data.response.results;
+        results.forEach((result) => {
+          let articleLink = document.createElement("a");
+          let articleEl = document.createElement("article");
+          let articleHeader = document.createElement("h2");
+          let articleImage = document.createElement("img");
+          articleEl.className = "article";
+          articleImage.className = "article-image";
+          articleHeader.innerText = result.webTitle;
+          articleLink.href = result.webUrl;
+          articleImage.src = result.fields.thumbnail;
+          articleEl.append(articleHeader);
+          articleEl.append(articleImage);
+          articleLink.append(articleEl);
+          articles.append(articleLink);
+        });
       };
       module.exports = displayArticle2;
     }
--- displayArticle.js
+++ displayArticle.js
@@ -1,46 +1,31 @@
 const displayArticle = (data) => {
   // get visibility of data response from API
   console.log(data);
+  
+  // Select the div with the id 'articles'
+  const articles = document.querySelector('#articles');
 
-  // populate HTML for article 1
-  const article1Header = document.querySelector("#article1Header");
-  article1Header.innerText = data.response.results[0].webTitle;
-  const article1Link = document.querySelector("#article1Link");
-  article1Link.href = data.response.results[0].webUrl;
-  const article1Image = document.querySelector("#article1Image");
-  article1Image.src = data.response.results[0].fields.thumbnail;
+  // Store the array needed from the response in results
+  let results = data.response.results;
 
-  // populate HTML for article 2
-  const article2Header = document.querySelector("#article2Header");
-  article2Header.innerText = data.response.results[1].webTitle;
-  const article2Link = document.querySelector("#article2Link");
-  article2Link.href = data.response.results[1].webUrl;
-  const article2Image = document.querySelector("#article2Image");
-  article2Image.src = data.response.results[1].fields.thumbnail;
+  // Iterate through the array to get each result and build the HTML needed
+  results.forEach((result) => {
+    let articleLink = document.createElement("a");
+    let articleEl = document.createElement('article');
+    let articleHeader = document.createElement('h2');
+    let articleImage = document.createElement('img');
+    articleEl.className = "article";
+    articleImage.className = "article-image";
 
-  // populate HTML for article 3
-  const article3Header = document.querySelector("#article3Header");
-  article3Header.innerText = data.response.results[2].webTitle;
-  const article3Link = document.querySelector("#article3Link");
-  article3Link.href = data.response.results[2].webUrl;
-  const article3Image = document.querySelector("#article3Image");
-  article3Image.src = data.response.results[2].fields.thumbnail;
-
-  // populate HTML for article 4
-  const article4Header = document.querySelector("#article4Header");
-  article4Header.innerText = data.response.results[3].webTitle;
-  const article4Link = document.querySelector("#article4Link");
-  article4Link.href = data.response.results[3].webUrl;
-  const article4Image = document.querySelector("#article4Image");
-  article4Image.src = data.response.results[3].fields.thumbnail;
-
-  // populate HTML for article 5
-  const article5Header = document.querySelector("#article5Header");
-  article5Header.innerText = data.response.results[4].webTitle;
-  const article5Link = document.querySelector("#article5Link");
-  article5Link.href = data.response.results[4].webUrl;
-  const article5Image = document.querySelector("#article5Image");
-  article5Image.src = data.response.results[4].fields.thumbnail;
+    articleHeader.innerText = result.webTitle;
+    articleLink.href = result.webUrl;
+    articleImage.src = result.fields.thumbnail;
+    
+    articleEl.append(articleHeader);
+    articleEl.append(articleImage);
+    articleLink.append(articleEl);
+    articles.append(articleLink);
+  });
 };
 
 module.exports = displayArticle;
