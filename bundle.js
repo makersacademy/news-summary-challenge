527f760621796adb3e960c079723f2d0fd1207c0
Mark McLeod <markmcleod45@gmail.com>
2021-12-05 13:05:04 +0000

    Set up project and access the makers api

---
+++ bundle.js
@@ -0,0 +1,9 @@
+(() => {
+  // index.js
+  console.log("hello console");
+  console.log("hello again");
+  var fetchArticle = () => {
+    fetch("http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/politics/2021/dec/05/raab-says-formal-party-in-no-10-last-christmas-would-have-broken-rules?show-fields=body").then((response) => response.json()).then((data) => console.log(data));
+  };
+  fetchArticle();
+})();
