1c47e04917791e325956226a8b2f84d762c015a2
BoneSponge <50413143+BoneSponge@users.noreply.github.com>
2022-01-14 23:22:59 +0000

    Added thumbnails

--- headlines.js
+++ headlines.js
@@ -3,17 +3,21 @@ class Headlines {
     this.mainContainerEl = document.querySelector("#main-container")
   }
   getByNewest = () => {
-    return fetch(`https://content.guardianapis.com/search?order-by=newest&api-key=${API_KEY}`)
+    return fetch(`https://content.guardianapis.com/search?order-by=newest&show-fields=thumbnail&api-key=${API_KEY}`)
     .then(res => res.json())
     .then(res => {return res.response.results})
   }
   
   displayNews = (data) => {
     for (let item of data) {
+      console.log(item)
       let newDiv = document.createElement("div")
+      let newImg = document.createElement("img")
       newDiv.append(item.webTitle)
       newDiv.className = "news"
+      newImg.src = item.fields.thumbnail
       this.mainContainerEl.append(newDiv)
+      this.mainContainerEl.append(newImg)
     }
   }
 
