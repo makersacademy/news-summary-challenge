1ce4ee197480f09706322930c303fe85e5b72eeb
AlfonsoGhislieri <alfonso.ghis@hotmail.fr>
2022-01-15 17:46:08 +0000

    fixed articleView tests

--- tests/articlesView.test.js
+++ tests/articlesView.test.js
@@ -19,9 +19,9 @@ describe('ArticlesView', () => {
 
     view.displayHeadlines();
 
-    expect(document.querySelectorAll('h2.article-title').length).toEqual(2);
-    expect(document.querySelectorAll('h2.article-title')[0].textContent).toEqual('article1');
-    expect(document.querySelectorAll('h2.article-title')[1].textContent).toEqual('article2');
+    expect(document.querySelectorAll('.article').length).toEqual(2);
+    expect(document.querySelectorAll('.article')[0].textContent).toEqual('article1');
+    expect(document.querySelectorAll('.article')[1].textContent).toEqual('article2');
   });
 
   test('.displayArticles clears previously displayed to avoid duplicating', () => {
@@ -29,8 +29,10 @@ describe('ArticlesView', () => {
 
     const ModelMock = { getArticles: () => [
       { webTitle: 'article1',
+        webUrl: 'url',
         fields: {thumbnail: 'ex'} },
       { webTitle: 'article2',
+        webUrl: 'url',
         fields: {thumbnail: 'ex'} }
       ]}
 
@@ -40,7 +42,7 @@ describe('ArticlesView', () => {
     view.displayHeadlines();
     view.displayHeadlines();
 
-    expect(document.querySelectorAll('h2.article-title').length).toEqual(2);
+    expect(document.querySelectorAll('.article').length).toEqual(2);
   });
 
 })
