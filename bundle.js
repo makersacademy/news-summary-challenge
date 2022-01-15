5a3ba3081d968225af827f48d6c17f8832a5e3cc
AlfonsoGhislieri <alfonso.ghis@hotmail.fr>
2022-01-15 14:21:16 +0000

    website now displays headlines fetched from guardian api

--- bundle.js
+++ bundle.js
@@ -13,15 +13,60 @@
           this.date = this.date.toISOString().split("T")[0];
         }
         loadArticles = (callback) => {
-          fetch(`http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?page=1&page-size=20&from-date=${this.date}`).then((response) => response.json()).then((data) => callback(data));
+          fetch(`http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?page=1&page-size=20&from-date=${this.date}`).then((response) => response.json()).then((data) => callback(data.response.results));
         };
       };
       module.exports = AritclesApi;
     }
   });
 
+  // lib/articlesModel.js
+  var require_articlesModel = __commonJS({
+    "lib/articlesModel.js"(exports, module) {
+      var ArticlesModel = class {
+        constructor() {
+          this._articlesArray = [];
+        }
+        addArticle = (article) => this._articlesArray.push(article);
+        getArticles = () => this._articlesArray;
+      };
+      module.exports = ArticlesModel;
+    }
+  });
+
+  // lib/articlesView.js
+  var require_articlesView = __commonJS({
+    "lib/articlesView.js"(exports, module) {
+      var ArticlesView = class {
+        constructor(modelClass) {
+          this.modelClass = modelClass;
+          this.mainContainerEl = document.querySelector("#main-container");
+        }
+        displayArticles = () => {
+          document.querySelectorAll(".article").forEach((element) => {
+            element.remove();
+          });
+          this.modelClass.getArticles().forEach((article) => {
+            let articleEl = document.createElement("div");
+            articleEl.textContent = article;
+            articleEl.className = "article";
+            this.mainContainerEl.append(articleEl);
+          });
+        };
+      };
+      module.exports = ArticlesView;
+    }
+  });
+
   // index.js
   var ApiClass = require_articlesApi();
+  var ModelClass = require_articlesModel();
+  var ViewClass = require_articlesView();
   var api = new ApiClass();
-  api.loadArticles();
+  var model = new ModelClass();
+  var view = new ViewClass(model);
+  api.loadArticles((data) => data.forEach((article) => {
+    model.addArticle(article.webTitle);
+    view.displayArticles();
+  }));
 })();
--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 
 const ApiClass = require('./lib/articlesApi')
+const ModelClass = require('./lib/articlesModel')
+const ViewClass = require('./lib/articlesView')
+
 const api = new ApiClass();
+const model = new ModelClass();
+const view = new ViewClass(model);
 
-api.loadArticles()
-
+api.loadArticles(data => data.forEach( article => {
+  model.addArticle(article.webTitle);
+  view.displayArticles();
+}));
