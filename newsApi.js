00a8e2e9e89252b12daf0d40d4af9b3dbfad6986
Chris Clement <chrisclement@Chriss-Air.connect>
2022-01-15 16:07:36 +0000

    Use of env variables to store api key

--- newsApi.js
+++ newsApi.js
@@ -1,6 +1,10 @@
-class NewsApi {
+const dotenv = require('dotenv')
+dotenv.config()
+
+class NewsApi {  
   loadNews(callback) {
-    fetch('https://content.guardianapis.com/search?page=1&q=debate&api-key=98e51f22-b91b-4a89-b784-aa14c541dfed')
+    const apiKey = env.API_KEY
+    fetch(`https://content.guardianapis.com/search?page=1&q=debate&api-key=${apiKey}`)
     .then(responseAnswer => responseAnswer.json())
     .then(data => {callback(data)})
     .catch((error) => {console.error('ERROR', error)})
