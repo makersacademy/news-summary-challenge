d87bbcebf8559cd9c5d7f8592fdd113b8f9e5e7a
Francesco <francescocollotto@gmail.com>
2022-01-15 16:30:21 +0000

    add ArticlesModel class

---
+++ tests/articlesModel.test.js
@@ -0,0 +1,21 @@
+const ArticlesModel = require('../articlesModel')
+const model = new ArticlesModel();
+const apiArticles = [
+  {
+    headline: "How the arrest of a far-right militia leader signals a new chapter in the January 6 inquiry"
+  }, 
+  {
+    headline: "Newcastle v Watford, Wolves v Southampton and more: football clockwatch live!"
+  },
+  {
+    headline: "Covid cases have hit plateau in parts of UK, says top medical adviser"
+  }
+]
+
+describe('ArticlesModel', () => {
+  it('add a list of articles from an external resource', () => {
+    model.setArticles(apiArticles)
+    expect(model.articles.length).toBe(3)
+    expect(model.articles[0].headline).toBe("How the arrest of a far-right militia leader signals a new chapter in the January 6 inquiry")
+  })
+})
