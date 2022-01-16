4f5c3db1f750e796413750279f9d5a6129dadedc
Chris Clement <chrisclement@Chriss-MacBook-Air.local>
2022-01-16 12:55:15 +0000

    View Article button added

--- bundle.js
+++ bundle.js
@@ -48,12 +48,15 @@
         displayHeadlines() {
           const headlines = this.model.getHeadlines();
           headlines.forEach((headline) => {
+            this.headlinesContainerEl = document.querySelector("#headline-container");
             const headlineEl = document.createElement("div");
             headlineEl.innerText = headline["headline"];
             headlineEl.className = "headline";
             headlineEl.style.cssText = "font-size:25px;color:darkblue";
-            this.headlinesContainerEl = document.querySelector("#headline-container");
             this.headlinesContainerEl.append(headlineEl);
+            const viewArticleButtonEl = document.createElement("button");
+            viewArticleButtonEl.innerText = "View Article";
+            this.headlinesContainerEl.append(viewArticleButtonEl);
             const imageEl = document.createElement("img");
             imageEl.className = "headlineImage";
             imageEl.src = headline["thumbnail"];
--- newsView.js
+++ newsView.js
@@ -1,18 +1,24 @@
 class NewsView {
   constructor(model) {
     this.model = model;
-    
+    // const buttonEl = document.querySelector('#headline-container');
+    // buttonEl.addEventListener("click", () => {
+    //   console.log("You clicked a headline")
+    // })
   }
 
   displayHeadlines() {
     const headlines = this.model.getHeadlines();
     headlines.forEach(headline => {
+      this.headlinesContainerEl = document.querySelector('#headline-container');
       const headlineEl = document.createElement('div');
       headlineEl.innerText = headline["headline"];
       headlineEl.className = 'headline';
       headlineEl.style.cssText = "font-size:25px;color:darkblue"
-      this.headlinesContainerEl = document.querySelector('#headline-container');
       this.headlinesContainerEl.append(headlineEl);
+      const viewArticleButtonEl = document.createElement('button')
+      viewArticleButtonEl.innerText = "View Article"
+      this.headlinesContainerEl.append(viewArticleButtonEl)
       const imageEl = document.createElement('img')
       imageEl.className = 'headlineImage'
       imageEl.src = headline["thumbnail"]
