8cfcb3e2676b353651f824e89873509aa16530a2
Chris Clement <chrisclement@Chriss-MacBook-Air.local>
2022-01-16 14:07:40 +0000

    Api was fetching news based on a keyword. Now it is the front page

--- bundle.js
+++ bundle.js
@@ -9,7 +9,7 @@
     "newsApi.js"(exports, module) {
       var NewsApi2 = class {
         loadNews(callback) {
-          fetch("https://content.guardianapis.com/search?page=1&q=debate&api-key=98e51f22-b91b-4a89-b784-aa14c541dfed&show-fields=thumbnail,body").then((responseAnswer) => responseAnswer.json()).then((data) => {
+          fetch("https://content.guardianapis.com/search?page=1&api-key=98e51f22-b91b-4a89-b784-aa14c541dfed&show-fields=thumbnail,body").then((responseAnswer) => responseAnswer.json()).then((data) => {
             callback(data);
           }).catch((error) => {
             console.error("ERROR", error);
