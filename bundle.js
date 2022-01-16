5f921950903df11e3f303a6ff8cd409cb55d9fe7
Chris Clement <chrisclement@Chriss-MacBook-Air.local>
2022-01-16 12:04:20 +0000

    Headlines now bigger and have a darkblue text

--- bundle.js
+++ bundle.js
@@ -51,6 +51,7 @@
             const headlineEl = document.createElement("div");
             headlineEl.innerText = headline["headline"];
             headlineEl.className = "headline";
+            headlineEl.style.cssText = "font-size:25px;color:darkblue";
             this.headlinesContainerEl = document.querySelector("#headline-container");
             this.headlinesContainerEl.append(headlineEl);
             const imageEl = document.createElement("img");
--- newsView.js
+++ newsView.js
@@ -10,6 +10,7 @@ class NewsView {
       const headlineEl = document.createElement('div');
       headlineEl.innerText = headline["headline"];
       headlineEl.className = 'headline';
+      headlineEl.style.cssText = "font-size:25px;color:darkblue"
       this.headlinesContainerEl = document.querySelector('#headline-container');
       this.headlinesContainerEl.append(headlineEl);
       const imageEl = document.createElement('img')
