7142b5544f6321429245a9e6e4130a1b126d00c0
Jason Humbert <jasonhumbert@outlook.com>
2022-02-05 21:27:43 +0000

    Modify main file to display article headlines on the page

--- bundle.js
+++ bundle.js
@@ -1,4 +1,66 @@
 (() => {
+  var __getOwnPropNames = Object.getOwnPropertyNames;
+  var __commonJS = (cb, mod) => function __require() {
+    return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+  };
+
+  // articlesModel.js
+  var require_articlesModel = __commonJS({
+    "articlesModel.js"(exports, module) {
+      var ArticlesModel2 = class {
+        constructor() {
+          this.articles = [];
+        }
+        setArticles(articles) {
+          this.articles = articles;
+        }
+        getArticles() {
+          return this.articles;
+        }
+        reset() {
+          this.articles = [];
+        }
+      };
+      module.exports = ArticlesModel2;
+    }
+  });
+
+  // articlesView.js
+  var require_articlesView = __commonJS({
+    "articlesView.js"(exports, module) {
+      var ArticlesView2 = class {
+        constructor(model2) {
+          this.model = model2;
+          this.mainContainerEl = document.querySelector("#main-container");
+        }
+        displayArticles() {
+          const articles = this.model.getArticles();
+          articles.forEach((article) => {
+            let articleEl = document.createElement("div");
+            articleEl.className = "article";
+            this.mainContainerEl.append(articleEl);
+            let articleHeadingEl = document.createElement("h3");
+            articleHeadingEl.className = "article-heading";
+            articleHeadingEl.innerText = article.data;
+            articleEl.append(articleHeadingEl);
+          });
+        }
+      };
+      module.exports = ArticlesView2;
+    }
+  });
+
   // index.js
+  var ArticlesModel = require_articlesModel();
+  var ArticlesView = require_articlesView();
+  model = new ArticlesModel();
+  view = new ArticlesView(model);
   console.log("Hello!");
+  var articlesData = [
+    { data: "First article" },
+    { data: "Second article" },
+    { data: "Third article" }
+  ];
+  model.setArticles(articlesData);
+  view.displayArticles();
 })();
--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const ArticlesModel = require('./articlesModel');
+const ArticlesView = require('./articlesView');
+
+model = new ArticlesModel();
+view = new ArticlesView(model);
+
 console.log('Hello!');
+
+const articlesData = [
+  {data: "First article"},
+  {data: "Second article"},
+  {data: "Third article"}
+];
+
+model.setArticles(articlesData);
+
+view.displayArticles();
