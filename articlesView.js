b1a3d6c0159c53e2ead6961b2cfcfb53274a04e7
Jason Humbert <jasonhumbert@outlook.com>
2022-02-06 12:30:43 +0000

    Update the view class display method to handle api data

--- articlesView.js
+++ articlesView.js
@@ -9,12 +9,13 @@ class ArticlesView {
     const articles = this.model.getArticles();
 
     articles.forEach(article => {
+      // console.log(article.fields.headline);  // log headline for each article
       let articleEl = document.createElement('div');
       articleEl.className = 'article';
       this.mainContainerEl.append(articleEl)
       let articleHeadingEl = document.createElement('h3');
       articleHeadingEl.className = 'article-heading';
-      articleHeadingEl.innerText = article.data;
+      articleHeadingEl.innerText = article.fields.headline;
       articleEl.append(articleHeadingEl);
     });
   }
