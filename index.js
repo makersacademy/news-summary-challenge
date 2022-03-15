d6092260c1906d040892a81199935a0a515f685e
icvotria <icvotria@gmail.com>
2022-03-15 22:35:52 +0000

    small change for heroku deployment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const NewsApi = require('./newsApi');
 const NewsView = require('./newsView');
 
 const newsModel = new NewsModel;
-const newsApi = new NewsApi(env.API_KEY);
+const newsApi = new NewsApi(process.env.API_KEY);
 const newsView = new NewsView(newsModel, newsApi);
 
 console.log('The news app is running');
