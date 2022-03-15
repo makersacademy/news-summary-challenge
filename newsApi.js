17672a8ec1276e3800dbcc7ef49674644450a533
icvotria <icvotria@gmail.com>
2022-03-15 21:22:58 +0000

    added dotenv file, newsapi constructor

--- newsApi.js
+++ newsApi.js
@@ -1,8 +1,10 @@
-const apiKey = require(`./apiKey`);
-
 class NewsApi {
+  constructor(apiKey) {
+    this.apiKey = apiKey
+  }
+
   getNews(searchTerm, callback) {
-    fetch(`https://content.guardianapis.com/search?q=${searchTerm}&query-fields=headline&show-fields=thumbnail,headline,byline&order-by=newest&api-key=${apiKey}`)
+    fetch(`https://content.guardianapis.com/search?q=${searchTerm}&query-fields=headline&show-fields=thumbnail,headline,byline&order-by=newest&api-key=${this.apiKey}`)
       .then(response => response.json())
       .then(data => {
         callback(data.response.results);
