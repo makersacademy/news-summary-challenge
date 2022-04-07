29e5703b16a868cdb95347a67c65d98f84a5e869
Valentina <maggio.vale@gmail.com>
2022-04-07 21:44:11 +0100

    Delete configuration test files

--- travistest.test.js
+++
@@ -1,9 +0,0 @@
-const TravisTest = require('./travistest');
-
-// Just testing out Travis CI configuration
-describe('Testing Travis CI setup', () => {
-  it('has everything properly setup', () => {
-    const test = new TravisTest();
-    expect(test.mymethod).toBe(true);
-    });
-});
