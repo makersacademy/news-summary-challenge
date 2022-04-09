0da379b6d1b7e783983b6e4123f7d6736fc8cbec
Valentina <maggio.vale@gmail.com>
2022-04-09 12:09:22 +0100

    Add Dotenv to store Api key
    - update GuardianApi class to retrieve key from .env

--- guardianApi.js
+++ guardianApi.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 class GuardianApi {
   getHeadlines(searchField, callback) {
-    fetch(`https://content.guardianapis.com/search?q=${searchField}&query-fields=headline&show-fields=thumbnail,headline,byline&order-by=newest&api-key=test`)
+    fetch(`https://content.guardianapis.com/search?q=${searchField}&query-fields=headline&show-fields=thumbnail,headline,byline&order-by=newest&api-key=process.env.GUARDIAN_API_KEY`)
       .then(response => response.json())
       .then(data => {
         callback(data)
