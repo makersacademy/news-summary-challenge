037b1425f9108fba9556a269c22a9ba65deaeea9
Michael Su <mailmsuu@gmail.com>
2022-05-07 11:09:00 +0100

    passed test for api

--- newsApi.js
+++ newsApi.js
@@ -1,5 +1,19 @@
+const apiKey = require('./apiKey');
+
 class newsApi {
 
+  fetchNews(callback) {
+    fetch(`https://content.guardianapis.com/search?q=&query-fields=headline&show-fields=thumbnail,headline,byline&order-by=newest&api-key=${apiKey}`)
+    .then(response => response.json())
+    .then((data) => {
+      console.log('Load', data);
+      callback(data);
+    })
+    .catch(() => {
+      console.error('Error');
+      callback();
+    })
+  }
 }
 
-module.export = newsApi;
+module.exports = newsApi;
