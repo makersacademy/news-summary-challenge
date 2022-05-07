c1b6e704a4e5bba05e1abd3f91cb91642a526d6c
Joe McNally <joe.t.mcnally@hotmail.com>
2022-05-07 22:03:42 +0100

    Added some css

--- src/articlesView.js
+++ src/articlesView.js
@@ -23,16 +23,20 @@ class ArticlesView {
       const articleEl = document.createElement('div');
       articleEl.classList.add('article');
 
+      const imgBorderEl = document.createElement('div');
+      imgBorderEl.classList.add('border');
+
       const pictureEl = document.createElement('img');
       pictureEl.classList.add('pic');
       pictureEl.src = article.image;
+      imgBorderEl.append(pictureEl);
 
       const nameEl = document.createElement('a');
       nameEl.classList.add('title');
       nameEl.href = article.url;
       nameEl.innerText = article.headline;
 
-      articleEl.append(pictureEl, nameEl);
+      articleEl.append(imgBorderEl, nameEl);
       
       this.mainContainerEl.append(articleEl);
     })
