1e40e8a49385081183325f62d3aa7b908f92634d
Michael Su <mailmsuu@gmail.com>
2022-05-07 09:46:33 +0100

    wrote next test to add news

--- newsModel.test.js
+++ newsModel.test.js
@@ -8,4 +8,11 @@ describe('NewsModel', () => {
   it('initially returns an empty array', () => {
     expect(newsModel.getNews()).toEqual([]);
   })
+
+  describe('#addNews', () => {
+    it('can add a news headline into the news array', () => {
+      newsModel.addNews('This is a news article');
+      expect(newsModel.getNews()).toEqual(['This is a news article']);
+    })
+  })
 })
