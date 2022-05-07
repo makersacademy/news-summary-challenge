44372e1f9f0fc33952f8a5e25fbaf80eeb0bbcf9
Joe McNally <joe.t.mcnally@hotmail.com>
2022-05-07 13:32:52 +0100

    Created a function to turn an article in the api into a more readable JS object that can be added to the articles array in the model.

--- src/articlesModel.js
+++ src/articlesModel.js
@@ -10,6 +10,19 @@ class ArticlesModel {
   addArticle(article) {
     this.articles.push(article);
   }
+
+  convertData(data) {
+    const title = data.webTitle;
+    const pic = data.fields.thumbnail;
+    const address = data.webUrl;
+
+    const article = {
+      headline: title,
+      picture: pic,
+      url: address
+    };
+    return article
+  }
 }
 
 module.exports = ArticlesModel;
--- src/articlesModel.test.js
+++ src/articlesModel.test.js
@@ -16,4 +16,20 @@ describe('ArticlesModel', () => {
     model.addArticle(article);
     expect(model.getArticles()).toEqual([article]);
   });
+
+  it('convertData converts the data for an article received from the API into a more readable JS object', () => {
+    const data = {
+      webTitle: 'Real Madrid reach the Champions League final',
+      webUrl: 'www.madridfinal.com',
+      fields: {
+        thumbnail: 'madridpicture.jpg'
+      }
+    };
+    const article = {
+      headline: 'Real Madrid reach the Champions League final',
+      picture: 'madridpicture.jpg',
+      url: 'www.madridfinal.com'
+    }
+    expect(model.convertData(data)).toEqual(article);
+  })
 });
