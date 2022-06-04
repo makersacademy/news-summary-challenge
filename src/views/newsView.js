c04d8760ccb00dc0218dd2aaa4ef91aea3e61344
Yaroslava Yates <yaroslava.yates@gmail.com>
2022-06-04 19:35:55 +0100

    css plus displaying headlines, img, links

--- src/views/newsView.js
+++ src/views/newsView.js
@@ -8,10 +8,31 @@ class NewsView {
   displayNews() {
     const headlines = this.model.getNews();
 
+    console.log(headlines);
+
     headlines.forEach((headline) => {
-      const headlineEl = document.createElement('div');
-      headlineEl.innerText = headline;
+      const headlineEl = document.createElement('a');
       headlineEl.className = 'headline';
+      headlineEl.href = headline.webUrl;
+
+      const imgDivEl = document.createElement('div');
+
+      const imgEl = document.createElement('img');
+      imgEl.className = 'left';
+      imgEl.src = headline.fields.thumbnail;
+
+      imgDivEl.append(imgEl);
+      headlineEl.append(imgDivEl);
+
+      const articleDivEl = document.createElement('div');
+      articleDivEl.className = 'right';
+
+      const articleEl = document.createElement('p');
+      articleEl.innerText = headline.webTitle;
+
+      articleDivEl.append(articleEl);
+      headlineEl.append(articleDivEl);
+
       this.mainContainerEl.append(headlineEl);
     });
   }
