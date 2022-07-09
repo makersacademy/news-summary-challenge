aeeb4ab4b356d20959a6696d73b1b52dc2ceca2f
Joe McGarry <joewmcgarry@gmail.com>
2022-07-09 11:26:02 +0100

    First test passing in Api class

--- newsApi.js
+++ newsApi.js
@@ -1,5 +1,11 @@
 class NewsApi {
-  
+  loadData(callback) {
+    fetch('http://localhost:3000/news')
+      .then(response => response.json())
+      .then(data => {
+        callback(data)
+      });
+  }
 }
 
 module.exports = NewsApi;
