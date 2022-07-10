f806429707167922adc18e719c1afb27e8fe7e05
Hafiz <hafizalimi77@gmail.com>
2022-07-10 13:10:03 +0100

    refactoring NewsApi

--- newsApi.js
+++ newsApi.js
@@ -1,24 +1,43 @@
 const ApiKey = require('./apiKey')
 
-class NewsApi {
-  constructor(){
-    this.url = 'https://content.guardianapis.com/search?q=';
-    this.apiKey = `${ApiKey}`;
-  }
 
-  fetchStories(newsCallback){
-    const fullUrl = this.url + this.apiKey
-    fetch(fullUrl)
+class NewsApi {
+  loadHeadlines(searchText, callBack,errorCallBack) {
+    console.log(searchText);
+    const url = `https://content.guardianapis.com/search?q=${searchText}&query-fields=headline&show-fields=thumbnail,headline,byline&order-by=newest&api-key=${ApiKey}`
+    fetch(url)
       .then(response => response.json())
-      .then((data) => {
-        newsCallback(data);
+      .then(data => {
+        callBack(data);
+      })
+      .catch((error) => {
+        console.log('error:', error);
+        errorCallBack(error);
       })
   }
-
-  setUrl(keyWord){
-    const fullUrl = this.url + keyWord + this.apiKey
-    fullUrl
-  }
 }
+
+// class NewsApi {
+//   constructor(){
+//     this.url = 'https://content.guardianapis.com/search?q=';
+//     this.apiKey = `${ApiKey}`;
+//   }
+
+//   fetchStories(newsCallback){
+//     const fullUrl = this.url + this.apiKey
+//     fetch(fullUrl)
+//       .then(response => response.json())
+//       .then((data) => {
+//         newsCallback(data);
+//       })
+//   }
+
+//   setUrl(keyWord){
+//     const fullUrl = this.url + keyWord + this.apiKey
+//     fullUrl
+//   }
+// }
+
+
 
 module.exports = NewsApi;
--- newsApi.test.js
+++ newsApi.test.js
@@ -2,34 +2,56 @@ const NewsApi = require('./newsApi')//requring from another module
 
 require('jest-fetch-mock').enableMocks()//fetch Mockup
 
-describe('NewsApi', () => {
-  it('gets all of todays headlines from news data Api from getNews()', () => {
-    const api = new NewsApi();//getting NewsApi from another class
-    fetch.mockResponseOnce(JSON.stringify({
-      response: {
-        results: [
-          {MainHeadline: 'first headline'},
-          {MainHeadline: 'second headline'}
-        ]
-      }
-    }));
-    api.fetchStories((dataFromApi) => {
-      expect(dataFromApi.response.results[0].MainHeadline).toBe('first headline')
+
+
+describe('#NewApi', () => {
+  it('calls and fetch data', () => {
+    const api = new NewsApi();
+
+    fetch.mockResponseOnce(
+      JSON.stringify({
+        headline: ('this is news')
+      })
+    )
+
+    api.loadHeadlines("",(callback) => {
+      expect(callback.headline).toStrictEqual('this is news');
+    })
+  });
+
+  it('gets all of todays headlines from news data Api', () => {
+    const api = new NewsApi();
+
+    fetch.mockResponseOnce(
+      JSON.stringify({
+        response: {
+          result: [
+            {headline: 'first headline'}
+          ]
+        }
+      })
+    )
+    api.loadHeadlines("",(callback) => {
+      expect(callback.response.result[0].headline).toBe('first headline')
     })
   })
-
   it('fetches stories when given keywords in the search query', () => {
     const api = new NewsApi();
-    fetch.mockResponseOnce(JSON.stringify({
-      response: {
-        results: [
-          {sectionId: 'football'},
-          {sectionName: 'Football'}
-        ]
-      }
-    }));
-    api.setUrl((dataFromApi) => {
-      expect(dataFromApi.response.results.toContain('/football/'))
+
+    fetch.mockResponseOnce(
+      JSON.stringify({
+        response: {
+          result: [
+            {sectionId: 'football'},
+            {sectionName: 'Football'}
+          ]
+        }
+      })
+    );
+    api.loadHeadlines("",(callback) => {
+      expect(callback.response.result[0].sectionId).toBe('football')
     });
   })
-})
+}) 
+
+
