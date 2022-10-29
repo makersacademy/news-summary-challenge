299cf80dee8cd4d3f06971e225eacb006c873481
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-10-29 12:26:46 +0100

    Fix bug on test as fetching guardian API mock as JS object instead of JSON

--- newsClient.js
+++ newsClient.js
@@ -1,3 +1,16 @@
-class NewsClient {}
+class NewsClient {
+  constructor() {
+    this._fetchUrl =
+      'https://content.guardianapis.com/search?q=&query-fields=headline&show-fields=thumbnail,headline,byline&order-by=newest&api-key=d744d242-e274-4f2f-987c-3dd2693c60f0';
+  }
+  loadNews(callback) {
+    fetch(this._fetchUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('data: ', data);
+        callback(data);
+      });
+  }
+}
 
 module.exports = NewsClient;
--- newsClient.test.js
+++ newsClient.test.js
@@ -6,14 +6,12 @@ describe('Client class', () => {
   it('calls fetch and loads data', (done) => {
     const newsClient = new NewsClient();
 
-    fetch.mockResponseOnce(mockJsonGuardian);
-    console.log('mock: ', mockJsonGuardian);
+    fetch.mockResponse(JSON.stringify(mockJsonGuardian));
 
     newsClient.loadNews((returnedDataFromApi) => {
       console.log('JO');
       console.log('returned data', returnedDataFromApi);
-      expect(returnedDataFromApi.response.status).toBe('ok');
-      expect(returnedDataFromApi.response.results).not.toBeNull();
+      expect(returnedDataFromApi).toEqual(mockJsonGuardian); // Asessing deep equality
       done();
     });
   });
