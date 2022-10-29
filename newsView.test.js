1b145a65fe262937830e954196c62a4b777f12a7
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-10-29 14:28:04 +0100

    Add second passing test for displaying several titles

--- newsView.test.js
+++ newsView.test.js
@@ -34,4 +34,33 @@ describe('NewsViews class', () => {
     expect(document.querySelector('h1').textContent).toEqual('Carrots');
     done();
   });
+
+  it('displays the title of two piece of news', (done) => {
+    document.body.innerHTML = fs.readFileSync('./index.html');
+
+    const mockClient = {
+      loadNews: (callback) => {
+        callback({
+          response: {
+            results: [
+              {
+                webTitle: 'Carrots',
+              },
+              {
+                webTitle: 'Potatos',
+              },
+            ],
+          },
+        });
+      },
+    };
+
+    const newsView = new NewsView(mockClient);
+
+    newsView.displayNews();
+
+    expect(document.querySelectorAll('div.news').length).toBe(2);
+    expect(document.querySelectorAll('h1')[1].textContent).toEqual('Potatos');
+    done();
+  });
 });
