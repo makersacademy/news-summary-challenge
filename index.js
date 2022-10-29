533e96af1f77cb2f229021bc9c16290a6a73552e
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-10-29 14:24:45 +0100

    Modify displayNews() to pass first  test

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 var guardianAPIJson = require('./guardianSearchWithThumbnails.json');
+const NewsClient = require('./newsClient');
 
 console.log(guardianAPIJson);
+
+const newsClient = new NewsClient();
+const newsView = new NewsView(newsClient);
+
+newsView.displayNews();
--- newsView.js
+++ newsView.js
@@ -1,3 +1,25 @@
-class NewsView {}
+class NewsView {
+  constructor(view) {
+    this.view = view;
+  }
+
+  displayNews() {
+    this.view.loadNews((news) => {
+      console.log('news', news);
+      news.response.results.forEach((individualNews) => {
+        console.log('individualNews: ', individualNews);
+        console.log('individual news title', individualNews.webTitle);
+        var newsEl = document.createElement('div');
+        newsEl.className = 'news';
+
+        var title = document.createElement('h1');
+        title.textContent = individualNews.webTitle;
+
+        newsEl.appendChild(title);
+        document.body.append(newsEl);
+      });
+    });
+  }
+}
 
 module.exports = NewsView;
