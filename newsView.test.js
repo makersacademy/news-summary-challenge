de4f431ed820f7704d6c37cdd801edf51f6b3caf
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-10-29 15:39:49 +0100

    Add passing test for image and title of two pieces of news, modify prev tests as failing once added the image feature

--- newsView.test.js
+++ newsView.test.js
@@ -34,6 +34,10 @@ describe('NewsViews class', () => {
             results: [
               {
                 webTitle: 'Carrots',
+                fields: {
+                  thumbnail:
+                    'https://media.guim.co.uk/a19e7fd0401a3c79c3edcfff62b511af069504ab/0_112_4478_2688/500.jpg',
+                },
               },
             ],
           },
@@ -60,9 +64,17 @@ describe('NewsViews class', () => {
             results: [
               {
                 webTitle: 'Carrots',
+                fields: {
+                  thumbnail:
+                    'https://media.guim.co.uk/a19e7fd0401a3c79c3edcfff62b511af069504ab/0_112_4478_2688/500.jpg',
+                },
               },
               {
                 webTitle: 'Potatos',
+                fields: {
+                  thumbnail:
+                    'https://media.guim.co.uk/a19e7fd0401a3c79c3edcfff62b511af069504ab/0_112_4478_2688/500.jpg',
+                },
               },
             ],
           },
@@ -114,8 +126,50 @@ describe('NewsViews class', () => {
     expect(newsContainer.querySelectorAll('h1')[0].textContent).toEqual(
       'Carrots'
     );
-    expect(newsContainer.querySelectorAll('img')[0].textContent).toEqual(
+    expect(newsContainer.querySelectorAll('img')[0].src).toEqual(
       'https://media.guim.co.uk/a19e7fd0401a3c79c3edcfff62b511af069504ab/0_112_4478_2688/500.jpg'
+    );
+    done();
+  });
+
+  it('displays the title and the image of two pieces of news', (done) => {
+    //document.body.innerHTML = fs.readFileSync('./index.html');
+
+    const mockClient = {
+      loadNews: (callback) => {
+        callback({
+          response: {
+            results: [
+              {
+                webTitle: 'Carrots',
+                fields: {
+                  thumbnail:
+                    'https://media.guim.co.uk/a19e7fd0401a3c79c3edcfff62b511af069504ab/0_112_4478_2688/500.jpg',
+                },
+              },
+              {
+                webTitle: 'Potatos',
+                fields: {
+                  thumbnail:
+                    'https://media.guim.co.uk/9c51164d2edbaf1a1ba3b39ee1561ad282507384/0_576_8640_5184/500.jpg',
+                },
+              },
+            ],
+          },
+        });
+      },
+    };
+
+    const newsView = new NewsView(mockClient, newsContainer);
+
+    newsView.displayNews();
+
+    expect(newsContainer.querySelectorAll('div.news').length).toBe(2);
+    expect(newsContainer.querySelectorAll('h1')[1].textContent).toEqual(
+      'Potatos'
+    );
+    expect(newsContainer.querySelectorAll('img')[1].src).toEqual(
+      'https://media.guim.co.uk/9c51164d2edbaf1a1ba3b39ee1561ad282507384/0_576_8640_5184/500.jpg'
     );
     done();
   });
