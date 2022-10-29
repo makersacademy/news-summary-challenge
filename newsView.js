25813b4b7167f2dca825ec383220ad4e7d4bf867
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-10-29 15:24:16 +0100

    Modify displayNews() to include thumbnail of news

--- newsView.js
+++ newsView.js
@@ -9,10 +9,12 @@ class NewsView {
       news.response.results.forEach((individualNews) => {
         var newsEl = document.createElement('div');
         newsEl.className = 'news';
-
+        var image = document.createElement('img');
+        image.src = individualNews.fields.thumbnail;
         var title = document.createElement('h1');
         title.textContent = individualNews.webTitle;
 
+        newsEl.appendChild(image);
         newsEl.appendChild(title);
         this.container.appendChild(newsEl);
       });
