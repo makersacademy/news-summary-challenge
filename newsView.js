a08f848cb4273dee83b02252aba01d8ecc4aed14
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-10-30 18:33:22 +0000

    Modify newsView and displayIndividualNews method to pass test

--- newsView.js
+++ newsView.js
@@ -2,46 +2,52 @@ class NewsView {
   constructor(client, container) {
     this.client = client;
     this.container = document.getElementById('container');
-    this.searchInput = document.getElementById('searchInput');
-    this.newsFromDOM = document.querySelectorAll('.news');
+    // this.newsFromDOM = document.querySelectorAll('.news');
     this.buttonEl = document.querySelector('#search-button');
+    // this.searchInput = searchInput;
 
     this.buttonEl.addEventListener('click', () => {
-      let searchInput = document.querySelector('#search-input');
-      console.log(this.newsFromDOM);
+      let searchInput = document.querySelector('#search-input').value;
+      let allNews = document.querySelectorAll('.news');
+      console.log('allNews', allNews);
 
-      this.newsFromDOM.forEach((individualNews) => {
+      document.querySelectorAll('.news').forEach((news) => {
+        news.remove();
+      });
+      this.displayIndividualNews(searchInput);
+      searchInput = '';
+    });
+  }
+
+  displayIndividualNews(searchInput) {
+    this.client.loadNews((news) => {
+      news.response.results.forEach((individualNews) => {
+        console.log('webtitle', individualNews.webTitle.toLowerCase());
+        console.log('searchinput', searchInput.toLowerCase());
         if (
           individualNews.webTitle
             .toLowerCase()
             .includes(searchInput.toLowerCase())
         ) {
-          this.displayIndividualNews(individualNews);
-        } else {
-          individualNews.remove();
+          var newsEl = document.createElement('div');
+          newsEl.className = 'news';
+
+          var image = document.createElement('img');
+          image.src = individualNews.fields.thumbnail;
+
+          var title = document.createElement('h1');
+
+          var link = document.createElement('a');
+          link.href = individualNews.webUrl;
+          link.textContent = individualNews.webTitle;
+
+          title.appendChild(link);
+          newsEl.appendChild(image);
+          newsEl.appendChild(title);
+          this.container.appendChild(newsEl);
         }
       });
-      document.querySelector('#search-input').value = '';
     });
-  }
-
-  displayIndividualNews(individualNews) {
-    var newsEl = document.createElement('div');
-    newsEl.className = 'news';
-
-    var image = document.createElement('img');
-    image.src = individualNews.fields.thumbnail;
-
-    var title = document.createElement('h1');
-
-    var link = document.createElement('a');
-    link.href = individualNews.webUrl;
-    link.textContent = individualNews.webTitle;
-
-    title.appendChild(link);
-    newsEl.appendChild(image);
-    newsEl.appendChild(title);
-    this.container.appendChild(newsEl);
   }
 
   displayNews() {
