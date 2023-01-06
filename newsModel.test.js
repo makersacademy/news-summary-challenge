44ef6a4e5460e4e53a0c99ae3f4940cfade6561e
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-01-06 13:15:43 +0000

    prepared initial model tests

---
+++ newsModel.test.js
@@ -0,0 +1,29 @@
+describe("NewsModel", () => {
+  it("returns an empy array", () => {
+    const model = new NewsModel();
+
+    expect(model.getNews()).toEqual([]);
+  });
+  it("returns an correct list", () => {
+    const model = new NewsModel();
+
+    const newsItemOne =
+      "Egypt says wreckage from flight found in Mediterranean";
+    const newsItemTwo =
+      "Humans damaging planet faster than it can recover, report finds";
+    model.addNewsItem(newsItemOne);
+    model.addNewsItem(newsItemTwo);
+
+    expect(model.getNews()).toEqual([newsItemOne, newsItemTwo]);
+  });
+  it("resets the array", () => {
+    const model = new NewsModel();
+    const newsItemOne =
+      "Egypt says wreckage from flight found in Mediterranean";
+
+    model.addNewsItem(newsItemOne);
+    model.reset();
+
+    expect(model.getNews()).toEqual([]);
+  });
+});
