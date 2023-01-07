60aa89356955a1d77c04ae5b6088bb61cbc7a636
Josh Carvell <j_carvell@hotmail.co.uk>
2023-01-07 16:54:46 +0000

    added fetchStories method to newsClient class

--- newsClient.js
+++ newsClient.js
@@ -11,12 +11,18 @@ class NewsClient {
   }
 
   // Make a GET request for today's stories 
-  fetchStories(){
+  fetchStories(callback){
 
     // Form APIURL
     const apiUrl = `https://content.guardianapis.com/search?from-date=${this.date}&api-key=${apiKey}`
     
-    
+    // Make a get request to the Guardian API Endpoint
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        callback(data);
+      });
+  
   }
 
 }
