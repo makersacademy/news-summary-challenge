6102ef6938dc5f8786fe7cbaee6a1bb174138474
Josh Carvell <j_carvell@hotmail.co.uk>
2023-01-07 16:44:23 +0000

    added constant to get the apiURL for the current date

--- newsClient.js
+++ newsClient.js
@@ -1,15 +1,21 @@
 const moment = require('moment')
 const apiKey = require('./apikey.js');
+
 class NewsClient {
 
   constructor(){
     // Get the date in the format needed for the APIURL
     this.date = moment().format('YYYY-MM-DD');
+    
     //
   }
 
   // Make a GET request for today's stories 
   fetchStories(){
+
+    // Form APIURL
+    const apiUrl = `https://content.guardianapis.com/search?from-date=${this.date}&api-key=${apiKey}`
+    
     
   }
 
