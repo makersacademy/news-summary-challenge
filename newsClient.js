84e7d24e7847eaf6ea10441c68d24efc19b08d8b
Josh Carvell <j_carvell@hotmail.co.uk>
2023-01-07 16:34:13 +0000

    added apiKey file to gitignore, added moment library to help handle date

--- newsClient.js
+++ newsClient.js
@@ -1,5 +1,18 @@
+const moment = require('moment')
+const apiKey = require('./apikey.js');
 class NewsClient {
+
+  constructor(){
+    // Get the date in the format needed for the APIURL
+    this.date = moment().format('YYYY-MM-DD');
+    //
+  }
+
+  // Make a GET request for today's stories 
+  fetchStories(){
+    
+  }
 
 }
 
-module.export = NewsClient;
+module.exports = NewsClient;
--- newsClient.test.js
+++ newsClient.test.js
@@ -1,7 +1,31 @@
-const NewsClient = require('./newsCient');
+const NewsClient = require('./newsClient');
+require("jest-fetch-mock").enableMocks();
+
 
 describe('NewsModel', () => {
-  it ('', () => {
+
+  describe("fetchStories", () => {
+    // Reset mocks before each test 
+    beforeEach(() => {
+      fetch.resetMocks();
+    });
+
+    it (' Makes an API request for the current date', () => {
+
+      //Instantiate client
+      const client = new NewsClient()
+      // Mocking the API 
+      fetch.mockResponseOnce(JSON.stringify({ status: "okay", total: 33 }));
+
+      // Call the fetch method and expect the mock response
+      client.fetchStories((data) => {
+        expect(data.status).toEqual("okay");
+        expect(data.total).toEqual(33);
+        done();
+      });
+    })
+
     
-  })
+  })  
+  
 })
