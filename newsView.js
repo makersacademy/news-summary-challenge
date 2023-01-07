93bf707e06023b7b9d05ec0037098ece28ae61d2
Josh Carvell <j_carvell@hotmail.co.uk>
2023-01-07 14:55:50 +0000

    added constructor to view class

--- newsView.js
+++ newsView.js
@@ -1,7 +1,12 @@
 class NewsView {
 
+  constructor(model) {
+    this.model = model
+    this.mainContainerEl = document.querySelector('#main-container')
+  }
+
   displayStories() {
-    
+
   }
   
 }
