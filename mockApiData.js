dab6f709df7eb7bfccea5b920cd84ab8d1ddde2a
Joe Burgess <josephburgess@gmail.com>
2023-02-01 11:40:51 +0000

    amending mock data to include 'standfirst' summary

--- mockApiData.js
+++ mockApiData.js
@@ -24,9 +24,111 @@ const apiData = {
         fields: {
           headline:
             'UK house price growth slows to lowest rate since mid-2020; all eyes on Fed decision – business live',
+          standfirst:
+            '<p>UK annual house price growth slows to 1.1%; investors await US Federal Reserve message on whether it will tighten monetary policy further</p>',
           byline: 'Julia Kollewe and Jasper Jolly',
           thumbnail:
             'https://media.guim.co.uk/4eb07f2f4bbd086197aa76ca2de731ad7fefc9fd/0_228_4500_2700/500.jpg',
+        },
+        isHosted: false,
+        pillarId: 'pillar/news',
+        pillarName: 'News',
+      },
+      {
+        id: 'politics/live/2023/feb/01/rishi-sunak-pmqs-keir-starmer-mass-strikes-teachers-trains-rail-border-uk-politics-live',
+        type: 'liveblog',
+        sectionId: 'politics',
+        sectionName: 'Politics',
+        webPublicationDate: '2023-02-01T11:20:20Z',
+        webTitle:
+          'Raab should be suspended until bullying inquiry concludes, civil servants’ union says – UK politics live ',
+        webUrl:
+          'https://www.theguardian.com/politics/live/2023/feb/01/rishi-sunak-pmqs-keir-starmer-mass-strikes-teachers-trains-rail-border-uk-politics-live',
+        apiUrl:
+          'https://content.guardianapis.com/politics/live/2023/feb/01/rishi-sunak-pmqs-keir-starmer-mass-strikes-teachers-trains-rail-border-uk-politics-live',
+        fields: {
+          headline:
+            'Raab should be suspended until bullying inquiry concludes, civil servants’ union says – UK politics live ',
+          standfirst:
+            '<p>Government insiders believe Dominic Raab is fighting for his political survival, with one <a href="https://www.theguardian.com/politics/2023/jan/31/raab-bullying-claims-inquiry-has-interviewed-three-top-mandarins">describing him as “toast”</a></p>',
+          byline: 'Andrew Sparrow',
+          thumbnail:
+            'https://media.guim.co.uk/caafd87c5e37337396dc36082a485748e25bf2e4/0_163_4371_2622/500.jpg',
+        },
+        isHosted: false,
+        pillarId: 'pillar/news',
+        pillarName: 'News',
+      },
+      {
+        id: 'uk-news/2023/feb/01/uk-police-removing-large-amount-of-online-gun-making-guides',
+        type: 'article',
+        sectionId: 'uk-news',
+        sectionName: 'UK news',
+        webPublicationDate: '2023-02-01T10:53:25Z',
+        webTitle:
+          'UK police removing ‘large amount’ of online gun-making guides',
+        webUrl:
+          'https://www.theguardian.com/uk-news/2023/feb/01/uk-police-removing-large-amount-of-online-gun-making-guides',
+        apiUrl:
+          'https://content.guardianapis.com/uk-news/2023/feb/01/uk-police-removing-large-amount-of-online-gun-making-guides',
+        fields: {
+          headline:
+            'UK police removing ‘large amount’ of online gun-making guides',
+          standfirst:
+            '<p>Exclusive: Fears growing that far-right extremists are producing 3D-printed firearms to use on streets of Britain</p>',
+          byline: 'Josh Halliday North of England correspondent',
+          thumbnail:
+            'https://media.guim.co.uk/da40e78f1f8cf218017fd47afb3f7a345a434c3c/0_241_3100_1859/500.jpg',
+        },
+        isHosted: false,
+        pillarId: 'pillar/news',
+        pillarName: 'News',
+      },
+      {
+        id: 'business/live/2023/feb/01/uk-annual-house-price-growth-slows-lowest-rate-since-mid-2020-chinas-factories-slump-us-federal-reserve-rate-decision',
+        type: 'liveblog',
+        sectionId: 'business',
+        sectionName: 'Business',
+        webPublicationDate: '2023-02-01T10:52:17Z',
+        webTitle:
+          'UK house price growth slowest since mid-2020; manufacturing contracts for sixth month in a row – business live',
+        webUrl:
+          'https://www.theguardian.com/business/live/2023/feb/01/uk-annual-house-price-growth-slows-lowest-rate-since-mid-2020-chinas-factories-slump-us-federal-reserve-rate-decision',
+        apiUrl:
+          'https://content.guardianapis.com/business/live/2023/feb/01/uk-annual-house-price-growth-slows-lowest-rate-since-mid-2020-chinas-factories-slump-us-federal-reserve-rate-decision',
+        fields: {
+          headline:
+            'UK house price growth slowest since mid-2020; manufacturing contracts for sixth month in a row – business live',
+          standfirst:
+            '<p>UK annual house price growth slows to 1.1%; investors await US Federal Reserve message on whether it will tighten monetary policy further</p>',
+          byline: 'Julia Kollewe and Jasper Jolly',
+          thumbnail:
+            'https://media.guim.co.uk/4eb07f2f4bbd086197aa76ca2de731ad7fefc9fd/0_228_4500_2700/500.jpg',
+        },
+        isHosted: false,
+        pillarId: 'pillar/news',
+        pillarName: 'News',
+      },
+      {
+        id: 'education/2023/feb/01/uk-international-students-visa-restrictions-economic-self-harm',
+        type: 'article',
+        sectionId: 'education',
+        sectionName: 'Education',
+        webPublicationDate: '2023-02-01T10:37:15Z',
+        webTitle:
+          'UK curbs on international student visas would be ‘act of economic self-harm’',
+        webUrl:
+          'https://www.theguardian.com/education/2023/feb/01/uk-international-students-visa-restrictions-economic-self-harm',
+        apiUrl:
+          'https://content.guardianapis.com/education/2023/feb/01/uk-international-students-visa-restrictions-economic-self-harm',
+        fields: {
+          headline:
+            'UK curbs on international student visas would be ‘act of economic self-harm’',
+          standfirst:
+            '<p>University leaders fear billions being lost as a result of battles within government over immigration policy</p><p></p><p></p>',
+          byline: 'Richard Adams, Education editor',
+          thumbnail:
+            'https://media.guim.co.uk/39e9e72f9e11dc72ca5fd4e1997c733ac107bce6/0_352_5315_3191/500.jpg',
         },
         isHosted: false,
         pillarId: 'pillar/news',
@@ -45,6 +147,8 @@ const apiData = {
           'https://content.guardianapis.com/money/2023/feb/01/uk-house-prices-fall-for-fifth-month-in-a-row',
         fields: {
           headline: 'UK house prices fall for fifth month in a row',
+          standfirst:
+            '<p>Average cost of home now 3.2% below August 2022 peak as mortgage demand  slumps</p>',
           byline: 'Joanna Partridge',
           thumbnail:
             'https://media.guim.co.uk/a7f3dcc9aba9ae2b9a21a5c292e06c039e9d7ad3/0_108_3000_1800/500.jpg',
@@ -67,6 +171,8 @@ const apiData = {
         fields: {
           headline:
             'Specieswatch: donkey work is done as UK population dwindles',
+          standfirst:
+            '<p><em>Equus asinus </em>is gradually disappearing from countryside and as a working animal is now a rarity</p>',
           byline: 'Paul Brown',
           thumbnail:
             'https://media.guim.co.uk/2b6ad490d20f7112f2de8e2c7e39e5709450a75e/452_733_3049_1829/500.jpg',
@@ -90,6 +196,8 @@ const apiData = {
         fields: {
           headline:
             'UK benefit changes have pushed people into dead-end, low-paid jobs, says IFS',
+          standfirst:
+            '<p>Tougher rules have boosted employment but jobs offer scant career progression and contribute little to tax revenue</p>',
           byline: 'Larry Elliott Economics editor',
           thumbnail:
             'https://media.guim.co.uk/e295474540148a99d0f0167eb0d230c22fd93d59/571_953_4090_2456/500.jpg',
@@ -113,6 +221,8 @@ const apiData = {
         fields: {
           headline:
             'UK supermarket price inflation hits record high, adding £788 to annual bills',
+          standfirst:
+            '<p>Milk, eggs and dog food were grocery items with fastest price rises, figures from Kantar show </p>',
           byline: 'Julia Kollewe and Richard Partington',
           thumbnail:
             'https://media.guim.co.uk/ac43170eba40d7c2912012bfac93eca37531002c/0_0_8256_4954/500.jpg',
@@ -136,6 +246,8 @@ const apiData = {
         fields: {
           headline:
             'Almost 13,000 offshore companies with UK property fail to declare owners',
+          standfirst:
+            '<p>The companies may now face fines and a ban on selling their land, the government has said</p>',
           byline: 'Carmen Aguilar García, Zeke Hunter-Green and Rowena Mason',
           thumbnail:
             'https://media.guim.co.uk/eeab3db9ee4e8b5f6fceecf7cd0b3799e0d986ce/950_0_4177_2508/500.jpg',
@@ -159,73 +271,11 @@ const apiData = {
         fields: {
           headline:
             'Up to half a million to strike across UK as talks go ‘backwards’',
+          standfirst:
+            '<p>Action by teachers, civil servants, Border Force staff and train drivers to go ahead, with ministers accused of ‘stonewalling’ </p>',
           byline: 'Jessica Elgot, Sally Weale and Gwyn Topham',
           thumbnail:
             'https://media.guim.co.uk/2886960838a33c7bf1bba61d245fb4f270f69712/0_192_5792_3476/500.jpg',
-        },
-        isHosted: false,
-        pillarId: 'pillar/news',
-        pillarName: 'News',
-      },
-      {
-        id: 'uk-news/2023/jan/31/three-russians-under-sanctions-own-uk-property-via-overseas-entities',
-        type: 'article',
-        sectionId: 'uk-news',
-        sectionName: 'UK news',
-        webPublicationDate: '2023-01-31T20:26:54Z',
-        webTitle:
-          'Three Russians under sanctions own UK property via overseas entities',
-        webUrl:
-          'https://www.theguardian.com/uk-news/2023/jan/31/three-russians-under-sanctions-own-uk-property-via-overseas-entities',
-        apiUrl:
-          'https://content.guardianapis.com/uk-news/2023/jan/31/three-russians-under-sanctions-own-uk-property-via-overseas-entities',
-        fields: {
-          headline:
-            'Three Russians under sanctions own UK property via overseas entities',
-          byline: 'Rowena Mason and Rob Davies',
-          thumbnail:
-            'https://media.guim.co.uk/70e7458a42f6d494c852853a4820d3bd1b00cce0/0_44_2537_1523/500.jpg',
-        },
-        isHosted: false,
-        pillarId: 'pillar/news',
-        pillarName: 'News',
-      },
-      {
-        id: 'world/2023/jan/31/us-will-not-supply-f-16-fighter-jets-to-ukraine-says-joe-biden',
-        type: 'article',
-        sectionId: 'world',
-        sectionName: 'World news',
-        webPublicationDate: '2023-01-31T18:30:29Z',
-        webTitle: 'US and UK rule out sending F-16 fighter jets to Ukraine',
-        webUrl:
-          'https://www.theguardian.com/world/2023/jan/31/us-will-not-supply-f-16-fighter-jets-to-ukraine-says-joe-biden',
-        apiUrl:
-          'https://content.guardianapis.com/world/2023/jan/31/us-will-not-supply-f-16-fighter-jets-to-ukraine-says-joe-biden',
-        fields: {
-          headline: 'US and UK rule out sending F-16 fighter jets to Ukraine',
-          byline: 'Dan Sabbagh in Kyiv',
-          thumbnail:
-            'https://media.guim.co.uk/34924d0408cb80ffccbd912e0866936f0e875844/437_0_4033_2422/500.jpg',
-        },
-        isHosted: false,
-        pillarId: 'pillar/news',
-        pillarName: 'News',
-      },
-      {
-        id: 'society/2023/jan/31/i-fear-for-my-trans-daughters-future-in-the-uk',
-        type: 'article',
-        sectionId: 'society',
-        sectionName: 'Society',
-        webPublicationDate: '2023-01-31T18:25:44Z',
-        webTitle: 'I fear for my trans daughter’s future in the UK | Letter',
-        webUrl:
-          'https://www.theguardian.com/society/2023/jan/31/i-fear-for-my-trans-daughters-future-in-the-uk',
-        apiUrl:
-          'https://content.guardianapis.com/society/2023/jan/31/i-fear-for-my-trans-daughters-future-in-the-uk',
-        fields: {
-          headline: 'I fear for my trans daughter’s future in the UK',
-          thumbnail:
-            'https://media.guim.co.uk/dc1b5c919f439e5217e53269c6962d884a9da73b/0_289_6720_4032/500.jpg',
         },
         isHosted: false,
         pillarId: 'pillar/news',
