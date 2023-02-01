17f7608cdfe31e0de0f48d47d78799df0ec81160
Joe Burgess <josephburgess@gmail.com>
2023-02-01 17:36:16 +0000

    deleting comments

--- src/newsClient.js
+++ src/newsClient.js
@@ -20,15 +20,6 @@ class NewsClient {
         callback(data);
       });
   }
-  // searchNews(searchTerm, callback) {
-  //   fetch(
-  //     `https://content.guardianapis.com/search?q=${searchTerm}&query-fields=headline&show-fields=thumbnail,headline,byline&order-by=newest&api-key=${API_KEY}`
-  //   )
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       callback(data);
-  //     });
-  // }
 }
 
 module.exports = NewsClient;
