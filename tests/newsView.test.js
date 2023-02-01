8bfd0c7fd8c3b69b69919a5cd81102e17d3a8115
Joe Burgess <josephburgess@gmail.com>
2023-02-01 17:21:26 +0000

    tidying tests

--- tests/newsView.test.js
+++ tests/newsView.test.js
@@ -10,11 +10,11 @@ const mockNews = require('../mock/mockNews');
 const apiData = require('../mock/mockApiData');
 jest.mock('../src/NewsClient.js');
 
-describe(NewsView, () => {
+describe('NewsView', () => {
   let newsModel, newsView, newsClient;
+
   beforeEach(() => {
     NewsClient.mockClear();
-
     document.body.innerHTML = fs.readFileSync('./index.html');
 
     newsClient = new NewsClient();
@@ -22,91 +22,84 @@ describe(NewsView, () => {
     newsView = new NewsView(newsModel, newsClient);
   });
 
-  it('should display no news if empty', () => {
-    newsView.displayNews();
-
-    expect(document.querySelectorAll('.news').length).toBe(0);
-  });
-  it('should display a single story on page', () => {
-    newsModel.getNews = jest.fn().mockReturnValue([mockNews[0]]);
-
-    newsView.displayNews();
-
-    expect(document.querySelectorAll('.news').length).toEqual(1);
-  });
-
-  it('should display multiple stories on page', () => {
-    newsModel.getNews = jest.fn().mockReturnValue([mockNews[0], mockNews[1]]);
-
-    newsView.displayNews();
-
-    expect(document.querySelectorAll('.news').length).toEqual(2);
-  });
-
-  it('should display the expected number of images and links', () => {
-    const news = [mockNews[0], mockNews[1]];
-    newsModel.getNews = jest.fn().mockReturnValue(news);
-
-    newsView.displayNews();
-
-    const images = document.querySelectorAll('.thumbnail');
-    expect(images.length).toBe(2);
-
-    const links = document.querySelectorAll('.news_link');
-    expect(links.length).toBe(2);
-    for (let i = 0; i < links.length; i++) {
-      expect(links[i].getAttribute('href')).toBe(news[i].webUrl);
-    }
-  });
-
-  it('should add API data to the model', () => {
-    const news = [mockNews[0], mockNews[1]];
-    newsClient.loadNews.mockImplementation((callback) => {
-      callback(apiData);
+  describe('displayNews', () => {
+    it('displays no news if empty', () => {
+      newsView.displayNews();
+      expect(document.querySelectorAll('.news').length).toBe(0);
     });
-    newsModel.getNews = jest.fn().mockReturnValue(news);
 
-    newsView.displayNewsFromApi();
+    it('displays a single story on page', () => {
+      newsModel.getNews = jest.fn().mockReturnValue([mockNews[0]]);
+      newsView.displayNews();
+      expect(document.querySelectorAll('.news').length).toEqual(1);
+    });
 
-    expect(document.querySelectorAll('.news').length).toEqual(2);
-    expect(document.querySelectorAll('.news')[0].textContent).toEqual(
-      'UK house price growth slows to lowest rate since mid-2020; all eyes on Fed decision – business live'
-    );
+    it('displays multiple stories on page', () => {
+      newsModel.getNews = jest.fn().mockReturnValue([mockNews[0], mockNews[1]]);
+      newsView.displayNews();
+      expect(document.querySelectorAll('.news').length).toEqual(2);
+    });
+
+    it('displays the expected number of images and links', () => {
+      const news = [mockNews[0], mockNews[1]];
+      newsModel.getNews = jest.fn().mockReturnValue(news);
+      newsView.displayNews();
+      const images = document.querySelectorAll('.thumbnail');
+      expect(images.length).toBe(2);
+      const links = document.querySelectorAll('.news_link');
+      expect(links.length).toBe(2);
+      links.forEach((link, index) => {
+        expect(link.getAttribute('href')).toBe(news[index].webUrl);
+      });
+    });
   });
 
-  describe('displayNewsFromSearch', () => {
-    it('should search news using the provided search term', () => {
-      newsView.displayNewsFromSearch('some search term');
-      expect(newsClient.searchNews).toHaveBeenCalledWith(
-        'some search term',
-        expect.any(Function)
+  describe('displayNewsFromApi', () => {
+    it('adds API data to the model', () => {
+      newsClient.loadNews.mockImplementation((callback) => {
+        callback(apiData);
+      });
+      const news = [mockNews[0], mockNews[1]];
+      newsModel.getNews = jest.fn().mockReturnValue(news);
+      newsView.displayNewsFromApi();
+      expect(document.querySelectorAll('.news').length).toEqual(2);
+      expect(document.querySelectorAll('.news')[0].textContent).toContain(
+        'UK house price growth slows'
       );
     });
   });
 
-  it('should update API data when searching', async () => {
-    const news = [mockNews[0], mockNews[1]];
-    newsClient.searchNews.mockImplementation((searchTerm, callback) => {
-      callback(apiData);
+  describe('displayNewsFromSearch', () => {
+    it('searches news using the search term', () => {
+      newsView.displayNewsFromSearch('search term');
+      expect(newsClient.searchNews).toHaveBeenCalledWith(
+        'search term',
+        expect.any(Function)
+      );
     });
-    newsModel.getNews = jest.fn().mockReturnValue(news);
 
-    await newsView.displayNewsFromSearch();
+    it('updates news data on search', () => {
+      newsClient.searchNews.mockImplementation((searchTerm, callback) => {
+        callback(apiData);
+      });
+      newsModel.getNews = jest.fn().mockReturnValue([mockNews[0], mockNews[1]]);
+      newsView.displayNewsFromSearch();
 
-    expect(document.querySelectorAll('.news').length).toEqual(2);
-    expect(document.querySelectorAll('.news')[0].textContent).toEqual(
-      'UK house price growth slows to lowest rate since mid-2020; all eyes on Fed decision – business live'
-    );
+      expect(document.querySelectorAll('.news').length).toEqual(2);
+      expect(document.querySelector('.news').textContent).toContain(
+        'UK house price growth slows'
+      );
+    });
   });
 
   describe('addEventListeners', () => {
-    it('should call displayNewsFromApi on DOMContentLoaded', () => {
+    it('calls displayNewsFromApi on DOMContentLoaded', () => {
       newsView.displayNewsFromApi = jest.fn();
       document.dispatchEvent(new Event('DOMContentLoaded'));
       expect(newsView.displayNewsFromApi).toHaveBeenCalled();
     });
 
-    it('should allow the user to search, with the input clearing each time', () => {
+    it('allows user to submit a search using the searchbar', () => {
       document.dispatchEvent(new Event('DOMContentLoaded'));
       newsView.displayNewsFromSearch = jest.fn();
       const searchForm = document.querySelector('.searchbar');
