bd0346785af00f038a66689e69c886c6bb5c5ff2
Joe Burgess <josephburgess@gmail.com>
2023-02-01 17:36:21 +0000

    cleaning up tests

--- tests/newsClient.test.js
+++ tests/newsClient.test.js
@@ -9,31 +9,32 @@ describe(NewsClient, () => {
     newsClient = new NewsClient();
   });
 
-  it('calls fetch and loads data', (done) => {
-    fetch.mockResponseOnce(JSON.stringify(apiData));
-    newsClient.loadNews((data) => {
-      const results = data.response.results;
-      const stories = results.map((article) => {
-        const { webUrl } = article;
-        const { headline, thumbnail, standfirst } = article.fields;
-        return { webUrl, headline, thumbnail, standfirst };
+  describe('loadNews', () => {
+    it('calls fetch and loads data', (done) => {
+      fetch.mockResponseOnce(JSON.stringify(apiData));
+      newsClient.loadNews((data) => {
+        const results = data.response.results;
+        const stories = results.map((article) => {
+          const { webUrl } = article;
+          const { headline, thumbnail, standfirst } = article.fields;
+          return { webUrl, headline, thumbnail, standfirst };
+        });
+        expect(stories[0].thumbnail).toBe(
+          'https://media.guim.co.uk/4eb07f2f4bbd086197aa76ca2de731ad7fefc9fd/0_228_4500_2700/500.jpg'
+        );
+        expect(stories[0].headline).toBe(
+          'UK house price growth slows to lowest rate since mid-2020; all eyes on Fed decision – business live'
+        );
+        expect(stories[0].webUrl).toBe(
+          'https://www.theguardian.com/business/live/2023/feb/01/uk-annual-house-price-growth-slows-lowest-rate-since-mid-2020-chinas-factories-slump-us-federal-reserve-rate-decision'
+        );
+        expect(stories[0].standfirst).toBe(
+          '<p>UK annual house price growth slows to 1.1%; investors await US Federal Reserve message on whether it will tighten monetary policy further</p>'
+        );
+        done();
       });
-      expect(stories[0].thumbnail).toBe(
-        'https://media.guim.co.uk/4eb07f2f4bbd086197aa76ca2de731ad7fefc9fd/0_228_4500_2700/500.jpg'
-      );
-      expect(stories[0].headline).toBe(
-        'UK house price growth slows to lowest rate since mid-2020; all eyes on Fed decision – business live'
-      );
-      expect(stories[0].webUrl).toBe(
-        'https://www.theguardian.com/business/live/2023/feb/01/uk-annual-house-price-growth-slows-lowest-rate-since-mid-2020-chinas-factories-slump-us-federal-reserve-rate-decision'
-      );
-      expect(stories[0].standfirst).toBe(
-        '<p>UK annual house price growth slows to 1.1%; investors await US Federal Reserve message on whether it will tighten monetary policy further</p>'
-      );
-      done();
     });
   });
-
   describe('searchNews', () => {
     it('calls fetch on the API with searchdata', (done) => {
       fetch.mockResponseOnce(JSON.stringify(apiData));
