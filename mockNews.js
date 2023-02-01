d2955d2110d6f12d988327305ce9c04a1e68512e
Joe Burgess <josephburgess@gmail.com>
2023-02-01 10:04:47 +0000

    adding mock data to avoid calling API

---
+++ mockNews.js
@@ -0,0 +1,107 @@
+const news = [
+  {
+    thumbnail:
+      'https://media.guim.co.uk/fa7d5a5a0d6c0b13c9db83f4cd51721d655832dc/0_72_3500_2101/500.jpg',
+    headline:
+      'Three Russians under sanctions own UK property via overseas entities',
+    webUrl:
+      'https://www.theguardian.com/uk-news/2023/jan/31/three-russians-under-sanctions-own-uk-property-via-overseas-entities',
+  },
+
+  {
+    thumbnail:
+      'https://media.guim.co.uk/4eb07f2f4bbd086197aa76ca2de731ad7fefc9fd/0_228_4500_2700/500.jpg',
+    headline:
+      'UK house price growth slows to lowest rate since mid-2020; all eyes on Fed decision – business live',
+    webUrl:
+      'https://www.theguardian.com/business/live/2023/feb/01/uk-annual-house-price-growth-slows-lowest-rate-since-mid-2020-chinas-factories-slump-us-federal-reserve-rate-decision',
+  },
+
+  {
+    webUrl:
+      'https://www.theguardian.com/business/live/2023/feb/01/uk-annual-house-price-growth-slows-lowest-rate-since-mid-2020-chinas-factories-slump-us-federal-reserve-rate-decision',
+    headline:
+      'UK house price growth slows to lowest rate since mid-2020; all eyes on Fed decision – business live',
+    thumbnail:
+      'https://media.guim.co.uk/4eb07f2f4bbd086197aa76ca2de731ad7fefc9fd/0_228_4500_2700/500.jpg',
+  },
+
+  {
+    webUrl:
+      'https://www.theguardian.com/money/2023/feb/01/uk-house-prices-fall-for-fifth-month-in-a-row',
+    headline: 'UK house prices fall for fifth month in a row',
+    thumbnail:
+      'https://media.guim.co.uk/a7f3dcc9aba9ae2b9a21a5c292e06c039e9d7ad3/0_108_3000_1800/500.jpg',
+  },
+
+  {
+    webUrl:
+      'https://www.theguardian.com/environment/2023/feb/01/specieswatch-donkey-work-uk-population-dwindles',
+    headline: 'Specieswatch: donkey work is done as UK population dwindles',
+    thumbnail:
+      'https://media.guim.co.uk/2b6ad490d20f7112f2de8e2c7e39e5709450a75e/452_733_3049_1829/500.jpg',
+  },
+
+  {
+    webUrl:
+      'https://www.theguardian.com/society/2023/feb/01/uk-benefit-changes-have-pushed-people-into-dead-end-low-paid-jobs-says-ifs',
+    headline:
+      'UK benefit changes have pushed people into dead-end, low-paid jobs, says IFS',
+    thumbnail:
+      'https://media.guim.co.uk/e295474540148a99d0f0167eb0d230c22fd93d59/571_953_4090_2456/500.jpg',
+  },
+
+  {
+    webUrl:
+      'https://www.theguardian.com/business/2023/jan/31/uk-supermarket-price-inflation-rise-bills-milk-eggs-dog-food',
+    headline:
+      'UK supermarket price inflation hits record high, adding £788 to annual bills',
+    thumbnail:
+      'https://media.guim.co.uk/ac43170eba40d7c2912012bfac93eca37531002c/0_0_8256_4954/500.jpg',
+  },
+
+  {
+    webUrl:
+      'https://www.theguardian.com/uk-news/2023/feb/01/almost-13000-offshore-companies-with-uk-property-fail-to-declare-owners',
+    headline:
+      'Almost 13,000 offshore companies with UK property fail to declare owners',
+    thumbnail:
+      'https://media.guim.co.uk/eeab3db9ee4e8b5f6fceecf7cd0b3799e0d986ce/950_0_4177_2508/500.jpg',
+  },
+
+  {
+    webUrl:
+      'https://www.theguardian.com/uk-news/2023/jan/31/up-to-half-a-million-to-strike-across-uk-as-talks-go-backwards',
+    headline:
+      'Up to half a million to strike across UK as talks go ‘backwards’',
+    thumbnail:
+      'https://media.guim.co.uk/2886960838a33c7bf1bba61d245fb4f270f69712/0_192_5792_3476/500.jpg',
+  },
+
+  {
+    webUrl:
+      'https://www.theguardian.com/uk-news/2023/jan/31/three-russians-under-sanctions-own-uk-property-via-overseas-entities',
+    headline:
+      'Three Russians under sanctions own UK property via overseas entities',
+    thumbnail:
+      'https://media.guim.co.uk/70e7458a42f6d494c852853a4820d3bd1b00cce0/0_44_2537_1523/500.jpg',
+  },
+
+  {
+    webUrl:
+      'https://www.theguardian.com/world/2023/jan/31/us-will-not-supply-f-16-fighter-jets-to-ukraine-says-joe-biden',
+    headline: 'US and UK rule out sending F-16 fighter jets to Ukraine',
+    thumbnail:
+      'https://media.guim.co.uk/34924d0408cb80ffccbd912e0866936f0e875844/437_0_4033_2422/500.jpg',
+  },
+
+  {
+    webUrl:
+      'https://www.theguardian.com/society/2023/jan/31/i-fear-for-my-trans-daughters-future-in-the-uk',
+    headline: 'I fear for my trans daughter’s future in the UK',
+    thumbnail:
+      'https://media.guim.co.uk/dc1b5c919f439e5217e53269c6962d884a9da73b/0_289_6720_4032/500.jpg',
+  },
+];
+
+module.exports = news;
