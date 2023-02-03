a716df4c1d7652557e9398d5abe45349fbc0a8e0
Joe Burgess <josephburgess@gmail.com>
2023-02-03 14:55:35 +0000

    adding private api key for Travis build

--- src/newsClient.js
+++ src/newsClient.js
@@ -1,4 +1,5 @@
-const API_KEY = require('./apiKey.js');
+require('dotenv').config();
+const API_KEY = process.env.API_KEY;
 
 class NewsClient {
   loadNews(callback) {
@@ -6,8 +7,9 @@ class NewsClient {
       `https://content.guardianapis.com/search?show-fields=thumbnail%2Cheadline%2Cbyline%2Cstandfirst&page-size=40&section=world&api-key=${API_KEY}`
     )
       .then((response) => response.json())
-      .then((data) => {
-        callback(data);
+      .then((data) => callback(data))
+      .catch((error) => {
+        errorCallback(error);
       });
   }
 
@@ -16,8 +18,9 @@ class NewsClient {
       `https://content.guardianapis.com/search?q=${searchTerm}&page-size=40&query-fields=headline&show-fields=thumbnail,headline,byline,standfirst&order-by=newest&api-key=${API_KEY}`
     )
       .then((response) => response.json())
-      .then((data) => {
-        callback(data);
+      .then((data) => callback(data))
+      .catch((error) => {
+        errorCallback(error);
       });
   }
 
@@ -26,8 +29,9 @@ class NewsClient {
       `https://content.guardianapis.com/search?section=${section}&page-size=40&show-fields=thumbnail,headline,byline,standfirst&order-by=newest&api-key=${API_KEY}`
     )
       .then((response) => response.json())
-      .then((data) => {
-        callback(data);
+      .then((data) => callback(data))
+      .catch((error) => {
+        errorCallback(error);
       });
   }
 }
