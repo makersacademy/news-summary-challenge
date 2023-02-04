358ec56f60e3998c8afd451cd58fbf8464631075
Adam Hoar <42139484+amh4@users.noreply.github.com>
2023-02-04 15:58:25 +0000

    gitignore updated

--- articlesClient.js
+++ articlesClient.js
@@ -1,9 +1,12 @@
+const apiKey = require('./config')
+
 class ArticlesClient{
   loadArticles(){
-    return fetch('https://content.guardianapis.com/search?q=&query-fields=headline&show-fields=thumbnail,headline,byline&order-by=newest&api-key=688e0b02-080a-4072-9909-1780b13e4fd6')
+    return fetch(`https://content.guardianapis.com/search?q=&query-fields=headline&show-fields=thumbnail,headline,byline&order-by=newest&api-key=${apiKey.apiKey}`)
       .then((response) => response.json())
       .then((articles) => {return articles})
   }
+
 }
 
 module.exports = ArticlesClient;
