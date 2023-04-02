7058e3f503afa0dc66b6afabef9834ad587e2f91
Sarah <sarahdavies186@gmail.com>
2023-04-02 22:43:49 +0100

    styling updates

--- newsView.js
+++ newsView.js
@@ -3,19 +3,19 @@ class NewsView {
     this.model = model;
     this.client = client;
     this.mainContainerEl = document.querySelector("#main-container");
-    this.buttonEl = document.querySelector('#search-button');
+    this.buttonEl = document.querySelector("#search-button");
 
-    this.buttonEl.addEventListener('click', () => {
-      this.searchValue = document.querySelector('#search-input').value; 
+    this.buttonEl.addEventListener("click", () => {
+      this.searchValue = document.querySelector("#search-input").value;
       this.client.loadSearchData(this.searchValue, () => {
         this.displayNewsFromSearch();
-      })
-    })
+      });
+    });
   }
 
   displayNews() {
     const news = this.model.allNews().response.results;
-    console.log(news)
+    console.log(news);
     news.forEach((news) => {
       // console.log(news)
 
@@ -23,7 +23,7 @@ class NewsView {
       newsElement.className += "col-lg-6 news";
 
       const cardElement = document.createElement("div");
-      cardElement.className = "card"
+      cardElement.className = "card";
 
       const newsTitleLink = document.createElement("a");
       newsTitleLink.href = news.webUrl;
@@ -32,11 +32,11 @@ class NewsView {
 
       const imageElement = document.createElement("img");
       imageElement.setAttribute("src", news.fields.thumbnail);
-      imageElement.className = "thumbnail-img"
+      imageElement.className = "thumbnail-img";
 
       cardElement.appendChild(imageElement);
       cardElement.appendChild(newsTitleLink);
-      
+
       newsElement.append(cardElement);
       this.mainContainerEl.append(newsElement);
     });
@@ -53,7 +53,7 @@ class NewsView {
 
   displayNewsFromSearch() {
     this.client.loadSearchData(this.searchValue, (data) => {
-      console.log(this.searchValue)
+      console.log(this.searchValue);
       // console.log(data)
       this.model.addNews(data);
       this.model.allNews();
