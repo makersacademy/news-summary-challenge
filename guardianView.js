d5023ad8cfef5e8dcd2238713af2d066c4c33ac5
Valeria Serci <balerty84@gmail.com>
2023-04-02 18:08:58 +0100

    test drive GuardianClient

---
+++ guardianView.js
@@ -0,0 +1,5 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const fs = require('fs');
---
+++ guardianView.test.js
@@ -0,0 +1,12 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const fs = require('fs');
+const GuardianView = require('./guardianView');
+
+describe('Page view', () => {
+  it('displays 10 paragraphs', () => {
+    
+  });
+})
