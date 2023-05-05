66434681328fbada681b3e2e5f9f4085a9da7450
umut <umut_baykan@hotmail.com>
2023-05-05 19:06:21 +0100

    display page basic functionality and tests

--- newsView.js
+++ newsView.js
@@ -9,22 +9,31 @@ class NewsView{
     this.homeButtonEl = document.querySelector('#home-button')
     this.homeButtonEl.addEventListener('click', async () => {
       this.resetPageContents()
-      await this.getArticles()
+      await this.retrieveArticlesFromApi()
       this.displayPage()
     })
   }
 
-  getArticles = async (input) => {
+  retrieveArticlesFromApi = async (input) => {
     const result = await this.client.loadArticles(input)
     this.model.setArticles(result.response.results)
   }
+  
 
   resetPageContents = () => {
-    //
+    const all_articles = document.querySelectorAll('div.article, div.error')
+    all_articles.forEach(article => article.remove())
   }
 
   displayPage = () => {
-    //
+    const all_article_data = this.model.getArticles()
+    if (all_article_data.length === 0) {throw new Error('No articles found.')}
+    all_article_data.forEach(article => {
+      const new_div = document.createElement('div')
+      new_div.className = 'article'
+      new_div.textContent = article.webTitle
+      this.mainContainerEl.append(new_div)
+    })
   }
 }
 
--- newsView.test.js
+++ newsView.test.js
@@ -3,15 +3,16 @@
  */
 
 const fs = require("fs");
+const jestFetchMock = require("jest-fetch-mock");
+jestFetchMock.enableMocks();
 const NewsModel = require("./newsModel");
 const NewsView = require("./newsView");
 const Responses = require('./serverResponses')
-const jestFetchMock = require("jest-fetch-mock");
-jestFetchMock.enableMocks();
 
 describe('NewsView class', () => {
   
   let model;
+  let mockClient;
 
   beforeEach(() => {
     document.body.innerHTML = fs.readFileSync("./index.html");
@@ -21,14 +22,46 @@ describe('NewsView class', () => {
     emptyResponse = Responses.emptyResponse()
     errorResponse = Responses.errorResponse()
     specificResponse = Responses.specificResponse()
+    mockClient = jest.fn()
   })
 
-  test('loads the articles inside the model class', async () => {
-    const mockClient = {loadArticles: jest.fn().mockResolvedValueOnce(okResponse)}
-    const view = new NewsView(model, mockClient);
-    await view.getArticles()
-    const articles = model.getArticles() 
-    expect(articles.length).toEqual(10)
-    expect(articles[0].webTitle).toEqual('Top 10 books about being poor in America')
+  describe('getArticles method', () => {    
+    test('loads the articles inside the model class', async() => {
+      const mockClient = {loadArticles: jest.fn().mockResolvedValueOnce(okResponse)}
+      const view = new NewsView(model, mockClient);
+      await view.retrieveArticlesFromApi()
+      const articles = model.getArticles() 
+      expect(articles.length).toEqual(10)
+      expect(articles[0].webTitle).toEqual('Top 10 books about being poor in America')
+      expect(mockClient.loadArticles).toHaveBeenCalledWith(undefined);
+    });
+    test('loads the specific articles inside the model class', async() => {
+      const mockClient = {loadArticles: jest.fn().mockResolvedValueOnce(specificResponse)}
+      const view = new NewsView(model, mockClient);
+      await view.retrieveArticlesFromApi('football')
+      const articles = model.getArticles() 
+      expect(articles.length).toEqual(10)
+      expect(articles[0].webTitle).toEqual('Football Daily | Sam Allardyce, Leeds and football heritage')
+      expect(mockClient.loadArticles.mock.calls.length).toEqual(1)
+      expect(mockClient.loadArticles).toHaveBeenCalledWith('football');
+    });
+  });
+
+  describe('displayPage method', () => {
+    test('displays the loaded articles on the page', () => {
+      model.setArticles(okResponse.response.results)
+      const view = new NewsView(model, mockClient);
+      view.displayPage()
+      const all_notes_on_page = document.querySelectorAll('div.article')
+      expect(all_notes_on_page.length).toEqual(10)
+      expect(all_notes_on_page[0].textContent).toEqual("Top 10 books about being poor in America")
+    })
+
+    test('throws an error if there are no articles in the model', () => {
+      model.setArticles(emptyResponse.response.results)
+      const view = new NewsView(model, mockClient);
+      expect(view.displayPage).toThrowError("No articles found.");
+    })
   })
+
 })
