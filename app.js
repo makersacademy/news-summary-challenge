7c9e3d7f8af62b6077ab04afa6d253a703eab50d
Jon P <jon.pillay@gmail.com>
2023-05-09 10:08:29 +0100

    commit before pull request on makers. Base features working, but a work in progress. Please read the README

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ const express = require('express');
 const serve = require('express-static');
 const session = require('express-session');
 const { redirect } = require('express/lib/response');
+const cors = require('cors')
+
 
 const app = express();
+app.use(cors({
+  origin: '*'
+}));
 const path = require('path');
 app.use(express.static('public'))
 app.use(express.static('views'))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
 
 
 app.set('view engine', 'ejs')
