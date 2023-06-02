a6b0de1e21c856e7a124e6939cc4bb636e04c070
awdem <wdemontmollin@gmail.com>
2023-06-02 18:54:15 +0100

    added a line break between image element and headline element

--- newsView.js
+++ newsView.js
@@ -19,15 +19,17 @@ class NewsView {
     storyArray.forEach((story) => {
       const newStory = document.createElement('div');
       newStory.className = 'story';
-      // creates and appends image
+      // sets image subelement
       const newStoryImage = document.createElement('img');
       newStoryImage.className = 'story_image'
       newStoryImage.src = story.thumbnailUrl;
 
       newStory.appendChild(newStoryImage);
-      // sets headline and link
+      // adds a line break
+      newStory.append(document.createElement('br'));
+      // sets headline and link subelement
       const newStoryHeadline = document.createElement('a')
-      newStoryHeadline.className = 'story-headline'
+      newStoryHeadline.className = 'story_headline'
       newStoryHeadline.textContent = story.headline;
       newStoryHeadline.href = story.webUrl;
 
