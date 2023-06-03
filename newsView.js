49bfdad1e1507f6deceb2da252624dee1b5a5b3b
sharmine-s <sharmine.ss@gmail.com>
2023-06-03 15:54:26 +0100

    links for articles now showing and opening in new tab

--- newsView.js
+++ newsView.js
@@ -2,22 +2,21 @@ class NewsView {
   constructor(model) {
     this.model = model;
     this.mainContainerEl = document.querySelector("#main-container");
-    document.querySelector("#header").innerHTML = "Headlines"
+    document.querySelector("#header").innerHTML = "Headlines";
   }
 
   showArticles() {
     this.model.fetchArticles().then(() => {
       const articles = this.model.articles;
-
       articles.forEach((article) => {
         const div = document.createElement("div");
         div.className = "article";
         const p = document.createElement("p");
-        p.textContent = article.title;
-        // a = document.createElement("a");
-        // a.href = article.url;
-        // a.innerHTML = article.title;
-        // div.append(a);
+        const a = document.createElement("a");
+        a.href = article.url;
+        a.innerText = article.title;
+        a.target = "_blank"
+        p.append(a);
         div.append(p);
         this.mainContainerEl.append(div);
       });
